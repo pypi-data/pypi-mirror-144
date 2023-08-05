11c51e68017546673922179d5f36567eb46dbcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package notecoin/0.14.29/notecoin-0.14.29.tar.gz

---
+++ packages/notecoin/notecoin-0.14.29.tar.gz/notecoin-0.14.29/notecoin/server/notecoin_server.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+from notecoin.okex.server.const import (account_account, base_api,
+                                        market_tickers)
+from notecoin.strategy.sell_strategy import AutoSeller
+
+seller = AutoSeller()
+
+app = FastAPI()
+app.include_router(account_account)
+app.include_router(market_tickers)
+app.include_router(base_api)
+app.include_router(seller)
+
+# uvicorn notecoin_server:app --host '0.0.0.0' --port 8444 --reload
+# uvicorn notecoin_server:app --host '0.0.0.0' --port 8444
+# uvicorn notecoin_server: app - -host '0.0.0.0' - -port 8444
+
+uvicorn.run(app, host='0.0.0.0', port=8444)
