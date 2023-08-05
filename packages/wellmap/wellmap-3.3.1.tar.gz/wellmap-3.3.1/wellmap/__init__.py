432a9c4224709eb38f18ec01d5d4c307de8ea046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package wellmap/3.3.1/wellmap-3.3.1.tar.gz

---
+++ packages/wellmap/wellmap-3.3.1.tar.gz/wellmap-3.3.1/wellmap/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""\
+File format for 96-well plate layouts.
+"""
+
+__version__ = '3.3.1'
+
+from .util import *
+from .file import *
+from .plot import show, UsageError
+from . import plot
