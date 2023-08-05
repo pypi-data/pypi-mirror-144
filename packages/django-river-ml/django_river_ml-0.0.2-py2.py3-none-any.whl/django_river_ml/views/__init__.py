114cb2d0544f47f48e508bcc52555c78ee853a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:55 +0000

    Add package django-river-ml/0.0.2/django_river_ml-0.0.2-py2.py3-none-any.whl

---
+++ packages/django-river-ml/django_river_ml-0.0.2-py2.py3-none-any.whl/django_river_ml/views/__init__.py
@@ -0,0 +1,7 @@
+from .auth import GetAuthToken
+from .base import ServiceInfo
+from .learn import LearnView
+from .metrics import MetricsView, stream_metrics, stream_events, StatsView
+from .model import ModelView, ModelsView, ModelDownloadView
+from .predict import PredictView
+from .label import LabelView
