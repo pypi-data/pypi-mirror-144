e9f71e3eedee0c31021012fb9ec4da20d43f1653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package pydude/0.12.0/pydude-0.12.0.tar.gz

---
+++ packages/pydude/pydude-0.12.0.tar.gz/pydude-0.12.0/dude/context.py
@@ -0,0 +1,25 @@
+import logging
+
+from .scraper import Scraper
+from .storage import save_csv, save_yaml
+
+logger = logging.getLogger(__name__)
+
+_scraper = Scraper()
+group = _scraper.group
+run = _scraper.run
+save = _scraper.save
+select = _scraper.select
+startup = _scraper.startup
+shutdown = _scraper.shutdown
+pre_setup = _scraper.pre_setup
+post_setup = _scraper.post_setup
+
+
+"""
+These storage options are not included by default to the Scraper class.
+"""
+
+save("csv")(save_csv)
+save("yaml")(save_yaml)
+save("yml")(save_yaml)
