dee79aec56a880f2c0b6b598c3cc4d16b6c06964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:28 +0000

    Add package scrapeanything/0.9.16/scrapeanything-0.9.16-py3-none-any.whl

---
+++ packages/scrapeanything/scrapeanything-0.9.16-py3-none-any.whl/scrapeanything/utils/constants.py
@@ -0,0 +1,34 @@
+class Integration:
+
+    class Types:
+        CSV = 'CSV'
+        EXCEL = 'EXCEL'
+        GOOGLESPREADSHEET = 'GOOGLE SPREADSHEET'
+
+    class Modes:
+        READ = 'READ'
+        WRITE = 'WRITE'
+
+class WebScraper:
+
+    class Functions:
+        SUBSTRING = 'substring'
+        EXPLODE = 'explode'
+        REPLACE = 'join'
+        JOIN = 'join'
+
+    class Types:
+        PERCENTAGE = 'PERCENTAGE'
+        BOOL = 'BOOL'
+        MONEY = 'MONEY'
+        NUMBER = 'NUMBER'
+        INTEGER = 'INTEGER'
+        GEO = 'GEO'
+        CHAR = 'CHAR'
+        URL = 'URL'
+        LIST = 'LIST'
+        DATE = 'DATE'
+
+    class Properties:
+        TEXT = 'text()'
+        HTML = 'html()'
