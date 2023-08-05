90e4d1ea8e91d1adf9d6b59d58d348b6069c41a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:48 +0000

    Add package terminwind/0.1.1/terminwind-0.1.1.tar.gz

---
+++ packages/terminwind/terminwind-0.1.1.tar.gz/terminwind-0.1.1/terminwind/sidebar.py
@@ -0,0 +1,33 @@
+from textual.reactive import Reactive
+from textual.views import GridView
+
+from .button import Button
+
+
+class Sidebar(GridView):
+    has_focus = Reactive(False)
+
+    def __init__(self, tree) -> None:
+        self.buttons = (
+            Button("Open Folder", quit),
+            Button("New File", quit),
+            Button("New Folder", quit),
+        )
+        self.tree = tree
+        super().__init__()
+
+    async def on_mount(self):
+        self.grid.set_gutter(1)
+        self.grid.set_gap(0, -2)
+        self.grid.set_align("center", "start")
+
+        self.grid.add_column("col")
+        self.grid.add_row("menu")
+        self.grid.add_row("directory_tree", fraction=6)
+        self.grid.add_areas(
+            menu="col,menu",
+            directory_tree="col,directory_tree",
+        )
+        # self.grid.place(menu=Menu())
+
+        self.grid.place(directory_tree=self.tree)
