80ef089ec8754fd76eb7d5483c63a7bcc8fa8185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:17 +0000

    Add package scrapeanything/0.9.18/scrapeanything-0.9.18.tar.gz

---
+++ packages/scrapeanything/scrapeanything-0.9.18.tar.gz/scrapeanything-0.9.18/src/scrapeanything/utils/constants.py
@@ -0,0 +1,36 @@
+class Integration:
+
+    class Types:
+        CSV = 'CSV'
+        EXCEL = 'EXCEL'
+        GOOGLESPREADSHEET = 'GOOGLE SPREADSHEET'
+
+    class Modes:
+        READ = 'READ'
+        WRITE = 'WRITE'
+
+class WebScraper:
+
+    class Functions:
+        SUBSTRING = 'substring'
+        EXPLODE = 'explode'
+        REPLACE = 'join'
+        JOIN = 'join'
+
+    class Types:
+        PERCENTAGE = 'PERCENTAGE'
+        BOOL = 'BOOL'
+        MONEY = 'MONEY'
+        NUMBER = 'NUMBER'
+        INTEGER = 'INTEGER'
+        GEO = 'GEO'
+        CHAR = 'CHAR'
+        URL = 'URL'
+        LIST = 'LIST'
+        DATE = 'DATE'
+        JSON = 'JSON'
+        STRING = 'STRING'
+
+    class Properties:
+        TEXT = 'text()'
+        HTML = 'html()'
