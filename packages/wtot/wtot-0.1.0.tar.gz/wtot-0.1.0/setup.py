316ab189d1b9d6ab93286ea317395f3edefa50fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package wtot/0.1.0/wtot-0.1.0.tar.gz

---
+++ packages/wtot/wtot-0.1.0.tar.gz/wtot-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#encoding:utf-8
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.0'
+
+setup(name='wtot',
+      version=version,
+      description="terminal查词工具",
+      long_description="""terminal查词工具""",
+      keywords='python wt dictionary terminal',
+      author='lmh',
+      author_email='554408064@sjtu.edu.cn',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'termcolor',
+      ],
+      entry_points={
+        'console_scripts':[
+            'wtot = wtot.wtot:main'
+        ]
+      },
+)
