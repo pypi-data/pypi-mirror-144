395197100cbd398917686ffe48e0e99ad2945520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package pogoocr/0.4.0b1/PogoOCR-0.4.0b1-py3-none-any.whl

---
+++ packages/pogoocr/PogoOCR-0.4.0b1-py3-none-any.whl/PogoOCR/__init__.py
@@ -0,0 +1,21 @@
+"""
+PogoOCR
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Python tool for running OCR on Pokemon Screenshots using Google Cloud Vision
+
+:copyright: (c) 2020 TrainerDex
+:licence: GNU GENERAL PUBLIC LICENSE 3.0
+
+"""
+
+__title__ = "PogoOCR"
+__author__ = "JayTurnr"
+__licence__ = "GNU GENERAL PUBLIC LICENSE 3.0"
+__copyright__ = "2021 TrainerDex"
+__version__ = "0.4.0b1"
+
+from .providers.cloudvision import Screenshot  # noqa: F401
+from .client import OCRClient  # noqa: F401
+from .constants import Factions, Levels, Language  # noqa: F401
+from .images import ScreenshotClass  # noqa: F401
