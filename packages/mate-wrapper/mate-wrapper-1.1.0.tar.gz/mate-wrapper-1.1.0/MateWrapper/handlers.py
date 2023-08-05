dc2bfad1e1f818652d88cb2f3cca4c40fd49d3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:20:04 +0000

    Add package mate-wrapper/1.1.0/mate-wrapper-1.1.0.tar.gz

---
+++ packages/mate-wrapper/mate-wrapper-1.1.0.tar.gz/mate-wrapper-1.1.0/MateWrapper/handlers.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+from telegram.ext import CallbackQueryHandler, MessageHandler, Filters, ConversationHandler
+
+from MateWrapper.generics import TelegramFunctionBlueprint
+
+
+class ButtonHandler(CallbackQueryHandler):
+    """ Handles Button inputs, it's just an alis for CallbackQueryHandler """
+    pass
+
+
+class TextHandler(MessageHandler):
+    """ Handles text messages, wraps MessageHandler """
+
+    def __init__(self, callback: callable or TelegramFunctionBlueprint, **kwargs):
+        super().__init__(Filters.text & (~Filters.command), callback, **kwargs)
+
+
+class PhotoHandler(MessageHandler):
+    """ Handles Photo messages, wraps MessageHandler """
+
+    def __init__(self, callback: callable or TelegramFunctionBlueprint, **kwargs):
+        super().__init__(Filters.photo, callback, **kwargs)
