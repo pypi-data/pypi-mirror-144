25aa8720af8b77640272f9088f1ce05784761fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:55 +0000

    Add package TMotorCANControl/0.1.6/TMotorCANControl-0.1.6.tar.gz

---
+++ packages/TMotorCANControl/TMotorCANControl-0.1.6.tar.gz/TMotorCANControl-0.1.6/src/TMotorCANControl/test/test_motor_connection.py
@@ -0,0 +1,12 @@
+from TMotorCANControl.TMotorManager import TMotorManager
+
+# CHANGE THESE TO MATCH YOUR dev!
+ID = 3
+Type = 'AK80-9'
+
+with TMotorManager(dev_type=Type, dev_ID=ID, CSV_file="log.csv") as dev:
+    if dev.check_can_connection():
+        print("\ndev is successfully connected!\n")
+    else:
+        print("\ndev not connected. Check dev power, network wiring, and CAN bus connection.\n")
+    
