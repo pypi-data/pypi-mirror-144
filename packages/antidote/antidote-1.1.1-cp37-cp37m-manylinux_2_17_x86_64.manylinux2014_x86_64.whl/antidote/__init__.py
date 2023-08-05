3053bbcb0ed54782675850da22ffa5bbe639e2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:21 +0000

    Add package antidote/1.1.1/antidote-1.1.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/antidote/antidote-1.1.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/antidote/__init__.py
@@ -0,0 +1,18 @@
+from . import world
+from .constants import const, Constants
+from .core import Arg, From, FromArg, Get, inject, Inject, Provide, Scope, wire, Wiring
+from .factory import Factory, factory
+from .implementation import implementation
+from .lazy import LazyCall, LazyMethodCall
+from .service import ABCService, Service, service
+from .utils import is_compiled
+
+try:
+    from ._internal.scm_version import version as __version__
+except ImportError:  # pragma: no cover
+    __version__ = ''
+
+__all__ = ['__version__', 'world', 'Get', 'From', 'FromArg', 'Provide', 'Inject',
+           'const', 'Constants', 'constants', 'inject', 'Arg', 'wire', 'Wiring', 'factory',
+           'Factory', 'implementation', 'LazyCall', 'LazyMethodCall',
+           'service', 'Scope', 'Service', 'ABCService', 'is_compiled']
