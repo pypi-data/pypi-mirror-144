0f6501c54d742d1b4f56c82464a9148d333d0961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:30 +0000

    Add package cassa/1.0.1/cassa-1.0.1.tar.gz

---
+++ packages/cassa/cassa-1.0.1.tar.gz/cassa-1.0.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['cassa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dask>=2022.2.1,<2023.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'scikit-learn>=1.0.2,<2.0.0',
+ 'scipy>=1.8.0,<2.0.0',
+ 'seaborn>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'cassa',
+    'version': '1.0.1',
+    'description': 'Python package to perform unsupervised and semi-supervised machine learning (ML) classification algorithms on generic tensors of pre-processed data',
+    'long_description': None,
+    'author': 'Karl Nordstrom',
+    'author_email': 'karl.am.nordstrom@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
