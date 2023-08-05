7983c59a7a91092d68b4a55d07011382dd96da65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:06 +0000

    Add package streamlit-vtkjs/0.3.0/streamlit_vtkjs-0.3.0-py2.py3-none-any.whl

---
+++ packages/streamlit-vtkjs/streamlit_vtkjs-0.3.0-py2.py3-none-any.whl/streamlit_vtkjs/frontend/build/precache-manifest.197de97e361c0e3ac10eb033719bd0be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8490c1199a6a01bfcec258e91a7aa55d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d21c048359ed9acf099d",
+    "url": "./static/css/2.fc555424.chunk.css"
+  },
+  {
+    "revision": "159f556bc68df46ade05",
+    "url": "./static/css/main.6f2db4ab.chunk.css"
+  },
+  {
+    "revision": "d21c048359ed9acf099d",
+    "url": "./static/js/2.81ec489d.chunk.js"
+  },
+  {
+    "revision": "45f1190b668f660d3267b656ad86214b",
+    "url": "./static/js/2.81ec489d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159f556bc68df46ade05",
+    "url": "./static/js/main.a06cd207.chunk.js"
+  },
+  {
+    "revision": "1d52cc22638050ace4c0",
+    "url": "./static/js/runtime-main.6eda6c42.js"
+  }
+]);
