0c937af1895c0ed32806b893497975a23db032ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package pyaitools/1.4.2/pyaitools-1.4.2-py3-none-any.whl

---
+++ packages/pyaitools/pyaitools-1.4.2-py3-none-any.whl/pyaitools/weight_pooled_output.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from collections import Counter
+
+def _get_slices(token_type_ids):
+    """由token_type_ids获取切片组"""
+    slices_list = []
+    for slices in token_type_ids:
+        slices = list(slices)
+        start_index = slices.index(1)
+        end_index = start_index + dict(Counter(slices))[1]
+        slices_list.append([start_index, end_index])
+    return slices_list
+
+
+def _get_sentence_tensor(T):
+    mT = nn.AvgPool2d((len(T), 1))
+    return mT(T.unsqueeze(0))[0]
+
+
+def get_output(sample, token_type_ids, weight=0.8):
+    output = []
+    slices = _get_slices(token_type_ids)
+    for i, batch in enumerate(sample):
+        A = batch[0 : slices[i][0]]
+        B = batch[slices[i][0] : slices[i][1]]
+        # 进行平均池化
+        A = _get_sentence_tensor(A)
+        B = _get_sentence_tensor(B)
+        C = weight * B + (1 - weight) * A
+        output.append(C.tolist())
+    return torch.tensor(output).squeeze(1)
+
+
+if __name__ == "__main__":
+    sample = torch.rand(8, 70, 768)
+    token_type_ids = [[0, 0, 0, 0, 0, 1, 1, 1, 1]] * 8
+    res = get_output(sample, token_type_ids, weight=0.8)
+    print(res)
+    print(res.shape)
