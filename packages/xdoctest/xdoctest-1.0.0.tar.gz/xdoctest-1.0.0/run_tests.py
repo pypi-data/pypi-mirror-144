69b3d9d7b6bca396018547d8a8b9bce097737bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:26 +0000

    Add package xdoctest/1.0.0/xdoctest-1.0.0.tar.gz

---
+++ packages/xdoctest/xdoctest-1.0.0.tar.gz/xdoctest-1.0.0/run_tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    import pytest
+    import sys
+    package_name = 'xdoctest'
+    package_dpath = package_name
+    pytest_args = [
+        '-p', 'pytester',
+        '-p', 'no:doctest',
+        '--xdoctest',
+        '--cov-config', '.coveragerc',
+        '--cov-report', 'html',
+        '--cov-report', 'term',
+        '--cov=' + package_name,
+        package_dpath, 'testing'
+    ]
+    pytest_args = pytest_args + sys.argv[1:]
+    print('pytest.__version__ = {!r}'.format(pytest.__version__))
+    print('pytest_args = {!r}'.format(pytest_args))
+    ret = pytest.main(pytest_args)
+    print('ret = {!r}'.format(ret))
+    sys.exit(ret)
