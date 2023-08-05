d71de1355c139aa6ed550d53051fcd5b6f67d27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package mite-cli/0.2.1/mite_cli-0.2.1-py3-none-any.whl

---
+++ packages/mite-cli/mite_cli-0.2.1-py3-none-any.whl/mite_cli/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""mite-cli
+
+Usage:
+    mite-cli new <project_dir> [options]
+
+Options:
+    --log-level=LEVEL       Set logger level: DEBUG, INFO, WARNING, ERROR, CRITICAL [default: INFO]
+    -n --novenv             Don't create a python virtual environment
+"""  # noqa: E501
+
+import logging
+
+from docopt import docopt
+
+from .new_project import new_project
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging(opts):
+    logging.basicConfig(
+        level=opts["--log-level"],
+        format="<%(levelname)s> %(message)s",
+        force=True,
+    )
+
+
+def main():
+    opts = docopt(__doc__)
+    setup_logging(opts)
+    if opts["new"]:
+        new_project(opts["<project_dir>"], opts["--novenv"])
+
+
+if __name__ == "__main__":
+    main()
