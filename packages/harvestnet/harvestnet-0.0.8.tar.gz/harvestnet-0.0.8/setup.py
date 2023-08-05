e4c5c3a956c0d8d7dbc73cdf026fdf7c6abd55bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:48 +0000

    Add package harvestnet/0.0.8/harvestnet-0.0.8.tar.gz

---
+++ packages/harvestnet/harvestnet-0.0.8.tar.gz/harvestnet-0.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+ name = 'harvestnet',
+ version = '0.0.8',
+ description = 'HarvestNet',
+ long_description = 'HarvestNet',
+ license = 'MIT License',
+ author = 'waterlily',
+ author_email = 'b1929595403@gmail.com',
+ platforms = 'any',
+ python_requires = '>=3.7.*',
+ # data_files = [(r'd:\\',['ResNet50_SGD.pth', ])],
+ install_requires = [],
+ packages = find_packages()
+ )
