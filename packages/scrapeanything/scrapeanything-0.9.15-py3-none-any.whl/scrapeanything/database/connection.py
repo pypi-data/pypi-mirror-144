66db27d1bf4695d7e43c1249e1b4d37ff82d73a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:27 +0000

    Add package scrapeanything/0.9.15/scrapeanything-0.9.15-py3-none-any.whl

---
+++ packages/scrapeanything/scrapeanything-0.9.15-py3-none-any.whl/scrapeanything/database/connection.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+from scrapeanything.utils.config import Config
+from scrapeanything.database.repository import Repository
+
+@contextmanager
+def Connection(config: Config):
+
+    repository = Repository(config=config)
+    try:
+        repository.connect()
+        yield repository
+        repository.commit()
+    except:
+        repository.rollback()
+    finally:
+        repository.close()
