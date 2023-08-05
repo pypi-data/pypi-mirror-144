669a5f789042733b933aeeb2f0323810d263944a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:56 +0000

    Add package git-interface/0.8.2/git-interface-0.8.2.tar.gz

---
+++ packages/git-interface/git-interface-0.8.2.tar.gz/git-interface-0.8.2/git_interface/__init__.py
@@ -0,0 +1,5 @@
+"""
+Use the git cli from Python
+"""
+__version__ = "0.8.2"
+__author__ = "Leo Spratt"
---
+++ packages/git-interface/git-interface-0.8.2.tar.gz/git-interface-0.8.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# Only requirements are listed here for GitHub dependency graph
+setup(
+    name="git-interface",
+    install_requires=[
+        "aiofiles >= 0.8",
+    ],
+    extras_require={
+        "quart": ["quart>=0.16.3", "async-timeout>=4.0.2"],
+    },
+)
