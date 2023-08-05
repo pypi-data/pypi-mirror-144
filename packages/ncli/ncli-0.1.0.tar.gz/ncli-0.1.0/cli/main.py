cde9ae6f85ad1337f64423bd51316a56de89fcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:54 +0000

    Add package ncli/0.1.0/ncli-0.1.0.tar.gz

---
+++ packages/ncli/ncli-0.1.0.tar.gz/ncli-0.1.0/cli/main.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+import typer
+from dotenv import load_dotenv
+
+from .commands import find
+from .utils.version import get_version
+
+load_dotenv()
+
+app = typer.Typer(add_completion=False)
+app.add_typer(find.app, name="find")
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"ncli version: {get_version()}")
+        raise typer.Exit()
+
+
+@app.callback()
+def version(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Returns CLI version",
+    )
+):
+    pass
