08bce9ee304e1c947f6a415de0bddbe2b22f5e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:10 +0000

    Add package SCALECAST/0.7.2/SCALECAST-0.7.2.tar.gz

---
+++ packages/SCALECAST/SCALECAST-0.7.2.tar.gz/SCALECAST-0.7.2/setup.py
@@ -0,0 +1,46 @@
+
+from setuptools import setup, find_packages
+import shutil
+from src.scalecast.__init__ import __version__ as version
+
+shutil.copy('./examples/eCommerce.ipynb','./docs/Forecaster/examples/eCommerce.ipynb')
+shutil.copy('./examples/LSTM.ipynb','./docs/Forecaster/examples/LSTM.ipynb')
+
+logo = """<p align="center">
+  <img src="https://github.com/mikekeith52/scalecast/blob/main/assets/logo2.png" />
+</p>"""
+
+long_description = open('README.md', 'r', encoding="UTF-8").read().replace(logo,"")
+
+setup(
+  name = 'SCALECAST',
+  version = version,
+  license='MIT',
+  description='Easy dynamic time series forecasting in Python',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Michael Keith',
+  author_email = 'mikekeith52@gmail.com',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  project_urls = {
+    'GitHub': 'https://github.com/mikekeith52/scalecast',
+    'Read the Docs': 'https://scalecast.readthedocs.io/en/latest/'
+  },
+  keywords = ['FORECAST', 'SCALE', 'DYNAMIC'],
+  install_requires = [
+    'scikit-learn',
+    'tensorflow',
+    'statsmodels',
+    'scipy',
+    'eli5',
+    'numpy',
+    'pandas',
+    'scipy',
+    'matplotlib',
+    'seaborn',
+    'xgboost',
+    'lightgbm',
+    'openpyxl'
+  ],
+)
