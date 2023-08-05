7f9d4bdd726f3e020da609ac4f6e535239531a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:17 +0000

    Add package formative/0.8.9/formative-0.8.9.tar.gz

---
+++ packages/formative/formative-0.8.9.tar.gz/formative-0.8.9/formative/stock/url.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from . import StockWidget
+
+class URLWidget(StockWidget):
+    TYPE = 'url'
+    
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        
+        self.template_name = 'url.html'
+        self.review_template_name = 'review.html'
+    
+    def fields(self):
+        field = models.URLField(blank=True)
+        
+        return [(self.field_name(), field)]
+    
+    def clean(self, data):
+        if '://' not in data[:8]:
+            return 'http://' + data
+        return data
+        
