7ecb812a7bef0d3ba6a9bcb30fe765c49468b3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:33 +0000

    Add package terminwind/0.1.0/terminwind-0.1.0.tar.gz

---
+++ packages/terminwind/terminwind-0.1.0.tar.gz/terminwind-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['terminwind', 'terminwind.terminal']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofile>=3.7.4,<4.0.0',
+ 'pyperclip>=1.8.2,<2.0.0',
+ 'rich>=11,<12',
+ 'textual>=0.1.15,<0.2.0']
+
+setup_kwargs = {
+    'name': 'terminwind',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Yubo Wang',
+    'author_email': 'yubowang2007@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
