60da7359a580c434eff6239cf8c2a3b4cb4cf9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:59 +0000

    Add package WxLand/0.0.1/WxLand-0.0.1.tar.gz

---
+++ packages/WxLand/WxLand-0.0.1.tar.gz/WxLand-0.0.1/src/WxL/WxCore.py
@@ -0,0 +1,53 @@
+import wx
+
+# Postion
+DefaultPostion = wx.DefaultPosition
+
+# Size
+DefaultSize = wx.DefaultSize
+
+# WindowStyleColour
+AppWorkSpace = wx.SYS_COLOUR_APPWORKSPACE
+ActiveBorder = wx.SYS_COLOUR_ACTIVEBORDER
+ActiveCaption = wx.SYS_COLOUR_ACTIVECAPTION
+HighlightText = wx.SYS_COLOUR_HIGHLIGHTTEXT
+ButtonFace = wx.SYS_COLOUR_BTNFACE
+ButtonHighlightText = wx.SYS_COLOUR_BTNHIGHLIGHT
+ButtonShadow = wx.SYS_COLOUR_BTNSHADOW
+Button = wx.SYS_COLOUR_BTNTEXT
+Window = wx.SYS_COLOUR_WINDOW
+WindowText = wx.SYS_COLOUR_WINDOWTEXT
+WindowFrame = wx.SYS_COLOUR_WINDOWFRAME
+ScrollBar = wx.SYS_COLOUR_SCROLLBAR
+Menu = wx.SYS_COLOUR_MENU
+InactiveBorder = wx.SYS_COLOUR_INACTIVEBORDER
+InactiveCaption = wx.SYS_COLOUR_INACTIVECAPTION
+InactiveCaptionText = wx.SYS_COLOUR_INACTIVECAPTIONTEXT
+ToolBarText = wx.ROLE_SYSTEM_TOOLBAR
+CaptionText = wx.SYS_COLOUR_CAPTIONTEXT
+
+# Event
+## KeyEvent
+OnChar = wx.EVT_CHAR
+OnCharHook = wx.EVT_CHAR_HOOK
+OnKeyDown = wx.EVT_KEY_DOWN
+OnKeyUp = wx.EVT_KEY_UP
+## MouseEvent
+OnEnterWindow = wx.EVT_ENTER_WINDOW
+OnLeaveWindow = wx.EVT_LEAVE_WINDOW
+OnLeftDownClick = wx.EVT_LEFT_DCLICK
+OnLeftDown = wx.EVT_LEFT_DOWN
+OnLeftUp = wx.EVT_LEFT_UP
+OnMiddleDownClick = wx.EVT_MIDDLE_DCLICK
+OnMiddleDown = wx.EVT_MIDDLE_DOWN
+OnMiddleUp = wx.EVT_MIDDLE_UP
+OnRightDownClick = wx.EVT_RIGHT_DCLICK
+OnRightDown = wx.EVT_RIGHT_DOWN
+OnRightUp = wx.EVT_RIGHT_UP
+
+def Colour(Colour):
+    Colour = wx.Colour(Colour)
+
+def Postion(X, Y):
+    Postion = wx.Point(X, Y)
+    return Postion
