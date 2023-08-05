f3a0d75789175b76d6d4fdd9c66fbae754fb1d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:21 +0000

    Add package rikai/0.1.6/rikai-0.1.6.tar.gz

---
+++ packages/rikai/rikai-0.1.6.tar.gz/rikai-0.1.6/rikai/pytorch/models/ssd.py
@@ -0,0 +1,40 @@
+#  Copyright 2022 Rikai Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import torchvision
+
+from .torchvision import ObjectDetectionModelType
+from rikai.spark.sql.codegen.dummy import DummyModelSpec
+from rikai.spark.sql.model import ModelSpec
+from rikai.mixin import Pretrained
+
+
+__all__ = ["MODEL_TYPE"]
+
+
+class SSDModelType(ObjectDetectionModelType, Pretrained):
+    def pretrained_model(self):
+        return torchvision.models.detection.ssd.ssd300_vgg16()
+
+    def find_model(self):
+        if isinstance(self.spec, DummyModelSpec):
+            return self.pretrained_model()
+        else:
+            return super().find_model()
+
+    def __init__(self):
+        super().__init__("SSD")
+
+
+MODEL_TYPE = SSDModelType()
