fdfa38140f1498ae662e991f7ebc3f6358dbfc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:25 +0000

    Add package telephone/0.0.5/telephone-0.0.5-py3-none-any.whl

---
+++ packages/telephone/telephone-0.0.5-py3-none-any.whl/telephone/__init__.py
@@ -0,0 +1,1 @@
+from .main import Telephonemizer, _phn2word_mapping_table
---
+++ packages/telephone/telephone-0.0.5-py3-none-any.whl/telephone/main.py
@@ -0,0 +1,102 @@
+from transformers import AutoProcessor, AutoModelForCTC, AutoModelForSeq2SeqLM
+from phonemizer.backend import EspeakBackend
+from phonemizer.separator import Separator
+import torch
+
+
+class Telephonemizer:
+    def __init__(self, load_audio_model=True):
+        self.load_audio_model = load_audio_model
+        if load_audio_model:
+            self.model = AutoModelForCTC.from_pretrained("facebook/wav2vec2-xlsr-53-espeak-cv-ft")
+            self.processor = AutoProcessor.from_pretrained("facebook/wav2vec2-xlsr-53-espeak-cv-ft")
+        self.separator = Separator(phone="", word="", syllable="")
+        self.backend = EspeakBackend(language="en-us",
+                                     language_switch="remove-flags", )
+
+    def convert_voice(self, audio_array):
+        if not self.load_audio_model:
+            assert ("should set load_audio_model to True when init")
+        input_values = self.processor(audio_array, return_tensors="pt").input_values
+        with torch.no_grad():
+            logits = self.model(input_values).logits
+
+        predicted_ids = torch.argmax(logits, dim=-1)
+        phonemes = self.processor.batch_decode(predicted_ids)[0].replace(" ", "")
+        for p, w in _phn2word_mapping_table.items():
+            phonemes = phonemes.replace(p, w)
+        return phonemes
+
+    def convert_text(self, text):
+        phonemes = self.backend.phonemize([text], separator=self.separator)
+        phonemes = phonemes[0].strip()
+        for p, w in _phn2word_mapping_table.items():
+            phonemes = phonemes.replace(p, w)
+        return phonemes
+
+
+_phn2word_mapping_table = {'ð': 'T',
+                           'ə': '@',
+                           'ʌ': 'u',
+                           'v': 'v',
+                           'æ': 'a',
+                           'n': 'n',
+                           'd': 'd',
+                           'ɪ': 'i',
+                           't': 't',
+                           'uː': 'U',
+                           'eɪ': 'A',
+                           'w': 'w',
+                           'z': 'z',
+                           'ɔ': 'o',
+                           'f': 'f',
+                           'ɔːɹ': 'Wr',
+                           'ɹ': 'r',
+                           'ɔː': 'W',
+                           'b': 'b',
+                           'aɪ': 'I',
+                           'h': 'h',
+                           'iː': 'E',
+                           'i': 'E',
+                           'm': 'm',
+                           'ɜː': '3',
+                           'tʃ': 'tS',
+                           'ʃ': 'S',
+                           's': 's',
+                           'ɑːɹ': '~r',
+                           'ɑː': '~',
+                           'ɛɹ': 'er',
+                           'ɛ': 'e',
+                           'ɚ': 'er',
+                           'l': 'l',
+                           'oʊ': 'O',
+                           'ʊ': 'U',
+                           'ʊɹ': 'Ur',
+                           'ŋ': 'N',
+                           'oːɹ': 'Or',
+                           'oː': 'O',
+                           'ɡ': 'g',
+                           'ɾ': 'd',
+                           'p': 'p',
+                           'θ': 'D',
+                           'ɐ': '@',
+                           'aʊ': '^U',
+                           'ᵻ': 'i',
+                           'j': 'j',
+                           'ɪɹ': 'ir',
+                           'k': 'k',
+                           'əl': '@l',
+                           'iə': 'E@',
+                           'dʒ': 'dZ',
+                           'ʒ': 'Z',
+                           'ɔɪ': 'oi',
+                           'ʔ': '|',
+                           'n̩': 'n',
+                           'aɪɚ': 'Ier',
+                           'aɪə': 'I@',
+                           'iːː': 'E',
+                           'x': 'H',
+                           'r': 'r',
+                           'ɑ̃': 'A',
+                           'ɡʲ': 'g',
+                           }
