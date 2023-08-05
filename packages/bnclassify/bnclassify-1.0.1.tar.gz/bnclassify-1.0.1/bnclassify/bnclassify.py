9db4cfdb6424132535cd3213215b08185388b057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:10 +0000

    Add package bnclassify/1.0.1/bnclassify-1.0.1.tar.gz

---
+++ packages/bnclassify/bnclassify-1.0.1.tar.gz/bnclassify-1.0.1/bnclassify/bnclassify.py
@@ -0,0 +1,6 @@
+# --------------------------------------------------
+# Author      : E.Taskesen
+# Contact     : erdogant@gmail.com
+# github      : https://erdogant.github.io/bnlearn
+# Licence     : MIT
+# --------------------------------------------------
