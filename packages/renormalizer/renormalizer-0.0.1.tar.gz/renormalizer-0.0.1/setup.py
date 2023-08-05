fb3a0fd152b4edc9dc5e8e1e84df9c53e408659e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:10 +0000

    Add package renormalizer/0.0.1/renormalizer-0.0.1.tar.gz

---
+++ packages/renormalizer/renormalizer-0.0.1.tar.gz/renormalizer-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "rb") as fin:
+    long_description = fin.read().decode("utf-8")
+
+with open("requirements.txt") as fin:
+    req = fin.readlines()
+
+
+setuptools.setup(
+    name="renormalizer",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shuaigroup/Renormalizer",
+    install_requires=req,
+    license="Apache",
+)
