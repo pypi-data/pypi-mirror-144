4a1cbf2e9a48408f86158561171a5628d0630a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package sw-aqueduct/0.0.1/sw-aqueduct-0.0.1.tar.gz

---
+++ packages/sw-aqueduct/sw-aqueduct-0.0.1.tar.gz/sw-aqueduct-0.0.1/aqueduct/models/__init__.py
@@ -0,0 +1,14 @@
+from .dashboard import (
+    Dashboard,
+    ReportItems
+)
+from .report import Report
+from .plugin import Plugin
+from .role import Role
+from .package import Package
+from .user import (
+    User,
+    UserLight
+)
+from .group import Group
+from .application import Application
