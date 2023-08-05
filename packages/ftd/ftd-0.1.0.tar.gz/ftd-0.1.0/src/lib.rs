3be4bc20b66d7243c45998c3079fd40143414b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package ftd/0.1.0/ftd-0.1.0.tar.gz

---
+++ packages/ftd/ftd-0.1.0.tar.gz/ftd-0.1.0/src/lib.rs
@@ -0,0 +1,40 @@
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn fpm_build(
+    py: pyo3::Python,
+    file: Option<String>,
+    base_url: Option<String>,
+    ignore_failed: Option<bool>,
+) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(
+        py,
+        async move {
+            let config = match fpm::Config::read().await {
+                Ok(c) => c,
+                _ => {return  Ok(Python::with_gil(|py| py.None()));}
+            };
+            fpm::build(
+                &config,
+                file.as_deref(),
+                base_url.unwrap_or("/".to_string()).as_str(), // unwrap okay because base is required
+                ignore_failed.unwrap_or(false),
+            ).await.ok();
+            Ok(Python::with_gil(|py| py.None()))
+        })
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn ftd(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(fpm_build, m)?)?;
+    Ok(())
+}
