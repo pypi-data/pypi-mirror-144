2b860a83d95b8be82c350da45e1eaf9f55dc0594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:32 +0000

    Add package torchinfo/1.6.5/torchinfo-1.6.5-py3-none-any.whl

---
+++ packages/torchinfo/torchinfo-1.6.5-py3-none-any.whl/torchinfo/enums.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from enum import Enum, IntEnum, unique
+
+
+@unique
+class Mode(Enum):
+    """Enum containing all model modes."""
+
+    TRAIN = "train"
+    EVAL = "eval"
+
+
+@unique
+class RowSettings(Enum):
+    """Enum containing all available row settings."""
+
+    DEPTH = "depth"
+    VAR_NAMES = "var_names"
+    ASCII_ONLY = "ascii_only"
+
+
+@unique
+class ColumnSettings(Enum):
+    """Enum containing all available column settings."""
+
+    KERNEL_SIZE = "kernel_size"
+    INPUT_SIZE = "input_size"
+    OUTPUT_SIZE = "output_size"
+    NUM_PARAMS = "num_params"
+    MULT_ADDS = "mult_adds"
+
+
+@unique
+class Verbosity(IntEnum):
+    """Contains verbosity levels."""
+
+    QUIET, DEFAULT, VERBOSE = 0, 1, 2
