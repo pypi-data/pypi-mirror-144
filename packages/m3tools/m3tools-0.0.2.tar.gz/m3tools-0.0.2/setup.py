28c4ac9600d4c4707d7d0cd5a8af03a5a9bfdc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:20:30 +0000

    Add package m3tools/0.0.2/m3tools-0.0.2.tar.gz

---
+++ packages/m3tools/m3tools-0.0.2.tar.gz/m3tools-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'm3tools'
+LONG_DESCRIPTION = 'A package to perform many useful tools'
+
+# Setting up
+setup(
+    name="m3tools",
+    version=VERSION,
+    author="fahiran",
+    author_email="fffahiran@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['mxtools'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
