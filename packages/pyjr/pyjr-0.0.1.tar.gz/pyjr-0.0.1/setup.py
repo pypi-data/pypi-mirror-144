80deac549d973e2650595c5696f304b2bf0236c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:09 +0000

    Add package pyjr/0.0.1/pyjr-0.0.1.tar.gz

---
+++ packages/pyjr/pyjr-0.0.1.tar.gz/pyjr-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+current_path = os.path.abspath('C:\\Users\\Peter\\Desktop\\Personal\\11_Repository\\Helper Functions\\')
+
+def read_file(*parts):
+    with open(os.path.join(current_path, *parts), encoding='utf-8') as reader:
+        return reader.read()
+
+setup(
+    name='pyjr',
+    version='0.0.1',
+    packages=['pyjr', ],
+    author='Peter Rigali',
+    author_email='peterjrigali@gmail.com',
+    license='MIT',
+    description='Functions and Classes for assisting with ETL.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='',
+)
