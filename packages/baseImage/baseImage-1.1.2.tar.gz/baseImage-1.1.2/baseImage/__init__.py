d4c63a43e059b07aff34bb2b91deba235660a819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:33 +0000

    Add package baseImage/1.1.2/baseImage-1.1.2.tar.gz

---
+++ packages/baseImage/baseImage-1.1.2.tar.gz/baseImage-1.1.2/baseImage/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .base_image import Image
+from .coordinate import Rect, Point, Size
+import cv2
+name = 'base_image'
+
+
+__all__ = ['Rect', 'Point', 'Size', 'Image']
---
+++ packages/baseImage/baseImage-1.1.2.tar.gz/baseImage-1.1.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def install_requires():
+    try:
+        import cv2
+        return None
+    except ModuleNotFoundError:
+        return [
+            'numpy>=1.21.4',
+            'opencv-python>=4.5.4.60',
+            'pydantic'
+        ]
+
+
+setup(
+    name='baseImage',
+    version='1.1.2',
+    author='hakaboom',
+    author_email='1534225986@qq.com',
+    license='Apache License 2.0',
+    description='This is a secondary package of OpenCV,for manage image data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hakaboom/base_image',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8, <=3.9',
+)
