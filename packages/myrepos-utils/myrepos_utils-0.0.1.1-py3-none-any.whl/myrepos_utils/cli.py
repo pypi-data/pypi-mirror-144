964ecfc83bf69c1a64c653e72f39331965c93a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:18 +0000

    Add package myrepos-utils/0.0.1.1/myrepos_utils-0.0.1.1-py3-none-any.whl

---
+++ packages/myrepos-utils/myrepos_utils-0.0.1.1-py3-none-any.whl/myrepos_utils/cli.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (c) Michel Alexandre Salim
+#
+# Fedora-License-Identifier: GPLv2+
+# SPDX-2.0-License-Identifier: GPL-2.0+
+# SPDX-3.0-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software.
+# For more information on the license, see COPYING.md.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.
+
+import click
+import json
+import sys
+from . import utils
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(help="find repo directories tracked by myrepos")
+@click.argument("query", nargs=-1)
+def find(query: list[str]):
+    import os
+
+    matches = utils.find_repo(query)
+    for repo in matches:
+        click.echo(repo)
---
+++ packages/myrepos-utils/myrepos_utils-0.0.1.1-py3-none-any.whl/myrepos_utils/utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (c) Michel Alexandre Salim
+#
+# Fedora-License-Identifier: GPLv2+
+# SPDX-2.0-License-Identifier: GPL-2.0+
+# SPDX-3.0-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software.
+# For more information on the license, see COPYING.md.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.
+
+import configparser
+import os
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.expanduser("~/.mrconfig"))
+
+
+def get_repos(config: configparser.ConfigParser = CONFIG) -> list[str]:
+    return list(config.keys())
+
+
+def find_repo(
+    query: list[str], config: configparser.ConfigParser = CONFIG
+) -> list[str]:
+    import re
+
+    repos = get_repos(config)
+    r = f".*{'.*'.join(query)}.*"
+    return [os.path.expanduser(f"~/{repo}") for repo in repos if re.match(r, repo)]
