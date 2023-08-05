1cc6459804578290bcca3ca845249b6b3b6a7f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:45 +0000

    Add package kolibri-desktop-auth-plugin/0.0.7/kolibri_desktop_auth_plugin-0.0.7-py2.py3-none-any.whl

---
+++ packages/kolibri-desktop-auth-plugin/kolibri_desktop_auth_plugin-0.0.7-py2.py3-none-any.whl/kolibri_desktop_auth_plugin/kolibri_plugin.py
@@ -0,0 +1,9 @@
+from kolibri.plugins import KolibriPluginBase
+
+from . import settings
+
+
+class DesktopUserAuth(KolibriPluginBase):
+    translated_view_urls = "urls"
+    settings_module = settings
+    kolibri_options = "options"
