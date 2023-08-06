28f934c26fef9f173be240d05044c64f98e32442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:40 +0000

    Add package httpy-cli/0.0.2/httpy_cli-0.0.2-py3-none-any.whl

---
+++ packages/httpy-cli/httpy_cli-0.0.2-py3-none-any.whl/httpy/core.py
@@ -0,0 +1,36 @@
+import requests
+
+from httpy.arguments import ArgumentParser
+from httpy.command.handler import CommandHandler
+from httpy.output.error import print_error
+from httpy.output.writer import write
+from httpy.request import Request
+from httpy.status import ExitStatus
+
+
+def main() -> ExitStatus:
+    args = ArgumentParser()
+    req = Request.from_args(args)
+
+    print()
+
+    if not args.command:
+        try:
+            res = req.make_request()
+        except requests.exceptions.InvalidURL:
+            print_error("Invalid URL: ", req.url)
+            return ExitStatus.ERROR
+        write(args, res)
+        return ExitStatus.SUCCESS
+
+    command_handler = CommandHandler(req, args.command)
+    reqs = command_handler.builder.build_requests()
+
+    for request in reqs:
+        try:
+            res = request.make_request()
+        except requests.exceptions.InvalidURL:
+            print_error("Invalid URL: ", req.url)
+            return ExitStatus.ERROR
+        write(args, res)
+    return ExitStatus.SUCCESS
