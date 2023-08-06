f8e8854b72987e8ebb19768458de85576f14568a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:16 +0000

    Add package spid-cie-oidc/0.6.0/spid_cie_oidc-0.6.0-py3-none-any.whl

---
+++ packages/spid-cie-oidc/spid_cie_oidc-0.6.0-py3-none-any.whl/spid_cie_oidc/provider/exceptions.py
@@ -0,0 +1,18 @@
+class AuthzRequestReplay(Exception):
+    pass
+
+
+class InvalidSession(Exception):
+    pass
+
+
+class RevokedSession(Exception):
+    pass
+
+
+class ValidationException(Exception):
+    pass
+
+
+class ExpiredAuthCode(Exception):
+    pass
