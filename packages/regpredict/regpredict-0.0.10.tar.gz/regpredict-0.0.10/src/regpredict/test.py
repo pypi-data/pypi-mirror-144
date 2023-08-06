1afe1e815fb931cb702e53b7945b9201ed2f0567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:31 +0000

    Add package regpredict/0.0.10/regpredict-0.0.10.tar.gz

---
+++ packages/regpredict/regpredict-0.0.10.tar.gz/regpredict-0.0.10/src/regpredict/test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from regbot import signal
+#from regpredict.regbot import signal
+df = pd.read_csv('../../../jupyter/regbot_v4.csv')
+
+y_pred = []
+def getSignal(open,close,bvr):
+    return signal(open,close,bvr)
+
+
+df = df[df['buy'] == 1]
+
+df['result'] = df.apply(lambda row: getSignal(row['open'], row['close'], row['b-v-r']), axis=1)
+
+print(df.head())
+
+print(len(df[df['result'] == df['buy']]), len(df))
+
+
+
