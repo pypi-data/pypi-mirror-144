650a18683aeae01fcc79e19e659ddc77f395f40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:01 +0000

    Add package cosecurity-amqp-lib/0.0.11/cosecurity_amqp_lib-0.0.11-py3-none-any.whl

---
+++ packages/cosecurity-amqp-lib/cosecurity_amqp_lib-0.0.11-py3-none-any.whl/cosecurity_amqp_lib/stub.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from typing import Any, Dict
+from cosecurity_amqp_lib.producer import Producer
+
+
+class Stub:
+    def __init__(self, destination:str) -> None:
+        self._producer = Producer()
+        self._destination = destination
+    
+    def _send(self, primitive:str, content:Dict[str, Any]) -> None:
+        self._producer.send_message(
+            destination=self._destination, 
+            primitive=primitive, 
+            content=content
+        )
+
+class DetectionStub(Stub):
+    def __init__(self):
+        super().__init__(
+            destination='detection_model'
+        )
+
+    def find_objects(self, camera_hash:str, image_hash:str, image:np.ndarray) -> None:
+        self._send(
+            primitive='find_objects',  
+            content={
+                'camera_hash': camera_hash,
+                'image_hash': image_hash,
+                'image': image
+            }
+        )
