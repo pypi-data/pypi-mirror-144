b5b8c23bebadc44b3c4e7f0d54efea2be47b33f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:19 +0000

    Add package rosnet/0.1.5/rosnet-0.1.5.tar.gz

---
+++ packages/rosnet/rosnet-0.1.5.tar.gz/rosnet-0.1.5/rosnet/decorator/_methods.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def check_vector(function):
+      def check(self, y_predict, y):
+        if len(y_predict.shape) == 1:
+            y_predict = y_predict.reshape(-1,1)
+        if len(y.shape) == 1:
+            y = y.reshape(-1,1) 
+        result = function(self, y_predict, y)
+        if len(result.shape) == 1:
+            result = result.reshape(-1,1)
+        return result
+      return check
