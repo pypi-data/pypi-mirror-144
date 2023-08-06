d45667d2f41cb426c6c3bb73314fd4c1670d70aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package jiayi-test/1.0.0/jiayi_test-1.0.0.tar.gz

---
+++ packages/jiayi-test/jiayi_test-1.0.0.tar.gz/jiayi_test-1.0.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+
+setup(name='jiayi_test',  # 包名
+      version='1.0.0',  # 版本号
+      description='A small example package',
+      long_description=long_description,
+      author='yinjiayi',
+      author_email='yinjiayi@live.cn',
+      url='https://a.com',
+      install_requires=[],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
