c5a76e32c05dbd024b906b404b216136e186e839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:40 +0000

    Add package pdme/0.6.0/pdme-0.6.0.tar.gz

---
+++ packages/pdme/pdme-0.6.0.tar.gz/pdme-0.6.0/pdme/measurement/dot_measure.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+import numpy
+import numpy.typing
+
+
+@dataclass
+class DotMeasurement():
+	'''
+	Representation of a dot measuring oscillating dipoles.
+
+	Parameters
+	----------
+	v : float
+		The voltage measured at the dot.
+	r : numpy.ndarray
+		The position of the dot.
+	f : float
+		The measurement frequency.
+	'''
+	v: float
+	r: numpy.ndarray
+	f: float
+
+	def __post_init__(self) -> None:
+		self.r = numpy.array(self.r)
+
+
+@dataclass
+class DotRangeMeasurement():
+	'''
+	Representation of a dot measuring oscillating dipoles.
+
+	Parameters
+	----------
+	v_low : float
+		The lower range of voltage measured at the dot.
+	v_high : float
+		The upper range of voltage measured at the dot.
+	r : numpy.ndarray
+		The position of the dot.
+	f : float
+		The measurement frequency.
+	'''
+	v_low: float
+	v_high: float
+	r: numpy.ndarray
+	f: float
+
+	def __post_init__(self) -> None:
+		self.r = numpy.array(self.r)
