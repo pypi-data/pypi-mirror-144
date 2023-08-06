73a633d31fdaa42f990f487860c3c6cb3ec02889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:50 +0000

    Add package SigProfilerAssignment/0.0.5/SigProfilerAssignment-0.0.5.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.5.tar.gz/SigProfilerAssignment-0.0.5/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.5'
+version = '0.0.5'
+Update = 'Enabling linking options with SigProfilerExtractor and can support different context types and devopts'
+    
+    
