6d1ef3b6ac2e0e0d8a331233fdc247ab83a97ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:24:16 +0000

    Add package AthenaColor/3.0.7/AthenaColor-3.0.7-py3-none-any.whl

---
+++ packages/AthenaColor/AthenaColor-3.0.7-py3-none-any.whl/AthenaColor/Objects/__init__.py
@@ -0,0 +1,3 @@
+from .HtmlColors import HtmlColors
+from .Rgb import rgb
+from .Hexadecimal import hexadecimal
---
+++ packages/AthenaColor/AthenaColor-3.0.7-py3-none-any.whl/AthenaColor/__init__.py
@@ -0,0 +1,61 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import os
+import sys
+
+# Custom Library
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - INIT class -
+# ----------------------------------------------------------------------------------------------------------------------
+class Init:
+    esc_hex = "\x1b"
+    esc_octal = "\033"
+    esc_uni = "\u001b"
+
+    esc = esc_hex
+
+    def __init__(self, escape_code:str=None):
+        self.set_esc(escape_code)
+
+        # prep the console for colors
+        if sys.platform == 'win32':
+            os.system("color")
+            
+    @classmethod
+    def set_esc(cls, escape_code:str=None):
+        # define escape codes
+        if escape_code is None:
+            cls.esc = cls.esc_hex
+        elif escape_code in [cls.esc_hex,cls.esc_octal, cls.esc_octal]:
+            cls.esc = escape_code
+        else:
+            raise ValueError("escape_code not defined correctly")
+
+init = Init()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - AthenaColor Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+from AthenaColor.Styling.Inline import (
+    Fore,
+    Back,
+    Style
+)
+from AthenaColor.Styling.Nested import (
+    Fore    as ForeNest ,
+    Back    as BackNest ,
+    Style   as StyleNest
+)
+
+
+from AthenaColor.Objects import (
+    rgb,
+    HtmlColors,
+    hexadecimal
+)
