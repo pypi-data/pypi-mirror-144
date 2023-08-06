3fb8a92ca286c99da513827b504ead749ca99150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:43 +0000

    Add package dpn-pyutils/3.0.0a6/dpn_pyutils-3.0.0a6.tar.gz

---
+++ packages/dpn-pyutils/dpn_pyutils-3.0.0a6.tar.gz/dpn_pyutils-3.0.0a6/src/dpn_pyutils/http.py
@@ -0,0 +1,11 @@
+from urllib import urlparse
+
+def is_url(url: str):
+    """ 
+    Code from https://stackoverflow.com/a/52455972 
+    """
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
