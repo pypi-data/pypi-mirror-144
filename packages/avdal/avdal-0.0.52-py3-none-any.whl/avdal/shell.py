c28bff2f649040b7dc95948c4c8c14b185303cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:00 +0000

    Add package avdal/0.0.52/avdal-0.0.52-py3-none-any.whl

---
+++ packages/avdal/avdal-0.0.52-py3-none-any.whl/avdal/shell.py
@@ -0,0 +1,30 @@
+import json
+import subprocess as proc
+
+
+def run(cmd, *args, cwd=None):
+    try:
+        if cwd is not None and len(cwd.strip()) == 0:
+            cwd = "."
+
+        exec = proc.run([cmd] + [str(a) for a in args],
+                        cwd=cwd,
+                        stdout=proc.PIPE,
+                        stderr=proc.PIPE,
+                        universal_newlines=True)
+    except Exception as err:
+        return 1, str(err), ""
+
+    return exec.returncode, exec.stdout, exec.stderr
+
+
+def runj(cmd, *args, cwd=None):
+    ret, stdout, stderr = run(cmd, *args, cwd=cwd)
+
+    if ret != 0:
+        ret, {"_stdout": stdout, "_stderr": stderr}
+
+    try:
+        return 0, json.loads(stdout)
+    except json.JSONDecodeError as err:
+        return 1, {"_decode_err": err, "_stdout": stdout, "_stderr": stderr}
