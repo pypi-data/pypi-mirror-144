87190e9fa0fe7ac71bd4dd0d060b632506ddc7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:55 +0000

    Add package x2wincur/0.0/x2wincur-0.0.tar.gz

---
+++ packages/x2wincur/x2wincur-0.0.tar.gz/x2wincur-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='quantum',
+    author_email='quantum2048@gmail.com',
+    description='win2xcur is a tool to convert Windows .cur and .ani cursors to Xcursor format.',
+    install_requires=['win2xcur'],
+    long_description='Use `win2xcur <https://pypi.python.org/pypi/win2xcur/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='x2wincur',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/win2xcur/',
+    version='0.0',
+    zip_safe=False,
+)
