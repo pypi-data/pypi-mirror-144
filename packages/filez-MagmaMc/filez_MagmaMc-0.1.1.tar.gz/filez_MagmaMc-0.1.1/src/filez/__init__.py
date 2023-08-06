2b10fe625ec56d7426d3847511e6f728458e0909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:37 +0000

    Add package filez-MagmaMc/0.1.1/filez_MagmaMc-0.1.1.tar.gz

---
+++ packages/filez-MagmaMc/filez_MagmaMc-0.1.1.tar.gz/filez_MagmaMc-0.1.1/src/filez/__init__.py
@@ -0,0 +1,44 @@
+import requests as request
+from tkinter import messagebox
+
+ID = "notset"
+baseurl = 'http://magma-mc.net/Modules/filez/filez.php/?ID='+ID
+ # ID = the folder name of where the files will be stored please make sure -
+ # you have created the project at http://magma-mc.net/projects.php
+def _colorit(rgb, text):
+    r, g, b = rgb
+    return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
+def _check():
+    if ID == "notset":
+        errortext = """
+               filez.ID, not set,
+               please set and make sure that the project exists,
+               http://magma-mc.net/projects.php
+              """
+              
+        print(errortext)
+        print(_colorit((255, 0, 0), errortext))
+        messagebox.showerror("ID Not Set", _colorit((255, 0, 0), errortext))
+        return False
+    else:
+        return True
+        
+def fread(file):
+    if _check():
+       return str(request.get(baseurl+"&filez=read&filename="+file).text)
+    else:
+        raise
+def scan(folder):
+    if _check():
+        return str(request.get(baseurl+"&filez=scan&filename="+folder).text)
+    else:
+        raise
+def fwrite(file, data, type):
+    try:
+        if _check():
+            request.post(baseurl+"&filez=write&content="+data+"&filename="+file+"&type="+type)
+        else:
+            raise
+    except:
+        return False
+    return True
