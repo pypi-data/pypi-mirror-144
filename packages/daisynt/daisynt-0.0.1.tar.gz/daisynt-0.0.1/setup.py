a896628311d61f7d392059d9b17dfc2a827b37a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:49 +0000

    Add package daisynt/0.0.1/daisynt-0.0.1.tar.gz

---
+++ packages/daisynt/daisynt-0.0.1.tar.gz/daisynt-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="daisynt",
+    version="0.0.1",
+    author="Giacomo Graffi",
+    author_email="giacomo.graffi@gmail.com",
+    description="Test Suite to enable Synthetic Data adoption",
+    python_requires='>=3.6'
+)
