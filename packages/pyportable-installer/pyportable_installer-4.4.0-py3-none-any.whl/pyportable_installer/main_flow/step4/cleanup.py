9de02d09148419cf8a8faa77844eba85dab064ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:53 +0000

    Add package pyportable-installer/4.4.0/pyportable_installer-4.4.0-py3-none-any.whl

---
+++ packages/pyportable-installer/pyportable_installer-4.4.0-py3-none-any.whl/pyportable_installer/main_flow/step4/cleanup.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+from lk_utils import find_dirs
+from lk_utils import find_files
+from lk_utils.read_and_write import dumps
+
+from ...path_model import dst_model
+from ...path_model import prj_model
+
+
+def main(pyproj_file, additional_conf):
+    _cleanup_intermediate_files()
+    _generate_manifest(pyproj_file, dst_model.manifest, additional_conf)
+
+
+def _cleanup_intermediate_files():
+    print('[I5325]', 'clean up intermediate files', ':v2')
+    
+    for d, _ in find_dirs(prj_model.temp):
+        shutil.rmtree(d)
+        # try:
+        #     shutil.rmtree(d)
+        # except PermissionError:
+        #     continue
+    
+    for f, _ in find_files(prj_model.temp):
+        if f.endswith('.gitkeep'):
+            continue
+        os.remove(f)
+
+
+def _generate_manifest(pyproj_file, file_o, additional_conf):
+    from ..step1 import main
+    conf = main(pyproj_file, additional_conf, 'relpath',
+                _skip_init_key_params=True)
+    try:
+        conf['build']['compiler']['options']['pyportable_crypto']['key'] = ''
+        conf['build']['compiler']['options']['zipapp']['password'] = ''
+    except KeyError:
+        pass
+    dumps(conf, file_o)
