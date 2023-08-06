6b2e87f4ddab5379f9e20514cfae948372a72f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:05 +0000

    Add package MeUtils/2022.3.31.14.13.30/MeUtils-2022.3.31.14.13.30-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.3.31.14.13.30-py3-none-any.whl/examples/betterme/weekmeet.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : wechat
+# @Time         : 2021/6/7 11:17 上午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : todo: 分享会通知
+import time
+
+from meutils.pipe import *
+from meutils.notice.wechat import Bot
+
+bot = Bot("3bf9d29e-0be9-471d-a021-2b0e73efcaad")
+
+json = {
+    "msgtype": "news",
+    "news": {
+        "articles": [
+            {
+                "title": "数据科学研发中心周会",  # 14个字换行
+                "description": "同学们，点击图片进教室，迟到发红包",  # 17个字换行
+                "url": "https://meeting.tencent.com/s/UJ5c5nOdCJI3",
+                "picurl": "https://i.loli.net/2021/08/03/CJeHpGuWXKrPgc3.jpg",  # 1068*455 150*150
+            },
+        ]
+    }
+}
+logger.info("周会通知")
+
+bot.send(json)
+
+# time.sleep(60 * 15)
+# json = {
+#     "msgtype": "text",
+#     "text": {
+#         "mentioned_mobile_list": ["@all"],
+#         "content": "点击图片进教室，周会已经开始啦",
+#     }
+# }
+# logger.info("周会开始")
+# bot.send(json)
