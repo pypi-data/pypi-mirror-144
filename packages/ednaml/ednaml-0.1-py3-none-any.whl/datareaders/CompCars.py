9f3a685a0ff1a89d66f3e76a9df60a803401324d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:12 +0000

    Add package ednaml/0.1/ednaml-0.1-py3-none-any.whl

---
+++ packages/ednaml/ednaml-0.1-py3-none-any.whl/datareaders/CompCars.py
@@ -0,0 +1,13 @@
+
+from ednaml.datareaders import DataReader
+from ednaml.crawlers import CoLabelCompCarsCrawler
+from ednaml.generators.ClassificationGenerator import ClassificationDataset, ClassificationGenerator
+
+class CompCars(DataReader):
+    name: str = "CompCars"
+    CRAWLER = CoLabelCompCarsCrawler
+    DATASET = ClassificationDataset
+    GENERATOR = ClassificationGenerator
+
+    def __init__(self):
+        super().__init__() 
---
+++ packages/ednaml/ednaml-0.1-py3-none-any.whl/datareaders/VeRi.py
@@ -0,0 +1,13 @@
+
+from ednaml.datareaders import DataReader
+from crawlers import VeRiDataCrawler
+from generators.ClassificationGenerator import ClassificationDataset, ClassificationGenerator
+
+class VeRi(DataReader):
+    name: str = "VeRi"
+    CRAWLER = VeRiDataCrawler
+    DATASET = ClassificationDataset
+    GENERATOR = ClassificationGenerator
+
+    def __init__(self):
+        super().__init__() 
