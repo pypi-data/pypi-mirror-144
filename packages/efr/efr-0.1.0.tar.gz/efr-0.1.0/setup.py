b0aed318a0ef1cc5df19990c2244f56b3520eeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:20 +0000

    Add package efr/0.1.0/efr-0.1.0.tar.gz

---
+++ packages/efr/efr-0.1.0.tar.gz/efr-0.1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+name='efr',
+version='0.1.0',
+description='Event-Driver Framework. Pure python. Efficient.',
+author="Eagle'sBaby",
+author_email='2229066748@qq.com',
+maintainer="Eagle'sBaby",
+maintainer_email='2229066748@qq.com',
+packages=find_packages(),
+platforms=["all"],
+license='Apache Licence 2.0',
+classifiers=[
+'Programming Language :: Python',
+'Programming Language :: Python :: 3',
+],
+keywords = ['pickle', 'files', "object"],
+python_requires='>=3', 
+)
