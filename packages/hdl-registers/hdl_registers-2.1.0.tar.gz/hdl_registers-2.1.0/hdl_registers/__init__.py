64b26ae8b86f3ed033473e46d2caa43b5b5196ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:39 +0000

    Add package hdl-registers/2.1.0/hdl_registers-2.1.0.tar.gz

---
+++ packages/hdl-registers/hdl_registers-2.1.0.tar.gz/hdl_registers-2.1.0/hdl_registers/__init__.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl_registers project.
+# https://hdl-registers.com
+# https://gitlab.com/tsfpga/hdl_registers
+# --------------------------------------------------------------------------------------------------
+
+"""
+A HDL register generator fast enough to be run in real time.
+"""
+
+
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).parent.parent.resolve()
+HDL_REGISTERS_PATH = REPO_ROOT / "hdl_registers"
+HDL_REGISTERS_DOC = REPO_ROOT / "doc"
+HDL_REGISTERS_GENERATED = REPO_ROOT / "generated"
+
+__version__ = "2.1.0"
