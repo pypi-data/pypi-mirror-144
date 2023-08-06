62b776d4ee9e6157984d63c180abca2484cdcc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:02 +0000

    Add package helix-mockserver-client/0.1.2/helix_mockserver_client-0.1.2.tar.gz

---
+++ packages/helix-mockserver-client/helix_mockserver_client-0.1.2.tar.gz/helix_mockserver_client-0.1.2/mockserver_client/exceptions/mock_server_request_not_found_exception.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, List, Optional
+
+from .mock_server_exception import MockServerException
+
+
+class MockServerRequestNotFoundException(MockServerException):
+    def __init__(
+        self,
+        method: Optional[str],
+        url: Optional[str],
+        json_list: Optional[List[Dict[str, Any]]],
+    ) -> None:
+        self.method: Optional[str] = method
+        self.url: Optional[str] = url
+        self.json_dict: Optional[List[Dict[str, Any]]] = json_list
+        assert (
+            not json_list or isinstance(json_list, dict) or isinstance(json_list, list)
+        ), type(json_list)
+        super().__init__(f"Request was not expected: {url} {json_list}")
