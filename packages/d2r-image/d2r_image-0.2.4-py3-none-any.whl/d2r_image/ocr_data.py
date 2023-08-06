8a579118d2720387da6f35c214dc53828dce5eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package d2r-image/0.2.4/d2r_image-0.2.4-py3-none-any.whl

---
+++ packages/d2r-image/d2r_image-0.2.4-py3-none-any.whl/d2r_image/ocr_data.py
@@ -0,0 +1,45 @@
+import re
+import os
+import pkgutil
+from enum import Enum
+
+I_1 = re.compile(r"(?<=[%I0-9\-+])I|I(?=[%I0-9\-+])")
+II_U = re.compile(r"(?<=[A-Z])II|II(?=[A-Z])|1?=[a-z]")
+One_I = re.compile(r"(?<=[A-Z])1|1(?=[A-Z])|1?=[a-z]")
+OneOne_U = re.compile(r"(?<=[A-Z])11|11(?=[A-Z])|1?=[a-z]")
+
+class TrainedDataSets(Enum):
+    engd2r_inv_th = 'engd2r_inv_th'
+    engd2r_inv_th_fast = 'engd2r_inv_th_fast'
+    engd2r_ui = "engd2r_ui"
+
+OCR_WORKING_DIR = os.path.join(os.getenv('APPDATA'), 'd2r_image')
+ocr_config_data = pkgutil.get_data(
+    __name__,
+    "resources/tessdata/ocr_config.txt")
+OCR_CONFIG = ocr_config_data.decode()
+
+ERROR_RESOLUTION_MAP = {
+    'SHIFLD': 'SHIELD',
+    'SPFAR': 'SPEAR',
+    'GLOVFS': 'GLOVES',
+    'GOLP': 'GOLD',
+    'TELEFORT': 'TELEPORT',
+    'TROPHV': 'TROPHY',
+    'CLAVMORE': 'CLAYMORE',
+    'MAKIMUM': 'MAXIMUM',
+    'DEKTERITY': 'DEXTERITY',
+    'DERTERITY': 'DEXTERITY',
+    'QUAHTITY': 'QUANTITY',
+    'DEFERSE': 'DEFENSE',
+    'ARMGR': 'ARMOR',
+    'ARMER': 'ARMOR',
+    'COMDAT': 'COMBAT',
+    'WEAPORS': 'WEAPONS',
+    'AXECLASS': 'AXE CLASS',
+    'IOX%': '10%',
+    'IO%': '10%',
+    'TWYO': 'TWO',
+    'ATTRIOUTES': 'ATTRIBUTES',
+    'MONARCHI': 'MONARCH',
+}
