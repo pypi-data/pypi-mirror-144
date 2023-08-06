ea128284cd9a2656ba635c23e57a663e8a4e09bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package smart-hg-3/1.0/smart-hg-3-1.0.tar.gz

---
+++ packages/smart-hg-3/smart-hg-3-1.0.tar.gz/smart-hg-3-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-3",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'scikit-image==0.14.5'],
+)
