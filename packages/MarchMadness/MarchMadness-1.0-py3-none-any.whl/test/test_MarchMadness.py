500202b308d492b200d11536a752d766ec861d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:08 +0000

    Add package MarchMadness/1.0/MarchMadness-1.0-py3-none-any.whl

---
+++ packages/MarchMadness/MarchMadness-1.0-py3-none-any.whl/test/test_MarchMadness.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+import MarchMadness
+
+def test_testTeams():
+  pass
+  # years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]
+  # MarchMadness.fillData()
+  # assert MarchMadness.testTeams(MarchMadness.dataSet, years) == "Teams parsed correctly."
