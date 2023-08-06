75df65261c58109ae1e7d0c2c89394ed7fc610ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:42 +0000

    Add package avsub/2.6.0/avsub-2.6.0-py3-none-any.whl

---
+++ packages/avsub/avsub-2.6.0-py3-none-any.whl/avsub/core/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+#
+# This file is part of AVsub
+# See https://github.com/serhatcelik/avsub for more information
+# Released under the GNU General Public License v3.0
+# Copyright (C) Serhat Çelik
+
+"""Core modules."""
