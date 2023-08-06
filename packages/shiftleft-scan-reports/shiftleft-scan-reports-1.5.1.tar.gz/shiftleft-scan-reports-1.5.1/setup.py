037249630dbf5ae41d0551f272d8d60a473e76bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:53 +0000

    Add package shiftleft-scan-reports/1.5.1/shiftleft-scan-reports-1.5.1.tar.gz

---
+++ packages/shiftleft-scan-reports/shiftleft-scan-reports-1.5.1.tar.gz/shiftleft-scan-reports-1.5.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shiftleft-scan-reports",
+    version="1.5.1",
+    author="Team ShiftLeft",
+    author_email="hello@shiftleft.io",
+    description="Library for producing SARIF & html reports from ShiftLeft or AppThreat scan results",
+    entry_points={"console_scripts": ["slreporter=reporter.cli:main"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShiftLeftSecurity/scan-reports",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["Jinja2", "markdown", "requests", "rich", "pyjwt", "joern2sarif", "PyGitHub==1.54.0.1"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Utilities",
+        "Topic :: Security",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
