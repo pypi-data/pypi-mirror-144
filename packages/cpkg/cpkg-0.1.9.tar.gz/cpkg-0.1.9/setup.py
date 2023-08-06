d89710f0721850af22c29950602c74f95e2b6a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:25:41 +0000

    Add package cpkg/0.1.9/cpkg-0.1.9.tar.gz

---
+++ packages/cpkg/cpkg-0.1.9.tar.gz/cpkg-0.1.9/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import os
+import stat
+from platform import system
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="cpkg",
+    version="0.1.9",
+    author="xiongtianshuo",
+    author_email="Mr_Xiongts@163.com",
+    description="A package that creates a package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seoul2k/cPkg",
+    project_urls={
+        "Bug Tracker": "https://github.com/seoul2k/cPkg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['cpkg/'],
+    python_requires=">=3.6",
+    install_requires=['cpkgtab==0.0.3'],
+    entry_points={
+        'console_scripts': [
+            'createpkg = cpkg.cpkg:main'
+        ]
+    }
+)
