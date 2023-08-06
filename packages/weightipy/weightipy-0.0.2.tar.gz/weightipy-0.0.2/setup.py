09bf68fb80c06b9ae5dc3b65d62000f9436b3767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:28 +0000

    Add package weightipy/0.0.2/weightipy-0.0.2.tar.gz

---
+++ packages/weightipy/weightipy-0.0.2.tar.gz/weightipy-0.0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+versions = dict()
+
+precisions = dict()
+
+libs = ['numpy',
+        'scipy',
+        'pandas',
+        'decorator',
+        'watchdog',
+        'requests']
+
+
+def version_libs(libs, precisions, versions):
+    return [lib + precisions[lib] + versions[lib]
+            if lib in versions.keys() else lib
+            for lib in libs]
+
+
+INSTALL_REQUIRES = version_libs(libs, precisions, versions)
+
+setup(name='weightipy',
+      version='0.0.2',
+      author='Remi Sebastian Kits',
+      author_email='kaitumisuuringute.keskus@gmail.com',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      install_requires=INSTALL_REQUIRES)
+
