577360b6aa60319dcf7a3aef7b2227037c433366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:01 +0000

    Add package mypylox/1.1.0/mypylox-1.1.0.tar.gz

---
+++ packages/mypylox/mypylox-1.1.0.tar.gz/mypylox-1.1.0/src/pylox/lox_types.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING, Union
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    Protocol = object  # pragma: no cover
+
+if TYPE_CHECKING:
+    from pylox.interpreter import Interpreter
+
+String = str
+Number = float
+Boolean = bool
+
+
+class LoxCallable(Protocol):
+    def call(self, interpreter: Interpreter, arguments: list[LoxType]) -> LoxType:
+        ...
+
+    def arity(self) -> int:
+        ...
+
+
+class LoxObject(Protocol):
+    def get(self, name: str) -> LoxType:
+        ...
+
+    def set(self, name: str, value: LoxType) -> None:
+        ...
+
+
+LoxType = Union[String, Number, Boolean, LoxCallable, LoxObject, None]
