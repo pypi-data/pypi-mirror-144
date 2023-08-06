3d50b66aca57f60f778ad1010cea85f8b78be7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:34 +0000

    Add package gnucash-uk-vat/1.4.0/gnucash-uk-vat-1.4.0.tar.gz

---
+++ packages/gnucash-uk-vat/gnucash-uk-vat-1.4.0.tar.gz/gnucash-uk-vat-1.4.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gnucash-uk-vat",
+    version="1.4.0",
+    author="Cybermaggedon",
+    author_email="mark@cyberapocalypse.co.uk",
+    description="UK HMRC VAT submission bridge for GnuCash users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cybermaggedon/gnucash-uk-vat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    download_url = "https://github.com/cybermaggedon/gnucash-uk-vat/archive/refs/tags/v1.4.0.tar.gz",
+    install_requires=[
+        'aiohttp',
+        'py-dmidecode',
+        'requests',
+        'piecash'
+    ],
+    scripts=[
+        "scripts/gnucash-uk-vat",
+        "scripts/vat-test-service"
+    ]
+)
