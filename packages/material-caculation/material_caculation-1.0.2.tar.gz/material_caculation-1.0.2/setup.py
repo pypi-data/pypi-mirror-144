f99654eba1bf0e9386c9ba1cc2c4b42c82860d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:24:53 +0000

    Add package material-caculation/1.0.2/material_caculation-1.0.2.tar.gz

---
+++ packages/material-caculation/material_caculation-1.0.2.tar.gz/material_caculation-1.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="material_caculation",
+    version="1.0.2",
+    author="jeffery",
+    author_email="lizuobang@163.com",
+    description="material caculation script",
+    long_description=open("README.rst").read(),
+    platforms=["all"],  # 平台
+    license="MIT",
+    url="https://github.com/Jeffreyliming/pandas_test",
+    packages=["material_caculation"],
+    install_requires=[
+        "pandas",
+        "flask"
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
