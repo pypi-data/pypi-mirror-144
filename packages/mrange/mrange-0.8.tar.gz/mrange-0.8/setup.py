49b29011e69e9139f7d503ae489ff86eaf81ee25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:23 +0000

    Add package mrange/0.8/mrange-0.8.tar.gz

---
+++ packages/mrange/mrange-0.8.tar.gz/mrange-0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mrange",
+    version = "0.8",
+    author = "Michael Schilling",
+    author_email = "michael@ntropic.de",
+    description  = "mrange is used to remove nested range loops",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Ntropic/mrange/archive/refs/tags/v0.8.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[], #["numba"],
+    python_requires=">=3.6",
+)
