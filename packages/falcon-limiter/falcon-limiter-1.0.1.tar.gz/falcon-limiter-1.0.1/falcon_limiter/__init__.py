8f715c2393a1a9853ba7bc0f6a54139c12852abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:46 +0000

    Add package falcon-limiter/1.0.1/falcon-limiter-1.0.1.tar.gz

---
+++ packages/falcon-limiter/falcon-limiter-1.0.1.tar.gz/falcon-limiter-1.0.1/falcon_limiter/__init__.py
@@ -0,0 +1,8 @@
+""" Falcon-Limiter - a rate limiting module for the Falcon web framework
+"""
+
+__version__ = "1.0.1"
+
+from falcon_limiter import utils
+from falcon_limiter.limiter import Limiter
+from falcon_limiter.async_limiter import AsyncLimiter
