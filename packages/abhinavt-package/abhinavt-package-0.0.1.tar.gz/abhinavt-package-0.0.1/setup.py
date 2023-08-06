1738b2c078aaee22d5dea4f5b7ab3f7f7ad185e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:54 +0000

    Add package abhinavt-package/0.0.1/abhinavt-package-0.0.1.tar.gz

---
+++ packages/abhinavt-package/abhinavt-package-0.0.1.tar.gz/abhinavt-package-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Abhinav TP twine Testing'
+LONG_DESCRIPTION = 'Abhinav TP twine Testing'
+
+setup(
+    name="abhinavt-package",
+    version=VERSION,
+    author="Abhinav",
+    author_email="abhinav@example.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'abhinavt-package'],
+)
