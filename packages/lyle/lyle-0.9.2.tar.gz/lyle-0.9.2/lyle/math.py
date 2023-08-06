744e8e9f1abe14ba64339f6eacd6ae76555d3755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package lyle/0.9.2/lyle-0.9.2.tar.gz

---
+++ packages/lyle/lyle-0.9.2.tar.gz/lyle-0.9.2/lyle/math.py
@@ -0,0 +1,23 @@
+def add(x: int, y: int) -> int:
+    """add two numbers together
+
+    Args:
+                    x (int): first number to add
+                    y (int): second number to add
+
+    Returns:
+                    int: sum of x and y
+    """
+    return x + y
+
+def subtract(x: int, y: int) -> int:
+    """subtract one number from another
+
+    Args:
+                    x (int): first number to subtract
+                    y (int): second number to subtract
+
+    Returns:
+                    int: resut of x subtract y
+    """
+    return x - y
---
+++ packages/lyle/lyle-0.9.2.tar.gz/lyle-0.9.2/tests/test_math.py
@@ -0,0 +1,9 @@
+from lyle.math import add, subtract
+
+
+def test_add():
+    assert add(3, 2) == 5
+
+
+def test_subtract():
+    assert subtract(3, 2) == 1
