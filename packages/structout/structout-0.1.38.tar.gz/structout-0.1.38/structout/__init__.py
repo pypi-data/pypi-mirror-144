4ea2f3996680829f4a75ca4dcffaf4d667f353a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:32 +0000

    Add package structout/0.1.38/structout-0.1.38.tar.gz

---
+++ packages/structout/structout-0.1.38.tar.gz/structout-0.1.38/structout/__init__.py
@@ -0,0 +1,19 @@
+
+
+from structout.graph import gprint
+from structout.intlist import dprint
+from structout.intlistV2 import lprint, npprint, iprint,heatmap
+
+
+#def bins(values,minmax=True,length=-1,method='bins',methodhow=16,**kwargs):
+#    npprint(values,minmax=minmax,length=length,method=method,methodhow=methodhow,**kwargs)
+
+
+
+def hist(values):
+    from collections import Counter
+    counts = Counter(values)
+    k=counts.keys()
+    val  = [counts.get(i,0) for i in range(min(k), max(k)+1)]
+    lprint(val)
+
