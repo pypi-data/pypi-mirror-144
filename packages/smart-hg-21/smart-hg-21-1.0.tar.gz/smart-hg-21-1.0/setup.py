61dff0335adcccaf5a776dc43fb4c4130de2ecbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-21/1.0/smart-hg-21-1.0.tar.gz

---
+++ packages/smart-hg-21/smart-hg-21-1.0.tar.gz/smart-hg-21-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-21",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['keras==2.4.3', 'tensorflow==2.4.0'],
+)
