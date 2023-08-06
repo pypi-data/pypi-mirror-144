4d7b85aa704e2cb8dc5a11ccd26e8dd8adb907c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:54 +0000

    Add package noaa-ftp/0.1.8/noaa_ftp-0.1.8.tar.gz

---
+++ packages/noaa-ftp/noaa_ftp-0.1.8.tar.gz/noaa_ftp-0.1.8/README.rst
@@ -0,0 +1,25 @@
+# NOAA-FTP
+> I needed to work with data from NOAA, so I write a code in jupyter notebook and solved my problem for viewing and downloading data.
+> Then I decided to convert that code to a python package.
+My Personal Website: [Water Directory](https://waterdirectory.ir//)
+
+
+To import, use command below:
+```
+from noaa_ftp import NOAA
+```
+
+Available functions:
+- dir()
+- download()
+
+## Get list of files and folders
+```
+noaa_dir = NOAA("ftp.ncdc.noaa.gov", 'pub/data/ghcn/daily').dir()
+noaa_dir
+```
+
+## Download custom file from the directory
+```
+noaa = NOAA("ftp.ncdc.noaa.gov", 'pub/data/ghcn/daily').download('ghcnd-stations.txt')
+```
---
+++ packages/noaa-ftp/noaa_ftp-0.1.8.tar.gz/noaa_ftp-0.1.8/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['noaa_ftp', 'noaa_ftp.noaa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['progressbar2==4.0.0']
+
+setup_kwargs = {
+    'name': 'noaa-ftp',
+    'version': '0.1.8',
+    'description': 'A python package to work with NOAA FTP',
+    'long_description': '# NOAA-FTP\n> I needed to work with data from NOAA, so I write a code in jupyter notebook and solved my problem for viewing and downloading data.\n> Then I decided to convert that code to a python package.\nMy Personal Website: [Water Directory](https://waterdirectory.ir//)\n\n\nTo import, use command below:\n```\nfrom noaa_ftp import NOAA\n```\n\nAvailable functions:\n- dir()\n- download()\n\n## Get list of files and folders\n```\nnoaa_dir = NOAA("ftp.ncdc.noaa.gov", \'pub/data/ghcn/daily\').dir()\nnoaa_dir\n```\n\n## Download custom file from the directory\n```\nnoaa = NOAA("ftp.ncdc.noaa.gov", \'pub/data/ghcn/daily\').download(\'ghcnd-stations.txt\')\n```',
+    'author': 'javad',
+    'author_email': 'javad.rezvanpour@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
