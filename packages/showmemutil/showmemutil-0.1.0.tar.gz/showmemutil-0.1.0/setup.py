b7643b20799a24660744395861b22f76ad566c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:20 +0000

    Add package showmemutil/0.1.0/showmemutil-0.1.0.tar.gz

---
+++ packages/showmemutil/showmemutil-0.1.0.tar.gz/showmemutil-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['showmemutil']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.4,<0.5.0', 'psutil>=5.9.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'showmemutil',
+    'version': '0.1.0',
+    'description': 'Show memory utilization in the terminal (sort of like htop)',
+    'long_description': None,
+    'author': 'Vikram Saraph',
+    'author_email': 'vikram.saraph.22@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
