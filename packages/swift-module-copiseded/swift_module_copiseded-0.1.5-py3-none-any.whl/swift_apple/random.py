2434bd6f3f1ebfce09c414dd7d69b9cf9917a0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:30 +0000

    Add package swift-module-copiseded/0.1.5/swift_module_copiseded-0.1.5-py3-none-any.whl

---
+++ packages/swift-module-copiseded/swift_module_copiseded-0.1.5-py3-none-any.whl/swift_apple/random.py
@@ -0,0 +1,28 @@
+import random
+def randomInt-Float(fromInt,toInt):
+    random.unifrom(fromInt,toInt)
+def randomInt-Integers(fromInt,toInt):
+    random.randint(fromInt,toInt)
+def randomInt_type_MaxMinNumber():
+    #使用循环取值支持输入，并检测异常，冒泡输入8位
+    list_1 = list()
+    try:
+        for i in range(8):
+            num = eval(input('请输入数字'))
+            list_1.append(num)
+        print(list_1)
+        c = list_1[0]
+        for i in list_1:
+            if c!=i:
+                if c<i:
+                    c=i
+                    list_max=i
+        for b in list_1:
+            if b!=c:
+                if c>b:
+                    c=b
+                    list_min=c
+        print(list_max)
+        print(list_min)
+    except Exception as e:
+        print(e)
