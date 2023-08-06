82c805186be2f12ae229dc44b16b0dbcd8da384e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:05 +0000

    Add package smart-COCO-Style-Dataset-Generator-GUI/1.0/smart-COCO-Style-Dataset-Generator-GUI-1.0.tar.gz

---
+++ packages/smart-COCO-Style-Dataset-Generator-GUI/smart-COCO-Style-Dataset-Generator-GUI-1.0.tar.gz/smart-COCO-Style-Dataset-Generator-GUI-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-COCO-Style-Dataset-Generator-GUI",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['matplotlib==3.0.3', 'numpy==1.16.4', 'opencv-python', 'Pillow', 'scikit-image', 'scipy'],
+)
