2d16c13239edbb257109aed38893651d82a3a13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:36 +0000

    Add package math-dioproject/0.0.4/math_dioproject-0.0.4.tar.gz

---
+++ packages/math-dioproject/math_dioproject-0.0.4.tar.gz/math_dioproject-0.0.4/math_dioproject/basicfunctions/complex.py
@@ -0,0 +1,8 @@
+import math
+
+def raiz(num1):
+    print(f'A raiz quadrada do numero é: {math.sqrt(num1)}')
+
+def  expo(num1, num2):
+    print(f'O numero {num1} elevado por {num2} é: {math.pow(num1, num2)}')
+
