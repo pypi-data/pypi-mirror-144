ba8d6a03605d627c4d75a9ff8622b7d87583d563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package types-aiobotocore-marketplacecommerceanalytics/2.2.0.post2/types_aiobotocore_marketplacecommerceanalytics-2.2.0.post2-py3-none-any.whl

---
+++ packages/types-aiobotocore-marketplacecommerceanalytics/types_aiobotocore_marketplacecommerceanalytics-2.2.0.post2-py3-none-any.whl/types_aiobotocore_marketplacecommerceanalytics/type_defs.py
@@ -0,0 +1,87 @@
+"""
+Type annotations for marketplacecommerceanalytics service type definitions.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_marketplacecommerceanalytics/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_marketplacecommerceanalytics.type_defs import GenerateDataSetRequestRequestTypeDef
+
+    data: GenerateDataSetRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, Mapping, Union
+
+from typing_extensions import NotRequired
+
+from .literals import DataSetTypeType, SupportDataSetTypeType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "GenerateDataSetRequestRequestTypeDef",
+    "GenerateDataSetResultTypeDef",
+    "ResponseMetadataTypeDef",
+    "StartSupportDataExportRequestRequestTypeDef",
+    "StartSupportDataExportResultTypeDef",
+)
+
+GenerateDataSetRequestRequestTypeDef = TypedDict(
+    "GenerateDataSetRequestRequestTypeDef",
+    {
+        "dataSetType": DataSetTypeType,
+        "dataSetPublicationDate": Union[datetime, str],
+        "roleNameArn": str,
+        "destinationS3BucketName": str,
+        "snsTopicArn": str,
+        "destinationS3Prefix": NotRequired[str],
+        "customerDefinedValues": NotRequired[Mapping[str, str]],
+    },
+)
+
+GenerateDataSetResultTypeDef = TypedDict(
+    "GenerateDataSetResultTypeDef",
+    {
+        "dataSetRequestId": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+StartSupportDataExportRequestRequestTypeDef = TypedDict(
+    "StartSupportDataExportRequestRequestTypeDef",
+    {
+        "dataSetType": SupportDataSetTypeType,
+        "fromDate": Union[datetime, str],
+        "roleNameArn": str,
+        "destinationS3BucketName": str,
+        "snsTopicArn": str,
+        "destinationS3Prefix": NotRequired[str],
+        "customerDefinedValues": NotRequired[Mapping[str, str]],
+    },
+)
+
+StartSupportDataExportResultTypeDef = TypedDict(
+    "StartSupportDataExportResultTypeDef",
+    {
+        "dataSetRequestId": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
