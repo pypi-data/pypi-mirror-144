8bd88d47197ed1c6dafab05cdcfcf60bac84c9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:49 +0000

    Add package formify/1.2.2/formify-1.2.2.tar.gz

---
+++ packages/formify/formify-1.2.2.tar.gz/formify-1.2.2/formify/__init__.py
@@ -0,0 +1,8 @@
+from formify import localization
+from formify.app import app, run, show_splashscreen, add_font
+from formify.controls import *
+from formify.layout import *
+from formify import tools
+from formify.SaveLoad import LoadSaveHandler
+from formify.window import MainWindow
+
