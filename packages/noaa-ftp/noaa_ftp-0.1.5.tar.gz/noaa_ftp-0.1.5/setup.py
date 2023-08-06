4007f417870d4307d8618e9816753605975bba4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:59 +0000

    Add package noaa-ftp/0.1.5/noaa_ftp-0.1.5.tar.gz

---
+++ packages/noaa-ftp/noaa_ftp-0.1.5.tar.gz/noaa_ftp-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['noaa_ftp', 'noaa_ftp.noaa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['progressbar2==4.0.0']
+
+setup_kwargs = {
+    'name': 'noaa-ftp',
+    'version': '0.1.5',
+    'description': 'A python package to work with NOAA FTP',
+    'long_description': None,
+    'author': 'javad',
+    'author_email': 'javad.rezvanpour@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
