606799bc1201970990bde10fb4ae593a9b459c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:43 +0000

    Add package img-proof/7.10.0/img-proof-7.10.0.tar.gz

---
+++ packages/img-proof/img-proof-7.10.0.tar.gz/img-proof-7.10.0/usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
@@ -0,0 +1,11 @@
+def test_sles_motd(host, get_release_value):
+    motd = host.file('/etc/motd')
+    version = get_release_value('VERSION')
+    assert version
+    assert motd.exists
+    assert motd.is_file
+    assert motd.contains(
+        'SUSE Linux Enterprise Server {0}'.format(
+            version.replace('-', ' ')
+        )
+    )
