67a4f07d3a25cfc7d0aafc1333654b3bb5b937b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:51 +0000

    Add package marketplace-sdk/0.1.1/marketplace_sdk-0.1.1-py3-none-any.whl

---
+++ packages/marketplace-sdk/marketplace_sdk-0.1.1-py3-none-any.whl/marketplace/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Software Development Toolkit to communicate with the Materials MarketPlace
+platform.
+
+.. currentmodule:: marketplace
+.. moduleauthor:: Carl Simon Adorf <simon.adorf@epfl.ch>
+"""
+
+from .core import MarketPlaceClient
+from .version import __version__
+
+__all__ = [
+    "MarketPlaceClient",
+    "__version__",
+]
