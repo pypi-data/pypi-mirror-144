413ebedcbdbdbf6d78ceeb357d6eb77786f2aa96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-13/1.0/smart-hg-13-1.0.tar.gz

---
+++ packages/smart-hg-13/smart-hg-13-1.0.tar.gz/smart-hg-13-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-13",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'scikit-image==0.14.5', 'scipy==1.2.3', 'theano==1.0.5', 'matplotlib==2.1.2', 'scikit-learn==0.20.4'],
+)
