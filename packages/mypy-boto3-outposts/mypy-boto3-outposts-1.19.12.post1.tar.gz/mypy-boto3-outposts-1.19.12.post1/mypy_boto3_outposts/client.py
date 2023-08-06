6a71bc841461ccc1185222b865893ff6505e9580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:49 +0000

    Add package mypy-boto3-outposts/1.19.12.post1/mypy-boto3-outposts-1.19.12.post1.tar.gz

---
+++ packages/mypy-boto3-outposts/mypy-boto3-outposts-1.19.12.post1.tar.gz/mypy-boto3-outposts-1.19.12.post1/mypy_boto3_outposts/client.py
@@ -0,0 +1,211 @@
+"""
+Type annotations for outposts service client.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_outposts.client import OutpostsClient
+
+    session = Session()
+    client: OutpostsClient = session.client("outposts")
+    ```
+"""
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .literals import PaymentOptionType
+from .type_defs import (
+    CreateOrderOutputTypeDef,
+    CreateOutpostOutputTypeDef,
+    GetOutpostInstanceTypesOutputTypeDef,
+    GetOutpostOutputTypeDef,
+    LineItemRequestTypeDef,
+    ListOutpostsOutputTypeDef,
+    ListSitesOutputTypeDef,
+    ListTagsForResourceResponseTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("OutpostsClient",)
+
+
+class BotocoreClientError(BaseException):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    NotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class OutpostsClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        OutpostsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.exceptions)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.can_paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#can_paginate)
+        """
+
+    def create_order(
+        self,
+        *,
+        OutpostIdentifier: str,
+        LineItems: Sequence["LineItemRequestTypeDef"],
+        PaymentOption: PaymentOptionType,
+        PaymentTerm: Literal["THREE_YEARS"] = ...
+    ) -> CreateOrderOutputTypeDef:
+        """
+        Creates an order for an Outpost.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.create_order)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#create_order)
+        """
+
+    def create_outpost(
+        self,
+        *,
+        Name: str,
+        SiteId: str,
+        Description: str = ...,
+        AvailabilityZone: str = ...,
+        AvailabilityZoneId: str = ...,
+        Tags: Mapping[str, str] = ...
+    ) -> CreateOutpostOutputTypeDef:
+        """
+        Creates an Outpost.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.create_outpost)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#create_outpost)
+        """
+
+    def delete_outpost(self, *, OutpostId: str) -> Dict[str, Any]:
+        """
+        Deletes the Outpost.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.delete_outpost)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#delete_outpost)
+        """
+
+    def delete_site(self, *, SiteId: str) -> Dict[str, Any]:
+        """
+        Deletes the site.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.delete_site)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#delete_site)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#generate_presigned_url)
+        """
+
+    def get_outpost(self, *, OutpostId: str) -> GetOutpostOutputTypeDef:
+        """
+        Gets information about the specified Outpost.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.get_outpost)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#get_outpost)
+        """
+
+    def get_outpost_instance_types(
+        self, *, OutpostId: str, NextToken: str = ..., MaxResults: int = ...
+    ) -> GetOutpostInstanceTypesOutputTypeDef:
+        """
+        Lists the instance types for the specified Outpost.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.get_outpost_instance_types)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#get_outpost_instance_types)
+        """
+
+    def list_outposts(
+        self,
+        *,
+        NextToken: str = ...,
+        MaxResults: int = ...,
+        LifeCycleStatusFilter: Sequence[str] = ...,
+        AvailabilityZoneFilter: Sequence[str] = ...,
+        AvailabilityZoneIdFilter: Sequence[str] = ...
+    ) -> ListOutpostsOutputTypeDef:
+        """
+        Create a list of the Outposts for your AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.list_outposts)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#list_outposts)
+        """
+
+    def list_sites(self, *, NextToken: str = ..., MaxResults: int = ...) -> ListSitesOutputTypeDef:
+        """
+        Lists the sites for the specified AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.list_sites)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#list_sites)
+        """
+
+    def list_tags_for_resource(self, *, ResourceArn: str) -> ListTagsForResourceResponseTypeDef:
+        """
+        Lists the tags for the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.list_tags_for_resource)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#list_tags_for_resource)
+        """
+
+    def tag_resource(self, *, ResourceArn: str, Tags: Mapping[str, str]) -> Dict[str, Any]:
+        """
+        Adds tags to the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.tag_resource)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#tag_resource)
+        """
+
+    def untag_resource(self, *, ResourceArn: str, TagKeys: Sequence[str]) -> Dict[str, Any]:
+        """
+        Removes tags from the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/outposts.html#Outposts.Client.untag_resource)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/client/#untag_resource)
+        """
---
+++ packages/mypy-boto3-outposts/mypy-boto3-outposts-1.19.12.post1.tar.gz/mypy-boto3-outposts-1.19.12.post1/mypy_boto3_outposts/type_defs.pyi
@@ -0,0 +1,332 @@
+"""
+Type annotations for outposts service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_outposts/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_outposts.type_defs import CreateOrderInputRequestTypeDef
+
+    data: CreateOrderInputRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, List, Mapping, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import OrderStatusType, PaymentOptionType
+
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "CreateOrderInputRequestTypeDef",
+    "CreateOrderOutputTypeDef",
+    "CreateOutpostInputRequestTypeDef",
+    "CreateOutpostOutputTypeDef",
+    "DeleteOutpostInputRequestTypeDef",
+    "DeleteSiteInputRequestTypeDef",
+    "GetOutpostInputRequestTypeDef",
+    "GetOutpostInstanceTypesInputRequestTypeDef",
+    "GetOutpostInstanceTypesOutputTypeDef",
+    "GetOutpostOutputTypeDef",
+    "InstanceTypeItemTypeDef",
+    "LineItemRequestTypeDef",
+    "LineItemTypeDef",
+    "ListOutpostsInputRequestTypeDef",
+    "ListOutpostsOutputTypeDef",
+    "ListSitesInputRequestTypeDef",
+    "ListSitesOutputTypeDef",
+    "ListTagsForResourceRequestRequestTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "OrderTypeDef",
+    "OutpostTypeDef",
+    "ResponseMetadataTypeDef",
+    "SiteTypeDef",
+    "TagResourceRequestRequestTypeDef",
+    "UntagResourceRequestRequestTypeDef",
+)
+
+_RequiredCreateOrderInputRequestTypeDef = TypedDict(
+    "_RequiredCreateOrderInputRequestTypeDef",
+    {
+        "OutpostIdentifier": str,
+        "LineItems": Sequence["LineItemRequestTypeDef"],
+        "PaymentOption": PaymentOptionType,
+    },
+)
+_OptionalCreateOrderInputRequestTypeDef = TypedDict(
+    "_OptionalCreateOrderInputRequestTypeDef",
+    {
+        "PaymentTerm": Literal["THREE_YEARS"],
+    },
+    total=False,
+)
+
+class CreateOrderInputRequestTypeDef(
+    _RequiredCreateOrderInputRequestTypeDef, _OptionalCreateOrderInputRequestTypeDef
+):
+    pass
+
+CreateOrderOutputTypeDef = TypedDict(
+    "CreateOrderOutputTypeDef",
+    {
+        "Order": "OrderTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+_RequiredCreateOutpostInputRequestTypeDef = TypedDict(
+    "_RequiredCreateOutpostInputRequestTypeDef",
+    {
+        "Name": str,
+        "SiteId": str,
+    },
+)
+_OptionalCreateOutpostInputRequestTypeDef = TypedDict(
+    "_OptionalCreateOutpostInputRequestTypeDef",
+    {
+        "Description": str,
+        "AvailabilityZone": str,
+        "AvailabilityZoneId": str,
+        "Tags": Mapping[str, str],
+    },
+    total=False,
+)
+
+class CreateOutpostInputRequestTypeDef(
+    _RequiredCreateOutpostInputRequestTypeDef, _OptionalCreateOutpostInputRequestTypeDef
+):
+    pass
+
+CreateOutpostOutputTypeDef = TypedDict(
+    "CreateOutpostOutputTypeDef",
+    {
+        "Outpost": "OutpostTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DeleteOutpostInputRequestTypeDef = TypedDict(
+    "DeleteOutpostInputRequestTypeDef",
+    {
+        "OutpostId": str,
+    },
+)
+
+DeleteSiteInputRequestTypeDef = TypedDict(
+    "DeleteSiteInputRequestTypeDef",
+    {
+        "SiteId": str,
+    },
+)
+
+GetOutpostInputRequestTypeDef = TypedDict(
+    "GetOutpostInputRequestTypeDef",
+    {
+        "OutpostId": str,
+    },
+)
+
+_RequiredGetOutpostInstanceTypesInputRequestTypeDef = TypedDict(
+    "_RequiredGetOutpostInstanceTypesInputRequestTypeDef",
+    {
+        "OutpostId": str,
+    },
+)
+_OptionalGetOutpostInstanceTypesInputRequestTypeDef = TypedDict(
+    "_OptionalGetOutpostInstanceTypesInputRequestTypeDef",
+    {
+        "NextToken": str,
+        "MaxResults": int,
+    },
+    total=False,
+)
+
+class GetOutpostInstanceTypesInputRequestTypeDef(
+    _RequiredGetOutpostInstanceTypesInputRequestTypeDef,
+    _OptionalGetOutpostInstanceTypesInputRequestTypeDef,
+):
+    pass
+
+GetOutpostInstanceTypesOutputTypeDef = TypedDict(
+    "GetOutpostInstanceTypesOutputTypeDef",
+    {
+        "InstanceTypes": List["InstanceTypeItemTypeDef"],
+        "NextToken": str,
+        "OutpostId": str,
+        "OutpostArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetOutpostOutputTypeDef = TypedDict(
+    "GetOutpostOutputTypeDef",
+    {
+        "Outpost": "OutpostTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+InstanceTypeItemTypeDef = TypedDict(
+    "InstanceTypeItemTypeDef",
+    {
+        "InstanceType": str,
+    },
+    total=False,
+)
+
+LineItemRequestTypeDef = TypedDict(
+    "LineItemRequestTypeDef",
+    {
+        "CatalogItemId": str,
+        "Quantity": int,
+    },
+    total=False,
+)
+
+LineItemTypeDef = TypedDict(
+    "LineItemTypeDef",
+    {
+        "CatalogItemId": str,
+        "LineItemId": str,
+        "Quantity": int,
+        "Status": str,
+    },
+    total=False,
+)
+
+ListOutpostsInputRequestTypeDef = TypedDict(
+    "ListOutpostsInputRequestTypeDef",
+    {
+        "NextToken": str,
+        "MaxResults": int,
+        "LifeCycleStatusFilter": Sequence[str],
+        "AvailabilityZoneFilter": Sequence[str],
+        "AvailabilityZoneIdFilter": Sequence[str],
+    },
+    total=False,
+)
+
+ListOutpostsOutputTypeDef = TypedDict(
+    "ListOutpostsOutputTypeDef",
+    {
+        "Outposts": List["OutpostTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListSitesInputRequestTypeDef = TypedDict(
+    "ListSitesInputRequestTypeDef",
+    {
+        "NextToken": str,
+        "MaxResults": int,
+    },
+    total=False,
+)
+
+ListSitesOutputTypeDef = TypedDict(
+    "ListSitesOutputTypeDef",
+    {
+        "Sites": List["SiteTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListTagsForResourceRequestRequestTypeDef = TypedDict(
+    "ListTagsForResourceRequestRequestTypeDef",
+    {
+        "ResourceArn": str,
+    },
+)
+
+ListTagsForResourceResponseTypeDef = TypedDict(
+    "ListTagsForResourceResponseTypeDef",
+    {
+        "Tags": Dict[str, str],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+OrderTypeDef = TypedDict(
+    "OrderTypeDef",
+    {
+        "OutpostId": str,
+        "OrderId": str,
+        "Status": OrderStatusType,
+        "LineItems": List["LineItemTypeDef"],
+        "PaymentOption": PaymentOptionType,
+        "OrderSubmissionDate": datetime,
+        "OrderFulfilledDate": datetime,
+    },
+    total=False,
+)
+
+OutpostTypeDef = TypedDict(
+    "OutpostTypeDef",
+    {
+        "OutpostId": str,
+        "OwnerId": str,
+        "OutpostArn": str,
+        "SiteId": str,
+        "Name": str,
+        "Description": str,
+        "LifeCycleStatus": str,
+        "AvailabilityZone": str,
+        "AvailabilityZoneId": str,
+        "Tags": Dict[str, str],
+        "SiteArn": str,
+    },
+    total=False,
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+SiteTypeDef = TypedDict(
+    "SiteTypeDef",
+    {
+        "SiteId": str,
+        "AccountId": str,
+        "Name": str,
+        "Description": str,
+        "Tags": Dict[str, str],
+        "SiteArn": str,
+    },
+    total=False,
+)
+
+TagResourceRequestRequestTypeDef = TypedDict(
+    "TagResourceRequestRequestTypeDef",
+    {
+        "ResourceArn": str,
+        "Tags": Mapping[str, str],
+    },
+)
+
+UntagResourceRequestRequestTypeDef = TypedDict(
+    "UntagResourceRequestRequestTypeDef",
+    {
+        "ResourceArn": str,
+        "TagKeys": Sequence[str],
+    },
+)
