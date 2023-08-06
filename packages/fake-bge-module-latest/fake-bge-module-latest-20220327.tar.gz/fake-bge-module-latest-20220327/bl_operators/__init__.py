49cb809ac24936e6014bcf81802b688562150f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:23:08 +0000

    Add package fake-bge-module-latest/20220327/fake-bge-module-latest-20220327.tar.gz

---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20220327.tar.gz/fake-bge-module-latest-20220327/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import anim
+from . import clip
+from . import add_mesh_torus
+from . import geometry_nodes
+from . import spreadsheet
+from . import node
+from . import object_randomize_transform
+from . import screen_play_rendered_anim
+from . import view3d
+from . import constraint
+from . import rigidbody
+from . import object_align
+from . import sequencer
+from . import object
+from . import userpref
+from . import file
+from . import bmesh
+from . import vertexpaint_dirt
+from . import mesh
+from . import presets
+from . import assets
+from . import uvcalc_lightmap
+from . import object_quick_effects
+from . import wm
+from . import console
+from . import freestyle
+from . import image
+from . import uvcalc_follow_active
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20220327.tar.gz/fake-bge-module-latest-20220327/gpu/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import state
+from . import texture
+from . import matrix
+from . import platform
+from . import shader
+from . import select
+from . import types
+from . import capabilities
