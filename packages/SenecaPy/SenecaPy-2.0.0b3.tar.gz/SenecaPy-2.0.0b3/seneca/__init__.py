00e8c2abcece85090f89cb6e7e9eed47ae6f3529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package SenecaPy/2.0.0b3/SenecaPy-2.0.0b3.tar.gz

---
+++ packages/SenecaPy/SenecaPy-2.0.0b3.tar.gz/SenecaPy-2.0.0b3/seneca/__init__.py
@@ -0,0 +1,18 @@
+from .Modules.memories import Memories
+from .Modules.token import Token
+from .Modules.info import Info
+from .Modules.course import Course
+from .Modules.Sessions import Sessions
+
+class User():
+    def __init__(self, username:str, password:str):
+        token = Token(username, password)
+        self.idToken = token.Refresh()
+        self.Keys = token.userKeys
+        self.id = token.user_id
+        self.info = Info(self.idToken)
+        self.memories = Memories(self.idToken)
+        self.Session = Sessions
+
+        
+course = Course()
