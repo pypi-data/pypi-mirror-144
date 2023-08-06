a69611c67fd37c0cae1510ccb9b9c9af3f780d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:18 +0000

    Add package ezephys/0.4.3/ezephys-0.4.3-py2-none-any.whl

---
+++ packages/ezephys/ezephys-0.4.3-py2-none-any.whl/ezephys/__init__.py
@@ -0,0 +1,9 @@
+"""Tools for manipulating electrophysiological data."""
+
+from . import pltools
+from . import rectools
+from . import stimtools
+
+__author__ = 'Emerson Harkin'
+__email__ = 'emerson@efharkin.com'
+__version__ = '0.4.3'
