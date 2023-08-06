96028f7320516ad2c6e4b826ef3ca86fc458aece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:16 +0000

    Add package CryptoLyzer/0.8.1/CryptoLyzer-0.8.1-py3-none-any.whl

---
+++ packages/CryptoLyzer/CryptoLyzer-0.8.1-py3-none-any.whl/cryptolyzer/ssh/ciphers.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import attr
+
+import six
+
+from cryptoparser.ssh.ciphersuite import (
+    SshCompressionAlgorithm,
+    SshEncryptionAlgorithm,
+    SshHostKeyAlgorithm,
+    SshKexAlgorithm,
+    SshMacAlgorithm,
+)
+from cryptoparser.ssh.subprotocol import SshMessageCode, SshKeyExchangeInit
+from cryptoparser.ssh.version import SshProtocolVersion, SshVersion
+
+from cryptolyzer.common.analyzer import AnalyzerSshBase
+from cryptolyzer.common.result import AnalyzerResultSsh, AnalyzerTargetSsh
+
+
+@attr.s
+class AnalyzerResultCiphers(AnalyzerResultSsh):  # pylint: disable=too-many-instance-attributes
+    kex_algorithms = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshKexAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'KEX Algorithms'}
+    )
+    host_key_algorithms = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshHostKeyAlgorithm, ) + six.string_types)
+        ),
+    )
+    encryption_algorithms_client_to_server = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshEncryptionAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'Encryption Algorithms Client to Server'}
+    )
+    encryption_algorithms_server_to_client = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshEncryptionAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'Encryption Algorithms Server to Client'}
+    )
+    mac_algorithms_client_to_server = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshMacAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'MAC Algorithms Client to Server'}
+    )
+    mac_algorithms_server_to_client = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshMacAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'MAC Algorithms Server to Client'}
+    )
+    compression_algorithms_client_to_server = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshCompressionAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'Compression Algorithms Client to Server'}
+    )
+    compression_algorithms_server_to_client = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of((SshCompressionAlgorithm, ) + six.string_types)
+        ),
+        metadata={'human_readable_name': 'Compression Algorithms Server to Client'}
+    )
+
+
+class AnalyzerCiphers(AnalyzerSshBase):
+    @classmethod
+    def get_name(cls):
+        return 'ciphers'
+
+    @classmethod
+    def get_help(cls):
+        return 'Check which cipher suites supported by the server(s)'
+
+    def analyze(self, analyzable):
+        server_messages = analyzable.do_handshake(last_message_type=SshMessageCode.KEXINIT)
+        key_exchange_init_message = server_messages[SshKeyExchangeInit]
+        return AnalyzerResultCiphers(
+            AnalyzerTargetSsh.from_l7_client(analyzable, SshProtocolVersion(SshVersion.SSH2)),
+            list(key_exchange_init_message.kex_algorithms),
+            list(key_exchange_init_message.host_key_algorithms),
+            list(key_exchange_init_message.encryption_algorithms_client_to_server),
+            list(key_exchange_init_message.encryption_algorithms_server_to_client),
+            list(key_exchange_init_message.mac_algorithms_client_to_server),
+            list(key_exchange_init_message.mac_algorithms_server_to_client),
+            list(key_exchange_init_message.compression_algorithms_client_to_server),
+            list(key_exchange_init_message.compression_algorithms_server_to_client),
+        )
---
+++ packages/CryptoLyzer/CryptoLyzer-0.8.1-py3-none-any.whl/cryptolyzer/tls/all.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+
+import attr
+
+from cryptoparser.common.algorithm import Authentication, KeyExchange
+from cryptoparser.tls.version import TlsProtocolVersionFinal, TlsVersion
+
+from cryptolyzer.common.analyzer import AnalyzerTlsBase, ProtocolHandlerBase
+from cryptolyzer.common.result import AnalyzerResultAllBase, AnalyzerTargetTls
+
+from cryptolyzer.tls.ciphers import AnalyzerCipherSuites, AnalyzerResultCipherSuites
+from cryptolyzer.tls.curves import AnalyzerCurves, AnalyzerResultCurves
+from cryptolyzer.tls.dhparams import AnalyzerDHParams, AnalyzerResultDHParams
+from cryptolyzer.tls.extensions import AnalyzerExtensions, AnalyzerResultExtensions
+from cryptolyzer.tls.pubkeyreq import AnalyzerPublicKeyRequest, AnalyzerResultPublicKeyRequest
+from cryptolyzer.tls.pubkeys import AnalyzerPublicKeys, AnalyzerResultPublicKeys
+from cryptolyzer.tls.sigalgos import AnalyzerSigAlgos, AnalyzerResultSigAlgos
+from cryptolyzer.tls.versions import AnalyzerVersions, AnalyzerResultVersions
+
+
+@attr.s
+class AnalyzerResultAll(AnalyzerResultAllBase):  # pylint: disable=too-few-public-methods,too-many-instance-attributes
+    versions = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultVersions)),
+        metadata={'human_readable_name': 'Supported Protocol Versions'}
+    )
+    ciphers = attr.ib(
+        validator=attr.validators.optional(attr.validators.deep_iterable(
+            member_validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultCipherSuites))
+        )),
+        metadata={'human_readable_name': 'Supported Cipher Suites'}
+    )
+    curves = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultCurves)),
+        metadata={'human_readable_name': 'Supported Elliptic Curves'}
+    )
+    dhparams = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultDHParams)),
+        metadata={'human_readable_name': 'Used Diffie-Hellman parameters'}
+    )
+    pubkeyreq = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultPublicKeyRequest)),
+        metadata={'human_readable_name': 'Requested Public Keys'}
+    )
+    pubkeys = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultPublicKeys)),
+        metadata={'human_readable_name': 'Used Public Keys'}
+    )
+    sigalgos = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultSigAlgos)),
+        metadata={'human_readable_name': 'Supported Signature Algorithms'}
+    )
+    extensions = attr.ib(
+        validator=attr.validators.optional(attr.validators.instance_of(AnalyzerResultExtensions)),
+        metadata={'human_readable_name': 'Supported Extensions'}
+    )
+
+
+class AnalyzerAll(AnalyzerTlsBase):
+    @classmethod
+    def get_name(cls):
+        return 'all'
+
+    @classmethod
+    def get_help(cls):
+        return 'Check TLS settings of the server(s)'
+
+    @staticmethod
+    def _get_result(analyzer_class, analyzable, protocol_version):
+        analyzer_name = analyzer_class.get_name()
+        if protocol_version is not None:
+            protocol_handler = ProtocolHandlerBase.from_protocol(protocol_version.identifier)
+            if not (analyzer_class in protocol_handler.get_analyzers() and
+                    protocol_handler.get_protocol_version() == protocol_version):
+                protocol_version = None
+
+        if protocol_version is None:
+            return {analyzer_name: None}
+
+        return {analyzer_name: analyzer_class().analyze(analyzable, protocol_version)}
+
+    @staticmethod
+    def get_versions_result(analyzable):
+        analyzer_class = AnalyzerVersions
+        analyzer_name = analyzer_class.get_name()
+
+        return {analyzer_name: analyzer_class().analyze(analyzable, None)}
+
+    @staticmethod
+    def _is_key_exchange_supported(cipher_suite_results, key_exchange):
+        for protocol_version, cipher_suite_result in cipher_suite_results.items():
+            for cipher_suite in cipher_suite_result.cipher_suites:
+                if cipher_suite.value.key_exchange == key_exchange:
+                    return protocol_version
+
+        return None
+
+    @staticmethod
+    def is_dhe_supported(cipher_suite_results):
+        return AnalyzerAll._is_key_exchange_supported(cipher_suite_results, KeyExchange.DHE)
+
+    @staticmethod
+    def is_ecdhe_supported(cipher_suite_results):
+        return AnalyzerAll._is_key_exchange_supported(cipher_suite_results, KeyExchange.ECDHE)
+
+    @staticmethod
+    def get_dhparams_result(analyzable, cipher_suite_results):
+        protocol_version = AnalyzerAll.is_dhe_supported(cipher_suite_results)
+        return AnalyzerAll._get_result(AnalyzerDHParams, analyzable, protocol_version)
+
+    @staticmethod
+    def get_curves_result(analyzable, cipher_suite_results):
+        protocol_version = AnalyzerAll.is_ecdhe_supported(cipher_suite_results)
+        return AnalyzerAll._get_result(AnalyzerCurves, analyzable, protocol_version)
+
+    @staticmethod
+    def get_pubkeyreq_result(analyzable, protocol_versions):
+        if protocol_versions:
+            protocol_version = protocol_versions[-1]
+        else:
+            protocol_version = None
+
+        return AnalyzerAll._get_result(AnalyzerPublicKeyRequest, analyzable, protocol_version)
+
+    @staticmethod
+    def is_public_key_supported(cipher_suite_results):
+        for protocol_version, cipher_suite_result in cipher_suite_results.items():
+            for cipher_suite in cipher_suite_result.cipher_suites:
+                if cipher_suite.value.authentication != Authentication.anon:
+                    return protocol_version
+
+        return None
+
+    @staticmethod
+    def get_pubkeys_result(analyzable, cipher_suite_results):
+        protocol_version = AnalyzerAll.is_public_key_supported(cipher_suite_results)
+        return AnalyzerAll._get_result(AnalyzerPublicKeys, analyzable, protocol_version)
+
+    @staticmethod
+    def get_sigalgos_result(analyzable, versions):
+        protocol_version = TlsProtocolVersionFinal(TlsVersion.TLS1_2)
+        if protocol_version not in versions:
+            protocol_version = None
+
+        return AnalyzerAll._get_result(AnalyzerSigAlgos, analyzable, protocol_version)
+
+    @staticmethod
+    def get_extensions_result(analyzable, versions):
+        for version in reversed(versions):
+            if version <= TlsProtocolVersionFinal(TlsVersion.TLS1_2):
+                protocol_version = version
+                break
+        else:
+            protocol_version = None
+
+        return AnalyzerAll._get_result(AnalyzerExtensions, analyzable, protocol_version)
+
+    def analyze(self, analyzable, protocol_version):
+        results = {
+            'target': AnalyzerTargetTls.from_l7_client(analyzable, protocol_version),
+        }
+
+        results.update(self.get_versions_result(analyzable))
+        versions = results[AnalyzerVersions.get_name()].versions
+
+        cipher_suite_results = OrderedDict([
+            (protocol_version, AnalyzerCipherSuites().analyze(analyzable, protocol_version))
+            for protocol_version in versions
+        ])
+
+        results.update(self.get_pubkeyreq_result(analyzable, versions))
+        results.update(self.get_pubkeys_result(analyzable, cipher_suite_results))
+        results.update(self.get_dhparams_result(analyzable, cipher_suite_results))
+        results.update(self.get_curves_result(analyzable, cipher_suite_results))
+        results.update(self.get_sigalgos_result(analyzable, versions))
+        results.update(self.get_extensions_result(analyzable, versions))
+        results.update({AnalyzerCipherSuites.get_name(): list(cipher_suite_results.values())})
+
+        return AnalyzerResultAll(**results)
