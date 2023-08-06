fa2d326add47014e0a7552c8f4252c52a9a45f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:21 +0000

    Add package seleniumbase/2.4.27/seleniumbase-2.4.27.tar.gz

---
+++ packages/seleniumbase/seleniumbase-2.4.27.tar.gz/seleniumbase-2.4.27/examples/xpath_test.py
@@ -0,0 +1,10 @@
+from seleniumbase import BaseCase
+
+
+class XPathTests(BaseCase):
+    def test_xpath(self):
+        self.open("https://seleniumbase.io/demo_page")
+        self.assert_element('//h1[(text()="Demo Page")]')
+        self.type('//*[@id="myTextInput"]', "XPath Test!")
+        self.click('//button[starts-with(text(),"Click Me")]')
+        self.assert_text("SeleniumBase", '//table/tbody/tr[1]/td[2]/h2')
