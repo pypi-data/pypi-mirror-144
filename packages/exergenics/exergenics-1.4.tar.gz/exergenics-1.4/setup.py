9e8a5bc3f1612720309c04192abed135e56f128f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:42 +0000

    Add package exergenics/1.4/exergenics-1.4.tar.gz

---
+++ packages/exergenics/exergenics-1.4.tar.gz/exergenics-1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='exergenics',
+    version='1.4',
+    author="Sanjeevani Avasthi",
+    author_email='sanjeevani.avasthi@exergenics.com',
+    packages=['exergenics'],
+    # package_dir={'': 'src'},
+    url='https://github.com/Exergenics/internal-portal-api',
+    keywords='exergenics portal api',
+    install_requires=[
+          'boto3',
+          'datetime',
+          'pandas',
+          'requests',
+          'urllib3',
+          'uuid',
+        'os'
+      ],
+)
