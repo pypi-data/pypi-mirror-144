a7c038931f753fa1db697bfc7076af2975aa8b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:41 +0000

    Add package runp3/0.0.1/runp3-0.0.1.tar.gz

---
+++ packages/runp3/runp3-0.0.1.tar.gz/runp3-0.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+long_description = readme
+
+setup(
+    name='runp3',
+    version='v0.0.1',
+    description='runp exports Python functions from files to the command line',
+    long_description=long_description,
+    author='Takashi Kusachi',
+    author_email='aisiars@gmail.com',
+    url='https://github.com/TakashiKusachi/runp',
+    packages=find_packages(),
+    test_suite='tests',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'runp = runp.runp:main',
+        ]
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development'
+    ],
+    python_requires='>3.7.11',
+)
