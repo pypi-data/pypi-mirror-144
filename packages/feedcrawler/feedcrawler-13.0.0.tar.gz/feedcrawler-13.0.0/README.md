e8de3903c7b0a4c96258ec6926f06b0a6948c97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:52 +0000

    Add package feedcrawler/13.0.0/feedcrawler-13.0.0.tar.gz

---
+++ packages/feedcrawler/feedcrawler-13.0.0.tar.gz/feedcrawler-13.0.0/README.md
@@ -0,0 +1,114 @@
+# FeedCrawler
+
+<img src="./feedcrawler/web/public/favicon.ico" data-canonical-src="./feedcrawler/web/img/favicon.ico" width="64" height="64" />
+
+FeedCrawler automatisiert bequem das Hinzufügen von Links für den JDownloader.
+
+[![Release Artifacts](https://github.com/rix1337/FeedCrawler/actions/workflows/CreateRelease.yml/badge.svg?branch=master)](https://github.com/rix1337/FeedCrawler/actions/workflows/CreateRelease.yml)
+[![PyPI version](https://badge.fury.io/py/feedcrawler.svg)](https://badge.fury.io/py/feedcrawler)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/feedcrawler)](https://github.com/rix1337/FeedCrawler/releases)
+[![Github Sponsorship](https://img.shields.io/badge/support-me-red.svg)](https://github.com/users/rix1337/sponsorship)
+[![Chat aufrufen unter https://gitter.im/FeedCrawler/community](https://badges.gitter.im/FeedCrawler/community.svg)](https://gitter.im/FeedCrawler/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![GitHub license](https://img.shields.io/github/license/rix1337/FeedCrawler.svg)](https://github.com/rix1337/FeedCrawler/blob/master/LICENSE.md)
+[![GitHub issues](https://img.shields.io/github/issues/rix1337/FeedCrawler.svg)](https://github.com/rix1337/FeedCrawler/issues)
+[![GitHub stars](https://img.shields.io/github/stars/rix1337/FeedCrawler.svg)](https://github.com/rix1337/FeedCrawler/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/rix1337/FeedCrawler.svg)](https://github.com/rix1337/FeedCrawler/network)
+
+***
+
+## Installation
+
+### Voraussetzungen
+
+* [Python 3.7](https://www.python.org/downloads/) oder neuer
+* [JDownloader 2](http://www.jdownloader.org/jdownloader2) mit [My JDownloader-Konto](https://my.jdownloader.org)
+* [FlareSolverr](https://github.com/FlareSolverr/FlareSolverr) ab v.2.0.0 um Cloudflare-Blockaden zu umgehen (optional)
+
+### Lokaler Build
+Benötigt [Node.js](https://nodejs.org/en/download/), [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) und [pip](https://pip.pypa.io/en/stable/installation/):
+
+1. Frontend-Pfad aufrufen: `cd feedcrawler/web`
+2. Dependencies installieren: `npm ci`
+3. [Vue.js 3](https://vuejs.org/) Frontend kompilieren: `npm run build`
+4. Zurück in das Hauptverzeichnis wechseln: `cd ../..`
+5. FeedCrawler auf Basis der _setup.py_ installieren: `pip install .`
+
+### [pip](https://pip.pypa.io/en/stable/installation/)
+
+#### Installieren
+
+```pip install feedcrawler```
+
+Hinweise zur manuellen Installation und Einrichtung finden sich im [Wiki](https://github.com/rix1337/FeedCrawler/wiki)!
+
+#### Update
+
+```pip install -U feedcrawler```
+
+#### Starten
+
+```feedcrawler``` in der Konsole (Python muss im System-PATH hinterlegt sein)
+
+
+### Docker Image
+
+* Offizielles Repo im Docker Hub: [docker-feedcrawler](https://hub.docker.com/r/rix1337/docker-feedcrawler/)
+* Der Betrieb als Docker-Container empfiehlt sich als Standardinstallation - vor allem für NAS-Systeme, Homeserver und
+  sonstige Geräte die dauerhaft und möglichst wartungsfrei (headless) betrieben werden sollen. Beim (Neu-)Start des
+  Containers wird automatisch die neueste Version heruntergeladen. Wird ein neues Image im Docker Hub bereitgestellt,
+  sollte dennoch auf dieses aktualisiert werden!
+* Für UNRAID-Server kann das Image direkt über die Community Applications bezogen und der Container so eingerichtet
+  werden.
+* Ein [FlareSolverr](https://github.com/FlareSolverr/FlareSolverr) ab v.2.0.0 muss lokal verfügbar sein um
+  Cloudflare-Blockaden zu umgehen (optional)
+
+### Windows Exe
+
+* Jedem [Release](https://github.com/rix1337/FeedCrawler/releases) wird eine selbstständig unter Windows lauffähige
+  Version des FeedCrawlers beigefügt.
+* Hierfür müssen weder Python, noch die Zusatzpakete installiert werden.
+* Einfach die jeweilige Exe herunterladen und ausführen bzw. bei Updates die Exe ersetzen.
+* Ein [FlareSolverr](https://github.com/FlareSolverr/FlareSolverr) ab v.2.0.0 muss lokal verfügbar sein um
+  Cloudflare-Blockaden zu umgehen (optional)
+
+### Hostnamen festlegen
+
+FeedCrawler kann zum durchsuchen beliebiger Webseiten verwendet werden. Ausschließlich der Anwender entscheidet, welche
+Seiten durchsucht werden sollen. Diese Entscheidung trifft der Anwender selbstständig, indem er die _FeedCrawler.ini_ in
+der Kategorie _[Hostnames]_ manuell befüllt (_ab = xyz.com_). Eingetragen werden dort reine Hostnamen (ohne _https://_).
+
+#### Dabei gilt
+
+* Welcher Hostname aufgerufen wird entscheidet allein der Anwender.
+* Ist nicht mindestens ein Hostname gesetzt, wird der FeedCrawler nicht starten.
+* Passt die aufgerufene Seite hinter dem jeweiligen Hostnamen nicht zum Suchmuster des FeedCrawlers, kann es zu Fehlern
+  kommen.
+* Weder FeedCrawler noch der Autor benennen oder befürworten spezifische Hostnamen. Fragen hierzu werden ignoriert!
+
+### Startparameter
+
+| Parameter | Erläuterung |
+|---|---|
+| ```--log-level=<LOGLEVEL>``` | Legt fest, wie genau geloggt wird (`CRITICAL`, `ERROR`, `WARNING`, `INFO`, `DEBUG`, `NOTSET`) |
+| ```--config="<CFGPFAD>"``` | Legt den Ablageort für Einstellungen und Logs fest |
+| ```--port=<PORT>``` | Legt den Port des Webservers fest |
+| ```--jd-user=<NUTZERNAME>``` | Legt den Nutzernamen für My JDownloader fest |
+| ```--jd-pass=<PASSWORT>``` | Legt das Passwort für My JDownloader fest |
+| ```--jd-device=<GERÄTENAME>``` | Legt den Gerätenamen für My JDownloader fest (optional, wenn nur ein Gerät vorhanden ist) |
+| ``` --keep-cdc``` | Leere die CDC-Tabelle (Feed ab hier bereits gecrawlt) nicht vor dem ersten Suchlauf |
+
+
+### Sicherheitshinweis
+
+Der Webserver sollte nie ohne Absicherung im Internet freigegeben werden. Dazu lassen sich im Webinterface Nutzername
+und Passwort festlegen.
+
+Es empfiehlt sich, zusätzlich einen Reverse-Proxy mit HTTPs-Zertifikat,
+bspw. [kostenlos von letsencrypt](https://letsencrypt.org/), zu verwenden.
+
+## Credits
+
+* [zapp-brannigan](https://github.com/zapp-brannigan/) (Idee)
+* Gutz-Pilz (Idee)
+* [mmarquezs](https://github.com/mmarquezs/) (MyJDownloader-API für Python)
+* [JetBrains PyCharm](https://www.jetbrains.com/?from=FeedCrawler) (Lizenz für die IDE)
---
+++ packages/feedcrawler/feedcrawler-13.0.0.tar.gz/feedcrawler-13.0.0/feedcrawler/search/shared/content_all.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+# FeedCrawler
+# Projekt von https://github.com/rix1337
+# Dieses Modul durchsucht die Web-Suchen vieler Seiten des Typs content_all auf Basis einer standardisierten Struktur.
+
+import re
+
+from bs4 import BeautifulSoup
+from rapidfuzz import fuzz
+
+from feedcrawler import internal
+from feedcrawler.common import sanitize, is_retail, decode_base64, check_is_site, check_hoster
+from feedcrawler.config import CrawlerConfig
+from feedcrawler.db import ListDb, FeedDb
+from feedcrawler.myjd import myjd_download
+from feedcrawler.notifiers import notify
+from feedcrawler.search.search import get
+from feedcrawler.sites.shared.internal_feed import add_decrypt_instead_of_download
+from feedcrawler.sites.shared.internal_feed import fx_get_download_links
+from feedcrawler.url import get_redirected_url
+from feedcrawler.url import get_url
+from feedcrawler.url import get_urls_async
+
+
+def get_best_result(title):
+    title = sanitize(title)
+    try:
+        bl_results = get(title, bl_only=True)[0]
+    except:
+        return False
+    results = []
+    i = len(bl_results)
+
+    j = 0
+    while i > 0:
+        try:
+            q = "result" + str(j + 1000)
+            results.append(bl_results.get(q).get('title'))
+        except:
+            pass
+        i -= 1
+        j += 1
+    best_score = 0
+    best_match = 0
+    for r in results:
+        r = re.sub(r'\(.*\)', '', r).strip()
+        r = r.replace(".", " ")
+        without_year = re.sub(
+            r'(|.UNRATED.*|.Unrated.*|.Uncut.*|.UNCUT.*)(|.Directors.Cut.*|.Final.Cut.*|.DC.*|.EXTENDED.*|.Extended.*|.Theatrical.*|.THEATRICAL.*)(|.3D.*|.3D.HSBS.*|.3D.HOU.*|.HSBS.*|.HOU.*)(|.)\d{4}(|.)(|.UNRATED.*|.Unrated.*|.Uncut.*|.UNCUT.*)(|.Directors.Cut.*|.Final.Cut.*|.DC.*|.EXTENDED.*|.Extended.*|.Theatrical.*|.THEATRICAL.*)(|.3D.*|.3D.HSBS.*|.3D.HOU.*|.HSBS.*|.HOU.*).(German|GERMAN)(|.AC3|.DTS|.DTS-HD)(|.DL)(|.AC3|.DTS).(2160|1080|720)p.(UHD.|Ultra.HD.|)(HDDVD|BluRay)(|.HDR)(|.AVC|.AVC.REMUX|.x264|.x265)(|.REPACK|.RERiP|.REAL.RERiP)-.*',
+            "", r)
+        with_year = re.sub(
+            r'(|.UNRATED.*|.Unrated.*|.Uncut.*|.UNCUT.*)(|.Directors.Cut.*|.Final.Cut.*|.DC.*|.EXTENDED.*|.Extended.*|.Theatrical.*|.THEATRICAL.*)(|.3D.*|.3D.HSBS.*|.3D.HOU.*|.HSBS.*|.HOU.*).(German|GERMAN)(|.AC3|.DTS|.DTS-HD)(|.DL)(|.AC3|.DTS|.DTS-HD).(2160|1080|720)p.(UHD.|Ultra.HD.|)(HDDVD|BluRay)(|.HDR)(|.AVC|.AVC.REMUX|.x264|.x265)(|.REPACK|.RERiP|.REAL.RERiP)-.*',
+            "", r)
+        score = fuzz.ratio(title, without_year) + fuzz.ratio(title, with_year)
+        if score > best_score:
+            best_score = score
+            best_match = i + 1000
+        i += 1
+    best_match = 'result' + str(best_match)
+    best_result = bl_results.get(best_match)
+    if best_result:
+        best_title = best_result.get('title')
+        if not re.match(r"^" + title.replace(" ", ".") + r".*$", best_title, re.IGNORECASE):
+            best_title = False
+        best_payload = best_result.get('payload')
+    else:
+        best_title = None
+        best_payload = None
+    if not best_title:
+        internal.logger.debug(u'Kein Treffer für die Suche nach ' + title + '! Suchliste ergänzt.')
+        liste = "List_ContentAll_Movies"
+        cont = ListDb(liste).retrieve()
+        if not cont:
+            cont = ""
+        if title not in cont:
+            ListDb(liste).store(title)
+        return False
+    if not is_retail(best_title, True):
+        internal.logger.debug(u'Kein Retail-Release für die Suche nach ' + title + ' gefunden! Suchliste ergänzt.')
+        liste = "List_ContentAll_Movies"
+        cont = ListDb(liste).retrieve()
+        if not cont:
+            cont = ""
+        if title not in cont:
+            ListDb(liste).store(title)
+        return best_payload
+    else:
+        internal.logger.debug('Bester Treffer fuer die Suche nach ' + title + ' ist ' + best_title)
+        return best_payload
+
+
+def download(payload):
+    config = CrawlerConfig('ContentAll')
+    db = FeedDb('FeedCrawler')
+    hostnames = CrawlerConfig('Hostnames')
+    by = hostnames.get('by')
+    nk = hostnames.get('nk')
+
+    payload = decode_base64(payload).split("|")
+    link = payload[0]
+    password = payload[1]
+
+    site = check_is_site(link)
+    if not site:
+        return False
+    else:
+        url = get_url(link)
+        if not url or "NinjaFirewall 429" in url:
+            return False
+        download_method = myjd_download
+        soup = BeautifulSoup(url, 'html5lib')
+
+        if "BY" in site:
+            key = soup.find("small").text
+            links = soup.find_all("iframe")
+            async_link_results = []
+            for link in links:
+                link = link["src"]
+                if 'https://' + by in link:
+                    async_link_results.append(link)
+            async_link_results = get_urls_async(async_link_results)
+            links = async_link_results[0]
+            url_hosters = []
+            for link in links:
+                if link:
+                    link = BeautifulSoup(link, 'html5lib').find("a", href=re.compile("/go\.php\?"))
+                    if link:
+                        url_hosters.append([link["href"], link.text.replace(" ", "")])
+        elif "NK" in site:
+            key = soup.find("span", {"class": "subtitle"}).text
+            url_hosters = []
+            hosters = soup.find_all("a", href=re.compile("/go/"))
+            for hoster in hosters:
+                url_hosters.append(['https://' + nk + hoster["href"], hoster.text])
+        elif "FX" in site:
+            download_method = add_decrypt_instead_of_download
+            key = payload[2]
+        elif "HW" in site:
+            download_method = add_decrypt_instead_of_download
+            download_links = soup.findAll("a", href=re.compile('filecrypt'))
+            links_string = ""
+            for link in download_links:
+                links_string += str(link)
+            url_hosters = re.findall(r'href="([^"\'>]*)".+?(.+?)<', links_string)
+            key = soup.find("h2", {"class": "entry-title"}).text.strip()
+            password = payload[1]
+        else:
+            return False
+
+        links = {}
+        if "FX" in site:
+            class FX:
+                unused = ""
+
+            download_links = fx_get_download_links(FX, url, key)
+        else:
+            for url_hoster in reversed(url_hosters):
+                try:
+                    link_hoster = url_hoster[1].lower().replace('target="_blank">', '').replace(" ", "-").replace(
+                        "ddownload", "ddl")
+                    if check_hoster(link_hoster):
+                        link = url_hoster[0]
+                        if by in link:
+                            demasked_link = get_redirected_url(link)
+                            if demasked_link:
+                                link = demasked_link
+                        links[link_hoster] = link
+                except:
+                    pass
+            if config.get("hoster_fallback") and not links:
+                for url_hoster in reversed(url_hosters):
+                    link_hoster = url_hoster[1].lower().replace('target="_blank">', '').replace(" ", "-").replace(
+                        "ddownload", "ddl")
+                    link = url_hoster[0]
+                    if by in link:
+                        demasked_link = get_redirected_url(link)
+                        if demasked_link:
+                            link = demasked_link
+                    links[link_hoster] = link
+            download_links = list(links.values())
+
+    englisch = False
+    if "*englisch" in key.lower() or "*english" in key.lower():
+        key = key.replace(
+            '*ENGLISCH', '').replace("*Englisch", "").replace("*ENGLISH", "").replace("*English",
+                                                                                      "").replace(
+            "*", "")
+        englisch = True
+
+    staffel = re.search(r"s\d{1,2}(-s\d{1,2}|-\d{1,2}|\.)", key.lower())
+
+    if download_links:
+        if staffel:
+            if download_method(key, "FeedCrawler", download_links, password):
+                db.store(
+                    key.replace(".COMPLETE", "").replace(".Complete", ""),
+                    'notdl' if config.get(
+                        'enforcedl') and '.dl.' not in key.lower() else 'added'
+                )
+                log_entry = '[Suche/Staffel] - ' + key.replace(".COMPLETE", "").replace(".Complete",
+                                                                                        "") + ' - [' + site + ']'
+                internal.logger.info(log_entry)
+                notify([log_entry])
+                return True
+        else:
+            retail = False
+            if config.get('cutoff') and '.COMPLETE.' not in key.lower():
+                if is_retail(key, True):
+                    retail = True
+            if download_method(key, "FeedCrawler", download_links, password):
+                db.store(
+                    key,
+                    'notdl' if config.get(
+                        'enforcedl') and '.dl.' not in key.lower() else 'added'
+                )
+                log_entry = '[Suche/Film' + ('/Englisch' if englisch and not retail else '') + (
+                    '/Englisch/Retail' if englisch and retail else '') + (
+                                '/Retail' if not englisch and retail else '') + '] - ' + key + ' - [' + site + ']'
+                internal.logger.info(log_entry)
+                notify([log_entry])
+                return [key]
+    else:
+        return False
