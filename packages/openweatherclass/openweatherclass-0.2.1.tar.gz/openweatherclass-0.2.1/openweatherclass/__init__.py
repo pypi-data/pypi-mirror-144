a3ae7caeae086aaaabc508426d9270b5a7b360b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:11 +0000

    Add package openweatherclass/0.2.1/openweatherclass-0.2.1.tar.gz

---
+++ packages/openweatherclass/openweatherclass-0.2.1.tar.gz/openweatherclass-0.2.1/openweatherclass/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.2.1'
+
+import openweatherclass.openweatherclass.geodataclass
+import openweatherclass.openweatherclass.weatherdataclass
+import openweatherclass.openweatherclass.historydataclass
+import openweatherclass.openweatherclass.functions
