cc8cc13f8f0d21180aa3dec31dd843de9ff6d035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package eircode/0.0.44/eircode-0.0.44.tar.gz

---
+++ packages/eircode/eircode-0.0.44.tar.gz/eircode-0.0.44/setup.py
@@ -0,0 +1,21 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+INSTALL_REQUIRES = (
+    'requests',
+    'cached_property',
+    'requests-ip-rotator'
+)
+
+setup(
+    name='eircode',
+    version='0.0.44',
+    python_requires='>=3.5',
+    author='Robert Lucey',
+    url='https://github.com/RobertLucey/eircode',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES
+)
