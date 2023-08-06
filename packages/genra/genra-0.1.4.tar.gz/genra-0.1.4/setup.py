b027f33f4075340328fd13ece43794077aed5071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:38 +0000

    Add package genra/0.1.4/genra-0.1.4.tar.gz

---
+++ packages/genra/genra-0.1.4.tar.gz/genra-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='genra',
+    packages=find_packages(),
+    version='0.1.4',
+    install_requires=['numpy', 'scipy', 'scikit-learn==1.0.1', 'lxml'],
+    description='Generalised Read Across (GenRA) in Python',
+    author='Imran Shah',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'],
+        keywords = 'genra'
+)
