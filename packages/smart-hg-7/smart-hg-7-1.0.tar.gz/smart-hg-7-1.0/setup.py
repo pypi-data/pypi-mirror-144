4a2459a9287193a670d9f52be6fad5e85170f0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:26 +0000

    Add package smart-hg-7/1.0/smart-hg-7-1.0.tar.gz

---
+++ packages/smart-hg-7/smart-hg-7-1.0.tar.gz/smart-hg-7-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-7",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['pillow==8.2.0', 'numpy==1.20.3', 'h5py==3.2.1', 'scikit-image==0.18.0'],
+)
