e246c85502e87b2835736d22988706de4b3f511a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:23 +0000

    Add package OSF-EIMTC/0.1.27/OSF_EIMTC-0.1.27.tar.gz

---
+++ packages/OSF-EIMTC/OSF_EIMTC-0.1.27.tar.gz/OSF_EIMTC-0.1.27/src/EIMTC/preprocessing/__init__.py
@@ -0,0 +1,9 @@
+from ._m1cnn import M1CNNPreprocessing
+from ._distiller import DistillerPreprocessing
+from ._maldist import MalDistPreprocessing
+
+__all__ = [
+    'M1CNNPreprocessing',
+    'DistillerPreprocessing',
+    'MalDistPreprocessing'
+]
