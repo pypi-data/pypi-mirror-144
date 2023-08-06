4c3765252da02c4d4c5666327c5747ead02607ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package malcolm/5.4/malcolm-5.4.tar.gz

---
+++ packages/malcolm/malcolm-5.4.tar.gz/malcolm-5.4/malcolm/modules/builtin/controllers/__init__.py
@@ -0,0 +1,20 @@
+# Expose a nice namespace
+from malcolm.core import submodule_all
+
+from .basiccontroller import ADescription, AMri, BasicController
+from .clientcomms import ClientComms
+from .managercontroller import (
+    AConfigDir,
+    ADescription,
+    AInitialDesign,
+    AMri,
+    ATemplateDesigns,
+    AUseGit,
+    ManagerController,
+    check_git_version,
+)
+from .proxycontroller import AComms, AMri, APublish, ProxyController
+from .servercomms import ADescription, AMri, ServerComms
+from .statefulcontroller import ADescription, AMri, StatefulController
+
+__all__ = submodule_all(globals())
