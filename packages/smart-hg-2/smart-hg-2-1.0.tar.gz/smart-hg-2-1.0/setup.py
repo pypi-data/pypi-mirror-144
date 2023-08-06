674dd786ae0f4b48c0f7ea0c747cdd9a18aa46df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-2/1.0/smart-hg-2-1.0.tar.gz

---
+++ packages/smart-hg-2/smart-hg-2-1.0.tar.gz/smart-hg-2-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-2",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['zope.interface==5.2.0', 'plone.api==1.10.4'],
+)
