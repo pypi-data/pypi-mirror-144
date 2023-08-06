e14f5d79523092e15d906ec7b106ff8ef51bc01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:00 +0000

    Add package xmp2jsonforybigta/1.0.0/xmp2jsonforybigta-1.0.0.tar.gz

---
+++ packages/xmp2jsonforybigta/xmp2jsonforybigta-1.0.0.tar.gz/xmp2jsonforybigta-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='xmp2jsonforybigta',
+    version='1.0.0',
+    author='ybigta',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
---
+++ packages/xmp2jsonforybigta/xmp2jsonforybigta-1.0.0.tar.gz/xmp2jsonforybigta-1.0.0/xmp2jsonforybigta/__init__.py
@@ -0,0 +1,3 @@
+from xmp2jsonforybigta import *
+
+from .__version__ import __version__
