d91006dfd9a2406484c35bc39279af6f318be1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package cyluhn/0.1.1/cyluhn-0.1.1.tar.gz

---
+++ packages/cyluhn/cyluhn-0.1.1.tar.gz/cyluhn-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import Extension
+
+# Before installation/distribution run `cythonize -i src/cyluhn.pyx`
+# Once the c code is generated, you can install with `pip install .`
+# To generate source distribution, run `python setup.py sdist`
+
+
+setup(
+    name='cyluhn',
+    version='0.1.1',
+    description='Fast library to validate and generate check digits using the Luhn algorithm',
+    long_description='file: README.md',
+    long_description_content_type='text/markdown',
+    ext_modules=[Extension(name='cyluhn', sources=['src/cyluhn.c'])],
+    entry_points={'console_scripts': ['cyluhn-generate=cyluhn:_cli_generate']},
+    author='Ian Kent',
+    author_email='iank1226@yahoo.com',
+    url='https://github.com/idemockle/cyluhn',
+    install_requires=['future; python_version < "3"'],
+    python_requires='>=2.7',
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Cython',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent'],
+    zip_safe=False
+)
