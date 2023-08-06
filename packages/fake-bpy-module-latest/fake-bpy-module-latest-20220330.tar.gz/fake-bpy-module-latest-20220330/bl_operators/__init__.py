a43af945823aa54f05b690291bcc7ed5097b348c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:13 +0000

    Add package fake-bpy-module-latest/20220330/fake-bpy-module-latest-20220330.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20220330.tar.gz/fake-bpy-module-latest-20220330/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import assets
+from . import sequencer
+from . import presets
+from . import vertexpaint_dirt
+from . import constraint
+from . import file
+from . import uvcalc_follow_active
+from . import anim
+from . import spreadsheet
+from . import freestyle
+from . import mesh
+from . import image
+from . import rigidbody
+from . import bmesh
+from . import object
+from . import object_quick_effects
+from . import wm
+from . import node
+from . import geometry_nodes
+from . import add_mesh_torus
+from . import object_align
+from . import object_randomize_transform
+from . import clip
+from . import userpref
+from . import screen_play_rendered_anim
+from . import uvcalc_lightmap
+from . import console
+from . import view3d
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20220330.tar.gz/fake-bpy-module-latest-20220330/bpy/ops/curves.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+
+
+def sculptmode_toggle():
+    ''' Enter/Exit sculpt mode for curves
+
+    '''
+
+    pass
