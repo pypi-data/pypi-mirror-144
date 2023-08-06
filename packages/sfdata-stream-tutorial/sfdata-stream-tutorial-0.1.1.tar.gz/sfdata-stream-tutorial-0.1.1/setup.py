4fc76ad6e31ed71b3b60ee2add5c0c09a88d4dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:43 +0000

    Add package sfdata-stream-tutorial/0.1.1/sfdata-stream-tutorial-0.1.1.tar.gz

---
+++ packages/sfdata-stream-tutorial/sfdata-stream-tutorial-0.1.1.tar.gz/sfdata-stream-tutorial-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sfdata_stream_tutorial']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Faker>=13.3.3,<14.0.0',
+ 'click>=8.0.4,<9.0.0',
+ 'sfdata-stream-parser>=0.2.0,<0.3.0',
+ 'tablib[xlsx]>=3.2.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'sfdata-stream-tutorial',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Kaj Siebert',
+    'author_email': 'kaj@k-si.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
