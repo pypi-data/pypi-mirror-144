c778533a80fc8b0e57bab264f813b3b4348391d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package mypy-boto3-account/1.21.27/mypy-boto3-account-1.21.27.tar.gz

---
+++ packages/mypy-boto3-account/mypy-boto3-account-1.21.27.tar.gz/mypy-boto3-account-1.21.27/mypy_boto3_account/type_defs.pyi
@@ -0,0 +1,91 @@
+"""
+Type annotations for account service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_account/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_account.type_defs import AlternateContactTypeDef
+
+    data: AlternateContactTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict
+
+from typing_extensions import NotRequired
+
+from .literals import AlternateContactTypeType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "AlternateContactTypeDef",
+    "DeleteAlternateContactRequestRequestTypeDef",
+    "GetAlternateContactRequestRequestTypeDef",
+    "GetAlternateContactResponseTypeDef",
+    "PutAlternateContactRequestRequestTypeDef",
+    "ResponseMetadataTypeDef",
+)
+
+AlternateContactTypeDef = TypedDict(
+    "AlternateContactTypeDef",
+    {
+        "AlternateContactType": NotRequired[AlternateContactTypeType],
+        "EmailAddress": NotRequired[str],
+        "Name": NotRequired[str],
+        "PhoneNumber": NotRequired[str],
+        "Title": NotRequired[str],
+    },
+)
+
+DeleteAlternateContactRequestRequestTypeDef = TypedDict(
+    "DeleteAlternateContactRequestRequestTypeDef",
+    {
+        "AlternateContactType": AlternateContactTypeType,
+        "AccountId": NotRequired[str],
+    },
+)
+
+GetAlternateContactRequestRequestTypeDef = TypedDict(
+    "GetAlternateContactRequestRequestTypeDef",
+    {
+        "AlternateContactType": AlternateContactTypeType,
+        "AccountId": NotRequired[str],
+    },
+)
+
+GetAlternateContactResponseTypeDef = TypedDict(
+    "GetAlternateContactResponseTypeDef",
+    {
+        "AlternateContact": "AlternateContactTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PutAlternateContactRequestRequestTypeDef = TypedDict(
+    "PutAlternateContactRequestRequestTypeDef",
+    {
+        "AlternateContactType": AlternateContactTypeType,
+        "EmailAddress": str,
+        "Name": str,
+        "PhoneNumber": str,
+        "Title": str,
+        "AccountId": NotRequired[str],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
