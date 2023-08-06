bc79fb59d41c225254cbba8541e4a7c3fa6ebc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:36 +0000

    Add package hjxdataconverter/1.0.0/hjxdataconverter-1.0.0-py3-none-any.whl

---
+++ packages/hjxdataconverter/hjxdataconverter-1.0.0-py3-none-any.whl/programs/hjxdataconverter.py
@@ -0,0 +1,5 @@
+def main():
+    print('version 1.0.0  (c) 2022 Isabel Sandstrom Hermit AS\n\nPython command line program for converting data between json, html and excel. We have four different commands: excel2json, json2excel, html2json, and json2html.')
+
+if __name__ == '__main__':
+    main()
