0e959f80e6570a6e839085a26d7c4ac1ca9959de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:30 +0000

    Add package statcore/0.0.1/statcore-0.0.1-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/statcore/statcore-0.0.1-cp37-cp37m-macosx_10_9_x86_64.whl/statcore/__init__.py
@@ -0,0 +1,6 @@
+#import cfast as fast
+
+#from cfast import mean, std
+#from .main import dot, correlation, lr
+from .tensor import get_shape_from_lists, Tensor, get_shape_from_keys, Array, Matrix
+from .generators import zeros, random
