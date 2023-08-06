ad1cdf68daac5b1a58175e638ae067889c1cd9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:07 +0000

    Add package smart-hg-15/1.0/smart-hg-15-1.0.tar.gz

---
+++ packages/smart-hg-15/smart-hg-15-1.0.tar.gz/smart-hg-15-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-15",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'opencv-python==4.0.1.24', 'scikit-image==0.14.5'],
+)
