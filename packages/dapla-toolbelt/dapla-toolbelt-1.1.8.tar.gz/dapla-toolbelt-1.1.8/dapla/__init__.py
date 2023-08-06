dde247cb80e4afa1eee8d5aab0082c0d7dc5a5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:23 +0000

    Add package dapla-toolbelt/1.1.8/dapla-toolbelt-1.1.8.tar.gz

---
+++ packages/dapla-toolbelt/dapla-toolbelt-1.1.8.tar.gz/dapla-toolbelt-1.1.8/dapla/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.1.8"
+
+from .auth import AuthClient
+from .files import FileClient
+from .guardian import GuardianClient
+from .collector import CollectorClient
+from .backports import show, details, read_pandas, write_pandas
---
+++ packages/dapla-toolbelt/dapla-toolbelt-1.1.8.tar.gz/dapla-toolbelt-1.1.8/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="dapla-toolbelt",
+    version="1.1.8",
+    license='Apache Software License',
+    description="Python module for use within Jupyterlab notebooks, specifically aimed for Statistics Norway's data "
+                "platform called Dapla",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/statisticsnorway/dapla-toolbelt",
+    author="Statistics Norway",
+    author_email="old@ssb.no",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=('tests', 'examples',)),
+    include_package_data=True,
+    install_requires=["pandas", "jupyterhub", "gcsfs", "ipython", "lxml", "pyarrow"],
+)
