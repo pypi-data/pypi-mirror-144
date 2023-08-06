e6539ed15c6fe7ba3d176818d9786e38167e3998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:08 +0000

    Add package elementy/0.0.3a0/elementy-0.0.3a0.tar.gz

---
+++ packages/elementy/elementy-0.0.3a0.tar.gz/elementy-0.0.3a0/elementy/element.py
@@ -0,0 +1,5 @@
+
+class Element():
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
---
+++ packages/elementy/elementy-0.0.3a0.tar.gz/elementy-0.0.3a0/elementy/periodictable.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+import elementy.elements as elements
+
+
+class PeriodicTable():
+    def __init__(self):
+        self.elements = elements.get_elements()
+
+        self.data = pd.DataFrame(
+            [element.__dict__ for element in self.elements])
