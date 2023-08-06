9b8c5b669668fa7edfce1be8668bbdf98810eae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:48 +0000

    Add package newsp/0.14/newsp-0.14.tar.gz

---
+++ packages/newsp/newsp-0.14.tar.gz/newsp-0.14/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name='newsp',
+    version='0.14',
+    scripts=['newsp'],
+    author="Thanh Hoa",
+    author_email="getmoneykhmt3@gmail.com",
+    description="A Des of newsp",
+    long_description="newsp",
+    long_description_content_type="text/markdown",
+    url="https://github.com/vtandroid/newsp",
+    packages=setuptools.find_packages(),
+    py_modules=['newspp'],
+    install_requires=[
+       'newspaper3k','awesome-slugify','pillow','spacy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
