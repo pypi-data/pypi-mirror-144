758d598a921c7c1cdb9dbdf9aa434c6264c25426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:23:11 +0000

    Add package comments-cleaner-Tifa/0.0.1/comments_cleaner-Tifa-0.0.1.tar.gz

---
+++ packages/comments-cleaner-Tifa/comments_cleaner-Tifa-0.0.1.tar.gz/comments_cleaner-Tifa-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="comments_cleaner-Tifa",
+    version="0.0.1",
+    author="Tifa",
+    author_email="tiphereth-a@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tiphereth-A/cmcleaner",
+    project_urls={
+        "Bug Tracker": "https://github.com/Tiphereth-A/cmcleaner/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license="GNU General Public License v3.0",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
