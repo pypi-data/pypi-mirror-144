4352d166f0d39bbfec0c246eaed50ff5b65c1a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:04 +0000

    Add package m-parse/0.1.4/m-parse-0.1.4.tar.gz

---
+++ packages/m-parse/m-parse-0.1.4.tar.gz/m-parse-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+install_requires = ['json', 'argparse', 'xlwt']
+
+setup(name="m-parse",
+    version="0.1.4",
+    packages=find_packages(),
+    package_dir={"":"m_parse"},
+    include_package_data=False,
+    package_data={"data":[]},
+    description="该工具用于解析json,输出csv,excel格式的数据",
+    author="miaoxin",
+    author_email="miaorulai@gmail.com",
+    url='',
+    license="MIT",
+    install_requires=[],
+    entry_points = {'console_scripts': "m-parse = main:main"}
+)
