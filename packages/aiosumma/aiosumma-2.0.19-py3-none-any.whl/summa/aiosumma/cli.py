f451e8d386a12d9f53a3aebc975b222af0c0f994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:06 +0000

    Add package aiosumma/2.0.19/aiosumma-2.0.19-py3-none-any.whl

---
+++ packages/aiosumma/aiosumma-2.0.19-py3-none-any.whl/summa/aiosumma/cli.py
@@ -0,0 +1,25 @@
+import asyncio
+import sys
+import traceback
+
+import fire
+from summa.aiosumma.client import SummaClient
+
+
+async def client_cli(endpoint):
+    try:
+        client = SummaClient(endpoint=endpoint, connection_timeout=3.0)
+        await client.start_and_wait()
+        return client.get_interface()
+    except asyncio.exceptions.TimeoutError:
+        # ToDo: process exception through fire.core.FiteError
+        print(traceback.format_exc())
+        sys.exit(1)
+
+
+def main():
+    fire.Fire(client_cli, name='summa-client')
+
+
+if __name__ == '__main__':
+    main()
