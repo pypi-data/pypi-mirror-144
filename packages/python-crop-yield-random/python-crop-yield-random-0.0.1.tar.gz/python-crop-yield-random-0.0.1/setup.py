1b2d5a50483e4c4936e3c47aae1c704b8649814b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:20 +0000

    Add package python-crop-yield-random/0.0.1/python-crop-yield-random-0.0.1.tar.gz

---
+++ packages/python-crop-yield-random/python-crop-yield-random-0.0.1.tar.gz/python-crop-yield-random-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python-crop-yield-random',
+    version='0.0.1',
+    description='Welcome to the mock package `python-crop-yield-random`',
+    author='David Doran',
+    author_email='daviddoran20161@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    license='AGPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Development Status :: 3 - Alpha'
+    ]
+)
