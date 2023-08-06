799f0ba439afd3125cb915045785afc2b3df11e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:59 +0000

    Add package function-measurer/0.2/function_measurer-0.2.tar.gz

---
+++ packages/function-measurer/function_measurer-0.2.tar.gz/function_measurer-0.2/measurer/__init__.py
@@ -0,0 +1,27 @@
+import time, sys
+
+class testRunner:
+    def __init__(self, function, *args, **kwargs) -> None:
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.name = function.__name__
+
+    def __call__(self, mode: str):
+        if mode == "runtime":
+            start = time.time()
+            self.function(*self.args, **self.kwargs)
+            end = time.time()
+            runningTime = start - end
+            return runningTime
+        elif mode == "memory":
+            return sys.getsizeof(self.function)
+
+
+class setTesting:
+    def __init__(self, *args, **kwargs) -> None:
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, function) -> testRunner:
+        return testRunner(function, *self.args, **self.kwargs)
