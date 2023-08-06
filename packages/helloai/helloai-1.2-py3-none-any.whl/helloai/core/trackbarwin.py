0a747394e599513c70630e62f90a9139f75fff5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package helloai/1.2/helloai-1.2-py3-none-any.whl

---
+++ packages/helloai/helloai-1.2-py3-none-any.whl/helloai/core/trackbarwin.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np 
+import uuid
+from .window import Window
+from .image import Image 
+
+__all__ = ['TrackbarWindow']
+
+
+def nothing(x):
+    pass
+
+
+class TrackbarWindow(Window):
+    def __init__(self, name):
+        super().__init__(name)
+        cv2.createTrackbar("Value", self.name, 0, 100, nothing)
+
+    def get_value(self):
+        val = cv2.getTrackbarPos('Value',self.name)
+        return val
