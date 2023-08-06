97ed62203a72046c25d3cbea6926bf7b292542a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:14 +0000

    Add package localstack-ext/0.14.1.8/localstack-ext-0.14.1.8.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.14.1.8.tar.gz/localstack-ext-0.14.1.8/localstack_ext/services/cloudformation/models/apigateway.py
@@ -0,0 +1,17 @@
+from localstack.services.cloudformation.service_models import GenericBaseModel
+from localstack.utils.aws import aws_stack
+class ApiGatewayAuthorizer(GenericBaseModel):
+ @staticmethod
+ def cloudformation_type():
+  return "AWS::ApiGateway::Authorizer"
+ def fetch_state(self,stack_name,resources):
+  props=self.props
+  client=aws_stack.connect_to_service("apigateway")
+  api_id=self.resolve_refs_recursively(stack_name,props["RestApiId"],resources)
+  auth_uri=self.resolve_refs_recursively(stack_name,props.get("AuthorizerUri"),resources)
+  authorizers=client.get_authorizers(restApiId=api_id,limit=200)["items"]
+  result=[a for a in authorizers if a["type"]==props.get("Type")and a.get("authorizerUri")==auth_uri]
+  return(result or[None])[0]
+ def get_physical_resource_id(self,attribute,**kwargs):
+  return self.props.get("id")
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
