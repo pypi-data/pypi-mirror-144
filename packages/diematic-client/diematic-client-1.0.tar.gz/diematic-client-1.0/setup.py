530ec62dc656addca781102243be3e377c3b07ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:38 +0000

    Add package diematic-client/1.0/diematic-client-1.0.tar.gz

---
+++ packages/diematic-client/diematic-client-1.0.tar.gz/diematic-client-1.0/setup.py
@@ -0,0 +1,45 @@
+# coding=UTF-8
+"""Setup for the pipy package"""
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as long_description_f:
+	long_description = long_description_f.read()
+
+setuptools.setup(
+  name = 'diematic-client',
+  version = '1.0',
+  description = 'Asynchronous Python client for diematic-server HTTP server',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown; charset=UTF-8',
+  author = 'Ignacio Hernández-Ros',
+  author_email = 'ignacio@hernandez-ros.com',
+  packages = ['diematic-client'],
+  license='LGPL',
+  url = 'https://github.com/IgnacioHR/diematic-client',
+  download_url = 'https://github.com/IgnacioHR/diematic-client/archive/refs/tags/v0.1-beta.tar.gz',
+  keywords = ['python', 'home-automation', 'iot', 'restful', 'de-dietrich', 'diematic', 'home-assistant'],
+  install_requires=[
+		'aiohttp',
+		'aiosignal',
+		'async-timeout',
+		'asynctest',
+		'attrs',
+		'charset-normalizer',
+		'deepmerge',
+		'frozenlist',
+		'idna',
+		'multidict',
+		'pkg-resources',
+		'typing-extensions',
+		'yarl',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+		'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+		'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+		'Topic :: Home Automation',
+	],
+)
