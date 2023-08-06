bc6a2990f7c2e8371122f3d8b9519e8c5a78560c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:46 +0000

    Add package mtms/1.0.3/mtms-1.0.3.tar.gz

---
+++ packages/mtms/mtms-1.0.3.tar.gz/mtms-1.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name							=	"mtms"
+	, version						=	"1.0.3"
+	, author						=	"Govind Grover"
+	, author_email					=	"ask@govindgrover.com"
+	, description					=	"Metro Train Management System"
+	, long_description				=	long_description
+	, long_description_content_type	=	"text/markdown"
+	, url							=	"https://github.com/govindgrover/mtms"
+	, project_urls					=	{
+	}
+	, classifiers					=	[
+		"Programming Language :: Python :: 3"
+		, "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+		, "Operating System :: OS Independent"
+		, "Natural Language :: English"
+		, "Natural Language :: Hindi"
+		, "Topic :: Education"
+	]
+	, package_dir					=	{
+		""	:	"src"
+	}
+	, package_data					=	{
+		""	:	["initials/database/*.sql", "initials/lang/*.json", "lang/*.json"]
+	}
+	, packages						=	setuptools.find_packages(where="src")
+	, python_requires				=	">=3.6"
+	, install_requires 				=	[
+		'mysql.connector'
+		, 'uuid'
+		, 'pyqrcode'
+	]
+)
