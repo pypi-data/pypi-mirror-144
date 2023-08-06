20a333fd3ccb30abb4db6e313204ae5e8219df11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:44 +0000

    Add package sciencer/0.1.1/sciencer-0.1.1.tar.gz

---
+++ packages/sciencer/sciencer-0.1.1.tar.gz/sciencer-0.1.1/sciencer/filters/filter.py
@@ -0,0 +1,21 @@
+"""Base class for filter
+"""
+
+from abc import ABC, abstractmethod
+from ..policies import HasPolicy
+from ..models import Paper
+
+
+class Filter(HasPolicy, ABC):
+    """Base class for filters"""
+
+    @abstractmethod
+    def is_valid(self, paper: Paper) -> bool:
+        """Verifies if the filter is satisfied.
+
+        Args:
+            paper (Paper): tested paper.
+
+        Returns:
+            bool: Returns True when the paper satisfies the criteria. Otherwise, returns false.
+        """
