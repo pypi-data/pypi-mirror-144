fc869f2ef4599109aa59e1650ed281d6e0af1232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:17 +0000

    Add package dccompiler/0.0.3/dccompiler-0.0.3.tar.gz

---
+++ packages/dccompiler/dccompiler-0.0.3.tar.gz/dccompiler-0.0.3/setup.py
@@ -0,0 +1,35 @@
+# -------------------------------
+# PipeBit
+# 'setup.py'
+# Author: Juan Carlos Juárez.
+# Licensed under MPL 2.0.
+# All rights reserved.
+# -------------------------------
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Dynamo Charlotte Compiler'
+LONG_DESCRIPTION = 'A Dynamo Charlotte Compiler for CLI.'
+
+# Setting up
+setup(
+    name="dccompiler",
+    version=VERSION,
+    author="Juan Carlos Juárez",
+    author_email="<jc.juarezgarcia@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['Dynamo Charlotte', 'Python', 'Compiler'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
