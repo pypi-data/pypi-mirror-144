55b84b3dc0d0bc5594451d6eb6684d6dffba7d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:29 +0000

    Add package types-aiobotocore-resourcegroupstaggingapi/2.2.0.post2/types-aiobotocore-resourcegroupstaggingapi-2.2.0.post2.tar.gz

---
+++ packages/types-aiobotocore-resourcegroupstaggingapi/types-aiobotocore-resourcegroupstaggingapi-2.2.0.post2.tar.gz/types-aiobotocore-resourcegroupstaggingapi-2.2.0.post2/types_aiobotocore_resourcegroupstaggingapi/type_defs.pyi
@@ -0,0 +1,295 @@
+"""
+Type annotations for resourcegroupstaggingapi service type definitions.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_resourcegroupstaggingapi/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_resourcegroupstaggingapi.type_defs import ComplianceDetailsTypeDef
+
+    data: ComplianceDetailsTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List, Mapping, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import ErrorCodeType, GroupByAttributeType, TargetIdTypeType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "ComplianceDetailsTypeDef",
+    "DescribeReportCreationOutputTypeDef",
+    "FailureInfoTypeDef",
+    "GetComplianceSummaryInputGetComplianceSummaryPaginateTypeDef",
+    "GetComplianceSummaryInputRequestTypeDef",
+    "GetComplianceSummaryOutputTypeDef",
+    "GetResourcesInputGetResourcesPaginateTypeDef",
+    "GetResourcesInputRequestTypeDef",
+    "GetResourcesOutputTypeDef",
+    "GetTagKeysInputGetTagKeysPaginateTypeDef",
+    "GetTagKeysInputRequestTypeDef",
+    "GetTagKeysOutputTypeDef",
+    "GetTagValuesInputGetTagValuesPaginateTypeDef",
+    "GetTagValuesInputRequestTypeDef",
+    "GetTagValuesOutputTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResourceTagMappingTypeDef",
+    "ResponseMetadataTypeDef",
+    "StartReportCreationInputRequestTypeDef",
+    "SummaryTypeDef",
+    "TagFilterTypeDef",
+    "TagResourcesInputRequestTypeDef",
+    "TagResourcesOutputTypeDef",
+    "TagTypeDef",
+    "UntagResourcesInputRequestTypeDef",
+    "UntagResourcesOutputTypeDef",
+)
+
+ComplianceDetailsTypeDef = TypedDict(
+    "ComplianceDetailsTypeDef",
+    {
+        "NoncompliantKeys": NotRequired[List[str]],
+        "KeysWithNoncompliantValues": NotRequired[List[str]],
+        "ComplianceStatus": NotRequired[bool],
+    },
+)
+
+DescribeReportCreationOutputTypeDef = TypedDict(
+    "DescribeReportCreationOutputTypeDef",
+    {
+        "Status": str,
+        "S3Location": str,
+        "ErrorMessage": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+FailureInfoTypeDef = TypedDict(
+    "FailureInfoTypeDef",
+    {
+        "StatusCode": NotRequired[int],
+        "ErrorCode": NotRequired[ErrorCodeType],
+        "ErrorMessage": NotRequired[str],
+    },
+)
+
+GetComplianceSummaryInputGetComplianceSummaryPaginateTypeDef = TypedDict(
+    "GetComplianceSummaryInputGetComplianceSummaryPaginateTypeDef",
+    {
+        "TargetIdFilters": NotRequired[Sequence[str]],
+        "RegionFilters": NotRequired[Sequence[str]],
+        "ResourceTypeFilters": NotRequired[Sequence[str]],
+        "TagKeyFilters": NotRequired[Sequence[str]],
+        "GroupBy": NotRequired[Sequence[GroupByAttributeType]],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetComplianceSummaryInputRequestTypeDef = TypedDict(
+    "GetComplianceSummaryInputRequestTypeDef",
+    {
+        "TargetIdFilters": NotRequired[Sequence[str]],
+        "RegionFilters": NotRequired[Sequence[str]],
+        "ResourceTypeFilters": NotRequired[Sequence[str]],
+        "TagKeyFilters": NotRequired[Sequence[str]],
+        "GroupBy": NotRequired[Sequence[GroupByAttributeType]],
+        "MaxResults": NotRequired[int],
+        "PaginationToken": NotRequired[str],
+    },
+)
+
+GetComplianceSummaryOutputTypeDef = TypedDict(
+    "GetComplianceSummaryOutputTypeDef",
+    {
+        "SummaryList": List["SummaryTypeDef"],
+        "PaginationToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetResourcesInputGetResourcesPaginateTypeDef = TypedDict(
+    "GetResourcesInputGetResourcesPaginateTypeDef",
+    {
+        "TagFilters": NotRequired[Sequence["TagFilterTypeDef"]],
+        "TagsPerPage": NotRequired[int],
+        "ResourceTypeFilters": NotRequired[Sequence[str]],
+        "IncludeComplianceDetails": NotRequired[bool],
+        "ExcludeCompliantResources": NotRequired[bool],
+        "ResourceARNList": NotRequired[Sequence[str]],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetResourcesInputRequestTypeDef = TypedDict(
+    "GetResourcesInputRequestTypeDef",
+    {
+        "PaginationToken": NotRequired[str],
+        "TagFilters": NotRequired[Sequence["TagFilterTypeDef"]],
+        "ResourcesPerPage": NotRequired[int],
+        "TagsPerPage": NotRequired[int],
+        "ResourceTypeFilters": NotRequired[Sequence[str]],
+        "IncludeComplianceDetails": NotRequired[bool],
+        "ExcludeCompliantResources": NotRequired[bool],
+        "ResourceARNList": NotRequired[Sequence[str]],
+    },
+)
+
+GetResourcesOutputTypeDef = TypedDict(
+    "GetResourcesOutputTypeDef",
+    {
+        "PaginationToken": str,
+        "ResourceTagMappingList": List["ResourceTagMappingTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetTagKeysInputGetTagKeysPaginateTypeDef = TypedDict(
+    "GetTagKeysInputGetTagKeysPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetTagKeysInputRequestTypeDef = TypedDict(
+    "GetTagKeysInputRequestTypeDef",
+    {
+        "PaginationToken": NotRequired[str],
+    },
+)
+
+GetTagKeysOutputTypeDef = TypedDict(
+    "GetTagKeysOutputTypeDef",
+    {
+        "PaginationToken": str,
+        "TagKeys": List[str],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetTagValuesInputGetTagValuesPaginateTypeDef = TypedDict(
+    "GetTagValuesInputGetTagValuesPaginateTypeDef",
+    {
+        "Key": str,
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetTagValuesInputRequestTypeDef = TypedDict(
+    "GetTagValuesInputRequestTypeDef",
+    {
+        "Key": str,
+        "PaginationToken": NotRequired[str],
+    },
+)
+
+GetTagValuesOutputTypeDef = TypedDict(
+    "GetTagValuesOutputTypeDef",
+    {
+        "PaginationToken": str,
+        "TagValues": List[str],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+ResourceTagMappingTypeDef = TypedDict(
+    "ResourceTagMappingTypeDef",
+    {
+        "ResourceARN": NotRequired[str],
+        "Tags": NotRequired[List["TagTypeDef"]],
+        "ComplianceDetails": NotRequired["ComplianceDetailsTypeDef"],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+StartReportCreationInputRequestTypeDef = TypedDict(
+    "StartReportCreationInputRequestTypeDef",
+    {
+        "S3Bucket": str,
+    },
+)
+
+SummaryTypeDef = TypedDict(
+    "SummaryTypeDef",
+    {
+        "LastUpdated": NotRequired[str],
+        "TargetId": NotRequired[str],
+        "TargetIdType": NotRequired[TargetIdTypeType],
+        "Region": NotRequired[str],
+        "ResourceType": NotRequired[str],
+        "NonCompliantResources": NotRequired[int],
+    },
+)
+
+TagFilterTypeDef = TypedDict(
+    "TagFilterTypeDef",
+    {
+        "Key": NotRequired[str],
+        "Values": NotRequired[Sequence[str]],
+    },
+)
+
+TagResourcesInputRequestTypeDef = TypedDict(
+    "TagResourcesInputRequestTypeDef",
+    {
+        "ResourceARNList": Sequence[str],
+        "Tags": Mapping[str, str],
+    },
+)
+
+TagResourcesOutputTypeDef = TypedDict(
+    "TagResourcesOutputTypeDef",
+    {
+        "FailedResourcesMap": Dict[str, "FailureInfoTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+TagTypeDef = TypedDict(
+    "TagTypeDef",
+    {
+        "Key": str,
+        "Value": str,
+    },
+)
+
+UntagResourcesInputRequestTypeDef = TypedDict(
+    "UntagResourcesInputRequestTypeDef",
+    {
+        "ResourceARNList": Sequence[str],
+        "TagKeys": Sequence[str],
+    },
+)
+
+UntagResourcesOutputTypeDef = TypedDict(
+    "UntagResourcesOutputTypeDef",
+    {
+        "FailedResourcesMap": Dict[str, "FailureInfoTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
