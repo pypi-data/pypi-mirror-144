09aee6c0586f0f38ea60f84bb4712e8b31e6f5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:49 +0000

    Add package rapidkode/1.0.6/rapidkode-1.0.6.tar.gz

---
+++ packages/rapidkode/rapidkode-1.0.6.tar.gz/rapidkode-1.0.6/test.py
@@ -0,0 +1,22 @@
+import rapidkode as rk
+
+var = rk.numbers.gen_sparsenum_upto(100)
+print(var)	
+
+var = rk.numbers.get_sparsenum_after(3289)		
+print(var)	
+
+var = rk.numbers.checkprime(8364)	
+print(var)	
+
+var = rk.numbers.getprimes.generate(100)	
+print(var)	
+
+var = rk.numbers.getprimes.inrange(100,500)	
+print(var)	
+
+var = rk.numbers.fib.getelement(58)	
+print(var)	
+
+var = rk.numbers.fib.generate(25)	
+print(var)
