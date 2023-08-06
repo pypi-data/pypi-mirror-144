10a521a7ee2415f5e35b2569e0ee7b3c0cb07089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:09 +0000

    Add package ev-audio-streaming-transcription-py/0.0.7/ev-audio-streaming-transcription-py-0.0.7.tar.gz

---
+++ packages/ev-audio-streaming-transcription-py/ev-audio-streaming-transcription-py-0.0.7.tar.gz/ev-audio-streaming-transcription-py-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ev-audio-streaming-transcription-py',
+    version='0.0.7',
+    description='Quick test package for RingCenTral Engage Voice Audio Streaming feature.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DaKingKong/ev-audio-streaming-transcription-py',
+    author='Da Kong',
+    author_email='da.kong@ringcentral.com',
+    license='MIT',
+    python_requires='>=3.0',
+    install_requires=[
+        'google',
+        'six'
+    ],
+    packages=find_packages()
+)
