b1f68e4b07af3367647e5b9d237be8d935451bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package mypy-boto3-macie/1.21.27/mypy-boto3-macie-1.21.27.tar.gz

---
+++ packages/mypy-boto3-macie/mypy-boto3-macie-1.21.27.tar.gz/mypy-boto3-macie-1.21.27/mypy_boto3_macie/type_defs.py
@@ -0,0 +1,245 @@
+"""
+Type annotations for macie service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_macie/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_macie.type_defs import AssociateMemberAccountRequestRequestTypeDef
+
+    data: AssociateMemberAccountRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import S3OneTimeClassificationTypeType
+
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "AssociateMemberAccountRequestRequestTypeDef",
+    "AssociateS3ResourcesRequestRequestTypeDef",
+    "AssociateS3ResourcesResultTypeDef",
+    "ClassificationTypeTypeDef",
+    "ClassificationTypeUpdateTypeDef",
+    "DisassociateMemberAccountRequestRequestTypeDef",
+    "DisassociateS3ResourcesRequestRequestTypeDef",
+    "DisassociateS3ResourcesResultTypeDef",
+    "FailedS3ResourceTypeDef",
+    "ListMemberAccountsRequestListMemberAccountsPaginateTypeDef",
+    "ListMemberAccountsRequestRequestTypeDef",
+    "ListMemberAccountsResultTypeDef",
+    "ListS3ResourcesRequestListS3ResourcesPaginateTypeDef",
+    "ListS3ResourcesRequestRequestTypeDef",
+    "ListS3ResourcesResultTypeDef",
+    "MemberAccountTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+    "S3ResourceClassificationTypeDef",
+    "S3ResourceClassificationUpdateTypeDef",
+    "S3ResourceTypeDef",
+    "UpdateS3ResourcesRequestRequestTypeDef",
+    "UpdateS3ResourcesResultTypeDef",
+)
+
+AssociateMemberAccountRequestRequestTypeDef = TypedDict(
+    "AssociateMemberAccountRequestRequestTypeDef",
+    {
+        "memberAccountId": str,
+    },
+)
+
+AssociateS3ResourcesRequestRequestTypeDef = TypedDict(
+    "AssociateS3ResourcesRequestRequestTypeDef",
+    {
+        "s3Resources": Sequence["S3ResourceClassificationTypeDef"],
+        "memberAccountId": NotRequired[str],
+    },
+)
+
+AssociateS3ResourcesResultTypeDef = TypedDict(
+    "AssociateS3ResourcesResultTypeDef",
+    {
+        "failedS3Resources": List["FailedS3ResourceTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ClassificationTypeTypeDef = TypedDict(
+    "ClassificationTypeTypeDef",
+    {
+        "oneTime": S3OneTimeClassificationTypeType,
+        "continuous": Literal["FULL"],
+    },
+)
+
+ClassificationTypeUpdateTypeDef = TypedDict(
+    "ClassificationTypeUpdateTypeDef",
+    {
+        "oneTime": NotRequired[S3OneTimeClassificationTypeType],
+        "continuous": NotRequired[Literal["FULL"]],
+    },
+)
+
+DisassociateMemberAccountRequestRequestTypeDef = TypedDict(
+    "DisassociateMemberAccountRequestRequestTypeDef",
+    {
+        "memberAccountId": str,
+    },
+)
+
+DisassociateS3ResourcesRequestRequestTypeDef = TypedDict(
+    "DisassociateS3ResourcesRequestRequestTypeDef",
+    {
+        "associatedS3Resources": Sequence["S3ResourceTypeDef"],
+        "memberAccountId": NotRequired[str],
+    },
+)
+
+DisassociateS3ResourcesResultTypeDef = TypedDict(
+    "DisassociateS3ResourcesResultTypeDef",
+    {
+        "failedS3Resources": List["FailedS3ResourceTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+FailedS3ResourceTypeDef = TypedDict(
+    "FailedS3ResourceTypeDef",
+    {
+        "failedItem": NotRequired["S3ResourceTypeDef"],
+        "errorCode": NotRequired[str],
+        "errorMessage": NotRequired[str],
+    },
+)
+
+ListMemberAccountsRequestListMemberAccountsPaginateTypeDef = TypedDict(
+    "ListMemberAccountsRequestListMemberAccountsPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListMemberAccountsRequestRequestTypeDef = TypedDict(
+    "ListMemberAccountsRequestRequestTypeDef",
+    {
+        "nextToken": NotRequired[str],
+        "maxResults": NotRequired[int],
+    },
+)
+
+ListMemberAccountsResultTypeDef = TypedDict(
+    "ListMemberAccountsResultTypeDef",
+    {
+        "memberAccounts": List["MemberAccountTypeDef"],
+        "nextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListS3ResourcesRequestListS3ResourcesPaginateTypeDef = TypedDict(
+    "ListS3ResourcesRequestListS3ResourcesPaginateTypeDef",
+    {
+        "memberAccountId": NotRequired[str],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListS3ResourcesRequestRequestTypeDef = TypedDict(
+    "ListS3ResourcesRequestRequestTypeDef",
+    {
+        "memberAccountId": NotRequired[str],
+        "nextToken": NotRequired[str],
+        "maxResults": NotRequired[int],
+    },
+)
+
+ListS3ResourcesResultTypeDef = TypedDict(
+    "ListS3ResourcesResultTypeDef",
+    {
+        "s3Resources": List["S3ResourceClassificationTypeDef"],
+        "nextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+MemberAccountTypeDef = TypedDict(
+    "MemberAccountTypeDef",
+    {
+        "accountId": NotRequired[str],
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+S3ResourceClassificationTypeDef = TypedDict(
+    "S3ResourceClassificationTypeDef",
+    {
+        "bucketName": str,
+        "classificationType": "ClassificationTypeTypeDef",
+        "prefix": NotRequired[str],
+    },
+)
+
+S3ResourceClassificationUpdateTypeDef = TypedDict(
+    "S3ResourceClassificationUpdateTypeDef",
+    {
+        "bucketName": str,
+        "classificationTypeUpdate": "ClassificationTypeUpdateTypeDef",
+        "prefix": NotRequired[str],
+    },
+)
+
+S3ResourceTypeDef = TypedDict(
+    "S3ResourceTypeDef",
+    {
+        "bucketName": str,
+        "prefix": NotRequired[str],
+    },
+)
+
+UpdateS3ResourcesRequestRequestTypeDef = TypedDict(
+    "UpdateS3ResourcesRequestRequestTypeDef",
+    {
+        "s3ResourcesUpdate": Sequence["S3ResourceClassificationUpdateTypeDef"],
+        "memberAccountId": NotRequired[str],
+    },
+)
+
+UpdateS3ResourcesResultTypeDef = TypedDict(
+    "UpdateS3ResourcesResultTypeDef",
+    {
+        "failedS3Resources": List["FailedS3ResourceTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
