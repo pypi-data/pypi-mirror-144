8521723ef358fae5193d187c0f3e00dc95717368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:03 +0000

    Add package DE-LogEventos/0.0.7/DE_LogEventos-0.0.7.tar.gz

---
+++ packages/DE-LogEventos/DE_LogEventos-0.0.7.tar.gz/DE_LogEventos-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='DE_LogEventos',
+    version='0.0.7',
+    packages=[''],
+    url='https://github.com/almirjgomes/DE_LogEventos.git',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='LogEventos',
+    long_description="Incluida condição de quebra de linha para a impressao na tela 'screen' "
+)
