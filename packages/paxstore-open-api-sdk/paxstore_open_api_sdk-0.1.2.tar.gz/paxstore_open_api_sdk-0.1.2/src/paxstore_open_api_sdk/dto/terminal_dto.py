8237cec9b5546b62c7153a966d095e9c096aab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:30 +0000

    Add package paxstore-open-api-sdk/0.1.2/paxstore_open_api_sdk-0.1.2.tar.gz

---
+++ packages/paxstore-open-api-sdk/paxstore_open_api_sdk-0.1.2.tar.gz/paxstore_open_api_sdk-0.1.2/src/paxstore_open_api_sdk/dto/terminal_dto.py
@@ -0,0 +1,14 @@
+class TerminalDTO:
+    def __init__(self):
+        self.id = None
+        self.name = ""
+        self.tid = ""
+        self.serialNo = ""
+        self.status = ""
+        self.merchantName = ""
+        self.modelName = ""
+        self.resellerName = ""
+        self.location = ""
+        self.geoLocation = None
+        self.installedFirmware = None
+        self.installedApks = None
