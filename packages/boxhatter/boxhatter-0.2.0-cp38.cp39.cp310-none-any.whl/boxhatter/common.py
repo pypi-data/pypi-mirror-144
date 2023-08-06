a39483b0649363f16a9bf3f28aa737672c80ceb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:28 +0000

    Add package boxhatter/0.2.0/boxhatter-0.2.0-cp38.cp39.cp310-none-any.whl

---
+++ packages/boxhatter/boxhatter-0.2.0-cp38.cp39.cp310-none-any.whl/boxhatter/common.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import enum
+import typing
+
+from hat import json
+
+
+package_path: Path = Path(__file__).parent
+
+json_schema_repo: json.SchemaRepository = json.SchemaRepository(
+    json.json_schema_repo,
+    json.SchemaRepository.from_json(package_path / 'json_schema_repo.json'))
+
+
+class Order(enum.Enum):
+    ASC = 'ASC'
+    DESC = 'DESC'
+
+
+class Status(enum.Enum):
+    PENDING = 0
+    RUNNING = 1
+    SUCCESS = 2
+    FAILURE = 3
+
+
+class Commit(typing.NamedTuple):
+    repo: str
+    hash: str
+    change: int
+    status: Status
+    output: str
