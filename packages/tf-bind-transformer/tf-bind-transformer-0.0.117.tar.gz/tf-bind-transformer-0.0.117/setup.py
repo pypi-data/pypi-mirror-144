abef544b8d9867c85bb2c1165620ff82abc667d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:46 +0000

    Add package tf-bind-transformer/0.0.117/tf-bind-transformer-0.0.117.tar.gz

---
+++ packages/tf-bind-transformer/tf-bind-transformer-0.0.117.tar.gz/tf-bind-transformer-0.0.117/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'tf-bind-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.0.117',
+  license='MIT',
+  description = 'Transformer for Transcription Factor Binding',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/tf-bind-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanism',
+    'transformers',
+    'transcription factors',
+    'gene expression'
+  ],
+  install_requires=[
+    'bidirectional-cross-attention',
+    'biopython',
+    'click',
+    'einops>=0.3',
+    'enformer-pytorch>=0.4.4',
+    'fair-esm',
+    'logavgexp-pytorch',
+    'polars',
+    'python-dotenv',
+    'sentencepiece',
+    'torch>=1.6',
+    'transformers>=4.0',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
