b861044b1f42cb23794d8e70f7d56ae058a01c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:39 +0000

    Add package mylib-donghao/0.1.6/mylib_donghao-0.1.6.tar.gz

---
+++ packages/mylib-donghao/mylib_donghao-0.1.6.tar.gz/mylib_donghao-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+'''
+python setup.py sdist
+twine upload dist/* 
+1120191060
+dh20001108
+'''
+
+
+setup(
+    name = 'mylib_donghao',
+    version = '0.1.6',
+    keywords='mylib_donghao',
+    description = 'A pythono package by DONG HAO about machine learing and other math problems, which is just in test.\n'
+                  + 'all classes have print function if needed and print it is of no use',
+    license = 'MIT License',
+    url = 'https://github.com/',
+    author = 'Hao Dong',
+    author_email = '1440027762@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = ['numpy', 'pulp', 'scipy', 'matplotlib', 'torch', ],
+)
