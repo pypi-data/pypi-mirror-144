263a8f081e9c7ff52d39fae975d32a6d3ad281dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:38 +0000

    Add package abilian-core/0.11.24/abilian-core-0.11.24.tar.gz

---
+++ packages/abilian-core/abilian-core-0.11.24.tar.gz/abilian-core-0.11.24/src/abilian/services/sandbox/follow.py
@@ -0,0 +1,16 @@
+"""Social graph API.
+
+May or may not be needed, since the followers / followees notions are
+already implemented in the core model.
+"""
+
+
+from __future__ import annotations
+
+
+class FollowService:
+    def get_followers(self, user):
+        pass
+
+    def get_followees(self, user):
+        pass
