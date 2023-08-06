2399d0f37186d9b7f51bb375298a4adf3016c75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:33 +0000

    Add package prismacloud-cli/0.3.13/prismacloud-cli-0.3.13.tar.gz

---
+++ packages/prismacloud-cli/prismacloud-cli-0.3.13.tar.gz/prismacloud-cli-0.3.13/prismacloud/cwpp/cmd_users.py
@@ -0,0 +1,11 @@
+import click
+
+import prismacloud.api as pc_lib
+from prismacloud.cli import cli_output, pass_environment
+
+
+@click.command("users", short_help="[CWPP] Retrieves a list of all users")
+@pass_environment
+def cli(ctx):
+    result = pc_lib.get_endpoint("users")
+    cli_output(result)
