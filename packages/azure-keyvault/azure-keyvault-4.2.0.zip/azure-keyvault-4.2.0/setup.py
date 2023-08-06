25636dda1c734ffcfa211ef3e5d74bad0d608f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:17 +0000

    Add package azure-keyvault/4.2.0/azure-keyvault-4.2.0.zip

---
+++ packages/azure-keyvault/azure-keyvault-4.2.0.zip/azure-keyvault-4.2.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+from setuptools import setup
+from io import open
+
+with open("README.md", encoding="utf-8") as f:
+    README = f.read()
+with open("CHANGELOG.md", encoding="utf-8") as f:
+    CHANGELOG = f.read()
+
+setup(
+    name='azure-keyvault',
+    version='4.2.0',
+    description='Microsoft Azure Key Vault Client Libraries for Python',
+    long_description=README + "\n\n" + CHANGELOG,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    author='Microsoft Corporation',
+    author_email="azurekeyvault@microsoft.com",
+    url="https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/keyvault/azure-keyvault",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'azure-keyvault-certificates~=4.4',
+        'azure-keyvault-secrets~=4.4',
+        'azure-keyvault-keys~=4.5',
+    ],
+)
