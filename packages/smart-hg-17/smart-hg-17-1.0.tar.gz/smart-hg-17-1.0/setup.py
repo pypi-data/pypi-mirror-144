4e69d1c50bd01f9cf50b9b64f6431849e1684f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:28 +0000

    Add package smart-hg-17/1.0/smart-hg-17-1.0.tar.gz

---
+++ packages/smart-hg-17/smart-hg-17-1.0.tar.gz/smart-hg-17-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-17",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'opencv-python==4.0.1.24', 'matplotlib==2.1.2', 'ipython==5.8.0', 'ipywidgets==7.5.1'],
+)
