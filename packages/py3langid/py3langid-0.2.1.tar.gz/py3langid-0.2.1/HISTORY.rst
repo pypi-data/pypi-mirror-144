50724b4d2eb190c04e3cc4c5d49bdd3b7217f18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:06 +0000

    Add package py3langid/0.2.1/py3langid-0.2.1.tar.gz

---
+++ packages/py3langid/py3langid-0.2.1.tar.gz/py3langid-0.2.1/HISTORY.rst
@@ -0,0 +1,30 @@
+=======
+History
+=======
+
+
+0.2.0
+-----
+
+* Change Numpy data type for features (``uint32`` → ``uint16``)
+* Code cleaning
+
+
+0.1.2
+-----
+
+* Include data in non-wheel package versions
+
+
+0.1.1
+-----
+
+* Faster module loading
+* Extended tests and readme
+
+
+0.1.0
+-----
+
+* Fork re-packaged
+* Efficiency improvements in ``langid.py``
---
+++ packages/py3langid/py3langid-0.2.1.tar.gz/py3langid-0.2.1/tests/test_langid.py
@@ -0,0 +1,54 @@
+
+import subprocess
+import sys
+
+from io import StringIO
+from pathlib import Path
+
+import py3langid as langid
+from py3langid.langid import LanguageIdentifier, MODEL_FILE
+
+
+def test_langid():
+    '''Test if the language detection functions work'''
+    # basic classification
+    text = b'This text is in English.'
+    assert langid.classify(text)[0] == 'en'
+    assert langid.rank(text)[0][0] == 'en'
+    text = 'This text is in English.'
+    assert langid.classify(text)[0] == 'en'
+    assert langid.rank(text)[0][0] == 'en'
+    text = 'Test Unicode sur du texte en français'
+    assert langid.classify(text)[0] == 'fr'
+    assert langid.rank(text)[0][0] == 'fr'
+    # normalization of probabilities
+    identifier = LanguageIdentifier.from_pickled_model(MODEL_FILE, norm_probs=True)
+    _, prob = identifier.classify(text)
+    assert 0 <= prob <= 1
+    identifier = LanguageIdentifier.from_pickled_model(MODEL_FILE, norm_probs=False)
+    _, prob = identifier.classify(text)
+    assert prob < 0
+    # subset of target languages
+    identifier.set_languages(['de', 'en', 'fr'])
+    assert identifier.classify('这样不好')[0] != 'zh'
+
+
+
+def test_redirection():
+    '''Test if STDIN redirection works'''
+    thisdir = Path(__file__).parent
+    langid_path = str(thisdir.parent / 'py3langid' / 'langid.py')
+    readme_path = str(thisdir.parent / 'README.rst')
+    with open(readme_path, 'rb') as f:
+        readme = f.read()
+    result = subprocess.check_output(['python3', langid_path, '-n'], input=readme)
+    assert b'en' in result and b'1.0' in result
+
+
+
+def test_cli():
+    '''Test console scripts entry point'''
+    result = subprocess.check_output(['langid', '-n'], input=b'This should be enough text.')
+    assert b'en' in result and b'1.0' in result
+    result = subprocess.check_output(['langid', '-n', '-l', 'bg,en,uk'], input=b'This should be enough text.')
+    assert b'en' in result and b'1.0' in result
