257672b75eececd5dd93e35b15eb4d3f24d8ea7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:04 +0000

    Add package smart-discord-wow-armory-bot/1.0/smart-discord-wow-armory-bot-1.0.tar.gz

---
+++ packages/smart-discord-wow-armory-bot/smart-discord-wow-armory-bot-1.0.tar.gz/smart-discord-wow-armory-bot-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-discord-wow-armory-bot",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['discord.py==0.16.12', 'aiohttp==3.5.4'],
+)
