bd19089afa1e42ca68f642f40fef3ce9f3cfd0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:18 +0000

    Add package tokenizers/0.12.0/tokenizers-0.12.0.tar.gz

---
+++ packages/tokenizers/tokenizers-0.12.0.tar.gz/tokenizers-0.12.0/tokenizers-lib/src/decoders/wordpiece.rs
@@ -0,0 +1,64 @@
+use crate::tokenizer::{Decoder, Result};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Clone, Debug, Serialize)]
+/// The WordPiece decoder takes care of decoding a list of wordpiece tokens
+/// back into a readable string.
+#[serde(tag = "type")]
+#[non_exhaustive]
+pub struct WordPiece {
+    /// The prefix to be used for continuing subwords
+    pub prefix: String,
+    /// Whether to cleanup some tokenization artifacts (spaces before punctuation, ...)
+    pub cleanup: bool,
+}
+
+impl WordPiece {
+    pub fn new(prefix: String, cleanup: bool) -> Self {
+        Self { prefix, cleanup }
+    }
+}
+
+impl Default for WordPiece {
+    fn default() -> Self {
+        Self {
+            prefix: "##".to_owned(),
+            cleanup: true,
+        }
+    }
+}
+pub fn cleanup(dirty_input: &str) -> String {
+    dirty_input
+        .replace(" .", ".")
+        .replace(" ?", "?")
+        .replace(" !", "!")
+        .replace(" ,", ",")
+        .replace(" ' ", "'")
+        .replace(" n't", "n't")
+        .replace(" 'm", "'m")
+        .replace(" do not", " don't")
+        .replace(" 's", "'s")
+        .replace(" 've", "'ve")
+        .replace(" 're", "'re")
+}
+
+impl Decoder for WordPiece {
+    fn decode(&self, mut tokens: Vec<String>) -> Result<Vec<String>> {
+        tokens
+            .iter_mut()
+            .enumerate()
+            .map(|(i, token)| {
+                if token.starts_with(&self.prefix) {
+                    *token = token.replacen(&self.prefix, "", 1);
+                } else if i != 0 {
+                    *token = format!(" {}", token);
+                }
+                if self.cleanup {
+                    *token = cleanup(token);
+                }
+                Ok(token.to_string())
+            })
+            .collect::<Result<_>>()
+    }
+}
