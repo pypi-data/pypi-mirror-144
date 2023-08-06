91c8bf327ec0911995214ddb902e8298fabdd5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:42 +0000

    Add package pyvqnet/2.0.1/pyvqnet-2.0.1-cp38-cp38-win_amd64.whl

---
+++ packages/pyvqnet/pyvqnet-2.0.1-cp38-cp38-win_amd64.whl/pyvqnet/qnn/__init__.py
@@ -0,0 +1,3 @@
+from .measure import *
+from .quantumlayer import *
+from .template import *
---
+++ packages/pyvqnet/pyvqnet-2.0.1-cp38-cp38-win_amd64.whl/pyvqnet/qnn/utils/__init__.py
@@ -0,0 +1,4 @@
+from .compatible_layer import Compatiblelayer
+from .qiskitlayer import QiskitLayer
+from .qiskitlayer import QiskitLayerV2
+from .cirqlayer import CirqLayer,CirqLayerV2
