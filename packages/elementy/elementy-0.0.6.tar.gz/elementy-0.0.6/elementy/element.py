527217741a2a9fea19f61d73cc816a01592cd03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:59 +0000

    Add package elementy/0.0.6/elementy-0.0.6.tar.gz

---
+++ packages/elementy/elementy-0.0.6.tar.gz/elementy-0.0.6/elementy/element.py
@@ -0,0 +1,14 @@
+import elementy.mulliken as mulliken
+import elementy.radii as radii
+
+
+class Element():
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+        self.electronegativity_mulliken = \
+            mulliken.calculate_mulliken_electronegativity(self)
+
+        self.radius = radii.calculate_radius(self)
+        self.atomic_volume = radii.calculate_atomic_volume(self)
