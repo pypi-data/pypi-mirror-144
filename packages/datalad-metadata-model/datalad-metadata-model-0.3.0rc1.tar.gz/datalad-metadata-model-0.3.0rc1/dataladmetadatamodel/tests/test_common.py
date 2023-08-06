cf1a31cd7018d3e2eafba3d5f3a3b59d5eba64d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:15 +0000

    Add package datalad-metadata-model/0.3.0rc1/datalad-metadata-model-0.3.0rc1.tar.gz

---
+++ packages/datalad-metadata-model/datalad-metadata-model-0.3.0rc1.tar.gz/datalad-metadata-model-0.3.0rc1/dataladmetadatamodel/tests/test_common.py
@@ -0,0 +1,67 @@
+import subprocess
+import tempfile
+import unittest
+
+from dataladmetadatamodel.common import (
+    get_top_level_metadata_objects,
+    get_top_nodes_and_metadata_root_record,
+)
+from dataladmetadatamodel.metadatapath import MetadataPath
+from dataladmetadatamodel.metadatarootrecord import MetadataRootRecord
+from dataladmetadatamodel.tests.utils import get_uuid
+from dataladmetadatamodel.uuidset import UUIDSet
+from dataladmetadatamodel.versionlist import TreeVersionList
+
+uuid_0 = get_uuid(0)
+
+
+class TestTopLevelObjects(unittest.TestCase):
+
+    # This is not exactly a unit test since it does not
+    # isolate the units from the other components, but
+    # more a module test.
+    def test_top_level_functions(self):
+
+        with tempfile.TemporaryDirectory() as realm:
+            subprocess.run(["git", "init", realm])
+
+            tvl, uuid_set = get_top_level_metadata_objects("git", realm)
+            self.assertIsNone(tvl)
+            self.assertIsNone(uuid_set)
+
+            tvl, uuid_set, mrr = get_top_nodes_and_metadata_root_record(
+                mapper_family="git",
+                realm=realm,
+                dataset_id=uuid_0,
+                primary_data_version="v1",
+                prefix_path=MetadataPath(""),
+                dataset_tree_path=MetadataPath("a/b/c"),
+                auto_create=False
+            )
+            self.assertIsNone(tvl)
+            self.assertIsNone(uuid_set)
+            self.assertIsNone(mrr)
+
+            tvl, uuid_set, mrr = get_top_nodes_and_metadata_root_record(
+                mapper_family="git",
+                realm=realm,
+                dataset_id=uuid_0,
+                primary_data_version="v1",
+                prefix_path=MetadataPath(""),
+                dataset_tree_path=MetadataPath("a/b/c"),
+                auto_create=True
+            )
+            self.assertIsInstance(tvl, TreeVersionList)
+            self.assertIsInstance(uuid_set, UUIDSet)
+            self.assertIsInstance(mrr, MetadataRootRecord)
+
+            tvl.write_out(realm)
+            uuid_set.write_out(realm)
+
+            tvl, uuid_set = get_top_level_metadata_objects("git", realm)
+            self.assertIsInstance(tvl, TreeVersionList)
+            self.assertIsInstance(uuid_set, UUIDSet)
+
+
+if __name__ == '__main__':
+    unittest.main()
