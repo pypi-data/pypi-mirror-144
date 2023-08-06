cdc175632d84b49cc1d30556b4322c9a59261bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:45 +0000

    Add package mypy-boto3-lookoutvision/1.19.12.post1/mypy_boto3_lookoutvision-1.19.12.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-lookoutvision/mypy_boto3_lookoutvision-1.19.12.post1-py3-none-any.whl/mypy_boto3_lookoutvision/__init__.py
@@ -0,0 +1,36 @@
+"""
+Main interface for lookoutvision service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_lookoutvision import (
+        Client,
+        ListDatasetEntriesPaginator,
+        ListModelsPaginator,
+        ListProjectsPaginator,
+        LookoutforVisionClient,
+    )
+
+    session = Session()
+    client: LookoutforVisionClient = session.client("lookoutvision")
+
+    list_dataset_entries_paginator: ListDatasetEntriesPaginator = client.get_paginator("list_dataset_entries")
+    list_models_paginator: ListModelsPaginator = client.get_paginator("list_models")
+    list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+    ```
+"""
+from .client import LookoutforVisionClient
+from .paginator import ListDatasetEntriesPaginator, ListModelsPaginator, ListProjectsPaginator
+
+Client = LookoutforVisionClient
+
+
+__all__ = (
+    "Client",
+    "ListDatasetEntriesPaginator",
+    "ListModelsPaginator",
+    "ListProjectsPaginator",
+    "LookoutforVisionClient",
+)
