85981221d3998217ac36d6c80b9c67cc3611ec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:54 +0000

    Add package easy-scraper-py/1.0.0/easy_scraper_py-1.0.0-py3-none-any.whl

---
+++ packages/easy-scraper-py/easy_scraper_py-1.0.0-py3-none-any.whl/easy_scraper/entity/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional, Union
+
+
+@dataclass
+class Elem:
+    tag: str
+    attrs: dict[str, str]
+    children: list[Html]
+    parent: Optional[Html]
+
+    def __repr__(self) -> str:
+        return (
+            f"<{self.tag} {self.attrs}>{ ' '.join(repr(c) for c in self.children) }</>"
+        )
+
+
+@dataclass
+class PlainText:
+    data: str
+
+
+@dataclass
+class Pattern:
+    name: str
+
+
+class Empty:
+    pass
+
+
+Html = Union[Elem, PlainText, Pattern, Empty]
