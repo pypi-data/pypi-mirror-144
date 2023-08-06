bb46779c754be218bd423815643c23e31000ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:28 +0000

    Add package smart-hg-34/1.0/smart-hg-34-1.0.tar.gz

---
+++ packages/smart-hg-34/smart-hg-34-1.0.tar.gz/smart-hg-34-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-34",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['seaborn==0.8.1', 'scikit-learn==0.18.2', 'numpy==1.16.6', 'matplotlib==2.1.2'],
+)
