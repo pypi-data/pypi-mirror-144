6a02c31bad86984f47674b3f7a6ff93732ace6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:19 +0000

    Add package Deskapp/0.0.2/deskapp-0.0.2.tar.gz

---
+++ packages/Deskapp/deskapp-0.0.2.tar.gz/deskapp-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='deskapp',
+	version = "0.0.2",
+    author='Ruckusist',
+    author_email='ruckusist@outlook.com',
+    url='https://github.com/ruckusist/deskapp',
+    packages=setuptools.find_packages(),
+    description="An Open Source Terminal basesd Program Manager. Write and manage your own apps in this framework.",
+    long_description=open('README.txt').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    # install_requires=list(x.strip('\n') for x in open('requirements.txt', 'r').readlines()),
+    install_requires=['termcolor', 'jinja2'],
+    #########
+    entry_points = {
+        'console_scripts': ['deskapp=deskapp.cli:main']
+    },
+    ########
+    # this did not work as expected and should not be doing this
+    # right now.
+    # 
+    # include_package_data=True,
+    # package_data = {
+    #     '': ['deskapp/mods/templates/*.j2']
+    # }
+)
