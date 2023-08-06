f37ab72105609ba44b6e42877d13e8acc1795dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:18 +0000

    Add package HarvestNetApi/0.0.2/HarvestNetApi-0.0.2.tar.gz

---
+++ packages/HarvestNetApi/HarvestNetApi-0.0.2.tar.gz/HarvestNetApi-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+ name = 'HarvestNetApi',
+ version = '0.0.2',
+ description = 'HarvestNet-Api',
+ long_description = 'HarvestNet-Api',
+ license = 'MIT License',
+ author = 'waterlily',
+ author_email = 'b1929595403@gmail.com',
+ platforms = 'any',
+ python_requires = '>=3.7.*',
+ install_requires = [],
+ package_dir={'': 'HarvestNetApi'},
+ packages = find_packages('HarvestNetApi')
+ )
