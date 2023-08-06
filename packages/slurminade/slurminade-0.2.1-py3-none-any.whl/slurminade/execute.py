a5fe0eec247f752930614f401e84be47d6acbb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:33 +0000

    Add package slurminade/0.2.1/slurminade-0.2.1-py3-none-any.whl

---
+++ packages/slurminade/slurminade-0.2.1-py3-none-any.whl/slurminade/execute.py
@@ -0,0 +1,21 @@
+import sys
+
+from slurminade.guard import prevent_distribution
+from .function import SlurmFunction
+
+
+def main():
+    prevent_distribution()
+    batchfile = sys.argv[1]
+    funcid = sys.argv[2]
+    args = sys.argv[3]
+    with open(batchfile, "r") as f:
+        code = "".join(f.readlines())
+        global __name__
+        __name__ = None
+        exec(code, globals())
+    SlurmFunction.call(funcid, args)
+
+
+if __name__ == "__main__":
+    main()
