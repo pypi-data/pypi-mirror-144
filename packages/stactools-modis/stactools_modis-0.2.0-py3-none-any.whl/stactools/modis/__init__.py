713a6ba47645c0b77f58d20a0e5e39c65b8289e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:59 +0000

    Add package stactools-modis/0.2.0/stactools_modis-0.2.0-py3-none-any.whl

---
+++ packages/stactools-modis/stactools_modis-0.2.0-py3-none-any.whl/stactools/modis/__init__.py
@@ -0,0 +1,17 @@
+import stactools.core
+from stactools.cli.registry import Registry
+
+from stactools.modis.stac import create_item
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry: Registry) -> None:
+    from stactools.modis import commands
+    registry.register_subcommand(commands.create_modis_command)
+
+
+__version__ = "0.2.0"
+"""Library version"""
+
+__all__ = ["create_item"]
