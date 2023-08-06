2e0678951514cea9e7b10505e223400ee90a5ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:57 +0000

    Add package intelligen/0.9.0/intelligen-0.9.0.tar.gz

---
+++ packages/intelligen/intelligen-0.9.0.tar.gz/intelligen-0.9.0/intelligen/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for intelligen."""
+
+__author__ = """Diego Bouchet"""
+__email__ = 'diegobouchet88@gmail.com'
+__version__ = '0.9.0'
+
+
+from .constants import *
---
+++ packages/intelligen/intelligen-0.9.0.tar.gz/intelligen-0.9.0/intelligen/constants.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+golden = golden_ratio = (1 + np.sqrt(5))/2
+igolden = inverse_golden_ratio = (1 - np.sqrt(5))/2
+
+G = 6.67430e-11
+g = 9.807
