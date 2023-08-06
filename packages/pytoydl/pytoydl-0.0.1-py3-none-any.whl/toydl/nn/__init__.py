8d247889e3520a05c8e11d95a07c5ac792c8cf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:22 +0000

    Add package pytoydl/0.0.1/pytoydl-0.0.1-py3-none-any.whl

---
+++ packages/pytoydl/pytoydl-0.0.1-py3-none-any.whl/toydl/nn/__init__.py
@@ -0,0 +1,19 @@
+'''initialize'''
+from .linear import (
+    Linear
+)
+from .module import (
+    Module
+)
+from .convolution import (
+    Conv2d
+)
+from .sequential import (
+    Sequential
+)
+from .criterion import (
+    MSELoss, CrossEntropy
+)
+from .activation import (
+    Softmax
+)
