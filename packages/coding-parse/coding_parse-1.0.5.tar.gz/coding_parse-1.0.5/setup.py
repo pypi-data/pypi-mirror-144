8bbc95d69243774113f485db45215b409d4396d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:03 +0000

    Add package coding-parse/1.0.5/coding_parse-1.0.5.tar.gz

---
+++ packages/coding-parse/coding_parse-1.0.5.tar.gz/coding_parse-1.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+long_description = "Package that takes as command-line input a file path specifying a source text and a search term and return the words in lines that contain the substring"
+
+setup(
+    name="coding_parse",
+    version="1.0.5",
+    author="Laura Pérez Vera",
+    author_email="lpvera22@gmail.com",
+    # url="",
+    description="Finding substrings in the lines of a file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["cpm_file = coding_parse.main:main"]},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    keywords="search keyword substring research substring",
+    install_requires=requirements,
+    zip_safe=False,
+)
