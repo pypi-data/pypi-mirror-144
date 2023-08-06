d14517bd97e89b3124d569b83aba24e0e03611c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:05 +0000

    Add package smart-django-web3-auth/1.0/smart-django-web3-auth-1.0.tar.gz

---
+++ packages/smart-django-web3-auth/smart-django-web3-auth-1.0.tar.gz/smart-django-web3-auth-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-django-web3-auth",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['Django>=2.0', 'ethereum>=2.3.0'],
+)
