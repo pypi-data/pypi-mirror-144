c92554956f8461c429c95b8141006bfae7bf24d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:18 +0000

    Add package types-aiobotocore-kinesis-video-media/2.2.0.post2/types-aiobotocore-kinesis-video-media-2.2.0.post2.tar.gz

---
+++ packages/types-aiobotocore-kinesis-video-media/types-aiobotocore-kinesis-video-media-2.2.0.post2.tar.gz/types-aiobotocore-kinesis-video-media-2.2.0.post2/types_aiobotocore_kinesis_video_media/type_defs.py
@@ -0,0 +1,73 @@
+"""
+Type annotations for kinesis-video-media service type definitions.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_kinesis_video_media/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_kinesis_video_media.type_defs import GetMediaInputRequestTypeDef
+
+    data: GetMediaInputRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, Union
+
+from botocore.response import StreamingBody
+from typing_extensions import NotRequired
+
+from .literals import StartSelectorTypeType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "GetMediaInputRequestTypeDef",
+    "GetMediaOutputTypeDef",
+    "ResponseMetadataTypeDef",
+    "StartSelectorTypeDef",
+)
+
+GetMediaInputRequestTypeDef = TypedDict(
+    "GetMediaInputRequestTypeDef",
+    {
+        "StartSelector": "StartSelectorTypeDef",
+        "StreamName": NotRequired[str],
+        "StreamARN": NotRequired[str],
+    },
+)
+
+GetMediaOutputTypeDef = TypedDict(
+    "GetMediaOutputTypeDef",
+    {
+        "ContentType": str,
+        "Payload": StreamingBody,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+StartSelectorTypeDef = TypedDict(
+    "StartSelectorTypeDef",
+    {
+        "StartSelectorType": StartSelectorTypeType,
+        "AfterFragmentNumber": NotRequired[str],
+        "StartTimestamp": NotRequired[Union[datetime, str]],
+        "ContinuationToken": NotRequired[str],
+    },
+)
