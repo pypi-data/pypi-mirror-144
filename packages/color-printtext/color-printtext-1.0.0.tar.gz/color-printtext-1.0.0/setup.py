fdc5bdee89c91a0f11ea383aa2c34676f7caded8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:31 +0000

    Add package color-printtext/1.0.0/color-printtext-1.0.0.tar.gz

---
+++ packages/color-printtext/color-printtext-1.0.0.tar.gz/color-printtext-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="color-printtext",
+    version="1.0.0",
+    author="gx1285",
+    author_email="runay2342@gmail.com",
+    description="PrintColor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gx1285/Print-Color",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
