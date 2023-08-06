e05732786638389f5ab6e2ff481c024ec93bae56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:06 +0000

    Add package smart-xontrib-readable-traceback/1.0/smart-xontrib-readable-traceback-1.0.tar.gz

---
+++ packages/smart-xontrib-readable-traceback/smart-xontrib-readable-traceback-1.0.tar.gz/smart-xontrib-readable-traceback-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-xontrib-readable-traceback",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['colorama>=0.3.7', 'backtrace'],
+)
