55615e580baa98ee839cb6410e6c382887dac8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:56 +0000

    Add package HappyRandomBirthday/0.1/HappyRandomBirthday-0.1.tar.gz

---
+++ packages/HappyRandomBirthday/HappyRandomBirthday-0.1.tar.gz/HappyRandomBirthday-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HappyRandomBirthday',
+    version='0.1',
+    packages=find_packages(),
+    author="Pola y Raza",
+    description="Virtual Random Birthday cards",
+    long_description="Don't know how to say Happy birthday to a friend ? Tired of saying the same thing every single year? No more! With this module you will always know how to cheer up your friend for their special day."
+)
