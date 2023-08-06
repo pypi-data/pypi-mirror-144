1aa31c3cbdce7f63597e996190817ab0936ab74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:05 +0000

    Add package simple-pass-manager/0.0.1/simple_pass_manager-0.0.1.tar.gz

---
+++ packages/simple-pass-manager/simple_pass_manager-0.0.1.tar.gz/simple_pass_manager-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="simple_pass_manager",
+    version="0.0.1",
+    author="Zhmishenko Valery Albertovich",
+    author_email="my_real_email@zhopa.com",
+    description="A small description like my d",
+    long_description="my supa mega fancy lib big description like my d",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["setuptools>=42", "cryptography>=36.0.2"],
+    python_requires=">=3.9.7",
+)
