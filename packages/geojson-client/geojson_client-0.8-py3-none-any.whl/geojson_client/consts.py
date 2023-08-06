598deb08d40e7eceabc77f021fdd811224867583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package geojson-client/0.8/geojson_client-0.8-py3-none-any.whl

---
+++ packages/geojson-client/geojson_client-0.8-py3-none-any.whl/geojson_client/consts.py
@@ -0,0 +1,26 @@
+"""
+Constants.
+
+Constants for feeds and feed entries.
+"""
+
+ATTR_ALERT = "alert"
+ATTR_ATTRIBUTION = "attribution"
+ATTR_CATEGORY = "category"
+ATTR_DESCRIPTION = "description"
+ATTR_ID = "id"
+ATTR_GUID = "guid"
+ATTR_MAG = "mag"
+ATTR_PLACE = "place"
+ATTR_PUB_DATE = "pubDate"
+ATTR_STATUS = "status"
+ATTR_TIME = "time"
+ATTR_TITLE = "title"
+ATTR_TYPE = "type"
+ATTR_UPDATED = "updated"
+
+FILTER_CATEGORIES = "categories"
+FILTER_MINIMUM_MAGNITUDE = "minimum_magnitude"
+FILTER_RADIUS = "radius"
+
+HTTP_ACCEPT_ENCODING_HEADER = {"Accept-Encoding": "deflate, gzip"}
