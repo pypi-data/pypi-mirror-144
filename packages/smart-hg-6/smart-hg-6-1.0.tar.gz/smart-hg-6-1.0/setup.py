0cfa6462a21622f04948d0cb2cc37b86a8ed7e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-6/1.0/smart-hg-6-1.0.tar.gz

---
+++ packages/smart-hg-6/smart-hg-6-1.0.tar.gz/smart-hg-6-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-6",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'pycocotools==2.0.2'],
+)
