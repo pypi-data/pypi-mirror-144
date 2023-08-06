3f137b1c813cf6b43a937e0ebc430d20fef4363c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:56 +0000

    Add package global-chem-extensions/0.6.9/global_chem_extensions-0.6.9.tar.gz

---
+++ packages/global-chem-extensions/global_chem_extensions-0.6.9.tar.gz/global_chem_extensions-0.6.9/global_chem_extensions/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# Intialization of the package
+#
+# ----------------------------
+
+from global_chem_extensions.global_chem_extensions.forcefields.cgenff.cgenff_molecule import CGenFFMolecule
+
+# Base Package
+
+from global_chem_extensions.global_chem_extensions import GlobalChemExtensions
+
+__all__ = ['GlobalChemExtensions', 'CGenFFMolecule']
+
+__name__ = 'GlobalChemExtensions'
