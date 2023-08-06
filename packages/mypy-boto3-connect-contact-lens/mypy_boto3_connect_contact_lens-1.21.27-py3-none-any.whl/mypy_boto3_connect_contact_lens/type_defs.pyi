9bb61cc8e61a5214dadf59cb1e7d97884d955f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package mypy-boto3-connect-contact-lens/1.21.27/mypy_boto3_connect_contact_lens-1.21.27-py3-none-any.whl

---
+++ packages/mypy-boto3-connect-contact-lens/mypy_boto3_connect_contact_lens-1.21.27-py3-none-any.whl/mypy_boto3_connect_contact_lens/type_defs.pyi
@@ -0,0 +1,127 @@
+"""
+Type annotations for connect-contact-lens service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_connect_contact_lens/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_connect_contact_lens.type_defs import CategoriesTypeDef
+
+    data: CategoriesTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List
+
+from typing_extensions import NotRequired
+
+from .literals import SentimentValueType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "CategoriesTypeDef",
+    "CategoryDetailsTypeDef",
+    "CharacterOffsetsTypeDef",
+    "IssueDetectedTypeDef",
+    "ListRealtimeContactAnalysisSegmentsRequestRequestTypeDef",
+    "ListRealtimeContactAnalysisSegmentsResponseTypeDef",
+    "PointOfInterestTypeDef",
+    "RealtimeContactAnalysisSegmentTypeDef",
+    "ResponseMetadataTypeDef",
+    "TranscriptTypeDef",
+)
+
+CategoriesTypeDef = TypedDict(
+    "CategoriesTypeDef",
+    {
+        "MatchedCategories": List[str],
+        "MatchedDetails": Dict[str, "CategoryDetailsTypeDef"],
+    },
+)
+
+CategoryDetailsTypeDef = TypedDict(
+    "CategoryDetailsTypeDef",
+    {
+        "PointsOfInterest": List["PointOfInterestTypeDef"],
+    },
+)
+
+CharacterOffsetsTypeDef = TypedDict(
+    "CharacterOffsetsTypeDef",
+    {
+        "BeginOffsetChar": int,
+        "EndOffsetChar": int,
+    },
+)
+
+IssueDetectedTypeDef = TypedDict(
+    "IssueDetectedTypeDef",
+    {
+        "CharacterOffsets": "CharacterOffsetsTypeDef",
+    },
+)
+
+ListRealtimeContactAnalysisSegmentsRequestRequestTypeDef = TypedDict(
+    "ListRealtimeContactAnalysisSegmentsRequestRequestTypeDef",
+    {
+        "InstanceId": str,
+        "ContactId": str,
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+    },
+)
+
+ListRealtimeContactAnalysisSegmentsResponseTypeDef = TypedDict(
+    "ListRealtimeContactAnalysisSegmentsResponseTypeDef",
+    {
+        "Segments": List["RealtimeContactAnalysisSegmentTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PointOfInterestTypeDef = TypedDict(
+    "PointOfInterestTypeDef",
+    {
+        "BeginOffsetMillis": int,
+        "EndOffsetMillis": int,
+    },
+)
+
+RealtimeContactAnalysisSegmentTypeDef = TypedDict(
+    "RealtimeContactAnalysisSegmentTypeDef",
+    {
+        "Transcript": NotRequired["TranscriptTypeDef"],
+        "Categories": NotRequired["CategoriesTypeDef"],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+TranscriptTypeDef = TypedDict(
+    "TranscriptTypeDef",
+    {
+        "Id": str,
+        "ParticipantId": str,
+        "ParticipantRole": str,
+        "Content": str,
+        "BeginOffsetMillis": int,
+        "EndOffsetMillis": int,
+        "Sentiment": SentimentValueType,
+        "IssuesDetected": NotRequired[List["IssueDetectedTypeDef"]],
+    },
+)
