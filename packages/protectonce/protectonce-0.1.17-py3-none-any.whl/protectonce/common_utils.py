1cfbc262b73ebead058ae2a9564ea3297cd378d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:37 +0000

    Add package protectonce/0.1.17/protectonce-0.1.17-py3-none-any.whl

---
+++ packages/protectonce/protectonce-0.1.17-py3-none-any.whl/protectonce/common_utils.py
@@ -0,0 +1,7 @@
+
+def is_action_blocked(data):
+    try:
+        return data['result'] and isinstance(data['result'], dict) and data['result'].get('action', '') in ['block', 'abort']
+    except Exception as e:
+        print(
+            f'[PROTECTONCE_ERROR] common_utils.is_action_blocked failed with error {e}')
