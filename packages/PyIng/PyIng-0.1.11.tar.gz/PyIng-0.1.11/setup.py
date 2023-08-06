e5b91ad54a6922b6ba5e7e1756c41f3f451d76c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:02 +0000

    Add package PyIng/0.1.11/PyIng-0.1.11.tar.gz

---
+++ packages/PyIng/PyIng-0.1.11.tar.gz/PyIng-0.1.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+import sys
+import os
+
+# circleci.py version
+VERSION = "0.1.11"
+
+def readme():
+    """print long description"""
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='PyIng',
+    packages=find_packages(include=['PyIng']),
+    version=VERSION,
+    description='Parses ingredient names into Name, Unit and Quantity',
+    author='Will White',
+    license='MIT',
+    install_requires=["tflite-runtime>=2.5", "numpy"],
+    url='https://github.com/whitew1994WW/PyIng',
+    test_suite='tests',
+    python_requires='>=3.7',
+    long_description=readme(),
+    long_description_content_type='text/markdown'
+)
