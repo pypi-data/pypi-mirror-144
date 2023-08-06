d0977d05aa6004aa34cdb7d8bf53be63b57f5a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:42 +0000

    Add package zarena/0.2.2/zarena-0.2.2.tar.gz

---
+++ packages/zarena/zarena-0.2.2.tar.gz/zarena-0.2.2/src/lib.rs
@@ -0,0 +1,49 @@
+#[macro_use]
+extern crate corrosion;
+
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+mod blackjack;
+mod checkers;
+mod chess;
+mod gato;
+mod poker;
+
+// GYMS
+#[cfg(feature = "python")]
+use blackjack::python::BlackjackEngine;
+#[cfg(feature = "python")]
+use checkers::python::CheckersEngine;
+#[cfg(feature = "python")]
+use chess::python::ChessEngine;
+#[cfg(feature = "python")]
+use gato::python::TictactoeEngine;
+#[cfg(feature = "python")]
+use poker::python::PokerEngine;
+
+// PYTHON MODULE
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+#[cfg(feature = "python")]
+#[pymodule]
+fn zarena(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<TictactoeEngine>()?;
+    m.add_class::<BlackjackEngine>()?;
+    m.add_class::<PokerEngine>()?;
+    m.add_class::<ChessEngine>()?;
+    m.add_class::<CheckersEngine>()?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::poker::Poker;
+    #[test]
+    fn poker_works() {
+        let mut poker = Poker::new(vec![100_000, 200_000], true);
+        print!("{:?}", poker.reset());
+        print!("{:?}", poker.step(0, true));
+    }
+}
