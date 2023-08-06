31b0f08881f1a3a49752b4417eda099a2c435e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:51 +0000

    Add package dimfred/0.3.0/dimfred-0.3.0-py3-none-any.whl

---
+++ packages/dimfred/dimfred-0.3.0-py3-none-any.whl/dimfred/__init__.py
@@ -0,0 +1,10 @@
+from .worker import Worker, Queue
+from .stopwatch import Stopwatch
+from .config import Config
+from .pipe import fpipe, pipe
+from . import fastapi_utils
+
+from tabulate import tabulate
+from easydict import EasyDict as edict
+
+
