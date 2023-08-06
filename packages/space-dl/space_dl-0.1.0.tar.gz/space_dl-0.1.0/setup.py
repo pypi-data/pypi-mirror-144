68e8ab3cc381e5b3de6c18f6e7cd9a01f93401bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:21 +0000

    Add package space-dl/0.1.0/space_dl-0.1.0.tar.gz

---
+++ packages/space-dl/space_dl-0.1.0.tar.gz/space_dl-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="space_dl",
+    version="0.1.0",
+    author="Ariyan Eghbal",
+    author_email="ariyan.eghbal@gmail.com",
+    description="Download Twitter Space",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RYNEQ/space_dl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['ffmpeg-python', 'm3u8', 'pymediainfo', 'requests']
+)
