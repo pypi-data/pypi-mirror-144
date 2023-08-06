13be44f2c2d802bdbf8db60f23c25dc3bdd76671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:17 +0000

    Add package Binance-Data-Getter/0.5/Binance_Data_Getter-0.5.tar.gz

---
+++ packages/Binance-Data-Getter/Binance_Data_Getter-0.5.tar.gz/Binance_Data_Getter-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='Binance_Data_Getter',
+      version='0.5',
+      description='Used to extract and process data from Binance',
+      url='https://github.com/mario-likes-camels/Binance_Data_Getter',
+      author='Patrick',
+      author_email='hi_in@hotmail.com',
+      license='MIT',
+      packages=['Binance_Data_Getter'],
+      zip_safe=False)
+      
+     
