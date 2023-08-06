29055a459655c7979fd2c22aa5a90a98f5b976cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:35 +0000

    Add package stripend/0.1.2/stripend-0.1.2-py3-none-any.whl

---
+++ packages/stripend/stripend-0.1.2-py3-none-any.whl/stripend/__init__.py
@@ -0,0 +1,9 @@
+from .stripend import *
+
+__title__ = "stripend"
+__summary__ = "A Python module that includes utility methods for making your code shorter, more flexible, and smarter."
+__uri__ = "https://github.com/TrueMyst/stripend"
+__version__ = "0.1.2"
+__author__ = "TrueMyst"
+__license__ = "MIT License"
+__copyright__ = f"Copyright 2022 {__author__}"
