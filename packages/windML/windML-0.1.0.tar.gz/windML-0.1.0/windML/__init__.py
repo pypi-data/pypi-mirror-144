44c830644942afa894a6cde0b41d7c95467c719a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:13 +0000

    Add package windML/0.1.0/windML-0.1.0.tar.gz

---
+++ packages/windML/windML-0.1.0.tar.gz/windML-0.1.0/windML/__init__.py
@@ -0,0 +1,6 @@
+from windML.oneshot.models.extreme_learning_machine import ELMClassifier
+from windML.oneshot.modelfree.matrix_factorisation import MFRClassifier
+from windML.fewshot.direct_feedback_alignment.dfa_classifier import DFAClassifier
+from windML.fewshot.recurrent_neural_network.model import charRNN
+
+__version__ = "0.1.0"
