09799dcaa868ca71489990b2455485156be232e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:00 +0000

    Add package qrogue/0.3.2.5.1/qrogue-0.3.2.5.1.tar.gz

---
+++ packages/qrogue/qrogue-0.3.2.5.1.tar.gz/qrogue-0.3.2.5.1/qrogue/util/achievements.py
@@ -0,0 +1,120 @@
+import enum
+from typing import List
+
+FinishedTutorial = "CompletedTutorial"
+EnteredPauseMenu = "EnteredPauseMenu"
+FirstDoorUnlocked = "UnlockedDoor"
+CompletedExpedition = "CompletedExpedition"
+
+
+class AchievementType(enum.Enum):
+    World = 0
+    Level = 1
+    Gate = 2
+    Secret = 3  # is not shown until unlocked (therefore also needs to be done in one go, i.e. done_score = 1)
+    Tutorial = 4
+    Event = 5    # in-level event
+    Expedition = 6
+
+
+class Achievement:
+    __DATA_SEPARATOR = ">q<"
+
+    @staticmethod
+    def from_string(text: str) -> "Achievement":
+        data = text.split(Achievement.__DATA_SEPARATOR)
+        if len(data) == 5:
+            name = data[0]
+            atype = AchievementType(int(data[1]))
+            score = float(data[2])
+            done_score = float(data[3])
+            return Achievement(name, atype, score, done_score)
+
+    # todo add unlock-date?
+    def __init__(self, name: str, atype: AchievementType, score: float, done_score: float):
+        self.__name = name
+        self.__type = atype
+        self.__score = score
+        self.__done_score = done_score
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def type(self) -> AchievementType:
+        return self.__type
+
+    @property
+    def score(self) -> float:
+        return self.__score
+
+    @property
+    def done_score(self) -> float:
+        return self.__done_score
+
+    def is_done(self) -> bool:
+        return self.score >= self.done_score
+
+    def add_score(self, score: float):
+        if score > 0:
+            self.__score += score
+
+    def to_string(self) -> str:
+        text = f"{self.name}{Achievement.__DATA_SEPARATOR}"
+        text += f"{self.type.value}{Achievement.__DATA_SEPARATOR}"
+        text += f"{self.score}{Achievement.__DATA_SEPARATOR}"
+        text += f"{self.done_score}{Achievement.__DATA_SEPARATOR}"
+        return text
+
+
+class AchievementManager:
+    def __init__(self, achievements: List[Achievement]):
+        self.__storage = {}
+        self.__temp_level_storage = {}
+
+        for achievement in achievements:
+            self.__storage[achievement.name] = achievement
+
+    def check_achievement(self, name: str) -> bool:
+        if name in self.__temp_level_storage:
+            achievement = self.__temp_level_storage[name]
+            return achievement.is_done()
+        elif name in self.__storage:
+            achievement = self.__storage[name]
+            return achievement.is_done()
+        return False
+
+    def reset_level_events(self):
+        self.__temp_level_storage.clear()
+
+    def add_to_achievement(self, name: str, score: float):
+        if name in self.__storage:
+            self.__storage[name].add_score(score)
+
+    def trigger_level_event(self, name: str, score: float = 1):
+        if name in self.__temp_level_storage:
+            self.__temp_level_storage[name].add_score(score)
+        else:
+            self.__temp_level_storage[name] = Achievement(name, AchievementType.Event, score, 1)
+
+    def finished_tutorial(self, tutorial: str):
+        self.__storage[tutorial] = Achievement(tutorial, AchievementType.Tutorial, 1, 1)
+
+    def finished_level(self, level: str):
+        self.__storage[level] = Achievement(level, AchievementType.Level, 1, 1)
+        if not self.check_achievement(FinishedTutorial):
+            self.finished_tutorial(FinishedTutorial)
+
+    def finished_world(self, world: str):
+        self.__storage[world] = Achievement(world, AchievementType.World, 1, 1)
+
+    def uncovered_secret(self, name: str):
+        if name not in self.__storage:
+            self.__storage[name] = Achievement(name, AchievementType.Secret, 1, 1)
+
+    def to_string(self) -> str:
+        text = ""
+        for value in self.__storage.values():
+            text += f"{value.to_string()}\n"
+        return text
---
+++ packages/qrogue/qrogue-0.3.2.5.1.tar.gz/qrogue-0.3.2.5.1/setup.py
@@ -0,0 +1,59 @@
+import os
+from setuptools import setup
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Readme.md')
+with open(README_PATH) as readme_file:
+	README = readme_file.read()
+
+setup(
+	name='qrogue',  # Name of package folder
+	packages=[
+		'qrogue',
+		'qrogue.util',
+		'qrogue.game',
+		'qrogue.game.logic',
+		'qrogue.game.logic.collectibles',
+		'qrogue.game.logic.actors',
+		'qrogue.game.logic.actors.controllables',
+		'qrogue.game.logic.actors.puzzles',
+		'qrogue.game.world',
+		'qrogue.game.world.navigation',
+		'qrogue.game.world.tiles',
+		'qrogue.game.world.map',
+		'qrogue.game.world.dungeon_generator',
+		'qrogue.game.world.dungeon_generator.dungeon_parser',
+		'qrogue.game.world.dungeon_generator.world_parser',
+		'qrogue.graphics',
+		'qrogue.graphics.rendering',
+		'qrogue.graphics.popups',
+		'qrogue.graphics.widgets',
+		'qrogue.management',
+	],
+	package_data={"data": ["qrogue/data"]},
+	include_package_data=True,
+	version='0.3.2.5.1',
+	license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+	description='Qrogue is a modernized Quantum Computing take of the classical game Rogue.',
+	long_description=README,
+	long_description_content_type="text/markdown",
+	author='Michael Artner',
+	author_email='michael.artner@jku.at',
+	url='https://github.com/7Magic7Mike7/Qrogue',
+	download_url='https://github.com/7Magic7Mike7/Qrogue/releases/tag/0.3.2.5',  # link to Release on GitHub
+	keywords=['Quantum Computing', 'Gamification', 'Video Game', 'PyCUI'],
+	install_requires=[
+		'py-cui==0.1.4',
+		'qiskit==0.34.2',
+		'antlr4-python3-runtime==4.9.3',
+	],
+	classifiers=[
+		'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+		'Intended Audience :: Education',
+		'Intended Audience :: Science/Research',
+		'Topic :: Games/Entertainment :: Puzzle Games',
+		'Programming Language :: Python :: 3.8',
+	],
+	entry_points={
+		'console_scripts': ['qrogue=qrogue.qrogue:start_game'],
+	}
+)
