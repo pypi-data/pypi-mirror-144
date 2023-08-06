deabdf2de5a70887cba97a08546651817162c598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:26 +0000

    Add package smart-hg-24/1.0/smart-hg-24-1.0.tar.gz

---
+++ packages/smart-hg-24/smart-hg-24-1.0.tar.gz/smart-hg-24-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-24",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['twisted==21.2.0', 'scrapy==2.5.0'],
+)
