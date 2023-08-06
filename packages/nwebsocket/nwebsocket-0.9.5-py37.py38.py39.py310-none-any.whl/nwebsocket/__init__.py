86e78bfa15d41d33155ba08d70745e3817e8a294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:23 +0000

    Add package nwebsocket/0.9.5/nwebsocket-0.9.5-py37.py38.py39.py310-none-any.whl

---
+++ packages/nwebsocket/nwebsocket-0.9.5-py37.py38.py39.py310-none-any.whl/nwebsocket/__init__.py
@@ -0,0 +1,124 @@
+"""
+nwebsocket
+~~~~~~~~~~
+
+WebSocket client without async.
+"""
+
+import time
+import curio
+import threading
+
+from curio import Queue, UniversalQueue
+
+from wsproto.events import (
+    AcceptConnection,
+    CloseConnection,
+    RejectConnection,
+)
+
+from .events import ws_socket_manage
+
+__version__ = '0.9.5'
+
+
+class WebSocket(object):
+    """
+    WebSocket for Python without async
+    """
+
+    CONNECTING = 0
+    OPEN = 1
+    CLOSING = 2
+    CLOSED = 3
+
+    def __init__(self, uri):
+        self.uri = uri
+        self.readyState = WebSocket.CONNECTING
+
+        self._send = None
+
+        # create event queues
+        self.rx_queue = Queue()
+        self.tx_queue = UniversalQueue()
+
+        # buffer chunks
+        self.buffer = None
+
+        # create detachable async context
+        def detach(f):
+            def wrap(*args):
+                thr = threading.Thread(target=f, args=args)
+                thr.daemon = True
+                thr.start()
+                return thr
+
+            return wrap
+
+        async_detached = detach(curio.run)
+
+        # create thread
+        self.manage = ws_socket_manage(
+            self.rx_queue,
+            self.tx_queue,
+            self.uri,
+            lambda m, s: self.handleRXEvent(m, s),
+        )
+
+        self.task = async_detached(self.manage)
+
+    def onopen(self):
+        pass
+
+    def onclose(self):
+        pass
+
+    def onerror(self, error):
+        pass
+
+    def onmessage(self, message):
+        pass
+
+    def send(self, message):
+        if(self._send is None):
+            raise RuntimeError('WebSocket not connected')
+        else:
+            self._send(message)
+            time.sleep(1e-5)
+
+    def close(self):
+        self.tx_queue.put(CloseConnection(0))
+        self.tx_queue.put(None)
+        while(self.readyState != WebSocket.CLOSED):
+            time.sleep(1e-5)
+
+    def handleRXEvent(self, message, send):
+        self._send = send
+
+        # fire onopen
+        if isinstance(message, AcceptConnection):
+            self.readyState = WebSocket.OPEN
+            self.onopen()
+
+        # errored out
+        elif isinstance(message, RejectConnection):
+            self.readyState = WebSocket.CLOSED
+            self.onerror(message)
+
+        # closed out
+        elif isinstance(message, CloseConnection):
+            self.readyState = WebSocket.CLOSED
+            self.onclose()
+
+        # message
+        elif isinstance(message, tuple):
+            chunk, complete = message
+
+            if self.buffer is None:
+                self.buffer = chunk
+            else:
+                self.buffer += chunk
+
+            if complete:
+                self.onmessage(self.buffer)
+                self.buffer = None
