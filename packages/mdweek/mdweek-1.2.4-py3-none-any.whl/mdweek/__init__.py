fe4edf206b8a71b92e11d99f72832bc0e272a26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:06 +0000

    Add package mdweek/1.2.4/mdweek-1.2.4-py3-none-any.whl

---
+++ packages/mdweek/mdweek-1.2.4-py3-none-any.whl/mdweek/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.2.4"
+
+
+from .week import (
+    Week as Week,
+    setup_week_config as setup_week_config,
+    get_week_config as get_week_config,
+    WeekConfig as WeekConfig
+)
