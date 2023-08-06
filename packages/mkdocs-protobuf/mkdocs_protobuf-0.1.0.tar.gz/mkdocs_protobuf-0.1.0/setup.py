7644309f86a16f277c45c135e26a7be9f0a54620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:03 +0000

    Add package mkdocs-protobuf/0.1.0/mkdocs_protobuf-0.1.0.tar.gz

---
+++ packages/mkdocs-protobuf/mkdocs_protobuf-0.1.0.tar.gz/mkdocs_protobuf-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# flake8: noqa
+"""The setup script."""
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requires = ["mkdocs"]
+
+setup(
+    author="Ryan Murray",
+    author_email="rymurr@gmail.com",
+    url="https://github.com/rymurr/mkdocs-protobuf",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires=">=3.6",
+    description="Mkdocs plugin to render protobuf messages",
+    entry_points={
+        "mkdocs.plugins": [
+            "mkdocs_protobuf=mkdocs_protobuf.plugin:ProtobufDisplay",
+        ],
+    },
+    install_requires=requires,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="mkdocs_protobuf,mkdocs,protobuf",
+    name="mkdocs_protobuf",
+    packages=find_packages(include=["mkdocs_protobuf", "pymkdocs_protobufnessie.*"]),
+    test_suite="tests",
+    version="0.1.0",
+    zip_safe=False,
+)
