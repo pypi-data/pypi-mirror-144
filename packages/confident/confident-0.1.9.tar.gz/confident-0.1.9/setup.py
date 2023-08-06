a92640d4373bcdf26feb2a754326d33339450045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:11 +0000

    Add package confident/0.1.9/confident-0.1.9.tar.gz

---
+++ packages/confident/confident-0.1.9.tar.gz/confident-0.1.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+      name='confident',
+      version='0.1.9',
+      description='Loading configurations from multiple sources into a data model.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/limonyellow/confident',
+      author='limonyellow',
+      author_email='lemon@example.com',
+      license='MIT',
+      zip_safe=False,
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      packages=['confident'],
+      install_requires=[
+            'pydantic',
+            'pyyaml',
+            'python-dotenv',
+      ],
+      python_requires=">=3.6",
+)
