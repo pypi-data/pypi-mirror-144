ed88e02bfb57999489473a6869efe29eadf660e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:52 +0000

    Add package alphaconfig/0.0.3/alphaconfig-0.0.3-py3-none-any.whl

---
+++ packages/alphaconfig/alphaconfig-0.0.3-py3-none-any.whl/alphaconfig/__init__.py
@@ -0,0 +1,9 @@
+
+from .AlphaConfig import AlphaConfig, Configs
+
+__title__ = "AlphaConfig"
+__version__ = "0.0.3"
+__author__ = "Yiqun Chen"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Yiqun Chen"
+__all__ = ["AlphaConfig", "Configs"]
