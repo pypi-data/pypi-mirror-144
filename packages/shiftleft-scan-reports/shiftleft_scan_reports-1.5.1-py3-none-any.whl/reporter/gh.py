16358c0534af11a920a441acb5a2eaadec0211f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:53 +0000

    Add package shiftleft-scan-reports/1.5.1/shiftleft_scan_reports-1.5.1-py3-none-any.whl

---
+++ packages/shiftleft-scan-reports/shiftleft_scan_reports-1.5.1-py3-none-any.whl/reporter/gh.py
@@ -0,0 +1,146 @@
+import os
+
+from github import Github
+
+from reporter.common import LOG
+
+
+def get_user(gh):
+    return gh.get_user()
+
+
+def get_context():
+    return {
+        "repoWorkspace": os.getenv("GITHUB_WORKSPACE"),
+        "runID": os.getenv("GITHUB_RUN_ID"),
+        "repoFullname": os.getenv("GITHUB_REPOSITORY"),
+        "triggerEvent": os.getenv("GITHUB_EVENT_NAME"),
+        "headRef": os.getenv("GITHUB_HEAD_REF"),
+        "baseRef": os.getenv("GITHUB_BASE_REF"),
+        "githubToken": os.getenv("GITHUB_TOKEN"),
+        "commitSHA": os.getenv("GITHUB_SHA"),
+        "workflow": os.getenv("GITHUB_WORKFLOW"),
+        "home": os.getenv("HOME"),
+        "actionId": os.getenv("GITHUB_ACTION"),
+        "trigger": os.getenv("GITHUB_ACTOR"),
+        "triggerBranchTag": os.getenv("GITHUB_REF"),
+        "triggerPath": os.getenv("GITHUB_EVENT_PATH"),
+    }
+
+
+def get_workflow(g, github_context):
+    if not github_context.get("repoFullname") or not github_context.get("runID"):
+        return
+    repo = g.get_repo(github_context.get("repoFullname"))
+    runID = github_context.get("runID")
+    if runID and runID.isdigit():
+        runID = int(runID)
+    try:
+        return repo.get_workflow_run(runID)
+    except Exception as e:
+        LOG.error(e)
+        return None
+
+
+def client():
+    gh = None
+    if not os.getenv("GITHUB_TOKEN"):
+        LOG.info(
+            "Please ensure GITHUB_TOKEN environment variable is set with permissions to read/write to pull requests"
+        )
+        return None
+    try:
+        # Try GitHub enterprise first
+        # Variables beginning with GITHUB_ cannot be overridden
+        server_url = os.getenv("GH_SERVER_URL")
+        if not server_url:
+            server_url = os.getenv("GITHUB_SERVER_URL")
+        if server_url and server_url != "https://github.com":
+            if not server_url.startswith("http"):
+                server_url = "https://" + server_url
+            if not server_url.endswith("/"):
+                server_url = server_url + "/"
+            LOG.info("Authenticating to GitHub Enterprise server: " + server_url)
+            gh = Github(
+                base_url=f"{server_url}api/v3",
+                login_or_token=os.getenv("GITHUB_TOKEN"),
+            )
+        else:
+            # Fallback to public GitHub
+            gh = Github(os.getenv("GITHUB_TOKEN"))
+        user = get_user(gh)
+        if not user:
+            return None
+    except Exception as e:
+        LOG.error(e)
+        return None
+    return gh
+
+
+def annotate(findings, scan_info):
+    github_context = get_context()
+    scan_version = scan_info.get("version")
+    g = client()
+    if not g:
+        LOG.info("Unable to authenticate with GitHub. Skipping PR annotation")
+        return
+    workflow_run = get_workflow(g, github_context)
+    if not workflow_run:
+        LOG.info("Unable to find the workflow run for this invocation")
+        return
+    pull_requests = workflow_run.pull_requests
+    if not pull_requests:
+        LOG.info("No Pull Requests are associated with this workflow run")
+        return
+    for pr in pull_requests:
+        commits = pr.get_commits()
+        last_commit = None
+        changed_files = None
+        if commits:
+            last_commit = commits.reversed[0]
+        elif github_context.get("commitSHA"):
+            last_commit = g.get_commit(github_context.get("commitSHA"))
+        if not last_commit:
+            continue
+        changed_files = [f.filename for f in last_commit.files]
+        if findings:
+            for f in findings:
+                details = f.get("details")
+                file_locations = details.get("file_locations")
+                owasp_category = f.get("owasp_category")
+                severity = f.get("severity")
+                version_first_seen = f.get("version_first_seen")
+                # Ignore legacy findings
+                if version_first_seen != scan_version:
+                    continue
+                # Ignore sensitive data exposure
+                if "a3-" in owasp_category or severity in ("INFO"):
+                    continue
+                if file_locations:
+                    deep_link = f"https://app.shiftleft.io/findingDetail/{f.get('app')}/{f.get('id')}"
+                    body = f'{f.get("title")}\n**Severity:** {severity}\n**OWASP Category:** {owasp_category}\n\n**Finding Link:** {deep_link}'
+                    source_loc = file_locations[0].split(":")
+                    source_path = source_loc[0]
+                    source_line = int(source_loc[-1]) if source_loc[-1].isdigit() else 0
+                    sink_loc = file_locations[-1].split(":")
+                    sink_path = sink_loc[0]
+                    sink_line = int(sink_loc[-1]) if sink_loc[-1].isdigit() else 0
+                    # Automatically prefix src/main/java or src/main/scala
+                    if source_path.endswith(".java") and not source_path.startswith(
+                        "src"
+                    ):
+                        source_path = "src/main/java/" + source_path
+                    if source_path.endswith(".scala") and not source_path.startswith(
+                        "src"
+                    ):
+                        source_path = "src/main/scala/" + source_path
+                    if sink_path.endswith(".java") and not sink_path.startswith("src"):
+                        sink_path = "src/main/java/" + sink_path
+                    if sink_path.endswith(".scala") and not sink_path.startswith("src"):
+                        sink_path = "src/main/scala/" + sink_path
+                    if source_path in changed_files:
+                        last_commit.create_comment(body, source_line, source_path)
+                        LOG.debug(f"Added comment to {source_path} {source_line}")
+                    if sink_path != source_path and sink_line in changed_files:
+                        last_commit.create_comment(body, sink_line, sink_path)
+                        LOG.debug(f"Added comment to {sink_path} {sink_line}")
---
+++ packages/shiftleft-scan-reports/shiftleft_scan_reports-1.5.1-py3-none-any.whl/reporter/sarif.py
@@ -0,0 +1,92 @@
+import json
+import logging
+from os.path import basename
+
+import markdown
+from jinja2 import Environment, PackageLoader, exceptions, select_autoescape
+from markupsafe import Markup
+
+from reporter.utils import (
+    auto_colourize,
+    auto_text_highlight,
+    linkify_rule,
+    remove_end_period,
+)
+
+md = markdown.Markdown(extensions=["meta"])
+
+env = Environment(
+    loader=PackageLoader("reporter", "templates"),
+    autoescape=select_autoescape(["html"]),
+)
+env.filters["markdown"] = lambda text: Markup(md.convert(text))
+env.filters["basename"] = basename
+env.filters["auto_text_highlight"] = auto_text_highlight
+env.filters["auto_colourize"] = auto_colourize
+env.filters["linkify_rule"] = linkify_rule
+env.filters["remove_end_period"] = remove_end_period
+
+
+def parse(sarif_file):
+    """
+    Parse sarif file
+    :param sarif_file: Sarif file from SAST scan
+    :return: Json data
+    """
+    report_data = {}
+    with open(sarif_file, mode="r") as report_file:
+        try:
+            report_data = json.loads(report_file.read())
+        except json.decoder.JSONDecodeError:
+            logging.warning("Unable to parse sarif file {}".format(sarif_file))
+            return None
+        return report_data
+
+
+def compute_metrics(report_data, results):
+    """
+    Compute summary metrics from results found in the sarif file
+    :param report_data: Report data
+    :param results: Results section from the sarif
+    :return: metrics - dict containing severity as key and count as the value
+    """
+    metrics = {"total": len(results), "critical": 0, "high": 0, "medium": 0, "low": 0}
+    for res in results:
+        severity = res.get("properties").get("issue_severity")
+        severity = severity.lower()
+        if severity == "moderate":
+            severity = "medium"
+        if not severity:
+            severity = "low"
+        metrics[severity] += 1
+    return metrics
+
+
+def render_html(report_data, out_file):
+    """
+    Renders the SAST report data as html
+    :param report_data: Report data from sarif files
+    :param out_file: Output filename
+    :return: HTML contents
+    """
+    template = env.get_template("sast-report.html")
+    try:
+        run = report_data.get("runs")[-1]
+        results = run.get("results")
+        key_issues = [r for r in results if r.get("level") == "error"]
+        metrics = run.get("properties", {}).get("metrics")
+        if not metrics:
+            metrics = compute_metrics(report_data, results)
+        if len(key_issues) > 4:
+            key_issues = key_issues[:4]
+        report_html = template.render(
+            report_data, key_issues=key_issues, metrics=metrics
+        )
+    except exceptions.TemplateSyntaxError as te:
+        logging.warning(te)
+        return None
+    if out_file:
+        with open(out_file, mode="w") as fp:
+            fp.write(report_html)
+            logging.debug("Report written to {}".format(report_html))
+    return report_html
