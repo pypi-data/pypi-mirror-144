40edcd9fab131ac144bfc097d0f06f4ac66d0af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:23 +0000

    Add package geneplexus/1.0.0.dev0/geneplexus-1.0.0.dev0.tar.gz

---
+++ packages/geneplexus/geneplexus-1.0.0.dev0.tar.gz/geneplexus-1.0.0.dev0/geneplexus/download.py
@@ -0,0 +1,240 @@
+"""Data download module."""
+import io
+import os.path as osp
+import time
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
+from threading import local
+from typing import List
+from typing import Tuple
+from typing import Union
+from urllib.parse import urljoin
+from zipfile import ZipFile
+
+import requests
+from requests.sessions import Session
+
+from . import util
+from ._config import logger
+from ._config.config import ALL_FEATURES
+from ._config.config import ALL_GSCS
+from ._config.config import ALL_NETWORKS
+from ._config.config import ALL_TASKS
+from ._config.config import FEATURE_SELECTION_TYPE
+from ._config.config import FEATURE_TYPE
+from ._config.config import GSC_SELECTION_TYPE
+from ._config.config import GSC_TYPE
+from ._config.config import NET_SELECTION_TYPE
+from ._config.config import NET_TYPE
+from ._config.config import TASK_SELECTION_TYPE
+from ._config.config import TASK_TYPE
+from ._config.config import URL_DATA
+
+thread_local = local()
+
+
+def download_select_data(
+    data_dir: str,
+    tasks: TASK_SELECTION_TYPE = "All",
+    networks: NET_SELECTION_TYPE = "All",
+    features: FEATURE_SELECTION_TYPE = "All",
+    GSCs: GSC_SELECTION_TYPE = "All",
+    n_jobs: int = 10,
+):
+    """Select subset of data to download.
+
+    Args:
+        data_dir: Location of data files.
+        tasks: Task of interest, accept multiple selection as a list. Do all
+            the tasks if set to "All".
+        networks: Networks of interest, accept multiple selection as a list. Do
+            all the networks if set to "All".
+        features: Network features of interest, accept multiple selection as a
+            list. Do all the features if set to "All".
+        GSCs: Gene set collection of interest, accept multiple selection as a
+            list. Do all the GSC if set to "All".
+        n_jobs: Number of concurrent downloading threads.
+
+    """
+    # Similarities and NetworkGraph will assume downloaded MachineLearning
+    tasks, networks, features, GSCs = make_download_options_lists(tasks, networks, features, GSCs)
+    all_files_to_do = []
+    for atask in tasks:
+        if atask == "IDconversion":
+            all_files_to_do.extend(get_IDconversion_filenames())
+        if atask == "MachineLearning":
+            all_files_to_do.extend(get_MachineLearning_filenames(networks, features, GSCs))
+        if atask == "Similarities":
+            all_files_to_do.extend(get_Similarities_filenames(networks, features, GSCs))
+        if atask == "NetworkGraph":
+            all_files_to_do.extend(get_NetworkGraph_filenames(networks))
+        if atask == "OriginalGSCs":
+            all_files_to_do.extend(get_OriginalGSCs_filenames())
+
+    all_files_to_do = list(set(all_files_to_do))
+    download_from_url(data_dir, all_files_to_do, n_jobs)
+
+
+def _get_session() -> Session:
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+
+def _download_file(file: str, data_dir: str):
+    session = _get_session()
+    url = urljoin(URL_DATA, f"{file}.zip")
+    while True:
+        with session.get(url) as r:
+            if r.ok:
+                ZipFile(io.BytesIO(r.content)).extractall(data_dir)
+                break
+            elif r.status_code == 429:  # Retry later
+                t = r.headers["Retry-after"]
+                logger.warning(f"Too many requests, waiting for {t} sec")
+                time.sleep(int(t))
+            else:
+                raise requests.exceptions.RequestException(r, url)
+    logger.info(f"Downloaded {file}")
+
+
+def _get_files_to_download(data_dir: str, files: List[str]) -> List[str]:
+    files_to_download = []
+    for file in files:
+        path = osp.join(data_dir, file)
+        if osp.exists(path):
+            logger.info(f"File exists, skipping download: {path}")
+        else:
+            files_to_download.append(file)
+    return files_to_download
+
+
+def download_from_url(data_dir: str, files_to_do: List[str], n_jobs: int = 10):
+    """Download file using the base url.
+
+    Args:
+        data_dir: Location of data files.
+        files_to_do: List of files to download from the the url.
+        n_jobs: Number of concurrent downloading threads.
+
+    """
+    files_to_download = _get_files_to_download(data_dir, files_to_do)
+    logger.info(f"Total number of files to download: {len(files_to_download)}")
+    with ThreadPoolExecutor(max_workers=n_jobs) as executor:
+        executor.map(_download_file, files_to_download, repeat(data_dir))
+
+
+def _make_download_options_list(
+    name: str,
+    opts: Union[str, List[str]],
+    check_list: List[str],
+) -> List[str]:
+    if isinstance(opts, str):
+        opts = check_list if opts == "All" else [opts]
+    elif not isinstance(opts, list):
+        raise TypeError(f"Expcted str type or list of str type, got {type(opts)}")
+
+    for i in opts:
+        if i not in check_list:
+            raise ValueError(f"Unexpected {name}: {opts!r}")
+
+    return opts
+
+
+def make_download_options_lists(
+    tasks: TASK_SELECTION_TYPE,
+    networks: NET_SELECTION_TYPE,
+    features: FEATURE_SELECTION_TYPE,
+    GSCs: GSC_SELECTION_TYPE,
+) -> Tuple[List[TASK_TYPE], List[NET_TYPE], List[FEATURE_TYPE], List[GSC_TYPE]]:
+    """Compile a list of files to download based on the selections."""
+    args = (
+        ("tasks", tasks, ALL_TASKS),
+        ("network", networks, ALL_NETWORKS),
+        ("feature", features, ALL_FEATURES),
+        ("GSC", GSCs, ALL_GSCS),
+    )
+    return tuple(map(_make_download_options_list, *zip(*args)))  # type: ignore
+
+
+def get_IDconversion_filenames() -> List[str]:
+    """Get gene ID conversion file names."""
+    files_to_do = []
+    for line in util.get_all_filenames():
+        if ("IDconversion" in line) or ("NodeOrder" in line):
+            files_to_do.append(line)
+    return files_to_do
+
+
+def get_MachineLearning_filenames(
+    networks: List[NET_TYPE],
+    features: List[FEATURE_TYPE],
+    GSCs: List[GSC_TYPE],
+) -> List[str]:
+    """Get dataset file names."""
+    files_to_do = []
+    for line in util.get_all_filenames():
+        if "NodeOrder" in line:
+            net_tmp = osp.splitext(line.split("Order_")[-1])[0]
+            if net_tmp in networks:
+                files_to_do.append(line)
+        if ("universe.txt" in line) or ("GoodSets.json" in line):
+            net_tmp = line.split("_")[2]
+            GSC_tmp = line.split("_")[1]
+            if (net_tmp in networks) and (GSC_tmp in GSCs):
+                files_to_do.append(line)
+        if "Data_" in line:
+            feature_tmp = line.split("_")[1]
+            net_tmp = osp.splitext(line.split("_")[2])[0]
+            if (feature_tmp in features) and (net_tmp in networks):
+                files_to_do.append(line)
+        if ("Entrez-to-Name" in line) or ("Entrez-to-Symbol" in line):
+            files_to_do.append(line)
+    return files_to_do
+
+
+def get_Similarities_filenames(
+    networks: List[NET_TYPE],
+    features: List[FEATURE_TYPE],
+    GSCs: List[GSC_TYPE],
+) -> List[str]:
+    """Get pretrained model similarity file names."""
+    files_to_do = []
+    for line in util.get_all_filenames():
+        if "CorrectionMatrix_" in line:
+            feature_tmp = osp.splitext(line.split("_")[-1])[0]
+            net_tmp = line.split("_")[3]
+            GSC_tmp = line.split("_")[1]
+            if (net_tmp in networks) and (feature_tmp in features) and (GSC_tmp in GSCs):
+                files_to_do.append(line)
+        if "CorrectionMatrixOrder" in line:
+            GSC_tmp = line.split("_")[1]
+            net_tmp = osp.splitext(line.split("_")[2])[0]
+            if (net_tmp in networks) and (GSC_tmp in GSCs):
+                files_to_do.append(line)
+        if "PreTrainedWeights" in line:
+            net_tmp = line.split("_")[2]
+            feature_tmp = osp.splitext(line.split("_")[-1])[0]
+            if (net_tmp in networks) and (feature_tmp in features):
+                files_to_do.append(line)
+    return files_to_do
+
+
+def get_NetworkGraph_filenames(networks: List[NET_TYPE]) -> List[str]:
+    """Get network file names."""
+    files_to_do = ["IDconversion_Homo-sapiens_Entrez-to-Symbol.json"]
+    for line in util.get_all_filenames():
+        if "Edgelist" in line:
+            net_tmp = osp.splitext(line.split("_")[-1])[0]
+            if net_tmp in networks:
+                files_to_do.append(line)
+    return files_to_do
+
+
+def get_OriginalGSCs_filenames() -> List[str]:
+    """Get original GSC file names."""
+    files_to_do = []
+    for line in util.get_all_filenames():
+        if "GSCOriginal" in line:
+            files_to_do.append(line)
+    return files_to_do
---
+++ packages/geneplexus/geneplexus-1.0.0.dev0.tar.gz/geneplexus-1.0.0.dev0/test/test_pipeline.py
@@ -0,0 +1,196 @@
+import os.path as osp
+import unittest
+
+import pandas as pd
+import pytest
+
+import geneplexus
+
+
+FILENAMES = [
+    "CorrectionMatrix_DisGeNet_DisGeNet_BioGRID_Embedding.npy",
+    "CorrectionMatrix_DisGeNet_GO_BioGRID_Embedding.npy",
+    "CorrectionMatrix_GO_DisGeNet_BioGRID_Embedding.npy",
+    "CorrectionMatrix_GO_GO_BioGRID_Embedding.npy",
+    "CorrectionMatrixOrder_DisGeNet_BioGRID.txt",
+    "CorrectionMatrixOrder_GO_BioGRID.txt",
+    "Data_Embedding_BioGRID.npy",
+    "Edgelist_BioGRID.edg",
+    "GSC_DisGeNet_BioGRID_GoodSets.json",
+    "GSC_DisGeNet_BioGRID_universe.txt",
+    "GSC_GO_BioGRID_GoodSets.json",
+    "GSC_GO_BioGRID_universe.txt",
+    "IDconversion_Homo-sapiens_ENSG-to-Entrez.json",
+    "IDconversion_Homo-sapiens_ENSP-to-Entrez.json",
+    "IDconversion_Homo-sapiens_ENST-to-Entrez.json",
+    "IDconversion_Homo-sapiens_Entrez-to-ENSG.json",
+    "IDconversion_Homo-sapiens_Entrez-to-Name.json",
+    "IDconversion_Homo-sapiens_Entrez-to-Symbol.json",
+    "IDconversion_Homo-sapiens_Symbol-to-Entrez.json",
+    "NodeOrder_BioGRID.txt",
+    "NodeOrder_GIANT-TN.txt",
+    "NodeOrder_STRING-EXP.txt",
+    "NodeOrder_STRING.txt",
+    "PreTrainedWeights_DisGeNet_BioGRID_Embedding.json",
+    "PreTrainedWeights_GO_BioGRID_Embedding.json",
+]
+
+
+def test_download_exist(caplog):
+    geneplexus.download.download_select_data(
+        pytest.DATADIR,
+        "All",
+        "BioGRID",
+        "Embedding",
+        ["GO", "DisGeNet"],
+    )
+    assert "File exists, skipping download:" in caplog.text
+
+
+@pytest.mark.usefixtures("data")
+class TestGenePlexusPipeline(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.gp = geneplexus.GenePlexus(None, "BioGRID", "Embedding", "GO")
+
+    @pytest.mark.order(0)
+    def test_filenames(self):
+        for filename in FILENAMES:
+            self.assertTrue(osp.isfile(osp.join(pytest.DATADIR, filename)))
+
+    @pytest.mark.order(1)
+    def test_init_geneplexus(self):
+        self.gp.file_loc = pytest.DATADIR
+        input_path = osp.join(pytest.HOMEDIR, "example", "input_genes.txt")
+        input_genes = geneplexus.util.read_gene_list(input_path)
+        self.gp.load_genes(input_genes)
+        self.assertEqual(self.gp.input_genes, input_genes)
+
+    @pytest.mark.order(2)
+    def test_convert_to_entrez(self):
+        self.gp.convert_to_Entrez()
+        df_convert_out = self.gp.df_convert_out.copy()
+        columns = ["Original ID", "Entrez ID"]
+        df_convert_out[columns] = df_convert_out[columns].astype(int)
+
+        path = osp.join(pytest.ANSWERDIR, "df_convert_out.tsv")
+        df_convert_out_expected = pd.read_csv(path, sep="\t")
+        self.assertEqual(
+            df_convert_out.values.tolist(),
+            df_convert_out_expected.values.tolist(),
+        )
+
+    @pytest.mark.order(3)
+    def test_get_pos_and_neg_genes(self):
+        self.gp.get_pos_and_neg_genes()
+
+    @pytest.mark.order(4)
+    def test_fit_and_predict(self):
+        # First check if the gene IDs and the corresponding attributes are
+        # aligned. Then check if the computed probabilities are close to the
+        # expected results (up to third places)
+        self.gp.fit_and_predict()
+        df_probs = self.gp.df_probs.copy()
+        df_probs["Entrez"] = df_probs["Entrez"].astype(int)
+
+        path = osp.join(pytest.ANSWERDIR, "df_probs.tsv")
+        df_probs_expected = pd.read_csv(path, sep="\t")
+
+        # Ignore rank and prob for now as they might be susceptible to randomns
+        columns = ["Entrez", "Symbol", "Name", "Known/Novel", "Class-Label"]
+        self.assertEqual(
+            df_probs.sort_values("Entrez")[columns].values.tolist(),
+            df_probs_expected.sort_values("Entrez")[columns].values.tolist(),
+        )
+
+        # But at least the proababilities should be close
+        for prob, prob_expected in zip(
+            df_probs.sort_values("Entrez")["Probability"],
+            df_probs_expected.sort_values("Entrez")["Probability"],
+        ):
+            self.assertAlmostEqual(prob, prob_expected, places=3)
+
+    @pytest.mark.order(5)
+    def test_make_sim_dfs(self):
+        # First check if ID and Name are aligned. Then check if the computed
+        # similarities are close to the expected results (up to third places)
+        df_sim_GO, df_sim_Dis, _, _ = self.gp.make_sim_dfs()
+        columns = ["ID", "Name"]
+
+        with self.subTest("GO similarity"):
+            path = osp.join(pytest.ANSWERDIR, "df_sim_GO.tsv")
+            df_sim_GO_expected = pd.read_csv(path, sep="\t").fillna("NA")
+            self.assertEqual(
+                df_sim_GO.sort_values("ID")[columns].values.tolist(),
+                df_sim_GO_expected.sort_values("ID")[columns].values.tolist(),
+            )
+            for sim, sim_expected in zip(
+                df_sim_GO.sort_values("ID")["Similarity"],
+                df_sim_GO_expected.sort_values("ID")["Similarity"],
+            ):
+                self.assertAlmostEqual(sim, sim_expected, places=3)
+
+        with self.subTest("Dis similarity"):
+            path = osp.join(pytest.ANSWERDIR, "df_sim_Dis.tsv")
+            df_sim_Dis_expected = pd.read_csv(path, sep="\t").fillna("NA")
+            self.assertEqual(
+                df_sim_Dis.sort_values("ID")[columns].values.tolist(),
+                df_sim_Dis_expected.sort_values("ID")[columns].values.tolist(),
+            )
+            for sim, sim_expected in zip(
+                df_sim_Dis.sort_values("ID")["Similarity"],
+                df_sim_Dis_expected.sort_values("ID")["Similarity"],
+            ):
+                self.assertAlmostEqual(sim, sim_expected, places=3)
+
+    @pytest.mark.order(6)
+    def test_make_small_edgelist(self):
+        self.gp.make_small_edgelist(num_nodes=50)
+        columns = ["Node1", "Node2"]
+
+        with self.subTest("Edge"):
+            df_edge = self.gp.df_edge.copy()
+            df_edge[columns] = df_edge[columns].astype(int)
+            path = osp.join(pytest.ANSWERDIR, "df_edge.tsv")
+            df_edge_expected = pd.read_csv(path, sep="\t")
+            self.assertEqual(
+                df_edge[columns].values.tolist(),
+                df_edge_expected[columns].values.tolist(),
+            )
+            for weight, weight_expected in zip(
+                df_edge["Weight"],
+                df_edge_expected["Weight"],
+            ):
+                self.assertAlmostEqual(weight, weight_expected)
+
+        with self.subTest("Edge sym"):
+            df_edge_sym = self.gp.df_edge_sym.copy()
+            path = osp.join(pytest.ANSWERDIR, "df_edge_sym.tsv")
+            df_edge_sym_expected = pd.read_csv(path, sep="\t")
+            self.assertEqual(
+                df_edge_sym[columns].values.tolist(),
+                df_edge_sym_expected[columns].values.tolist(),
+            )
+            for weight, weight_expected in zip(
+                df_edge_sym["Weight"],
+                df_edge_sym_expected["Weight"],
+            ):
+                self.assertAlmostEqual(weight, weight_expected)
+
+    @pytest.mark.order(7)
+    def test_alter_validation_df(self):
+        self.gp.alter_validation_df()
+        df_convert_out_subset = self.gp.df_convert_out_subset.copy()
+        columns = ["Original ID", "Entrez ID"]
+        df_convert_out_subset[columns] = df_convert_out_subset[columns].astype(int)
+
+        path = osp.join(pytest.ANSWERDIR, "df_convert_out_subset.tsv")
+        df_convert_out_subset_expected = pd.read_csv(path, sep="\t")
+        self.assertEqual(
+            df_convert_out_subset.values.tolist(),
+            df_convert_out_subset_expected.values.tolist(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
