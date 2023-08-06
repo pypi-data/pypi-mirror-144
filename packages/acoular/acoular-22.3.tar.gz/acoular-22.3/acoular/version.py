5cdd6c1a616bf388338f0d6243f6be2f518707d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:10 +0000

    Add package acoular/22.3/acoular-22.3.tar.gz

---
+++ packages/acoular/acoular-22.3.tar.gz/acoular-22.3/acoular/version.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+#------------------------------------------------------------------------------
+# Copyright (c) Acoular Development Team.
+#------------------------------------------------------------------------------
+
+# separate file to find out about version without importing the acoular lib
+__author__ = "Acoular Development Team"
+__date__ = "28 March 2022"
+__version__ = "22.3"
