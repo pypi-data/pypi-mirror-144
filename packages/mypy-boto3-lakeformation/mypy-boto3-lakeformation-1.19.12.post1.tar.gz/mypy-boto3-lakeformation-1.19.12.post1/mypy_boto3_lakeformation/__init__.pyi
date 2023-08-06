3bd3d4a5061b9933d309ab16b017501bc2540a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:42 +0000

    Add package mypy-boto3-lakeformation/1.19.12.post1/mypy-boto3-lakeformation-1.19.12.post1.tar.gz

---
+++ packages/mypy-boto3-lakeformation/mypy-boto3-lakeformation-1.19.12.post1.tar.gz/mypy-boto3-lakeformation-1.19.12.post1/mypy_boto3_lakeformation/__init__.pyi
@@ -0,0 +1,21 @@
+"""
+Main interface for lakeformation service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_lakeformation import (
+        Client,
+        LakeFormationClient,
+    )
+
+    session = Session()
+    client: LakeFormationClient = session.client("lakeformation")
+    ```
+"""
+from .client import LakeFormationClient
+
+Client = LakeFormationClient
+
+__all__ = ("Client", "LakeFormationClient")
