09d37c92dccc70a3b2780d5d6d24e25b3954d226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-29/1.0/smart-hg-29-1.0.tar.gz

---
+++ packages/smart-hg-29/smart-hg-29-1.0.tar.gz/smart-hg-29-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-29",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['zope.interface==5.4.0', 'plone.api==1.10.4'],
+)
