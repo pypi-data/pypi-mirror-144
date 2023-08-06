0aab746c582ea42361b5354433adf7548544ca52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:46 +0000

    Add package fake-bge-module-latest/20220330/fake-bge-module-latest-20220330.tar.gz

---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20220330.tar.gz/fake-bge-module-latest-20220330/bge/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import typing
+from . import types
+from . import events
+from . import constraints
+from . import render
+from . import logic
+from . import texture
+from . import app
---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20220330.tar.gz/fake-bge-module-latest-20220330/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import sequencer
+from . import clip
+from . import uvcalc_follow_active
+from . import uvcalc_lightmap
+from . import view3d
+from . import userpref
+from . import spreadsheet
+from . import geometry_nodes
+from . import vertexpaint_dirt
+from . import wm
+from . import freestyle
+from . import rigidbody
+from . import bmesh
+from . import presets
+from . import file
+from . import assets
+from . import node
+from . import object_randomize_transform
+from . import object_align
+from . import object_quick_effects
+from . import constraint
+from . import object
+from . import add_mesh_torus
+from . import mesh
+from . import console
+from . import screen_play_rendered_anim
+from . import image
+from . import anim
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
