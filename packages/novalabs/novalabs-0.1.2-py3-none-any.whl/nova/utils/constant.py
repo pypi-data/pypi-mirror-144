a72b272d82f98b22cafcbe9ec1b55e1e0cf59e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:43 +0000

    Add package novalabs/0.1.2/novalabs-0.1.2-py3-none-any.whl

---
+++ packages/novalabs/novalabs-0.1.2-py3-none-any.whl/nova/utils/constant.py
@@ -0,0 +1,22 @@
+EXCEPTION_LIST_BINANCE = [
+    'BTCSTUSDT', 'BTCDOMUSDT', '1000XECUSDT', 'ETHUSDT_220325',
+    '1000BTTCUSDT', '1000SHIBUSDT', 'DEFIUSDT', 'BTCUSDT_220325',
+    'API3USDT', 'ANCUSDT', 'IMXUSDT', 'FLOWUSDT'
+]
+
+VAR_NEEDED_FOR_POSITION = [
+    'all_entry_time', 'all_entry_point', 'all_entry_price',
+    'all_exit_time', 'all_exit_point', 'all_tp', 'all_sl'
+]
+
+DEFINITION_STATISTIC = {
+    'per_position' : {
+        'nb_minutes_in_position': ''
+    },
+    'per_pair_agg': {
+
+    },
+    'per_pair_time': {
+
+    }
+}
