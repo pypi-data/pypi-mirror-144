93bfd74d033b9c1411f8f1f43a567003f308818c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package scenariogeneration/0.7.11/scenariogeneration-0.7.11.tar.gz

---
+++ packages/scenariogeneration/scenariogeneration-0.7.11.tar.gz/scenariogeneration-0.7.11/scenariogeneration/xosc/exceptions.py
@@ -0,0 +1,35 @@
+""" Some custom exceptions
+
+"""
+
+
+class NoActionsDefinedError(Exception):
+    """ Raised when no actions are defined in a class
+    """
+    pass
+
+class NoCatalogFoundError(Exception):
+    """ Raised when no catalog is found with the given name
+    """
+    pass
+
+class OpenSCENARIOVersionError(Exception):
+    """ Raised when trying to use features from a newer version of OpenSCENARIO than the desiered version
+    """
+    pass
+
+class ToManyOptionalArguments(Exception):
+    """ Raised when one of the needed "optional" inputs are not used
+    """
+    pass
+
+
+class NotEnoughInputArguments(Exception):
+    """ Raised when one of the needed "optional" inputs are not used
+    """
+    pass
+
+class NotAValidElement(Exception):
+    """ Raised when one of the needed "optional" inputs are not used
+    """
+    pass
