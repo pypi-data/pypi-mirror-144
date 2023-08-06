fc736a36c925ac3c2944e3dccfc790a28b5a8c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:29 +0000

    Add package cpm-file/2.2/cpm_file-2.2.tar.gz

---
+++ packages/cpm-file/cpm_file-2.2.tar.gz/cpm_file-2.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import sys
+print(sys.path)
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+long_description = "Package that takes as command-line input a file path specifying a source text and a search term and return the words in lines that contain the substring"
+
+setup(
+    name="cpm_file",
+    version="2.2",
+    author="Laura Pérez Vera",
+    author_email="lpvera22@gmail.com",
+    # url="",
+    description="Finding substrings in the lines of a file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["cpm_file = main:main"]},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    keywords="search keyword substring research substring",
+    install_requires=requirements,
+    zip_safe=False,
+    
+    
+)
