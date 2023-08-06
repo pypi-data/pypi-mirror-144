a54c8f58a1792fcc086643815f6a3cd505f04472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:49 +0000

    Add package mypy-boto3-transfer/1.19.12.post1/mypy-boto3-transfer-1.19.12.post1.tar.gz

---
+++ packages/mypy-boto3-transfer/mypy-boto3-transfer-1.19.12.post1.tar.gz/mypy-boto3-transfer-1.19.12.post1/mypy_boto3_transfer/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for transfer service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_transfer import (
+        Client,
+        ListServersPaginator,
+        TransferClient,
+    )
+
+    session = Session()
+    client: TransferClient = session.client("transfer")
+
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    ```
+"""
+from .client import TransferClient
+from .paginator import ListServersPaginator
+
+Client = TransferClient
+
+
+__all__ = ("Client", "ListServersPaginator", "TransferClient")
