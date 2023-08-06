784e80009a11940614213f0c1100b0c15f327c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:37 +0000

    Add package rnutil/0.3.3/rnutil-0.3.3-py3-none-any.whl

---
+++ packages/rnutil/rnutil-0.3.3-py3-none-any.whl/rnutil/__init__.py
@@ -0,0 +1,11 @@
+
+print(f"{__name__}: Los archivos de los conjuntos de datos que ofrece este paquete también están disponibles en https://github.com/facundoq/redes-neuronales-util/tree/main/rnutil/data para descargar de forma individual")
+print(f"{__name__}: Agregando el parámetro local=True en las funciones {__name__}.load_dataset_numpy, {__name__}.load_dataset_pandas y {__name__}.load_image se puede cargar una versión local de un archivo en lugar de la versión que ofrece este paquete.")
+
+from .datasets import load_dataset_numpy,load_dataset_pandas,load_image
+
+from .numpy import equals_vector,verificar_igualdad
+
+from .plot import plot_regresion_lineal,plot_regresion_lineal_univariada,plot_regresion_logistica2D,plot_fronteras_keras,plot_loss,plot_loss_accuracy,plot_loss_accuracy_keras
+
+from .plot_logistic import plot_regresion_logistica1d
