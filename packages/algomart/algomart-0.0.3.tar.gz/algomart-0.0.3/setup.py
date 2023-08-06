1179661edcc257b8758adf02bbc782b0bd462bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:26:27 +0000

    Add package algomart/0.0.3/algomart-0.0.3.tar.gz

---
+++ packages/algomart/algomart-0.0.3.tar.gz/algomart-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algomart",
+    version="0.0.3",
+    license="MIT",
+    author="Yash Jain",
+    author_email="yash0307jain@gmail.com",
+    description="Collections of algorithms in python",
+    packages=find_packages(),
+    url="https://github.com/AlgoMart/python"
+)
