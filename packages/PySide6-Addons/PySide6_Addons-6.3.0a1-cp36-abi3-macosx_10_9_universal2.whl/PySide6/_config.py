e436f2d6ad2882b8c866cb301da3992cb931b724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:05 +0000

    Add package PySide6-Addons/6.3.0a1.dev1648535514/PySide6_Addons-6.3.0a1-cp36-abi3-macosx_10_9_universal2.whl

---
+++ packages/PySide6-Addons/PySide6_Addons-6.3.0a1-cp36-abi3-macosx_10_9_universal2.whl/PySide6/_config.py
@@ -0,0 +1,16 @@
+built_modules = list(name for name in
+    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;Designer;Xml;Help;Multimedia;MultimediaWidgets;OpenGL;OpenGLWidgets;Positioning;NetworkAuth;Nfc;Qml;Quick;Quick3D;QuickControls2;QuickWidgets;RemoteObjects;Scxml;Sensors;SerialPort;StateMachine;Charts;Svg;SvgWidgets;DataVisualization;Bluetooth;UiTools;WebChannel;WebEngineCore;WebEngineWidgets;WebEngineQuick;WebSockets;DBus;3DCore;3DRender;3DInput;3DLogic;3DAnimation;3DExtras"
+    .split(";"))
+
+shiboken_library_soversion = str(6.3)
+pyside_library_soversion = str(6.3)
+
+version = "6.3.0a1.dev1648535514"
+version_info = (6, 3, 0, "a", "1")
+
+__build_date__ = '2022-03-29T06:36:41+00:00'
+
+
+
+__setup_py_package_timestamp__ = '1648535514'
+__setup_py_package_version__ = '6.3.0a1.dev1648535514'
