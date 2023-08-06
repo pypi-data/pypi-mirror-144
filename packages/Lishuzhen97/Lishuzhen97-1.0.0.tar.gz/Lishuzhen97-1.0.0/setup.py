fb0f460d6424374af656b93e402992165db4eda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:28 +0000

    Add package Lishuzhen97/1.0.0/Lishuzhen97-1.0.0.tar.gz

---
+++ packages/Lishuzhen97/Lishuzhen97-1.0.0.tar.gz/Lishuzhen97-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Lishuzhen97", 
+    version="1.0.0", 
+    author="Li shuzhen", 
+    author_email="shuzhen_li97@163.com", 
+    description="test", 
+    long_description=open("README.md").read(), 
+    license="MIT", 
+    url="https://github.com/lishuzhen97/lishuzhen",
+) 
