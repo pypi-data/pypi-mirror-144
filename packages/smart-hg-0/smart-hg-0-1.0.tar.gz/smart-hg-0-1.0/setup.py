bbf34790922acb60e22af1e86f9494302fd7582c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:25 +0000

    Add package smart-hg-0/1.0/smart-hg-0-1.0.tar.gz

---
+++ packages/smart-hg-0/smart-hg-0-1.0.tar.gz/smart-hg-0-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-0",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'scipy==1.2.3', 'matplotlib==2.1.2', 'librosa==0.7.2'],
+)
