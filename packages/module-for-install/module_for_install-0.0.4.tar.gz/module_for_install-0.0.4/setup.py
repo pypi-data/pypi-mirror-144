393240f40fe1cc9b6f8439f6326d995235de8ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:00 +0000

    Add package module-for-install/0.0.4/module_for_install-0.0.4.tar.gz

---
+++ packages/module-for-install/module_for_install-0.0.4.tar.gz/module_for_install-0.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='module_for_install',
+    version='0.0.4',
+    author='Pavel Khomenko',
+    author_email='your@domain.com',
+    description='My Package for university task',
+    packages=setuptools.find_packages(),
+)
