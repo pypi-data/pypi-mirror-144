cef41118f297cbb35f2f04ff19347f080d24fc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:55 +0000

    Add package awesome-sso/0.3.5/awesome_sso-0.3.5-py3-none-any.whl

---
+++ packages/awesome-sso/awesome_sso-0.3.5-py3-none-any.whl/awesome_sso/mail/mailgun.py
@@ -0,0 +1,67 @@
+import json
+from typing import List, TextIO, Tuple
+
+import requests
+from pydantic import EmailStr, HttpUrl
+
+
+class MailGun:
+    def __init__(
+        self,
+        base_url: HttpUrl,
+        api_key: str,
+    ):
+        self.base_url = base_url
+        self.api_key = api_key
+
+    def send_simple_message(
+        self,
+        from_name: str,
+        from_email: EmailStr,
+        to: List[EmailStr],
+        subject: str,
+        text: str,
+        files: List[Tuple[str, TextIO]] = [],
+        cc: List[EmailStr] = [],
+        bcc: List[EmailStr] = [],
+    ):
+        return requests.post(
+            self.base_url + "/messages",
+            auth=("api", self.api_key),
+            files=files,
+            data={
+                "from": "%s <%s>" % (from_name, from_email),
+                "to": to,
+                "cc": cc,
+                "bcc": bcc,
+                "subject": subject,
+                "text": text,
+            },
+        )
+
+    def send_template(
+        self,
+        from_name: str,
+        from_email: EmailStr,
+        to: List[EmailStr],
+        subject: str,
+        template: str,
+        data: dict,
+        files: List[Tuple[str, TextIO]] = [],
+        cc: List[EmailStr] = [],
+        bcc: List[EmailStr] = [],
+    ):
+        return requests.post(
+            self.base_url + "/messages",
+            auth=("api", self.api_key),
+            files=files,
+            data={
+                "from": "%s <%s>" % (from_name, from_email),
+                "to": to,
+                "cc": cc,
+                "bcc": bcc,
+                "subject": subject,
+                "template": template,
+                "h:X-Mailgun-Variables": json.dumps(data),
+            },
+        )
