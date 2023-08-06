10ed872d57f509c8ed83175a3b1db48d7033093e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:21 +0000

    Add package RugbyPython/0.0.6/RugbyPython-0.0.6.tar.gz

---
+++ packages/RugbyPython/RugbyPython-0.0.6.tar.gz/RugbyPython-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='RugbyPython',
+  version='0.0.6',
+  description='A package for rugby analytics.',
+  long_description='A package for rugby analytics that makes it easy to plot custom pitches with matplotlib.',
+  url='',  
+  author='Maverick Guest',
+  author_email='maverickguest@outlook.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='sport', 
+  packages=find_packages(),
+  install_requires=['Matplotlib'] 
+)
