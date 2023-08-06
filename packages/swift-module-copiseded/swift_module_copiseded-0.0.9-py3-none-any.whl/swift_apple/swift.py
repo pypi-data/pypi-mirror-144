3ffcf1ef8691734cab0951b810bda682aef610c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:30 +0000

    Add package swift-module-copiseded/0.0.9/swift_module_copiseded-0.0.9-py3-none-any.whl

---
+++ packages/swift-module-copiseded/swift_module_copiseded-0.0.9-py3-none-any.whl/swift_apple/swift.py
@@ -0,0 +1,15 @@
+def addOne(number):
+    return number + 1
+def addDouble(number):
+    return number * 2
+def add(distance,number):
+    return number + distance
+def stepUpTimes(times,number):
+    return number + times * number
+def numberTimes(times,number):
+    return number * times
+def aNumberOfFactorial(number):
+    result = 1
+    for item in range(1,number+1):
+        result = result * item
+    return result
