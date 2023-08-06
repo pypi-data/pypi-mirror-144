2798f2dbb6649324f880df68d27962fdc38dc679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:42 +0000

    Add package serverless-container-constructs/0.1.21/serverless_container_constructs-0.1.21-py3-none-any.whl

---
+++ packages/serverless-container-constructs/serverless_container_constructs-0.1.21-py3-none-any.whl/serverless_container_constructs/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import cdk_nag._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "serverless-container-constructs",
+    "0.1.21",
+    __name__[0:-6],
+    "serverless-container-constructs@0.1.21.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
