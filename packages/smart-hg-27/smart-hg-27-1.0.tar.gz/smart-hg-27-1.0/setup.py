b8118411d34a52e9349fbce3ecd4f36374b7c0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:28 +0000

    Add package smart-hg-27/1.0/smart-hg-27-1.0.tar.gz

---
+++ packages/smart-hg-27/smart-hg-27-1.0.tar.gz/smart-hg-27-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-27",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['channels==1.1.8', 'djangorestframework==3.12.2'],
+)
