5a6286a87cb80dd9e17b8599e240087be3951cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:02 +0000

    Add package django-serverside/22.3.65/django-serverside-22.3.65.tar.gz

---
+++ packages/django-serverside/django-serverside-22.3.65.tar.gz/django-serverside-22.3.65/src/serverside/apps.py
@@ -0,0 +1,24 @@
+"""
+The file contains the app config required for Django.
+
+You may read more about this here:
+https://docs.djangoproject.com/en/4.0/ref/applications/
+"""
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+from .signals import create_permissions_and_grant_privileges
+
+
+class ServersideConfig(AppConfig):
+    """
+    App configuration class.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "serverside"
+    verbose_name = "ServerSide"
+
+    def ready(self):
+        post_migrate.connect(create_permissions_and_grant_privileges)
