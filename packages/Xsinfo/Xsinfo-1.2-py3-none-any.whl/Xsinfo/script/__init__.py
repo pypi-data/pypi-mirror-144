bce7fc885e7b40056c0d4b642d67db648a154308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:59 +0000

    Add package Xsinfo/1.2/Xsinfo-1.2-py3-none-any.whl

---
+++ packages/Xsinfo/Xsinfo-1.2-py3-none-any.whl/Xsinfo/script/__init__.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022, Franck Lejzerowicz.
+#
+# Distributed under the terms of the Modified BSD License.
+# ----------------------------------------------------------------------------
+
+from ._standalone_xsinfo import standalone_xsinfo
+
+__all__ = ["standalone_xsinfo"]
