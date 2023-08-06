9384824a982eb0765b6d51b059a439d5cc2ae051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:25 +0000

    Add package smart-hg-11/1.0/smart-hg-11-1.0.tar.gz

---
+++ packages/smart-hg-11/smart-hg-11-1.0.tar.gz/smart-hg-11-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-11",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.19.4', 'keras==2.4.3', 'tensorflow==2.4.0'],
+)
