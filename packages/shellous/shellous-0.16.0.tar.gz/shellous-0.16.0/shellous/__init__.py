437ea979ea0aa534523c7fff8ca2ed9a351c6648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:04 +0000

    Add package shellous/0.16.0/shellous-0.16.0.tar.gz

---
+++ packages/shellous/shellous-0.16.0.tar.gz/shellous-0.16.0/shellous/__init__.py
@@ -0,0 +1,32 @@
+"""
+.. include:: ../README.md
+"""
+__docformat__ = "restructuredtext"
+__version__ = "0.16.0"
+
+import sys
+
+# pylint: disable=cyclic-import
+from .command import CmdContext, Command, Options  # noqa: F401
+from .pipeline import Pipeline  # noqa: F401
+from .pty_util import cbreak, cooked, raw  # noqa: F401
+from .redirect import Redirect
+from .result import PipeResult, Result, ResultError  # noqa: F401
+from .runner import AUDIT_EVENT_SUBPROCESS_SPAWN  # noqa: F401
+
+if sys.platform != "win32":
+    from .watcher import DefaultChildWatcher  # noqa: F401
+
+STDOUT = Redirect.STDOUT
+DEVNULL = Redirect.DEVNULL
+CAPTURE = Redirect.CAPTURE
+INHERIT = Redirect.INHERIT
+
+
+def context() -> CmdContext:
+    "Construct a new execution context."
+    return CmdContext()
+
+
+sh = CmdContext()
+"Default execution context. Immutable."
