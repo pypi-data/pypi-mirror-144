8ac6e888196cba6de950e7bebe02c1a518972f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:44 +0000

    Add package fawaris-fastapi/0.1.1/fawaris_fastapi-0.1.1.tar.gz

---
+++ packages/fawaris-fastapi/fawaris_fastapi-0.1.1.tar.gz/fawaris_fastapi-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fawaris_fastapi', 'fawaris_fastapi.scripts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiosqlite>=0.17.0,<0.18.0',
+ 'databases>=0.5.5,<0.6.0',
+ 'fastapi>=0.75.0,<0.76.0',
+ 'fawaris>=0.1.37,<0.2.0',
+ 'overrides>=6.1.0,<7.0.0',
+ 'python-multipart>=0.0.5,<0.0.6',
+ 'typer>=0.4.0,<0.5.0',
+ 'uvicorn>=0.17.6,<0.18.0']
+
+setup_kwargs = {
+    'name': 'fawaris-fastapi',
+    'version': '0.1.1',
+    'description': 'Stellar Anchor implementation using FastAPI',
+    'long_description': None,
+    'author': 'Yuri Escalianti',
+    'author_email': 'yuriescl@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/yuriescl/fawaris-fastapi',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
