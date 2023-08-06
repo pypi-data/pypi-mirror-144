aebb9b00fafd5674c6dc64501bc790260a3185cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:58 +0000

    Add package organize-tool/2.2.0/organize-tool-2.2.0.tar.gz

---
+++ packages/organize-tool/organize-tool-2.2.0.tar.gz/organize-tool-2.2.0/organize/actions/move.py
@@ -0,0 +1,145 @@
+from os.path import commonpath
+from typing import Callable, Union
+
+from fs import errors, open_fs
+from fs.base import FS
+from fs.errors import NoSysPath
+from fs.move import move_dir, move_file
+from fs.opener.errors import OpenerError
+from fs.osfs import OSFS
+from fs.path import dirname, frombase
+from schema import Optional, Or
+
+from organize.utils import SimulationFS, Template, safe_description
+
+from .action import Action
+from .copymove_utils import CONFLICT_OPTIONS, check_conflict, dst_from_options
+
+
+def move_file_optimized(src_fs, src_path, dst_fs, dst_path):
+    try:
+        if isinstance(src_fs, OSFS) and isinstance(dst_fs, OSFS):
+            src_syspath = src_fs.getsyspath(src_path)
+            dst_syspath = dst_fs.getsyspath(dst_path)
+            common = commonpath([src_syspath, dst_syspath])
+            rel_src = frombase(common, src_syspath)
+            rel_dst = frombase(common, dst_syspath)
+            with open_fs(common, writeable=True) as base:
+                base.move(rel_src, rel_dst)
+            return
+    except (ValueError, NoSysPath):
+        pass
+    move_file(src_fs, src_path, dst_fs, dst_path)
+
+
+class Move(Action):
+
+    """Move a file to a new location.
+
+    The file can also be renamed.
+    If the specified path does not exist it will be created.
+
+    If you only want to rename the file and keep the folder, it is
+    easier to use the `rename` action.
+
+    Args:
+        dest (str):
+            The destination where the file / dir should be moved to.
+            If `dest` ends with a slash, it is assumed to be a target directory
+            and the file / dir will be moved into `dest` and keep its name.
+
+        on_conflict (str):
+            What should happen in case **dest** already exists.
+            One of `skip`, `overwrite`, `trash`, `rename_new` and `rename_existing`.
+            Defaults to `rename_new`.
+
+        rename_template (str):
+            A template for renaming the file / dir in case of a conflict.
+            Defaults to `{name} {counter}{extension}`.
+
+        filesystem (str):
+            (Optional) A pyfilesystem opener url of the filesystem you want to copy to.
+            If this is not given, the local filesystem is used.
+
+    The next action will work with the moved file / dir.
+    """
+
+    name = "move"
+    arg_schema = Or(
+        str,
+        {
+            "dest": str,
+            Optional("on_conflict"): Or(*CONFLICT_OPTIONS),
+            Optional("rename_template"): str,
+            Optional("filesystem"): object,
+        },
+    )
+
+    def __init__(
+        self,
+        dest: str,
+        on_conflict="rename_new",
+        rename_template="{name} {counter}{extension}",
+        filesystem: Union[str, FS] = "",
+    ) -> None:
+        if on_conflict not in CONFLICT_OPTIONS:
+            raise ValueError(
+                "on_conflict must be one of %s" % ", ".join(CONFLICT_OPTIONS)
+            )
+
+        self.dest = Template.from_string(dest)
+        self.conflict_mode = on_conflict
+        self.rename_template = Template.from_string(rename_template)
+        self.filesystem = filesystem
+
+    def pipeline(self, args: dict, simulate: bool):
+        src_fs = args["fs"]  # type: FS
+        src_path = args["fs_path"]
+
+        move_action: Callable[[FS, str, FS, str], None]
+        if src_fs.isdir(src_path):
+            move_action = move_dir
+        elif src_fs.isfile(src_path):
+            move_action = move_file_optimized
+
+        dst_fs, dst_path = dst_from_options(
+            src_path=src_path,
+            dest=self.dest,
+            filesystem=self.filesystem,
+            args=args,
+        )
+
+        # check for conflicts
+        skip, dst_path = check_conflict(
+            src_fs=src_fs,
+            src_path=src_path,
+            dst_fs=dst_fs,
+            dst_path=dst_path,
+            conflict_mode=self.conflict_mode,
+            rename_template=self.rename_template,
+            simulate=simulate,
+            print=self.print,
+        )
+
+        try:
+            dst_fs = open_fs(dst_fs, create=False, writeable=True)
+        except (errors.CreateFailed, OpenerError):
+            if not simulate:
+                dst_fs = open_fs(dst_fs, create=True, writeable=True)
+            else:
+                dst_fs = SimulationFS(dst_fs)
+
+        if not skip:
+            self.print("Move to %s" % safe_description(dst_fs, dst_path))
+            if not simulate:
+                dst_fs.makedirs(dirname(dst_path), recreate=True)
+                move_action(src_fs, src_path, dst_fs, dst_path)
+
+        # the next action should work with the newly created copy
+        return {
+            "fs": dst_fs,
+            "fs_path": dst_path,
+        }
+
+    def __str__(self) -> str:
+        return "Move(dest=%s, conflict_mode=%s)" % (self.dest, self.conflict_mode)
