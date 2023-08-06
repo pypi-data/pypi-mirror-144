4b6c39869204a75d8c5268d1139593a0c441f6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:00 +0000

    Add package pygrove/0.1.1a8/pygrove-0.1.1a8.tar.gz

---
+++ packages/pygrove/pygrove-0.1.1a8.tar.gz/pygrove-0.1.1a8/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+modules = \
+['pygrove']
+install_requires = \
+['pyforest>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pygrove',
+    'version': '0.1.1a8',
+    'description': 'Common data science imports based on pyforest, with a NLP focus',
+    'long_description': None,
+    'author': 'sileod',
+    'author_email': 'damien.sileo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
