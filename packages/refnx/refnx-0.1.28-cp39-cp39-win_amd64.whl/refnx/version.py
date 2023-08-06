69498b29e8bf69c19e212d3e4177ac5fb96377da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:01 +0000

    Add package refnx/0.1.28/refnx-0.1.28-cp39-cp39-win_amd64.whl

---
+++ packages/refnx/refnx-0.1.28-cp39-cp39-win_amd64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.28'
+version = '0.1.28'
+full_version = '0.1.28'
+git_revision = 'f4eda566bf7c68620ea2531d461a8bd89240e223'
+release = True
+if not release:
+    version = full_version
