78f43576fc12b4994995728f7c17faac66104afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:10 +0000

    Add package flask-log-sku1/0.0.1/flask_log_sku1-0.0.1-py3-none-any.whl

---
+++ packages/flask-log-sku1/flask_log_sku1-0.0.1-py3-none-any.whl/flask_log_sku1/__init__.py
@@ -0,0 +1,20 @@
+#Used to print out messages server side, and make them pretty
+import datetime
+
+class log:
+    def __init__(self,*messages):
+        emptystring = ''
+        for msg in messages:
+            emptystring += str(msg)
+        self.string = emptystring
+        self.main = '[LOG] '
+        self.timestamp = '[ '+str(get_current_time())+' ]'
+    def success(self):
+        print (self.main,self.timestamp,'[**Success**]',self.string)
+    def fail(self):
+        print (self.main,self.timestamp,'[**Failed**]',self.string)
+    def error(self):
+        print (self.main,self.timestamp,'[**Error**]',self.string)
+
+def get_current_time():
+    return datetime.datetime.now().strftime("[%d/%b/%Y %H:%M:%S]")
