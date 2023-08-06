4f004e4b250327696c2266952ef81659d26816aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:46 +0000

    Add package flightcondition/22.3/flightcondition-22.3.tar.gz

---
+++ packages/flightcondition/flightcondition-22.3.tar.gz/flightcondition-22.3/src/flightcondition/__init__.py
@@ -0,0 +1,16 @@
+"""
+Aerospace utilities.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Author: Matthew C. Jones
+Email: matt.c.jones.aoe@gmail.com
+
+:copyright: 2021 Matthew C. Jones
+:license: MIT License, see LICENSE for more details.
+"""
+
+from .flightcondition import FlightCondition
+from .atmosphere import Atmosphere
+from .units import unit, dimless, printv
+
+from .version import __version__
