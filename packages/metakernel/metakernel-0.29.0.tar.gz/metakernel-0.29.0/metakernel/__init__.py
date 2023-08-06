e592cb1ba022eb246b7125536aa5a2c38791651f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package metakernel/0.29.0/metakernel-0.29.0.tar.gz

---
+++ packages/metakernel/metakernel-0.29.0.tar.gz/metakernel-0.29.0/metakernel/__init__.py
@@ -0,0 +1,12 @@
+from ._metakernel import (
+    ExceptionWrapper, MetaKernel, IPythonKernel, register_ipython_magics, get_metakernel,
+    MetaKernelApp)
+from . import pexpect
+from .replwrap import REPLWrapper, u
+from .process_metakernel import ProcessMetaKernel
+from .magic import Magic, option
+from .parser import Parser
+
+__all__ = ['Magic', 'MetaKernel', 'option']
+
+__version__ = '0.29.0'
