9990aaccfb6b62197c727c1e7a7454843f094b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package mypy-boto3-pricing/1.21.27/mypy_boto3_pricing-1.21.27-py3-none-any.whl

---
+++ packages/mypy-boto3-pricing/mypy_boto3_pricing-1.21.27-py3-none-any.whl/mypy_boto3_pricing/type_defs.pyi
@@ -0,0 +1,175 @@
+"""
+Type annotations for pricing service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_pricing/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_pricing.type_defs import AttributeValueTypeDef
+
+    data: AttributeValueTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List, Sequence
+
+from typing_extensions import NotRequired
+
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "AttributeValueTypeDef",
+    "DescribeServicesRequestDescribeServicesPaginateTypeDef",
+    "DescribeServicesRequestRequestTypeDef",
+    "DescribeServicesResponseTypeDef",
+    "FilterTypeDef",
+    "GetAttributeValuesRequestGetAttributeValuesPaginateTypeDef",
+    "GetAttributeValuesRequestRequestTypeDef",
+    "GetAttributeValuesResponseTypeDef",
+    "GetProductsRequestGetProductsPaginateTypeDef",
+    "GetProductsRequestRequestTypeDef",
+    "GetProductsResponseTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+    "ServiceTypeDef",
+)
+
+AttributeValueTypeDef = TypedDict(
+    "AttributeValueTypeDef",
+    {
+        "Value": NotRequired[str],
+    },
+)
+
+DescribeServicesRequestDescribeServicesPaginateTypeDef = TypedDict(
+    "DescribeServicesRequestDescribeServicesPaginateTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "FormatVersion": NotRequired[str],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+DescribeServicesRequestRequestTypeDef = TypedDict(
+    "DescribeServicesRequestRequestTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "FormatVersion": NotRequired[str],
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+DescribeServicesResponseTypeDef = TypedDict(
+    "DescribeServicesResponseTypeDef",
+    {
+        "Services": List["ServiceTypeDef"],
+        "FormatVersion": str,
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+FilterTypeDef = TypedDict(
+    "FilterTypeDef",
+    {
+        "Type": Literal["TERM_MATCH"],
+        "Field": str,
+        "Value": str,
+    },
+)
+
+GetAttributeValuesRequestGetAttributeValuesPaginateTypeDef = TypedDict(
+    "GetAttributeValuesRequestGetAttributeValuesPaginateTypeDef",
+    {
+        "ServiceCode": str,
+        "AttributeName": str,
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetAttributeValuesRequestRequestTypeDef = TypedDict(
+    "GetAttributeValuesRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "AttributeName": str,
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+GetAttributeValuesResponseTypeDef = TypedDict(
+    "GetAttributeValuesResponseTypeDef",
+    {
+        "AttributeValues": List["AttributeValueTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetProductsRequestGetProductsPaginateTypeDef = TypedDict(
+    "GetProductsRequestGetProductsPaginateTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "Filters": NotRequired[Sequence["FilterTypeDef"]],
+        "FormatVersion": NotRequired[str],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+GetProductsRequestRequestTypeDef = TypedDict(
+    "GetProductsRequestRequestTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "Filters": NotRequired[Sequence["FilterTypeDef"]],
+        "FormatVersion": NotRequired[str],
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+GetProductsResponseTypeDef = TypedDict(
+    "GetProductsResponseTypeDef",
+    {
+        "FormatVersion": str,
+        "PriceList": List[str],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+ServiceTypeDef = TypedDict(
+    "ServiceTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "AttributeNames": NotRequired[List[str]],
+    },
+)
