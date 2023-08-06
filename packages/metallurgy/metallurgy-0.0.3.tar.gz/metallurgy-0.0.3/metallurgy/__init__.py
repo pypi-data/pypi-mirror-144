d5925d91c3cc9eadfe0ac75fb0065cb0c8a79920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:57 +0000

    Add package metallurgy/0.0.3/metallurgy-0.0.3.tar.gz

---
+++ packages/metallurgy/metallurgy-0.0.3.tar.gz/metallurgy-0.0.3/metallurgy/__init__.py
@@ -0,0 +1,34 @@
+from elementy.periodictable import PeriodicTable
+from .linear_mixture import linear_mixture
+from .deviation import deviation
+from .alloy import Alloy
+from . import density
+from . import valence
+from . import entropy
+from . import enthalpy
+from . import radii
+from . import structures
+from . import viscosity
+from . import price
+from . import binary
+from . import ternary
+
+
+periodic_table = PeriodicTable()
+
+__all__ = [
+    "linear_mixture",
+    "deviation",
+    "density",
+    "valence",
+    "entropy",
+    "enthalpy",
+    "radii",
+    "structures",
+    "price",
+    "viscosity",
+    "Alloy",
+    "periodic_table",
+    "binary",
+    "ternary"
+]
---
+++ packages/metallurgy/metallurgy-0.0.3.tar.gz/metallurgy-0.0.3/metallurgy/deviation.py
@@ -0,0 +1,61 @@
+import numpy as np
+import metallurgy as mg
+from .alloy import Alloy
+
+
+def deviation(alloy, feature_name):
+
+    if not isinstance(alloy, Alloy):
+        alloy = Alloy(alloy)
+
+    data = mg.periodic_table.data
+
+    if(len(alloy.elements) > 1):
+
+        if(isinstance(data[alloy.elements[0]][feature_name], (int, float, list))):
+
+            mean = 0
+            for element in alloy.elements:
+                value = data[element][feature_name]
+                if value is None:
+                    return None
+
+                if(isinstance(value, list)):
+                    value = value[0]
+
+                mean += alloy.composition[element] * value
+
+            deviation = 0
+            for element in alloy.elements:
+                value = data[element][feature_name]
+
+                if(isinstance(value, list)):
+                    value = value[0]
+
+                deviation += alloy.composition[element] * \
+                    ((value - mean)**2)
+
+            return deviation**0.5
+
+        else:
+            values = {}
+            for element in alloy.elements:
+                value = data[element][feature_name]
+                if value is None:
+                    return None
+
+                if value not in values:
+                    values[value] = 0
+                values[value] += alloy.composition[element]
+
+            if(len(values) > 1):
+                shannonEntropy = 0
+                for value in values:
+                    shannonEntropy -= values[value] * \
+                        np.log(values[value])
+                return shannonEntropy
+            else:
+                return 0
+
+    else:
+        return 0
