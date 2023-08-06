db1f9f1b11deec54bc4a51d0f7b2c568528ae115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:36 +0000

    Add package simple-pass-manager/0.0.4/simple_pass_manager-0.0.4.tar.gz

---
+++ packages/simple-pass-manager/simple_pass_manager-0.0.4.tar.gz/simple_pass_manager-0.0.4/simple_pass_manager/utils/encryption/__init__.py
@@ -0,0 +1,9 @@
+from .encryption import generate_key, key_encrypt, key_decrypt, password_encrypt, password_decrypt
+
+__all__ = [
+    'generate_key',
+    'key_encrypt',
+    'key_decrypt',
+    'password_encrypt',
+    'password_decrypt'
+]
