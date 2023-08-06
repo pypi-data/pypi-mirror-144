fb0816551744e92ed638da06ef4e9f3834577999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:15 +0000

    Add package immute/0.2.2/immute-0.2.2.tar.gz

---
+++ packages/immute/immute-0.2.2.tar.gz/immute-0.2.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['immute']
+setup_kwargs = {
+    'name': 'immute',
+    'version': '0.2.2',
+    'description': 'Create immutable python classes',
+    'long_description': '# Immute\n\nCreate "immutable" classes in python.\n\nI created a simple python object that, when inherited in another class, will prevent assignment operations outside the `__init__()` method.\n\n**Note: DOES NOT CREATE TRUELY IMMUTABLE REFERENCES**\n\nClasses inheriting from `Immutable` are not immutable references, only immutable from assignments, reassignment and deletion operations.\n\nClasses inheriting from `Immutable` are still mutable reference types, and not truly immutable like python\'s built-in types (int, float, bool, str, tuples).\n\n## Example\n\n```python\nfrom immute import Immutable\n\nclass Thing(Immutable):\n    def __init__(self) -> None:\n        self.num = 42\n\nthing = Thing()\n\nthing.num = 21\n# ^^^^^^ This will raise a TypeError exception\n```\n',
+    'author': 'fitzypop',
+    'author_email': 'fitzypop@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/fitzypop/immute',
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
