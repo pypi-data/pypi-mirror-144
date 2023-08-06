2ecd38e3c65c0fab354d06596fb18193cb1f37b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:05 +0000

    Add package smart-CoinMarketCap-Historical-Prices/1.0/smart-CoinMarketCap-Historical-Prices-1.0.tar.gz

---
+++ packages/smart-CoinMarketCap-Historical-Prices/smart-CoinMarketCap-Historical-Prices-1.0.tar.gz/smart-CoinMarketCap-Historical-Prices-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-CoinMarketCap-Historical-Prices",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['beautifulsoup4==4.7.1', 'bs4==0.0.1', 'certifi==2018.11.29', 'chardet==3.0.4', 'idna==2.8', 'requests==2.21.0', 'urllib3==1.25'],
+)
