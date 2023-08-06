a3c4f188455484c7ece043773bfe37471fa12ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:58 +0000

    Add package ivanov/0.0.2/ivanov-0.0.2-py3-none-any.whl

---
+++ packages/ivanov/ivanov-0.0.2-py3-none-any.whl/ivanov/sasma.py
@@ -0,0 +1,22 @@
+from core import get_sas_connect
+
+
+class SASMA():
+    def __init__(self, sasobjsp_user,
+                       sasobjsp_pass,
+                       sasobjsp_host='vs246.imb.ru',
+                       sasobjsp_port='8591',  **kwargs):
+        self.sasobjsp_host = sasobjsp_host
+        self.sasobjsp_port = sasobjsp_port
+        self.sasobjsp_user = sasobjsp_user
+        self.sasobjsp_pass = sasobjsp_pass
+        self.connected = False
+
+    def connect(self):
+        connection = get_sas_connect(sasobjsp_host=self.sasobjsp_host,
+                                     sasobjsp_port=self.sasobjsp_port,
+                                     sasobjsp_user=self.sasobjsp_user,
+                                     sasobjsp_pass=self.sasobjsp_pass)
+        if connection:
+            self.connected = True
+            return connection
