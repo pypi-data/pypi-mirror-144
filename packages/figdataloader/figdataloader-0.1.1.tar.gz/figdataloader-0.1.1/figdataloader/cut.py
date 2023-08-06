6a04fa34aba1f03170a11b6098d81d0bd90a3774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:42 +0000

    Add package figdataloader/0.1.1/figdataloader-0.1.1.tar.gz

---
+++ packages/figdataloader/figdataloader-0.1.1.tar.gz/figdataloader-0.1.1/figdataloader/cut.py
@@ -0,0 +1,69 @@
+def org(dataset_name="", *, fileconv: bool = False, countstart=0):
+    import promptlib
+    import os
+    from pathlib import Path
+    from PIL import Image
+    import glob
+    import os
+    import os
+    import logging
+    logging.basicConfig(filename="newfile.log",
+                        format='%(asctime)s %(message)s',
+                        filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    if fileconv == True:
+        prompter = promptlib.Files()
+        if not prompter:
+            return
+        dir = prompter.dir()
+        print(dir)
+        for count, filename in enumerate(os.listdir(dir), countstart):
+            dst = f"{str(dataset_name)}.{str(count)}.png"
+            src = f"{dir}/{filename}"  # foldername/filename, si el archivo .py está fuera de la carpeta.
+            dst = f"{dir}/{dst}"
+
+            # rename() cambiará el nombre a los archivos.
+            try:
+                os.rename(src, dst)
+            except FileExistsError:
+                logger.info("El archivo ya existe.")
+                break
+        inputPath = Path(dir)
+        inputFiles = inputPath.glob("**/*.png")
+        outputPath = Path(dir)
+
+        for f in inputFiles:
+            outputFile = outputPath / Path(f.stem + ".jpg")
+            im = Image.open(f)
+            try:
+                im.save(outputFile)
+            except FileExistsError:
+                logger.info("El archivo ya existe.")
+        test = os.listdir(dir)
+        for f2 in test:
+            if f2.endswith(".png"):
+                os.remove(os.path.join(dir, f2))
+            elif FileNotFoundError:
+                logger.info("El archivo no se encuentra.")
+
+    elif fileconv == False:
+        prompter = promptlib.Files()
+        if not prompter:
+            return
+        dir = prompter.dir()
+        if not dir:
+            return
+        print(dir)
+        for count, filename in enumerate(os.listdir(dir), countstart):
+            dst = f"{str(dataset_name)}.{str(count)}.png"
+            src = f"{dir}/{filename}"  # foldername/filename, si el archivo .py está fuera de la carpeta.
+            dst = f"{dir}/{dst}"
+
+            # rename() cambiará el nombre a los archivos.
+            try:
+                os.rename(src, dst)
+            except FileExistsError:
+                logger.info("El archivo ya existe.")
+                break
