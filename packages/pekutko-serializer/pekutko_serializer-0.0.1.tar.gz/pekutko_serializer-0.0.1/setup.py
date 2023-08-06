0c398f5684904e5713ee0adf426b79cf710f3819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:46 +0000

    Add package pekutko-serializer/0.0.1/pekutko_serializer-0.0.1.tar.gz

---
+++ packages/pekutko-serializer/pekutko_serializer-0.0.1.tar.gz/pekutko_serializer-0.0.1/setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pekutko_serializer",
+    version="0.0.1",
+    description="Dmitry Pekutko, gruop 053504, format cenverter (Serializer)",
+    author="Dmitry Pekutko",
+    author_email="dkefir03@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/DimaPekutko/ISP-2022-053504/",
+)
