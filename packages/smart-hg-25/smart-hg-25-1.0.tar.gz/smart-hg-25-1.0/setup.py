9faa5374854c4c3f05cc8a3b9836a562cc88def4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-25/1.0/smart-hg-25-1.0.tar.gz

---
+++ packages/smart-hg-25/smart-hg-25-1.0.tar.gz/smart-hg-25-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-25",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.20.3', 'torch==1.8.1', 'torchvision'],
+)
