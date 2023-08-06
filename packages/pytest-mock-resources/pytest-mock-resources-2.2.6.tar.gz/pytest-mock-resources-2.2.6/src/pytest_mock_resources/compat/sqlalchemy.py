7f78f898c0e6b7897fb92f7da54b48b5b73f303c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:57 +0000

    Add package pytest-mock-resources/2.2.6/pytest-mock-resources-2.2.6.tar.gz

---
+++ packages/pytest-mock-resources/pytest-mock-resources-2.2.6.tar.gz/pytest-mock-resources-2.2.6/src/pytest_mock_resources/compat/sqlalchemy.py
@@ -0,0 +1,21 @@
+import sqlalchemy.engine.url
+
+from pytest_mock_resources.compat.import_ import ImportAdaptor
+
+try:
+    from sqlalchemy.ext import asyncio
+except ImportError:
+    asyncio = ImportAdaptor(  # type: ignore
+        "SQLAlchemy",
+        "SQLAlchemy >= 1.4",
+        fail_message="Cannot use sqlalchemy async features with SQLAlchemy < 1.4.\n",
+    )
+
+URL = sqlalchemy.engine.url.URL
+try:
+    # Attempt to use the newly recommended `URL.create` method when available
+    URL = URL.create  # type: ignore
+except AttributeError:
+    # But if it's not available, the top-level contructor should still exist,
+    # as this is the only available option in sqlalchemy<1.4 versions.
+    pass
