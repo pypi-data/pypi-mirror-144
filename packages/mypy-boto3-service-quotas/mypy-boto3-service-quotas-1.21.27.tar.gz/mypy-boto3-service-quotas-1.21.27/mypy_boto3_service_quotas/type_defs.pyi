14fdd00d42523c9a8c006e7595e0948be08e450c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:17 +0000

    Add package mypy-boto3-service-quotas/1.21.27/mypy-boto3-service-quotas-1.21.27.tar.gz

---
+++ packages/mypy-boto3-service-quotas/mypy-boto3-service-quotas-1.21.27.tar.gz/mypy-boto3-service-quotas-1.21.27/mypy_boto3_service_quotas/type_defs.pyi
@@ -0,0 +1,504 @@
+"""
+Type annotations for service-quotas service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_service_quotas/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_service_quotas.type_defs import DeleteServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef
+
+    data: DeleteServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, List, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import (
+    ErrorCodeType,
+    PeriodUnitType,
+    RequestStatusType,
+    ServiceQuotaTemplateAssociationStatusType,
+)
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "DeleteServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef",
+    "ErrorReasonTypeDef",
+    "GetAWSDefaultServiceQuotaRequestRequestTypeDef",
+    "GetAWSDefaultServiceQuotaResponseTypeDef",
+    "GetAssociationForServiceQuotaTemplateResponseTypeDef",
+    "GetRequestedServiceQuotaChangeRequestRequestTypeDef",
+    "GetRequestedServiceQuotaChangeResponseTypeDef",
+    "GetServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef",
+    "GetServiceQuotaIncreaseRequestFromTemplateResponseTypeDef",
+    "GetServiceQuotaRequestRequestTypeDef",
+    "GetServiceQuotaResponseTypeDef",
+    "ListAWSDefaultServiceQuotasRequestListAWSDefaultServiceQuotasPaginateTypeDef",
+    "ListAWSDefaultServiceQuotasRequestRequestTypeDef",
+    "ListAWSDefaultServiceQuotasResponseTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryByQuotaRequestListRequestedServiceQuotaChangeHistoryByQuotaPaginateTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryByQuotaRequestRequestTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryByQuotaResponseTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryRequestListRequestedServiceQuotaChangeHistoryPaginateTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryRequestRequestTypeDef",
+    "ListRequestedServiceQuotaChangeHistoryResponseTypeDef",
+    "ListServiceQuotaIncreaseRequestsInTemplateRequestListServiceQuotaIncreaseRequestsInTemplatePaginateTypeDef",
+    "ListServiceQuotaIncreaseRequestsInTemplateRequestRequestTypeDef",
+    "ListServiceQuotaIncreaseRequestsInTemplateResponseTypeDef",
+    "ListServiceQuotasRequestListServiceQuotasPaginateTypeDef",
+    "ListServiceQuotasRequestRequestTypeDef",
+    "ListServiceQuotasResponseTypeDef",
+    "ListServicesRequestListServicesPaginateTypeDef",
+    "ListServicesRequestRequestTypeDef",
+    "ListServicesResponseTypeDef",
+    "ListTagsForResourceRequestRequestTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "MetricInfoTypeDef",
+    "PaginatorConfigTypeDef",
+    "PutServiceQuotaIncreaseRequestIntoTemplateRequestRequestTypeDef",
+    "PutServiceQuotaIncreaseRequestIntoTemplateResponseTypeDef",
+    "QuotaPeriodTypeDef",
+    "RequestServiceQuotaIncreaseRequestRequestTypeDef",
+    "RequestServiceQuotaIncreaseResponseTypeDef",
+    "RequestedServiceQuotaChangeTypeDef",
+    "ResponseMetadataTypeDef",
+    "ServiceInfoTypeDef",
+    "ServiceQuotaIncreaseRequestInTemplateTypeDef",
+    "ServiceQuotaTypeDef",
+    "TagResourceRequestRequestTypeDef",
+    "TagTypeDef",
+    "UntagResourceRequestRequestTypeDef",
+)
+
+DeleteServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef = TypedDict(
+    "DeleteServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+        "AwsRegion": str,
+    },
+)
+
+ErrorReasonTypeDef = TypedDict(
+    "ErrorReasonTypeDef",
+    {
+        "ErrorCode": NotRequired[ErrorCodeType],
+        "ErrorMessage": NotRequired[str],
+    },
+)
+
+GetAWSDefaultServiceQuotaRequestRequestTypeDef = TypedDict(
+    "GetAWSDefaultServiceQuotaRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+    },
+)
+
+GetAWSDefaultServiceQuotaResponseTypeDef = TypedDict(
+    "GetAWSDefaultServiceQuotaResponseTypeDef",
+    {
+        "Quota": "ServiceQuotaTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetAssociationForServiceQuotaTemplateResponseTypeDef = TypedDict(
+    "GetAssociationForServiceQuotaTemplateResponseTypeDef",
+    {
+        "ServiceQuotaTemplateAssociationStatus": ServiceQuotaTemplateAssociationStatusType,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetRequestedServiceQuotaChangeRequestRequestTypeDef = TypedDict(
+    "GetRequestedServiceQuotaChangeRequestRequestTypeDef",
+    {
+        "RequestId": str,
+    },
+)
+
+GetRequestedServiceQuotaChangeResponseTypeDef = TypedDict(
+    "GetRequestedServiceQuotaChangeResponseTypeDef",
+    {
+        "RequestedQuota": "RequestedServiceQuotaChangeTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef = TypedDict(
+    "GetServiceQuotaIncreaseRequestFromTemplateRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+        "AwsRegion": str,
+    },
+)
+
+GetServiceQuotaIncreaseRequestFromTemplateResponseTypeDef = TypedDict(
+    "GetServiceQuotaIncreaseRequestFromTemplateResponseTypeDef",
+    {
+        "ServiceQuotaIncreaseRequestInTemplate": "ServiceQuotaIncreaseRequestInTemplateTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GetServiceQuotaRequestRequestTypeDef = TypedDict(
+    "GetServiceQuotaRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+    },
+)
+
+GetServiceQuotaResponseTypeDef = TypedDict(
+    "GetServiceQuotaResponseTypeDef",
+    {
+        "Quota": "ServiceQuotaTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListAWSDefaultServiceQuotasRequestListAWSDefaultServiceQuotasPaginateTypeDef = TypedDict(
+    "ListAWSDefaultServiceQuotasRequestListAWSDefaultServiceQuotasPaginateTypeDef",
+    {
+        "ServiceCode": str,
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListAWSDefaultServiceQuotasRequestRequestTypeDef = TypedDict(
+    "ListAWSDefaultServiceQuotasRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListAWSDefaultServiceQuotasResponseTypeDef = TypedDict(
+    "ListAWSDefaultServiceQuotasResponseTypeDef",
+    {
+        "NextToken": str,
+        "Quotas": List["ServiceQuotaTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryByQuotaRequestListRequestedServiceQuotaChangeHistoryByQuotaPaginateTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryByQuotaRequestListRequestedServiceQuotaChangeHistoryByQuotaPaginateTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+        "Status": NotRequired[RequestStatusType],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryByQuotaRequestRequestTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryByQuotaRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+        "Status": NotRequired[RequestStatusType],
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryByQuotaResponseTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryByQuotaResponseTypeDef",
+    {
+        "NextToken": str,
+        "RequestedQuotas": List["RequestedServiceQuotaChangeTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryRequestListRequestedServiceQuotaChangeHistoryPaginateTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryRequestListRequestedServiceQuotaChangeHistoryPaginateTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "Status": NotRequired[RequestStatusType],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryRequestRequestTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryRequestRequestTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "Status": NotRequired[RequestStatusType],
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListRequestedServiceQuotaChangeHistoryResponseTypeDef = TypedDict(
+    "ListRequestedServiceQuotaChangeHistoryResponseTypeDef",
+    {
+        "NextToken": str,
+        "RequestedQuotas": List["RequestedServiceQuotaChangeTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListServiceQuotaIncreaseRequestsInTemplateRequestListServiceQuotaIncreaseRequestsInTemplatePaginateTypeDef = TypedDict(
+    "ListServiceQuotaIncreaseRequestsInTemplateRequestListServiceQuotaIncreaseRequestsInTemplatePaginateTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "AwsRegion": NotRequired[str],
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListServiceQuotaIncreaseRequestsInTemplateRequestRequestTypeDef = TypedDict(
+    "ListServiceQuotaIncreaseRequestsInTemplateRequestRequestTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "AwsRegion": NotRequired[str],
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListServiceQuotaIncreaseRequestsInTemplateResponseTypeDef = TypedDict(
+    "ListServiceQuotaIncreaseRequestsInTemplateResponseTypeDef",
+    {
+        "ServiceQuotaIncreaseRequestInTemplateList": List[
+            "ServiceQuotaIncreaseRequestInTemplateTypeDef"
+        ],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListServiceQuotasRequestListServiceQuotasPaginateTypeDef = TypedDict(
+    "ListServiceQuotasRequestListServiceQuotasPaginateTypeDef",
+    {
+        "ServiceCode": str,
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListServiceQuotasRequestRequestTypeDef = TypedDict(
+    "ListServiceQuotasRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListServiceQuotasResponseTypeDef = TypedDict(
+    "ListServiceQuotasResponseTypeDef",
+    {
+        "NextToken": str,
+        "Quotas": List["ServiceQuotaTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListServicesRequestListServicesPaginateTypeDef = TypedDict(
+    "ListServicesRequestListServicesPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListServicesRequestRequestTypeDef = TypedDict(
+    "ListServicesRequestRequestTypeDef",
+    {
+        "NextToken": NotRequired[str],
+        "MaxResults": NotRequired[int],
+    },
+)
+
+ListServicesResponseTypeDef = TypedDict(
+    "ListServicesResponseTypeDef",
+    {
+        "NextToken": str,
+        "Services": List["ServiceInfoTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListTagsForResourceRequestRequestTypeDef = TypedDict(
+    "ListTagsForResourceRequestRequestTypeDef",
+    {
+        "ResourceARN": str,
+    },
+)
+
+ListTagsForResourceResponseTypeDef = TypedDict(
+    "ListTagsForResourceResponseTypeDef",
+    {
+        "Tags": List["TagTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+MetricInfoTypeDef = TypedDict(
+    "MetricInfoTypeDef",
+    {
+        "MetricNamespace": NotRequired[str],
+        "MetricName": NotRequired[str],
+        "MetricDimensions": NotRequired[Dict[str, str]],
+        "MetricStatisticRecommendation": NotRequired[str],
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+PutServiceQuotaIncreaseRequestIntoTemplateRequestRequestTypeDef = TypedDict(
+    "PutServiceQuotaIncreaseRequestIntoTemplateRequestRequestTypeDef",
+    {
+        "QuotaCode": str,
+        "ServiceCode": str,
+        "AwsRegion": str,
+        "DesiredValue": float,
+    },
+)
+
+PutServiceQuotaIncreaseRequestIntoTemplateResponseTypeDef = TypedDict(
+    "PutServiceQuotaIncreaseRequestIntoTemplateResponseTypeDef",
+    {
+        "ServiceQuotaIncreaseRequestInTemplate": "ServiceQuotaIncreaseRequestInTemplateTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+QuotaPeriodTypeDef = TypedDict(
+    "QuotaPeriodTypeDef",
+    {
+        "PeriodValue": NotRequired[int],
+        "PeriodUnit": NotRequired[PeriodUnitType],
+    },
+)
+
+RequestServiceQuotaIncreaseRequestRequestTypeDef = TypedDict(
+    "RequestServiceQuotaIncreaseRequestRequestTypeDef",
+    {
+        "ServiceCode": str,
+        "QuotaCode": str,
+        "DesiredValue": float,
+    },
+)
+
+RequestServiceQuotaIncreaseResponseTypeDef = TypedDict(
+    "RequestServiceQuotaIncreaseResponseTypeDef",
+    {
+        "RequestedQuota": "RequestedServiceQuotaChangeTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+RequestedServiceQuotaChangeTypeDef = TypedDict(
+    "RequestedServiceQuotaChangeTypeDef",
+    {
+        "Id": NotRequired[str],
+        "CaseId": NotRequired[str],
+        "ServiceCode": NotRequired[str],
+        "ServiceName": NotRequired[str],
+        "QuotaCode": NotRequired[str],
+        "QuotaName": NotRequired[str],
+        "DesiredValue": NotRequired[float],
+        "Status": NotRequired[RequestStatusType],
+        "Created": NotRequired[datetime],
+        "LastUpdated": NotRequired[datetime],
+        "Requester": NotRequired[str],
+        "QuotaArn": NotRequired[str],
+        "GlobalQuota": NotRequired[bool],
+        "Unit": NotRequired[str],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+ServiceInfoTypeDef = TypedDict(
+    "ServiceInfoTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "ServiceName": NotRequired[str],
+    },
+)
+
+ServiceQuotaIncreaseRequestInTemplateTypeDef = TypedDict(
+    "ServiceQuotaIncreaseRequestInTemplateTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "ServiceName": NotRequired[str],
+        "QuotaCode": NotRequired[str],
+        "QuotaName": NotRequired[str],
+        "DesiredValue": NotRequired[float],
+        "AwsRegion": NotRequired[str],
+        "Unit": NotRequired[str],
+        "GlobalQuota": NotRequired[bool],
+    },
+)
+
+ServiceQuotaTypeDef = TypedDict(
+    "ServiceQuotaTypeDef",
+    {
+        "ServiceCode": NotRequired[str],
+        "ServiceName": NotRequired[str],
+        "QuotaArn": NotRequired[str],
+        "QuotaCode": NotRequired[str],
+        "QuotaName": NotRequired[str],
+        "Value": NotRequired[float],
+        "Unit": NotRequired[str],
+        "Adjustable": NotRequired[bool],
+        "GlobalQuota": NotRequired[bool],
+        "UsageMetric": NotRequired["MetricInfoTypeDef"],
+        "Period": NotRequired["QuotaPeriodTypeDef"],
+        "ErrorReason": NotRequired["ErrorReasonTypeDef"],
+    },
+)
+
+TagResourceRequestRequestTypeDef = TypedDict(
+    "TagResourceRequestRequestTypeDef",
+    {
+        "ResourceARN": str,
+        "Tags": Sequence["TagTypeDef"],
+    },
+)
+
+TagTypeDef = TypedDict(
+    "TagTypeDef",
+    {
+        "Key": str,
+        "Value": str,
+    },
+)
+
+UntagResourceRequestRequestTypeDef = TypedDict(
+    "UntagResourceRequestRequestTypeDef",
+    {
+        "ResourceARN": str,
+        "TagKeys": Sequence[str],
+    },
+)
