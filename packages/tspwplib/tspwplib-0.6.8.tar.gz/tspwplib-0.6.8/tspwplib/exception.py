de5f4599ab3bc72c846056e8a33eefcf4f0c3930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:58 +0000

    Add package tspwplib/0.6.8/tspwplib-0.6.8.tar.gz

---
+++ packages/tspwplib/tspwplib-0.6.8.tar.gz/tspwplib-0.6.8/tspwplib/exception.py
@@ -0,0 +1,27 @@
+"""Exceptions for Travelling Salesman Problem with Profits"""
+
+from networkx import exception as ex
+
+
+class EdgeNotFoundException(ex.NetworkXException):
+    """An edge could not be found in the graph"""
+
+
+class EdgesNotAdjacentException(ex.AmbiguousSolution):
+    """An edge list has been given non-adjacent edges which is ambiguous"""
+
+
+class NotSimpleException(ex.NetworkXException):
+    """A path, cycle or walk is not simple"""
+
+
+class NotSimpleCycleException(NotSimpleException):
+    """The walk was not a simple cycle"""
+
+
+class NotSimplePathException(NotSimpleException):
+    """The walk was not a simple path"""
+
+
+class UnexpectedSelfLoopException(ex.NetworkXException):
+    """A self loop was found where it wasn't exprected"""
