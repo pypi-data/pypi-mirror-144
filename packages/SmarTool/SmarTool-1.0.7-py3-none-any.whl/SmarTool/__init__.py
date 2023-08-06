2499cb6c452303318ac7a867a5a3ca5be3793d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:53 +0000

    Add package SmarTool/1.0.7/SmarTool-1.0.7-py3-none-any.whl

---
+++ packages/SmarTool/SmarTool-1.0.7-py3-none-any.whl/SmarTool/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+from .dtutil import DTUtil
+from .retry import retry
+from .timeout import TimeoutException, timeout
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+
+
+def hello():
+    welcome_msg = "======= Hello, this is SmarTool! ======="
+    print(welcome_msg)
+    print(" Author: Harpsichord")
+    print(" Email: tliu1217@163.com")
+    print(" Install: pip install SmarTool")
+    print(" Github: https://github.com/Harpsichord1207/SmarTool")
+    print("=" * len(welcome_msg))
+
+
+__all__ = [retry.__name__, hello.__name__, timeout.__name__, TimeoutException.__name__]
