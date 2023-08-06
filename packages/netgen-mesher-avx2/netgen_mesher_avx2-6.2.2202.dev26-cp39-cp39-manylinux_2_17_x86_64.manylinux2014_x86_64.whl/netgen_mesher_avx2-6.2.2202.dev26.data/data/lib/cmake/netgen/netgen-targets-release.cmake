b77e4fef2bf9597e5913357ece3ec5a18928ed53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:29 +0000

    Add package netgen-mesher-avx2/6.2.2202.dev26/netgen_mesher_avx2-6.2.2202.dev26-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/netgen-mesher-avx2/netgen_mesher_avx2-6.2.2202.dev26-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/netgen_mesher_avx2-6.2.2202.dev26.data/data/lib/cmake/netgen/netgen-targets-release.cmake
@@ -0,0 +1,149 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ngcore" for configuration "Release"
+set_property(TARGET ngcore APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ngcore PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libngcore.so"
+  IMPORTED_SONAME_RELEASE "libngcore.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ngcore )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ngcore "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libngcore.so" )
+
+# Import target "mesh" for configuration "Release"
+set_property(TARGET mesh APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(mesh PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libmesh.so"
+  IMPORTED_SONAME_RELEASE "libmesh.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS mesh )
+list(APPEND _IMPORT_CHECK_FILES_FOR_mesh "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libmesh.so" )
+
+# Import target "visual" for configuration "Release"
+set_property(TARGET visual APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(visual PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libvisual.so"
+  IMPORTED_SONAME_RELEASE "libvisual.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS visual )
+list(APPEND _IMPORT_CHECK_FILES_FOR_visual "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libvisual.so" )
+
+# Import target "csg" for configuration "Release"
+set_property(TARGET csg APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(csg PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libcsg.so"
+  IMPORTED_SONAME_RELEASE "libcsg.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS csg )
+list(APPEND _IMPORT_CHECK_FILES_FOR_csg "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libcsg.so" )
+
+# Import target "csgvis" for configuration "Release"
+set_property(TARGET csgvis APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(csgvis PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libcsgvis.so"
+  IMPORTED_SONAME_RELEASE "libcsgvis.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS csgvis )
+list(APPEND _IMPORT_CHECK_FILES_FOR_csgvis "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libcsgvis.so" )
+
+# Import target "geom2d" for configuration "Release"
+set_property(TARGET geom2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(geom2d PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgeom2d.so"
+  IMPORTED_SONAME_RELEASE "libgeom2d.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS geom2d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_geom2d "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgeom2d.so" )
+
+# Import target "geom2dvis" for configuration "Release"
+set_property(TARGET geom2dvis APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(geom2dvis PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgeom2dvis.so"
+  IMPORTED_SONAME_RELEASE "libgeom2dvis.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS geom2dvis )
+list(APPEND _IMPORT_CHECK_FILES_FOR_geom2dvis "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgeom2dvis.so" )
+
+# Import target "occ" for configuration "Release"
+set_property(TARGET occ APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(occ PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libocc.so"
+  IMPORTED_SONAME_RELEASE "libocc.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS occ )
+list(APPEND _IMPORT_CHECK_FILES_FOR_occ "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libocc.so" )
+
+# Import target "occvis" for configuration "Release"
+set_property(TARGET occvis APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(occvis PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/liboccvis.so"
+  IMPORTED_SONAME_RELEASE "liboccvis.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS occvis )
+list(APPEND _IMPORT_CHECK_FILES_FOR_occvis "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/liboccvis.so" )
+
+# Import target "stl" for configuration "Release"
+set_property(TARGET stl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(stl PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libstl.so"
+  IMPORTED_SONAME_RELEASE "libstl.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS stl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_stl "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libstl.so" )
+
+# Import target "stlvis" for configuration "Release"
+set_property(TARGET stlvis APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(stlvis PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libstlvis.so"
+  IMPORTED_SONAME_RELEASE "libstlvis.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS stlvis )
+list(APPEND _IMPORT_CHECK_FILES_FOR_stlvis "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libstlvis.so" )
+
+# Import target "interface" for configuration "Release"
+set_property(TARGET interface APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(interface PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libinterface.so"
+  IMPORTED_SONAME_RELEASE "libinterface.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS interface )
+list(APPEND _IMPORT_CHECK_FILES_FOR_interface "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libinterface.so" )
+
+# Import target "gui" for configuration "Release"
+set_property(TARGET gui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gui PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgui.so"
+  IMPORTED_SONAME_RELEASE "libgui.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gui )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gui "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libgui.so" )
+
+# Import target "nglib" for configuration "Release"
+set_property(TARGET nglib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(nglib PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libnglib.so"
+  IMPORTED_SONAME_RELEASE "libnglib.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS nglib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_nglib "${_IMPORT_PREFIX}/lib/python3.9/site-packages/netgen_mesher_avx2.libs/libnglib.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
