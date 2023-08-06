5eab69c6480f61a6a8118a52f7da3c1ab891ce24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:16 +0000

    Add package perceptron-pkg-pkamit/0.0.2/perceptron_pkg-pkamit-0.0.2.tar.gz

---
+++ packages/perceptron-pkg-pkamit/perceptron_pkg-pkamit-0.0.2.tar.gz/perceptron_pkg-pkamit-0.0.2/README.md
@@ -0,0 +1,7 @@
+# python package for testing pamit
+
+## how to use
+```python
+from amitAi.perceptron import Perceptron
+model= Perceptron(eta=0.3,epochs=10)
+```
---
+++ packages/perceptron-pkg-pkamit/perceptron_pkg-pkamit-0.0.2.tar.gz/perceptron_pkg-pkamit-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+PROJECT_NAME = "perceptron_pkg"
+USER_NAME = "pkamit"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=USER_NAME,
+    author_email="amit.pandey812@gmail.com",
+    description="A small package for perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy==1.21.5",
+        "pandas==1.3.5",
+        "joblib==1.1.0"
+    ]
+)
