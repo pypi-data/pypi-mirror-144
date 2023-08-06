935e51e028b082f3285fe2b4521f95e09a4ac872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package backend.ai-manager/21.3.32/backend.ai_manager-21.3.32-py3-none-any.whl

---
+++ packages/backend.ai-manager/backend.ai_manager-21.3.32-py3-none-any.whl/ai/backend/manager/defs.py
@@ -0,0 +1,42 @@
+"""
+Common definitions/constants used throughout the manager.
+"""
+
+import enum
+import re
+from typing import Final
+
+from ai.backend.common.types import SlotName, SlotTypes
+
+
+INTRINSIC_SLOTS: Final = {
+    SlotName('cpu'): SlotTypes('count'),
+    SlotName('mem'): SlotTypes('bytes'),
+}
+
+
+# The default container role name for multi-container sessions
+DEFAULT_ROLE: Final = "main"
+
+_RESERVED_VFOLDER_PATTERNS = [r'^\.[a-z0-9]+rc$', r'^\.[a-z0-9]+_profile$']
+RESERVED_DOTFILES = ['.terminfo', '.jupyter', '.ssh', '.ssh/authorized_keys', '.local', '.config']
+RESERVED_VFOLDERS = ['.terminfo', '.jupyter', '.tmux.conf', '.ssh']
+RESERVED_VFOLDER_PATTERNS = [re.compile(x) for x in _RESERVED_VFOLDER_PATTERNS]
+
+# Redis database IDs depending on purposes
+REDIS_STAT_DB: Final = 0
+REDIS_RLIM_DB: Final = 1
+REDIS_LIVE_DB: Final = 2
+REDIS_IMAGE_DB: Final = 3
+REDIS_STREAM_DB: Final = 4
+
+
+# PostgreSQL session-level advisory lock indentifiers
+class AdvisoryLock(enum.IntEnum):
+    LOCKID_TEST = 42
+    LOCKID_SCHEDULE = 91
+    LOCKID_PREPARE = 92
+    LOCKID_SCHEDULE_TIMER = 191
+    LOCKID_PREPARE_TIMER = 192
+    LOCKID_LOG_CLEANUP_TIMER = 195
+    LOCKID_IDLE_CHECK_TIMER = 196
