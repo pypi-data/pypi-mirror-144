263809dabd34edb4ff554ee5f3420c8ce09889da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:48 +0000

    Add package stnavbar/0.0.1/stnavbar-0.0.1.tar.gz

---
+++ packages/stnavbar/stnavbar-0.0.1.tar.gz/stnavbar-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A navbar for streamlit'
+LONG_DESCRIPTION = 'A simple extension for streamlit which allows you to add a navbar to streamlit.'
+
+# Setting up
+setup(
+    name="stnavbar",
+    version=VERSION,
+    author="BugzTheBunny",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['streamlit'],
+    keywords=['streamlit_nav', 'streamlit navbar', 'navbar', 'streamlit'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
