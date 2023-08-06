1e77a7da626c9d60f9211f8e4ea71295e781fa74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:03 +0000

    Add package ptrmetagen/1.0.0/ptrmetagen-1.0.0-py3-none-any.whl

---
+++ packages/ptrmetagen/ptrmetagen-1.0.0-py3-none-any.whl/metagen/__init__.py
@@ -0,0 +1,3 @@
+from metagen.base import Generator
+from elements import Application, Configuration, Scope, Place, Period, LayerTemplate, Attribute, SpatialWMS, \
+    RelationSpatial, SpatialAttribute, SpatialWMTS, SpatialVector, Style, Case, RelationAttribute
