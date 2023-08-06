e5543da30ccdbdef7df9e7fc84415c2aa99a56c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:14 +0000

    Add package randomheader/0.4/randomheader-0.4.tar.gz

---
+++ packages/randomheader/randomheader-0.4.tar.gz/randomheader-0.4/randomheader/__init__.py
@@ -0,0 +1,5 @@
+"""Generates random HTTP headers for requests"""
+
+__version__ = "0.4"
+
+from randomheader.header import RandomHeader
---
+++ packages/randomheader/randomheader-0.4.tar.gz/randomheader-0.4/randomheader/header.py
@@ -0,0 +1,40 @@
+from random import choice, sample, shuffle
+from typing import Iterator
+
+import os
+path = os.path.abspath(os.path.dirname(__file__))
+
+
+class RandomHeader:
+
+    def __init__(self) -> None:
+        self.accepts = [*self.readlines(f'{path}/agents/accepts.txt')]
+        self.encodings = [*self.readlines(f'{path}/agents/encodings.txt')]
+        self.referrers = [*self.readlines(f'{path}/agents/referrer.txt')]
+        self.user_agents = [*self.readlines(f'{path}/agents/user-agents.txt')]
+
+    def header(self) -> dict:
+        return self.shuffle_header({
+            'Accept': choice(self.accepts),
+            'Accept-Encoding': choice(self.encodings),
+            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'Referer': choice(self.referrers),
+            'Dnt': choice(('0', '1')),
+            'Connection': 'keep-alive',
+            'X-Forwarded-For': self.random_ip(),
+            'User-Agent': choice(self.user_agents),
+            'Upgrade-Insecure-Requests': '1'
+        })
+
+    def random_ip(self) -> str:
+        return '{}.{}.{}.{}'.format(*sample(range(0, 255), 4))
+
+    def readlines(self, filename: str) -> Iterator[str]:
+        with open(filename, 'r') as f:
+            for line in f:
+                yield line.strip()
+
+    def shuffle_header(self, header: dict) -> dict:
+        keys = list(header.keys())
+        shuffle(keys)
+        return {k:header[k] for k in keys}
