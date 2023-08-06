1834448b79e886341fc8fb6cbfe3e672b01d7db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package barotropic/3.0.1/barotropic-3.0.1.tar.gz

---
+++ packages/barotropic/barotropic-3.0.1.tar.gz/barotropic-3.0.1/barotropic/__init__.py
@@ -0,0 +1,16 @@
+from . import diagnostics, plot, rhs, init, io
+from .constants import *
+from .grid import Grid
+from .model import BarotropicModel
+from .state import State
+
+__version__ = "3.0.1"
+
+# Explicitly define the public interface
+__all__ = [
+    "diagnostics", "init", "plot", "rhs", "io",
+    "Grid", "BarotropicModel", "State",
+    "ZONAL", "MERIDIONAL",
+    "MIN", "HOUR", "DAY", "WEEK"
+]
+
