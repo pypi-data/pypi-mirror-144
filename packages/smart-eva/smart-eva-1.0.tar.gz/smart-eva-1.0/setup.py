9e24d0450119cbe5e7b89ae6c7f93d4acfb37937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:14 +0000

    Add package smart-eva/1.0/smart-eva-1.0.tar.gz

---
+++ packages/smart-eva/smart-eva-1.0.tar.gz/smart-eva-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-eva",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['torch==1.7.0', 'scipy==1.5.2', 'numpy==1.19.2'],
+)
