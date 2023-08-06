0a0c9d62d39f9171c40f12fff8b671768d10fd1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:28 +0000

    Add package env-alias/0.4.0/env-alias-0.4.0.tar.gz

---
+++ packages/env-alias/env-alias-0.4.0.tar.gz/env-alias-0.4.0/src/env_alias/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2020 Nicholas de Jong
+
+__title__ = "env-alias"
+__author__ = "Nicholas de Jong <contact@nicholasdejong.com>"
+__version__ = "0.4.0"
+__license__ = "BSD2"
+
+__logger_default_level__ = "error"
+__env_alias_generator__ = "env-alias-generator"
