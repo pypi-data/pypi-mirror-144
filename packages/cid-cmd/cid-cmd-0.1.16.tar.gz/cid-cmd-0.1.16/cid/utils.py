9ad604bfa59a54cb06638450637355b123c07219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:21:50 +0000

    Add package cid-cmd/0.1.16/cid-cmd-0.1.16.tar.gz

---
+++ packages/cid-cmd/cid-cmd-0.1.16.tar.gz/cid-cmd-0.1.16/cid/utils.py
@@ -0,0 +1,52 @@
+import boto3
+
+from botocore.exceptions import NoCredentialsError, CredentialRetrievalError, NoRegionError
+
+import logging
+logger = logging.getLogger(__name__)
+
+def get_aws_region():
+    return get_boto_session().region_name
+
+def get_boto_session(**kwargs):
+    """
+    Initiates boto's session object
+    :param region: region name
+    :param args: arguments
+    :return: Boto3 Client
+    """
+    try:
+        session = boto3.session.Session(**kwargs)
+        logger.info('Boto3 session created')
+        logger.debug(session)
+        if not session.region_name:
+            raise NoRegionError
+        return session
+    except (NoCredentialsError, CredentialRetrievalError):
+        print('Error: unable to initialize session, please check your AWS credentials, exiting')
+        exit(1)
+    except NoRegionError:
+        logger.info('No AWS region set, defaulting to us-east-1')
+        kwargs.update({'region_name': 'us-east-1'})
+        return get_boto_session(**kwargs)
+    except Exception as e:
+        logger.debug(e, stack_info=True)
+        raise
+
+def get_boto_client(service_name, **kwargs):
+    """
+    Initiates boto's client object
+    :param service_name: service name
+    :param region: region name
+    :param args: arguments
+    :return: Boto3 Client
+    """
+    try:
+        session = get_boto_session(**kwargs)
+        return session.client(service_name)
+    except (NoCredentialsError, CredentialRetrievalError):
+        print('Error: unable to initialize boto client, please check your AWS credentials, exiting')
+        exit(1)
+    except Exception as e:
+        logger.debug(e, stack_info=True)
+        raise
