d6a09e7537520553acee72f92630445451d752b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:34 +0000

    Add package blipleo/0.1a5.post1/blipleo-0.1a5.post1.tar.gz

---
+++ packages/blipleo/blipleo-0.1a5.post1.tar.gz/blipleo-0.1a5.post1/blipleo/__version__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# chirptext's package version information
+__author__ = "Le Tuan Anh"
+__email__ = "tuananh.le@ntu.edu.sg"
+__copyright__ = "Copyright (c) 2020, BLIP lab, NTU, Singapore"
+__credits__ = []
+__license__ = "GNU General Public License v3 (GPLv3)"
+__description__ = "Language Experience Overview (LEO) Report Generator"
+__url__ = "https://blipntu.github.io/LEO/"
+__maintainer__ = "Le Tuan Anh"
+__version_major__ = "0.1"
+__version__ = "{}a5.post1".format(__version_major__)
+__version_long__ = "{} - Alpha".format(__version_major__)
+__status__ = "3 - Alpha"
