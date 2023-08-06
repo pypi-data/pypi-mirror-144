2c677123fc9d989c420db0064935bb7784371214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:08 +0000

    Add package caosdb/0.7.2/caosdb-0.7.2-py3-none-any.whl

---
+++ packages/caosdb/caosdb-0.7.2-py3-none-any.whl/caosdb/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM caosdb SETUP.PY
+#
+short_version = '0.7.2'
+version = '0.7.2'
+full_version = '0.7.2'
+git_revision = '360d94ca426ab65fc4789ca03c20797d5fc7bcf6'
+release = True
+
+if not release:
+    version = full_version
