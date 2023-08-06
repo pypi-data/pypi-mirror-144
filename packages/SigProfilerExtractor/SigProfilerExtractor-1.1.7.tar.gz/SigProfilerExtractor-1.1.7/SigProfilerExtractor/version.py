6dbf0943e343cf37192540ec6cb6729412c7b033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:32 +0000

    Add package SigProfilerExtractor/1.1.7/SigProfilerExtractor-1.1.7.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.7.tar.gz/SigProfilerExtractor-1.1.7/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.7'
+version = '1.1.7'
+Update = 'Integrating SigProfilerAssignment for decomposition and subsequent attribution of de novo signatures'
+    
+    
