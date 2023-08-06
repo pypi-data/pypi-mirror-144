ad5b206b0b029e8ca5c4cf6d2ab6dec4f44b1d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:20 +0000

    Add package nexxT/0.9.1/nexxT-0.9.1.zip

---
+++ packages/nexxT/nexxT-0.9.1.zip/nexxT-0.9.1/nexxT/src/cnexxT.h
@@ -0,0 +1,20 @@
+/* 
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2020 ifm electronic gmbh
+ *
+ * THE PROGRAM IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
+ */
+
+#define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
+
+#include "DataSamples.hpp"
+#include "Ports.hpp"
+#include "OutputPortInterface.hpp"
+#include "InputPortInterface.hpp"
+#include "Filters.hpp"
+#include "FilterEnvironment.hpp"
+#include "Services.hpp"
+#include "PropertyCollection.hpp"
+#include "NexxTPlugins.hpp"
+#include "Logger.hpp"
+#include "SharedPointerTypes.hpp"
