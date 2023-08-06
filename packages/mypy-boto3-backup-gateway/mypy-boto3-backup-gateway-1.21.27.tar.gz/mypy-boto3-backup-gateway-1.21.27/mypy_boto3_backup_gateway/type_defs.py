f577825859ac170c15dffd2c70b62b3d22dca658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:10 +0000

    Add package mypy-boto3-backup-gateway/1.21.27/mypy-boto3-backup-gateway-1.21.27.tar.gz

---
+++ packages/mypy-boto3-backup-gateway/mypy-boto3-backup-gateway-1.21.27.tar.gz/mypy-boto3-backup-gateway-1.21.27/mypy_boto3_backup_gateway/type_defs.py
@@ -0,0 +1,417 @@
+"""
+Type annotations for backup-gateway service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_backup_gateway/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_backup_gateway.type_defs import AssociateGatewayToServerInputRequestTypeDef
+
+    data: AssociateGatewayToServerInputRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, List, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import HypervisorStateType
+
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "AssociateGatewayToServerInputRequestTypeDef",
+    "AssociateGatewayToServerOutputTypeDef",
+    "CreateGatewayInputRequestTypeDef",
+    "CreateGatewayOutputTypeDef",
+    "DeleteGatewayInputRequestTypeDef",
+    "DeleteGatewayOutputTypeDef",
+    "DeleteHypervisorInputRequestTypeDef",
+    "DeleteHypervisorOutputTypeDef",
+    "DisassociateGatewayFromServerInputRequestTypeDef",
+    "DisassociateGatewayFromServerOutputTypeDef",
+    "GatewayTypeDef",
+    "HypervisorTypeDef",
+    "ImportHypervisorConfigurationInputRequestTypeDef",
+    "ImportHypervisorConfigurationOutputTypeDef",
+    "ListGatewaysInputListGatewaysPaginateTypeDef",
+    "ListGatewaysInputRequestTypeDef",
+    "ListGatewaysOutputTypeDef",
+    "ListHypervisorsInputListHypervisorsPaginateTypeDef",
+    "ListHypervisorsInputRequestTypeDef",
+    "ListHypervisorsOutputTypeDef",
+    "ListTagsForResourceInputRequestTypeDef",
+    "ListTagsForResourceOutputTypeDef",
+    "ListVirtualMachinesInputListVirtualMachinesPaginateTypeDef",
+    "ListVirtualMachinesInputRequestTypeDef",
+    "ListVirtualMachinesOutputTypeDef",
+    "PaginatorConfigTypeDef",
+    "PutMaintenanceStartTimeInputRequestTypeDef",
+    "PutMaintenanceStartTimeOutputTypeDef",
+    "ResponseMetadataTypeDef",
+    "TagResourceInputRequestTypeDef",
+    "TagResourceOutputTypeDef",
+    "TagTypeDef",
+    "TestHypervisorConfigurationInputRequestTypeDef",
+    "UntagResourceInputRequestTypeDef",
+    "UntagResourceOutputTypeDef",
+    "UpdateGatewayInformationInputRequestTypeDef",
+    "UpdateGatewayInformationOutputTypeDef",
+    "UpdateHypervisorInputRequestTypeDef",
+    "UpdateHypervisorOutputTypeDef",
+    "VirtualMachineTypeDef",
+)
+
+AssociateGatewayToServerInputRequestTypeDef = TypedDict(
+    "AssociateGatewayToServerInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+        "ServerArn": str,
+    },
+)
+
+AssociateGatewayToServerOutputTypeDef = TypedDict(
+    "AssociateGatewayToServerOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+CreateGatewayInputRequestTypeDef = TypedDict(
+    "CreateGatewayInputRequestTypeDef",
+    {
+        "ActivationKey": str,
+        "GatewayDisplayName": str,
+        "GatewayType": Literal["BACKUP_VM"],
+        "Tags": NotRequired[Sequence["TagTypeDef"]],
+    },
+)
+
+CreateGatewayOutputTypeDef = TypedDict(
+    "CreateGatewayOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DeleteGatewayInputRequestTypeDef = TypedDict(
+    "DeleteGatewayInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+    },
+)
+
+DeleteGatewayOutputTypeDef = TypedDict(
+    "DeleteGatewayOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DeleteHypervisorInputRequestTypeDef = TypedDict(
+    "DeleteHypervisorInputRequestTypeDef",
+    {
+        "HypervisorArn": str,
+    },
+)
+
+DeleteHypervisorOutputTypeDef = TypedDict(
+    "DeleteHypervisorOutputTypeDef",
+    {
+        "HypervisorArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DisassociateGatewayFromServerInputRequestTypeDef = TypedDict(
+    "DisassociateGatewayFromServerInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+    },
+)
+
+DisassociateGatewayFromServerOutputTypeDef = TypedDict(
+    "DisassociateGatewayFromServerOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+GatewayTypeDef = TypedDict(
+    "GatewayTypeDef",
+    {
+        "GatewayArn": NotRequired[str],
+        "GatewayDisplayName": NotRequired[str],
+        "GatewayType": NotRequired[Literal["BACKUP_VM"]],
+        "HypervisorId": NotRequired[str],
+        "LastSeenTime": NotRequired[datetime],
+    },
+)
+
+HypervisorTypeDef = TypedDict(
+    "HypervisorTypeDef",
+    {
+        "Host": NotRequired[str],
+        "HypervisorArn": NotRequired[str],
+        "KmsKeyArn": NotRequired[str],
+        "Name": NotRequired[str],
+        "State": NotRequired[HypervisorStateType],
+    },
+)
+
+ImportHypervisorConfigurationInputRequestTypeDef = TypedDict(
+    "ImportHypervisorConfigurationInputRequestTypeDef",
+    {
+        "Host": str,
+        "Name": str,
+        "KmsKeyArn": NotRequired[str],
+        "Password": NotRequired[str],
+        "Tags": NotRequired[Sequence["TagTypeDef"]],
+        "Username": NotRequired[str],
+    },
+)
+
+ImportHypervisorConfigurationOutputTypeDef = TypedDict(
+    "ImportHypervisorConfigurationOutputTypeDef",
+    {
+        "HypervisorArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListGatewaysInputListGatewaysPaginateTypeDef = TypedDict(
+    "ListGatewaysInputListGatewaysPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListGatewaysInputRequestTypeDef = TypedDict(
+    "ListGatewaysInputRequestTypeDef",
+    {
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+    },
+)
+
+ListGatewaysOutputTypeDef = TypedDict(
+    "ListGatewaysOutputTypeDef",
+    {
+        "Gateways": List["GatewayTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListHypervisorsInputListHypervisorsPaginateTypeDef = TypedDict(
+    "ListHypervisorsInputListHypervisorsPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListHypervisorsInputRequestTypeDef = TypedDict(
+    "ListHypervisorsInputRequestTypeDef",
+    {
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+    },
+)
+
+ListHypervisorsOutputTypeDef = TypedDict(
+    "ListHypervisorsOutputTypeDef",
+    {
+        "Hypervisors": List["HypervisorTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListTagsForResourceInputRequestTypeDef = TypedDict(
+    "ListTagsForResourceInputRequestTypeDef",
+    {
+        "ResourceArn": str,
+    },
+)
+
+ListTagsForResourceOutputTypeDef = TypedDict(
+    "ListTagsForResourceOutputTypeDef",
+    {
+        "ResourceArn": str,
+        "Tags": List["TagTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListVirtualMachinesInputListVirtualMachinesPaginateTypeDef = TypedDict(
+    "ListVirtualMachinesInputListVirtualMachinesPaginateTypeDef",
+    {
+        "PaginationConfig": NotRequired["PaginatorConfigTypeDef"],
+    },
+)
+
+ListVirtualMachinesInputRequestTypeDef = TypedDict(
+    "ListVirtualMachinesInputRequestTypeDef",
+    {
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+    },
+)
+
+ListVirtualMachinesOutputTypeDef = TypedDict(
+    "ListVirtualMachinesOutputTypeDef",
+    {
+        "NextToken": str,
+        "VirtualMachines": List["VirtualMachineTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+PutMaintenanceStartTimeInputRequestTypeDef = TypedDict(
+    "PutMaintenanceStartTimeInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+        "HourOfDay": int,
+        "MinuteOfHour": int,
+        "DayOfMonth": NotRequired[int],
+        "DayOfWeek": NotRequired[int],
+    },
+)
+
+PutMaintenanceStartTimeOutputTypeDef = TypedDict(
+    "PutMaintenanceStartTimeOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+TagResourceInputRequestTypeDef = TypedDict(
+    "TagResourceInputRequestTypeDef",
+    {
+        "ResourceARN": str,
+        "Tags": Sequence["TagTypeDef"],
+    },
+)
+
+TagResourceOutputTypeDef = TypedDict(
+    "TagResourceOutputTypeDef",
+    {
+        "ResourceARN": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+TagTypeDef = TypedDict(
+    "TagTypeDef",
+    {
+        "Key": str,
+        "Value": str,
+    },
+)
+
+TestHypervisorConfigurationInputRequestTypeDef = TypedDict(
+    "TestHypervisorConfigurationInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+        "Host": str,
+        "Password": NotRequired[str],
+        "Username": NotRequired[str],
+    },
+)
+
+UntagResourceInputRequestTypeDef = TypedDict(
+    "UntagResourceInputRequestTypeDef",
+    {
+        "ResourceARN": str,
+        "TagKeys": Sequence[str],
+    },
+)
+
+UntagResourceOutputTypeDef = TypedDict(
+    "UntagResourceOutputTypeDef",
+    {
+        "ResourceARN": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+UpdateGatewayInformationInputRequestTypeDef = TypedDict(
+    "UpdateGatewayInformationInputRequestTypeDef",
+    {
+        "GatewayArn": str,
+        "GatewayDisplayName": NotRequired[str],
+    },
+)
+
+UpdateGatewayInformationOutputTypeDef = TypedDict(
+    "UpdateGatewayInformationOutputTypeDef",
+    {
+        "GatewayArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+UpdateHypervisorInputRequestTypeDef = TypedDict(
+    "UpdateHypervisorInputRequestTypeDef",
+    {
+        "HypervisorArn": str,
+        "Host": NotRequired[str],
+        "Password": NotRequired[str],
+        "Username": NotRequired[str],
+    },
+)
+
+UpdateHypervisorOutputTypeDef = TypedDict(
+    "UpdateHypervisorOutputTypeDef",
+    {
+        "HypervisorArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+VirtualMachineTypeDef = TypedDict(
+    "VirtualMachineTypeDef",
+    {
+        "HostName": NotRequired[str],
+        "HypervisorId": NotRequired[str],
+        "LastBackupDate": NotRequired[datetime],
+        "Name": NotRequired[str],
+        "Path": NotRequired[str],
+        "ResourceArn": NotRequired[str],
+    },
+)
