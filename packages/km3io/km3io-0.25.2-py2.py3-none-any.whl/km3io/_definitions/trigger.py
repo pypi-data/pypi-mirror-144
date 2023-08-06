efc5f9437de48770f1fea587f40c36e9f3ce0a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:42 +0000

    Add package km3io/0.25.2/km3io-0.25.2-py2.py3-none-any.whl

---
+++ packages/km3io/km3io-0.25.2-py2.py3-none-any.whl/km3io/_definitions/trigger.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+KM3NeT Data Definitions v2.1.0-1-ga85a9c1
+https://git.km3net.de/common/km3net-dataformat
+"""
+
+# trigger
+data = dict(
+    JTRIGGER3DSHOWER=1,
+    JTRIGGERMXSHOWER=2,
+    JTRIGGER3DMUON=4,
+    JTRIGGERNB=5,
+    FACTORY_LIMIT=31,
+)
