ba7c2c2065acdbe35e638e75c9e834b653402748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:57 +0000

    Add package django-camomilla-cms/5.7.1/django_camomilla_cms-5.7.1-py2.py3-none-any.whl

---
+++ packages/django-camomilla-cms/django_camomilla_cms-5.7.1-py2.py3-none-any.whl/camomilla/templatetags/camomilla_filters.py
@@ -0,0 +1,23 @@
+from django import template
+from django.utils.translation import get_language
+
+
+register = template.Library()
+
+
+@register.filter(name="filter_content")
+def filter_content(page, args):
+    try:
+        content = page.contents.get(identifier=args)
+    except page.contents.model.DoesNotExist:
+        content, _ = page.contents.get_or_create(identifier=args)
+    if get_language() not in content.get_available_languages():
+        content.translate(get_language())
+        content.save()
+    return content
+
+
+@register.filter(name="alternate_urls")
+def alternate_urls(page, request):
+    alternates = page.alternate_urls(request)
+    return alternates.get("alternate_urls", alternates).items()
