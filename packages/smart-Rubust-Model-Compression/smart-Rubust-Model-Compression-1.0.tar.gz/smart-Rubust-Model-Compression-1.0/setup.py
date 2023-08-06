e121e99efd4f6189ef7ac641e73eec57c39a9f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:43 +0000

    Add package smart-Rubust-Model-Compression/1.0/smart-Rubust-Model-Compression-1.0.tar.gz

---
+++ packages/smart-Rubust-Model-Compression/smart-Rubust-Model-Compression-1.0.tar.gz/smart-Rubust-Model-Compression-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-Rubust-Model-Compression",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['astroid==2.3.3', 'backcall==0.1.0', 'certifi==2020.4.5.1', 'cffi==1.14.0', 'colorama==0.4.3', 'decorator==4.4.2', 'imageio==2.8.0', 'ipykernel==5.3.0', 'ipython==7.14.0', 'ipython-genutils==0.2.0', 'isort==4.3.21', 'jedi==0.17.0', 'joblib==0.15.1', 'jupyter-client==6.1.3', 'jupyter-core==4.6.3', 'lazy-object-proxy==1.4.3', 'lime==0.2.0.0', 'mccabe==0.6.1', 'networkx==2.4', 'numpy==1.18.1', 'scikit-learn==0.22.1', 'scikit-image==0.17.2', 'scipy==1.4.1', 'pandas==1.0.3', 'parso==0.7.0', 'pickleshare==0.7.5', 'prompt-toolkit==3.0.5', 'pycparser==2.20', 'Pygments==2.6.1', 'pylint==2.4.4', 'python-dateutil==2.8.1', 'pytz==2020.1', 'PyWavelets==1.1.1', 'pywin32==227', 'pyzmq==19.0.1', 'SALib==1.3.11', 'six==1.14.0', 'tifffile==2020.5.25', 'traitlets==4.3.3', 'typed-ast==1.4.1', 'wcwidth==0.1.9', 'wincertstore==0.2', 'wrapt==1.11.2'],
+)
