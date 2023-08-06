a1dde6d54d5250149424068001680d46347b3338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:24:41 +0000

    Add package PyZohoBooksAPI/0.0.0/PyZohoBooksAPI-0.0.0.tar.gz

---
+++ packages/PyZohoBooksAPI/PyZohoBooksAPI-0.0.0.tar.gz/PyZohoBooksAPI-0.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Py Zoho Books API'
+
+
+# Setting up
+setup(
+    name="PyZohoBooksAPI",
+    version=VERSION,
+    author="marwan mohamed",
+    author_email="marawan6569@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=['python', 'API', 'zoho', 'zoho books',],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
