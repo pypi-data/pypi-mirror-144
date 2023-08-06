4c9279f9d277fb10f6c3405751def433d445804a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:19 +0000

    Add package NN4MSP/2.11/NN4MSP-2.11.tar.gz

---
+++ packages/NN4MSP/NN4MSP-2.11.tar.gz/NN4MSP-2.11/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.11' 
+DESCRIPTION = 'Neural network based control charting for multiple stream processes'
+
+def readme():
+    with open('README.md', encoding='latin-1') as f:
+        return f.read()
+
+# Setting up
+
+setup(
+        name="NN4MSP", 
+        version=VERSION,
+        author='Antonio Lepore, Biagio Palumbo, Gianluca Sposito',
+        author_email='antonio.lepore@unina.it, biagio.palumbo@unina.it, gianluca.sposito@unina.it',
+        description=DESCRIPTION,
+        long_description=readme(),
+        long_description_content_type="text/markdown",
+        url='https://github.com/unina-sfere/NN4MSP',
+        license='GNU General Public License v3.0',
+        packages=find_packages(),
+        install_requires=['numpy', 'matplotlib', 'sklearn', 'tensorflow', 'keras'],
+        keywords=['Multiple stream process', 'Artificial neural networks', 'Statistical process control', 'Multilayer perceptron', 'Railway HVAC systems'],
+        classifiers=[
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Manufacturing",
+            "Intended Audience :: Developers",    
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        include_package_data = True,
+        package_data={'': ['data/*.csv']},
+)
+
+
