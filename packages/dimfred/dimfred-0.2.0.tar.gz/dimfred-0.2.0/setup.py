62a245aeb8959dbf83f11b3324eed3aeed875abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:01 +0000

    Add package dimfred/0.2.0/dimfred-0.2.0.tar.gz

---
+++ packages/dimfred/dimfred-0.2.0.tar.gz/dimfred-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dimfred']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['easydict>=1.9,<2.0', 'jsoncomment>=0.4.2,<0.5.0', 'tabulate>=0.8.9,<0.9.0']
+
+setup_kwargs = {
+    'name': 'dimfred',
+    'version': '0.2.0',
+    'description': 'Tools and abbrevations I use often.',
+    'long_description': None,
+    'author': 'Dmitrij Vinokour',
+    'author_email': 'dimfred.1337@web.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
