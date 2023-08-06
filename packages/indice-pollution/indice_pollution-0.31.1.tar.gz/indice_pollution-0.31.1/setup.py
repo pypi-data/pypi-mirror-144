3b2c0e8c335d9ec280e79f30cf03818158c6e456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:27 +0000

    Add package indice-pollution/0.31.1/indice_pollution-0.31.1.tar.gz

---
+++ packages/indice-pollution/indice_pollution-0.31.1.tar.gz/indice_pollution-0.31.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+DEPENDENCIES = [
+    'alembic',
+    'celery',
+    'celery[redis]',
+    'flask',
+    'Flask-caching',
+    'flask-celery',
+    'flask-cors',
+    'flask-Migrate',
+    'flask-SQLAlchemy',
+    'requests',
+    'python-dateutil',
+    'orjson',
+    'pytz',
+    'beautifulsoup4',
+    'html5lib',
+    'psycopg2',
+    'unidecode',
+    'sentry-sdk[flask]',
+    'sqlalchemy',
+    'redis',
+]
+
+setup(
+    name='indice_pollution',
+    version='0.31.1',
+    description='API giving air pollution level in France',
+    url='https://github.com/l-vincent-l/indice_pollution',
+    download_url='https://github.com/l-vincent-l/indice_pollution/archive/0.1.2.tar.gz',
+    author='Vincent Lara',
+    author_email='vincent.lara@beta.gouv.fr',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='air quality aasqa atmo iqa',
+    packages=find_packages(),
+    install_requires=DEPENDENCIES
+)
+
