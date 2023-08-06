5ddb4692358f6b9975a7446fce3762e6ab9b8a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:42 +0000

    Add package thirdweb-sdk/2.0.1b2/thirdweb_sdk-2.0.1b2-py3-none-any.whl

---
+++ packages/thirdweb-sdk/thirdweb_sdk-2.0.1b2-py3-none-any.whl/thirdweb/core/classes/base_contract.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from web3 import Web3
+from eth_account.account import LocalAccount
+
+from thirdweb.core.classes.contract_wrapper import ContractWrapper
+
+
+class BaseContract:
+    """
+    Base contract class to define usage of the contract wrapper and enable
+    easy provider and signer switching across all contracts
+    """
+
+    _contract_wrapper: ContractWrapper
+
+    def __init__(self, contract_wrapper: ContractWrapper):
+        self._contract_wrapper = contract_wrapper
+
+    def on_provider_updated(self, provider: Web3):
+        """
+        Updates the contract provider when the SDK provider is updated
+
+        :param provider: web3 provider instance to use
+        """
+
+        self._contract_wrapper.update_provider(provider)
+
+    def on_signer_updated(self, signer: Optional[LocalAccount]):
+        """
+        Updates the contract signer when the SDK signer is updated
+
+        :param signer: optional account to use for signing transactions
+        """
+
+        self._contract_wrapper.update_signer(signer)
+
+    def get_address(self) -> str:
+        """
+        Get the address of the contract
+        """
+
+        return self._contract_wrapper._contract_abi.contract_address
