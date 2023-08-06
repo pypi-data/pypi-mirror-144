e2198973c99959ea303ef356dbfe9961a40f50d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:16 +0000

    Add package smart-DGGAN/1.0/smart-DGGAN-1.0.tar.gz

---
+++ packages/smart-DGGAN/smart-DGGAN-1.0.tar.gz/smart-DGGAN-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-DGGAN",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['tensorflow', 'numpy', 'scikit-learn'],
+)
