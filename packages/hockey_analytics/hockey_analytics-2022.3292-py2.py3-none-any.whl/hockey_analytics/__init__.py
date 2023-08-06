be8c84ab74959d6ad31de3fc8766f4ce7042344a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:41 +0000

    Add package hockey_analytics/2022.3292/hockey_analytics-2022.3292-py2.py3-none-any.whl

---
+++ packages/hockey_analytics/hockey_analytics-2022.3292-py2.py3-none-any.whl/hockey_analytics/__init__.py
@@ -0,0 +1,8 @@
+"""A python package to facilitate working with hockey data."""
+
+# change to calendar versioning of sorts 
+# YYYY.MMDDv where v is 1 based within the month/day combo
+# TODO:  follow best practices here
+__version__ = '2022.03292'
+
+from .viz import *
