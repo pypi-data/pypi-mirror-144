7f39721d02a7411c7bcf52770fe3751c3b88eb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:38 +0000

    Add package easyconfig/0.2.2/easyconfig-0.2.2.tar.gz

---
+++ packages/easyconfig/easyconfig-0.2.2.tar.gz/easyconfig-0.2.2/src/easyconfig/__const__.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from typing import Final, Literal
+
+
+class _MissingType(Enum):
+    MISSING_OBJ = object()
+
+    def __str__(self):
+        return '<Missing>'
+
+
+MISSING: Final = _MissingType.MISSING_OBJ
+MISSING_TYPE: Final = Literal[_MissingType.MISSING_OBJ]
---
+++ packages/easyconfig/easyconfig-0.2.2.tar.gz/easyconfig-0.2.2/src/easyconfig/__init__.py
@@ -0,0 +1,10 @@
+from easyconfig import errors
+from easyconfig.__version__ import __version__
+
+# isort: split
+
+from easyconfig.models import AppBaseModel, AppBaseSettings, AppConfigMixin, BaseModel, BaseSettings, ConfigMixin
+
+# isort: split
+
+from easyconfig.config_objs import create_app_config
