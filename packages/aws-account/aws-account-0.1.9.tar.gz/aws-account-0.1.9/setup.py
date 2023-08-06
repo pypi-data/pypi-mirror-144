0315bcf45c977926c01f0ade1a6e683100ae073e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:31 +0000

    Add package aws-account/0.1.9/aws-account-0.1.9.tar.gz

---
+++ packages/aws-account/aws-account-0.1.9.tar.gz/aws-account-0.1.9/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aws_account', 'aws_account.test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.20.5', 'click>=8.0.3', 'colorama>=0.4']
+
+entry_points = \
+{'console_scripts': ['aws-account = aws_account.cli:main']}
+
+setup_kwargs = {
+    'name': 'aws-account',
+    'version': '0.1.9',
+    'description': 'Print out AWS account and identity information to verify which account/organization is currently in use.',
+    'long_description': None,
+    'author': 'M. Rollwagen',
+    'author_email': 'rollwagen@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rollwagen/aws-account',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
