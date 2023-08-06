bfc9a4dda25367823db149daa2104abb1ff73e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:06 +0000

    Add package kkloader/0.1.5/kkloader-0.1.5-py3-none-any.whl

---
+++ packages/kkloader/kkloader-0.1.5-py3-none-any.whl/kkloader/__init__.py
@@ -0,0 +1,5 @@
+from .KoikatuCharaData import *  # noqa isort: skip
+from .KoikatuSaveData import KoikatuSaveData  # noqa isort: skip
+from .EmocreCharaData import EmocreCharaData  # noqa
+from .EmocreMapData import EmocreMapData  # noqa
+from .EmocreSceneData import EmocreSceneData  # noqa
