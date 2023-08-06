312e89ff3b6602105abb12ac8eb3dea983ca47ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:35 +0000

    Add package beaker-py/0.2.8/beaker_py-0.2.8-py3-none-any.whl

---
+++ packages/beaker-py/beaker_py-0.2.8-py3-none-any.whl/beaker/exceptions.py
@@ -0,0 +1,46 @@
+from requests.exceptions import (  # noqa: F401, re-imported here for convenience
+    HTTPError,
+)
+
+
+class BeakerError(Exception):
+    """
+    Base class for all Beaker errors other than :exc:`HTTPError` which is re-exported
+    from :exc:`requests.exceptions.HTTPError`.
+    """
+
+
+class ConfigurationError(BeakerError):
+    pass
+
+
+class ImageNotFound(BeakerError):
+    pass
+
+
+class ImageConflict(BeakerError):
+    pass
+
+
+class WorkspaceNotFound(BeakerError):
+    pass
+
+
+class ExperimentNotFound(BeakerError):
+    pass
+
+
+class ExperimentConflict(BeakerError):
+    pass
+
+
+class DatasetConflict(BeakerError):
+    pass
+
+
+class DatasetNotFound(BeakerError):
+    pass
+
+
+class JobNotFound(BeakerError):
+    pass
