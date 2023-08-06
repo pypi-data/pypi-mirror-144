96cbcfc9583273ac9b1b35efef9604bdc228af21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:08 +0000

    Add package di/0.67.0/di-0.67.0-py3-none-any.whl

---
+++ packages/di/di-0.67.0-py3-none-any.whl/di/api/solved.py
@@ -0,0 +1,36 @@
+import typing
+from typing import Any, Generic, List, Mapping, TypeVar
+
+from di.api.dependencies import DependantBase, DependencyParameter
+
+Dependency = Any
+
+DependencyType = TypeVar("DependencyType")
+
+
+class SolvedDependant(Generic[DependencyType]):
+    """Representation of a fully solved dependency as DAG"""
+
+    __slots__ = ("dependency", "dag", "container_cache")
+
+    dependency: DependantBase[DependencyType]
+    dag: Mapping[DependantBase[Any], List[DependencyParameter]]
+    # container_cache can be used by the creating container to store data that is tied
+    # to the SolvedDependant
+    container_cache: typing.Any
+
+    def __init__(
+        self,
+        dependency: DependantBase[DependencyType],
+        dag: Mapping[DependantBase[Any], List[DependencyParameter]],
+        container_cache: typing.Any,
+    ):
+        self.dependency = dependency
+        self.dag = dag
+        self.container_cache = container_cache
+
+    def get_flat_subdependants(self) -> List[DependantBase[Any]]:
+        """Get an exhaustive list of all of the dependencies of this dependency,
+        in no particular order.
+        """
+        return list(self.dag.keys() - {self.dependency})
