db731130785089919def6e9d4769a2c9120268f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:21:11 +0000

    Add package slackoff/0.0.1/slackoff-0.0.1-py3-none-any.whl

---
+++ packages/slackoff/slackoff-0.0.1-py3-none-any.whl/slackoff/utils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import applescript
+
+FUNCTIONS = Path(__file__).parent / "slack.applescript"
+
+
+def signin_workspace(name: str) -> bool:
+    functions = FUNCTIONS.read_text("utf-8")
+    result = applescript.run(functions + f' signin("{name}")')
+    return result.code == 0
+
+
+def signout_workspace(name: str) -> bool:
+    functions = FUNCTIONS.read_text("utf-8")
+    result = applescript.run(functions + f' signout("{name}")')
+    return result.code == 0
