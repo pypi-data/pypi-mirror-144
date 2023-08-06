53155ad204e383a5a09b3c0ee4814003a4513edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package datalad-next/0.1.0/datalad_next-0.1.0-py3-none-any.whl

---
+++ packages/datalad-next/datalad_next-0.1.0-py3-none-any.whl/datalad_next/__init__.py
@@ -0,0 +1,51 @@
+"""DataLad NEXT extension"""
+
+__docformat__ = 'restructuredtext'
+
+import logging
+lgr = logging.getLogger('datalad.next')
+
+# Defines a datalad command suite.
+# This variable must be bound as a setuptools entrypoint
+# to be found by datalad
+command_suite = (
+    # description of the command suite, displayed in cmdline help
+    "What is next in DataLad",
+    [
+        # specification of a command, any number of commands can be defined
+        (
+            # importable module that contains the command implementation
+            'datalad_next.credentials',
+            # name of the command class implementation in above module
+            'Credentials',
+        ),
+    ]
+)
+
+
+# patch datalad-core
+import datalad_next.patches
+
+# register additional configuration items in datalad-core
+from datalad.interface.common_cfg import register_config
+from datalad.support.constraints import EnsureBool
+register_config(
+    'datalad.credentials.repeat-secret-entry',
+    'Require entering secrets twice for interactive specification?',
+    type=EnsureBool(),
+    default=True,
+    dialog='yesno')
+register_config(
+    'datalad.credentials.hidden-secret-entry',
+    'Hide secret in interactive entry?',
+    type=EnsureBool(),
+    default=True,
+    dialog='yesno')
+
+
+from datalad import setup_package
+from datalad import teardown_package
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
