b7c98ae293a31f349e53a28070b6c035fb26ed4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:39 +0000

    Add package expressmoney/6.4.0/expressmoney-6.4.0.tar.gz

---
+++ packages/expressmoney/expressmoney-6.4.0.tar.gz/expressmoney-6.4.0/expressmoney/api/payments/bank_cards.py
@@ -0,0 +1,59 @@
+__all__ = ('BankCardPoint', 'BankCard3dsPoint')
+
+from expressmoney.api import *
+
+
+SERVICE = 'payments'
+
+
+class GenericBankCard(Contract):
+    bin = serializers.CharField(max_length=6)
+    number = serializers.CharField(max_length=4)
+    expiry_month = serializers.IntegerField(min_value=1, max_value=12)
+    expiry_year = serializers.IntegerField(min_value=20, max_value=100)
+
+
+class BankCardCreateContract(GenericBankCard):
+    ip = serializers.IPAddressField(allow_blank=True)
+    cryptogram = serializers.CharField(max_length=1024, allow_blank=True)
+
+
+class BankCardReadContract(GenericBankCard):
+    PAYPAL = "PAYPAL"
+    CLOUDPAYMENTS = "CLOUDPAYMENTS"
+    GATEWAY_CHOICES = (
+        (PAYPAL, "PayPal"),
+        (CLOUDPAYMENTS, "CloudPayments")
+    )
+    pagination = PaginationContract()
+    id = serializers.IntegerField(min_value=1)
+    created = serializers.DateTimeField()
+    is_active = serializers.BooleanField()
+
+
+class BankCard3dsContract(Contract):
+    md = serializers.CharField(max_length=512)
+    pa_res = serializers.CharField(max_length=512)
+
+
+class BankCardID(ID):
+    _service = SERVICE
+    _app = 'bank_cards'
+    _view_set = 'bank_card'
+
+
+class BankCard3dsID(ID):
+    _service = SERVICE
+    _app = 'bank_cards'
+    _view_set = 'bank_card_3ds'
+
+
+class BankCardPoint(ListPointMixin, CreatePointMixin, ContractPoint):
+    _point_id = BankCardID()
+    _create_contract = BankCardCreateContract
+    _read_contract = BankCardReadContract
+
+
+class BankCard3dsPoint(CreatePointMixin, ContractPoint):
+    _point_id = BankCard3dsID()
+    _create_contract = BankCard3dsContract
