0345bcf08d9e2a7ea5bd4c97b2dcfc846c4c9e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:20:35 +0000

    Add package robotoff/1.5.1/robotoff-1.5.1.tar.gz

---
+++ packages/robotoff/robotoff-1.5.1.tar.gz/robotoff-1.5.1/robotoff/app/core.py
@@ -0,0 +1,211 @@
+import functools
+from enum import Enum
+from typing import Dict, Iterable, List, NamedTuple, Optional, Union
+
+import peewee
+
+from robotoff import settings
+from robotoff.insights.annotate import (
+    ALREADY_ANNOTATED_RESULT,
+    SAVED_ANNOTATION_VOTE_RESULT,
+    UNKNOWN_INSIGHT_RESULT,
+    AnnotationResult,
+    InsightAnnotatorFactory,
+)
+from robotoff.models import AnnotationVote, ProductInsight, db
+from robotoff.off import OFFAuthentication
+from robotoff.utils import get_logger
+
+logger = get_logger(__name__)
+
+
+class SkipVotedType(Enum):
+    DEVICE_ID = 1
+    USERNAME = 2
+
+
+class SkipVotedOn(NamedTuple):
+    """A helper class to specify whether a voted-on insight should be dropped from
+    the get_insights results."""
+
+    by: SkipVotedType
+    id: str
+
+
+def _add_vote_exclusions(
+    query: peewee.Query, exclusion: Optional[SkipVotedOn]
+) -> peewee.Query:
+    if not exclusion:
+        return query
+
+    if exclusion.by == SkipVotedType.DEVICE_ID:
+        criteria = AnnotationVote.device_id == exclusion.id
+    elif exclusion.by == SkipVotedType.USERNAME:
+        criteria = AnnotationVote.username == exclusion.id
+    else:
+        raise ValueError("Unknown SkipVoteType: {exclusion.by}")
+
+    return query.join(
+        AnnotationVote,
+        join_type=peewee.JOIN.LEFT_OUTER,
+        on=((AnnotationVote.insight_id == ProductInsight.id) & (criteria)),
+    ).where(AnnotationVote.id.is_null())
+
+
+def get_insights(
+    barcode: Optional[str] = None,
+    keep_types: List[str] = None,
+    country: str = None,
+    brands: List[str] = None,
+    annotated: Optional[bool] = False,
+    annotation: Optional[int] = None,
+    order_by: Optional[str] = None,
+    value_tag: Optional[str] = None,
+    server_domain: Optional[str] = None,
+    reserved_barcode: Optional[bool] = None,
+    as_dict: bool = False,
+    limit: Optional[int] = 25,
+    offset: Optional[int] = None,
+    count: bool = False,
+    avoid_voted_on: Optional[SkipVotedOn] = None,
+) -> Iterable[ProductInsight]:
+    if server_domain is None:
+        server_domain = settings.OFF_SERVER_DOMAIN
+
+    where_clauses = [ProductInsight.server_domain == server_domain]
+
+    if annotated is not None:
+        where_clauses.append(ProductInsight.annotation.is_null(not annotated))
+
+    if annotation is not None:
+        where_clauses.append(ProductInsight.annotation == annotation)
+
+    if barcode:
+        where_clauses.append(ProductInsight.barcode == barcode)
+
+    if value_tag:
+        where_clauses.append(ProductInsight.value_tag == value_tag)
+
+    if keep_types:
+        where_clauses.append(ProductInsight.type.in_(keep_types))
+
+    if country is not None:
+        where_clauses.append(ProductInsight.countries.contains(country))
+
+    if brands:
+        where_clauses.append(ProductInsight.brands.contains_any(brands))
+
+    if reserved_barcode is not None:
+        where_clauses.append(ProductInsight.reserved_barcode == reserved_barcode)
+
+    query = _add_vote_exclusions(ProductInsight.select(), avoid_voted_on)
+
+    if where_clauses:
+        query = query.where(*where_clauses)
+
+    if count:
+        return query.count()
+
+    if limit is not None:
+        query = query.limit(limit)
+
+    if offset is not None and order_by != "random":
+        query = query.offset(offset)
+
+    if order_by is not None:
+        if order_by == "random":
+            # The +1 is here to avoid 0*rand() = 0
+            query = query.order_by(
+                (peewee.fn.Random() * (ProductInsight.n_votes + 1)).desc()
+            )
+
+        elif order_by == "popularity":
+            query = query.order_by(ProductInsight.unique_scans_n.desc())
+
+        elif order_by == "n_votes":
+            query = query.order_by(ProductInsight.n_votes.desc())
+
+    if as_dict:
+        query = query.dicts()
+
+    return query.iterator()
+
+
+def save_annotation(
+    insight_id: str,
+    annotation: int,
+    device_id: str,
+    update: bool = True,
+    data: Optional[Dict] = None,
+    auth: Optional[OFFAuthentication] = None,
+    trusted_annotator: bool = False,
+) -> AnnotationResult:
+    """Saves annotation either by using a single response as ground truth or by using several responses.
+
+    trusted_annotator: defines whether the given annotation comes from an authoritative source (e.g.
+    a trusted user), ot whether the annotation should be subject to the voting system.
+    """
+    try:
+        insight: Union[ProductInsight, None] = ProductInsight.get_by_id(insight_id)
+    except ProductInsight.DoesNotExist:
+        insight = None
+
+    if not insight:
+        return UNKNOWN_INSIGHT_RESULT
+
+    if insight.annotation is not None:
+        return ALREADY_ANNOTATED_RESULT
+
+    if not trusted_annotator:
+        verified: bool = False
+
+        AnnotationVote.create(
+            insight_id=insight_id,
+            username=auth.get_username() if auth else None,
+            value=annotation,
+            device_id=device_id,
+        )
+
+        with db.atomic() as tx:
+            try:
+                existing_votes = list(
+                    AnnotationVote.select(
+                        AnnotationVote.value,
+                        peewee.fn.COUNT(AnnotationVote.value).alias("num_votes"),
+                    )
+                    .where(AnnotationVote.insight_id == insight_id)
+                    .group_by(AnnotationVote.value)
+                    .order_by(peewee.SQL("num_votes").desc())
+                )
+                insight.n_votes = functools.reduce(
+                    lambda sum, row: sum + row.num_votes, existing_votes, 0
+                )
+                insight.save()
+            except Exception as e:
+                tx.rollback()
+                raise e
+
+        # If the top annotation has more than 2 votes, consider applying it to the insight.
+        if existing_votes[0].num_votes > 2:
+            annotation = existing_votes[0].value
+            verified = True
+
+        # But first check for the following cases:
+        #  1) The 1st place annotation has >2 votes, and the 2nd place annotation has >= 2 votes.
+        #  2) 1st place and 2nd place have 2 votes each.
+        #
+        # In both cases, we consider this an ambiguous result and mark it with 'I don't know'.
+        if (
+            existing_votes[0].num_votes >= 2
+            and len(existing_votes) > 1
+            and existing_votes[1].num_votes >= 2
+        ):
+            # This code credits the last person to contribute a vote with a potentially not their annotation.
+            annotation = 0
+            verified = True
+
+        if not verified:
+            return SAVED_ANNOTATION_VOTE_RESULT
+
+    annotator = InsightAnnotatorFactory.get(insight.type)
+    return annotator.annotate(insight, annotation, update, data=data, auth=auth)
---
+++ packages/robotoff/robotoff-1.5.1.tar.gz/robotoff-1.5.1/robotoff/slack.py
@@ -0,0 +1,275 @@
+import json
+import operator
+from typing import Dict, List, Optional
+
+import requests
+
+from robotoff import settings
+from robotoff.insights.dataclass import InsightType
+from robotoff.logo_label_type import LogoLabelType
+from robotoff.models import LogoAnnotation, ProductInsight
+from robotoff.prediction.types import Prediction
+from robotoff.utils import get_logger, http_session
+from robotoff.utils.types import JSONType
+
+logger = get_logger(__name__)
+
+
+class SlackException(Exception):
+    pass
+
+
+class SlackNotifierInterface:
+    """SlackNotifierInterface is an interface for posting Robotoff-related alerts and notifications to the OFF Slack channels."""
+
+    def notify_image_flag(
+        self, predictions: List[Prediction], source: str, barcode: str
+    ):
+        pass
+
+    def notify_automatic_processing(self, insight: ProductInsight):
+        pass
+
+    def send_logo_notification(
+        self, logo: LogoAnnotation, probs: Dict[LogoLabelType, float]
+    ):
+        pass
+
+
+class NotifierFactory:
+    """NotifierFactory is responsible for creating a notifier to post notifications to."""
+
+    @staticmethod
+    def get_notifier() -> SlackNotifierInterface:
+        token = settings.slack_token()
+        if token == "":
+            return NoopSlackNotifier()
+        return SlackNotifier(token)
+
+
+def _sensitive_image(flag_type: str, flagged_label: str) -> bool:
+    """Determines whether the given flagged image should be considered as sensitive."""
+    is_human: bool = flagged_label in {
+        "face",
+        "head",
+        "selfie",
+        "hair",
+        "forehead",
+        "chin",
+        "cheek",
+        "tooth",
+        "eyebrow",
+        "ear",
+        "neck",
+        "jaw",
+        "nose",
+        "facial expression",
+        "glasses",
+        "eyewear",
+        "child",
+        "baby",
+        "human",
+    }
+    return (
+        is_human and flag_type == "label_annotation"
+    ) or flag_type == "safe_search_annotation"
+
+
+def _slack_message_block(
+    message_text: str, with_image: Optional[str] = None
+) -> List[Dict]:
+    """Formats given parameters into a Slack message block."""
+    block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": message_text,
+        },
+    }
+
+    if with_image:
+        block["accessory"] = {
+            "type": "image",
+            "image_url": with_image,
+            "alt_text": "-",
+        }
+    return [block]
+
+
+class SlackNotifier(SlackNotifierInterface):
+    """SlackNotifier implements the real SlackNotifier."""
+
+    # Slack channel IDs.
+    ROBOTOFF_ALERT_CHANNEL = "CGKPALRCG"
+    ROBOTOFF_USER_ALERT_CHANNEL = "CGWSXDGSF"
+    ROBOTOFF_PRIVATE_IMAGE_ALERT_CHANNEL = "GGMRWLEF2"
+    ROBOTOFF_PUBLIC_IMAGE_ALERT_CHANNEL = "CT2N423PA"
+    NUTRISCORE_ALERT_CHANNEL = "CJZNFCSNP"
+
+    BASE_URL = "https://slack.com/api"
+    POST_MESSAGE_URL = BASE_URL + "/chat.postMessage"
+
+    NUTRISCORE_LABELS = {
+        "en:nutriscore",
+        "en:nutriscore-grade-a",
+        "en:nutriscore-grade-b",
+        "en:nutriscore-grade-c",
+        "en:nutriscore-grade-d",
+        "en:nutriscore-grade-e",
+    }
+
+    COLLAPSE_LINKS_PARAMS = {
+        "unfurl_links": False,
+        "unfurl_media": False,
+    }
+
+    def __init__(self, slack_token: str):
+        """Should not be called directly, use the NotifierFactory instead."""
+        self.slack_token = slack_token
+
+    def notify_image_flag(
+        self, predictions: List[Prediction], source_image: str, barcode: str
+    ):
+        """Sends alerts to Slack channels for flagged images."""
+        if len(predictions) < 1:
+            return
+
+        text = ""
+        slack_channel: str = self.ROBOTOFF_PUBLIC_IMAGE_ALERT_CHANNEL
+
+        for flagged in predictions:
+            flag_type = flagged.data["type"]
+            label = flagged.data["label"]
+
+            if _sensitive_image(flag_type, label):
+                slack_channel = self.ROBOTOFF_PRIVATE_IMAGE_ALERT_CHANNEL
+
+            if flag_type in ("safe_search_annotation", "label_annotation"):
+                likelihood = flagged.data["likelihood"]
+                text += f"type: {flag_type}\nlabel: *{label}*, score: {likelihood}\n"
+            else:
+                match_text = flagged.data["text"]
+                text += f"type: {flag_type}\nlabel: *{label}*, match: {match_text}\n"
+
+        edit_url = f"{settings.BaseURLProvider().get()}/cgi/product.pl?type=edit&code={barcode}"
+        image_url = settings.OFF_IMAGE_BASE_URL + source_image
+
+        full_text = f"{text}\n <{image_url}|Image> -- <{edit_url}|*Edit*>"
+        message = _slack_message_block(full_text, with_image=image_url)
+
+        self._post_message(message, slack_channel, **self.COLLAPSE_LINKS_PARAMS)
+
+    def notify_automatic_processing(self, insight: ProductInsight):
+        product_url = f"{settings.BaseURLProvider().get()}/product/{insight.barcode}"
+
+        if insight.source_image:
+            image_url = f"{settings.BaseURLProvider().static().get()}/images/products{insight.source_image}"
+            metadata_text = f"(<{product_url}|product>, <{image_url}|source image>)"
+        else:
+            metadata_text = f"(<{product_url}|product>)"
+        value = insight.value or insight.value_tag
+
+        if insight.type in {
+            InsightType.product_weight.name,
+            InsightType.expiration_date.name,
+        }:
+            text = f"The {insight.type} `{value}` (match: `{insight.data['raw']}`) was automatically added to product {insight.barcode}"
+        else:
+            text = f"The `{value}` {insight.type} was automatically added to product {insight.barcode}"
+
+        message = _slack_message_block(text + " " + metadata_text)
+
+        if insight.value_tag in self.NUTRISCORE_LABELS:
+            self._post_message(
+                message, self.NUTRISCORE_ALERT_CHANNEL, **self.COLLAPSE_LINKS_PARAMS
+            )
+            return
+
+        self._post_message(
+            message, self.ROBOTOFF_ALERT_CHANNEL, **self.COLLAPSE_LINKS_PARAMS
+        )
+
+    def _get_base_params(self) -> JSONType:
+        return {
+            "username": "robotoff-bot",
+            "token": self.slack_token,
+            "icon_url": "https://s3-us-west-2.amazonaws.com/slack-files2/"
+            "bot_icons/2019-03-01/565595869687_48.png",
+        }
+
+    def send_logo_notification(
+        self, logo: LogoAnnotation, probs: Dict[LogoLabelType, float]
+    ):
+        crop_url = logo.get_crop_image_url()
+        prob_text = "\n".join(
+            (
+                f"{label[0]} - {label[1]}: {prob:.2g}"
+                for label, prob in sorted(
+                    probs.items(), key=operator.itemgetter(1), reverse=True
+                )
+            )
+        )
+        barcode = logo.image_prediction.image.barcode
+        base_off_url = settings.BaseURLProvider().get()
+        text = (
+            f"Prediction for <{crop_url}|image> "
+            f"(<https://hunger.openfoodfacts.org/logos?logo_id={logo.id}|annotate logo>, "
+            f"<{base_off_url}/product/{barcode}|product>):\n{prob_text}"
+        )
+        self._post_message(_slack_message_block(text), self.ROBOTOFF_ALERT_CHANNEL)
+
+    def _post_message(
+        self,
+        blocks: List[Dict],
+        channel: str,
+        **kwargs,
+    ):
+        try:
+            params: JSONType = {
+                **(self._get_base_params()),
+                "channel": channel,
+                "blocks": json.dumps(blocks),
+                **kwargs,
+            }
+
+            r = http_session.post(self.POST_MESSAGE_URL, data=params)
+            response_json = _get_slack_json(r)
+            return response_json
+        except Exception as e:
+            logger.error(
+                "An exception occurred when sending a Slack notification", exc_info=e
+            )
+
+
+class NoopSlackNotifier(SlackNotifier):
+    """NoopSlackNotifier is a NOOP SlackNotifier used in dev/local executions of Robotoff."""
+
+    def __init__(self):
+        super().__init__("")
+
+    def _post_message(
+        self,
+        blocks: List[Dict],
+        channel: str,
+        **kwargs,
+    ):
+        """Overrides the actual posting to Slack with logging of the args that would've been posted."""
+        logger.info(
+            f"Alerting on slack channel '{channel}', with message:\n{blocks}\nand additional args:\n{kwargs}"
+        )
+
+
+def _get_slack_json(response: requests.Response) -> JSONType:
+    json_data = response.json()
+
+    if not response.ok:
+        raise SlackException(
+            "Non-200 status code from Slack: "
+            "{}, response: {}"
+            "".format(response.status_code, json_data)
+        )
+
+    if not json_data.get("ok", False):
+        raise SlackException("Non-ok response: {}".format(json_data))
+
+    return json_data
