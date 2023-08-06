acaeda22352306d125f3f888dd0a2657a53fb7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package smart-hg-18/1.0/smart-hg-18-1.0.tar.gz

---
+++ packages/smart-hg-18/smart-hg-18-1.0.tar.gz/smart-hg-18-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-18",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['joblib==1.0.0', 'numpy==1.20.3', 'six==1.16.0', 'networkx==2.5', 'scipy==1.6.3', 'nltk==3.6.2', 'scikit-learn==0.24.2', 'spacy==3.0.6'],
+)
