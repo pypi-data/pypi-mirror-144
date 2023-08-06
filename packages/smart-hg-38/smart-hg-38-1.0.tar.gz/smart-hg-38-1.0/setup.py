6a6b24c4692369e2421e8ba9c91f4c08c1d07eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:28 +0000

    Add package smart-hg-38/1.0/smart-hg-38-1.0.tar.gz

---
+++ packages/smart-hg-38/smart-hg-38-1.0.tar.gz/smart-hg-38-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-38",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['googleapis-common-protos==1.4.0', 'google-cloud-storage==1.5.0'],
+)
