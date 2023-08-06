081403420987f70e24ff9216a3253071f55dc12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:27 +0000

    Add package smart-hg-35/1.0/smart-hg-35-1.0.tar.gz

---
+++ packages/smart-hg-35/smart-hg-35-1.0.tar.gz/smart-hg-35-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-35",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.19.4', 'tqdm==4.54.1', 'keras==2.4.3', 'tensorflow==2.4.0'],
+)
