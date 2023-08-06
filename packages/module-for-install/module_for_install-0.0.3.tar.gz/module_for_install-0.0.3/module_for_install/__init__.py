222608f6c308d6f87ad5133193c637a8166989c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:51 +0000

    Add package module-for-install/0.0.3/module_for_install-0.0.3.tar.gz

---
+++ packages/module-for-install/module_for_install-0.0.3.tar.gz/module_for_install-0.0.3/module_for_install/__init__.py
@@ -0,0 +1,1 @@
+from module_for_install import *
---
+++ packages/module-for-install/module_for_install-0.0.3.tar.gz/module_for_install-0.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='module_for_install',
+    version='0.0.3',
+    author='Pavel Khomenko',
+    author_email='your@domain.com',
+    description='My Package for university task',
+    packages=setuptools.find_packages(),
+)
