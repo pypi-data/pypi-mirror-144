659482f73a87d40113f263e330b99ea701c3e313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:49 +0000

    Add package supervisor-gateway/0.3.2/supervisor-gateway-0.3.2.tar.gz

---
+++ packages/supervisor-gateway/supervisor-gateway-0.3.2.tar.gz/supervisor-gateway-0.3.2/supervisor_gateway/event_listener.py
@@ -0,0 +1,82 @@
+import asyncio
+import sys
+from asyncio import StreamReader
+from asyncio import StreamReaderProtocol
+from asyncio import StreamWriter
+from typing import Callable
+from typing import Dict
+from typing import Union
+
+from supervisor_gateway.log import logger
+from supervisor_gateway.supervisor import ACKNOWLEDGED
+from supervisor_gateway.supervisor import READY
+
+
+class StandardStreamReaderProtocol(StreamReaderProtocol):
+    def connection_made(self, transport):
+        if self._stream_reader._transport is not None:  # noqa
+            return
+        super().connection_made(transport)
+
+
+async def write(stream: StreamWriter, msg: str):
+    stream.write(msg.encode())
+    await stream.drain()
+
+
+async def read(stream: StreamReader) -> Dict[str, Union[str, Dict]]:
+    data = await stream.readline()
+    header_line = data.decode()
+    event: Dict = {}
+    for line in header_line.split():
+        k, v = line.split(":", 1)
+        event[k] = v
+    data = await stream.read(int(event["len"]))
+    payload_str = data.decode()
+    payload = {}
+    for line in payload_str.split():
+        k, v = line.split(":", 1)
+        payload[k] = v
+    event["payload"] = payload
+    return event
+
+
+async def open_connection(
+    in_pipe=sys.stdin,
+    out_pipe=sys.stdout,
+) -> (StreamReader, StreamWriter):
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = StandardStreamReaderProtocol(reader)
+    await loop.connect_read_pipe(lambda: protocol, in_pipe)
+
+    out_transport_connect = loop.connect_write_pipe(lambda: protocol, out_pipe)
+    out_transport, _ = await out_transport_connect
+    writer = asyncio.StreamWriter(out_transport, protocol, reader, loop)
+    return reader, writer
+
+
+class Listener:
+    def __init__(self, handler: Callable[[dict], None] = None):
+        self.handler = handler
+        self.running = False
+
+    def set_handler(self, handler: Callable[[dict], None]):
+        self.handler = handler
+
+    def stop(self):
+        self.running = False
+
+    async def start(self):
+        self.running = True
+        logger.info("event_listener started")
+        reader, writer = await open_connection()
+        while self.running:
+            await write(writer, READY)
+            event = await read(reader)
+            self.handler(event)
+            logger.debug(f"event: {event}")
+            await write(writer, ACKNOWLEDGED)
+
+
+listener = Listener()
---
+++ packages/supervisor-gateway/supervisor-gateway-0.3.2.tar.gz/supervisor-gateway-0.3.2/supervisor_gateway/xml_rpc.py
@@ -0,0 +1,93 @@
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from aiohttp import ClientSession
+from aiohttp import UnixConnector
+from aiohttp_xmlrpc.client import ServerProxy
+from aiohttp_xmlrpc.exceptions import ServerError
+
+from supervisor_gateway.config import conf
+from supervisor_gateway.supervisor import Faults
+
+
+class RPC:
+    def __init__(self, url: str):
+        self.url = url
+        self.client: Optional[ServerProxy] = None
+
+    def init_client(self):
+        if self.url.startswith("unix://"):
+            conn = UnixConnector(path=self.url[7:])
+            session = ClientSession(connector=conn)
+            client = ServerProxy("http://127.0.0.1/RPC2", client=session)
+        else:
+            client = ServerProxy(self.url)
+        self.client = client
+
+    async def close(self):
+        if self.client:
+            await self.client.close()
+            self.client = None
+
+    async def list_methods(self) -> List[str]:
+        return await self.client.system.listMethods()
+
+    async def get_state(self) -> Dict:
+        state = await self.client.supervisor.getState()
+        return state
+
+    async def reread(self) -> List[List[str]]:
+        data = await self.client.supervisor.reloadConfig()
+        added, changed, dropped = data[0]
+        return [added, changed, dropped]
+
+    async def get_all_process_info(self) -> List[Dict]:
+        return await self.client.supervisor.getAllProcessInfo()
+
+    async def get_process_info(self, name: str) -> bool:
+        if ":" not in name:
+            name = f"{name}:{name}"
+        return await self.client.supervisor.getProcessInfo(name)
+
+    async def start_process(self, name: str) -> bool:
+        if ":" not in name:
+            name = f"{name}:{name}"
+        try:
+            return await self.client.supervisor.startProcess(name)
+        except ServerError as e:
+            if e.code == Faults.ALREADY_STARTED:
+                return True
+            raise
+
+    async def restart_process(self, name: str) -> bool:
+        await self.stop_process(name)
+        return await self.start_process(name)
+
+    async def stop_process(self, name: str) -> bool:
+        if ":" not in name:
+            name = f"{name}:{name}"
+        try:
+            return await self.client.supervisor.stopProcess(name)
+        except ServerError as e:
+            if e.code == Faults.NOT_RUNNING:
+                return True
+            raise
+
+    async def stop_process_group(self, name: str) -> bool:
+        return await self.client.supervisor.stopProcessGroup(name)
+
+    async def add_process_group(self, name: str) -> bool:
+        try:
+            return await self.client.supervisor.addProcessGroup(name)
+        except ServerError as e:
+            if e.code == Faults.ALREADY_ADDED:
+                return True
+            raise
+
+    async def remove_process_group(self, name: str) -> bool:
+        await self.stop_process_group(name)
+        return await self.client.supervisor.removeProcessGroup(name)
+
+
+rpc = RPC(conf.rpc_url)
