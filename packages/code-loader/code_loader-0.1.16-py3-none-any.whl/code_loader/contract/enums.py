a7e1ef6eca6c617fa451dd39eb0c78399cf2f4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:24 +0000

    Add package code-loader/0.1.16/code_loader-0.1.16-py3-none-any.whl

---
+++ packages/code-loader/code_loader-0.1.16-py3-none-any.whl/code_loader/contract/enums.py
@@ -0,0 +1,40 @@
+from enum import Enum, IntEnum
+
+
+class DatasetInputType(Enum):
+    Image = "Image"
+    Text = "Text"
+    Word = "Word"
+    Numeric = "Numeric"
+    Time_series = "Time_series"
+
+
+class DatasetOutputType(Enum):
+    Numeric = "Numeric"
+    Classes = "Classes"
+    Mask = "Mask"
+
+
+class DatasetMetadataType(Enum):
+    float = "float"
+    string = "string"
+    int = "int"
+    boolean = "boolean"
+
+
+class DataStateType(Enum):
+    training = "training"
+    validation = "validation"
+    test = "test"
+
+
+class DataStateEnum(IntEnum):
+    training = 0
+    validation = 1
+    test = 2
+
+
+# todo: handle test not run due to error in pre process and Add to TestingSectionEnum Enum didn't run
+class TestingSectionEnum(Enum):
+    Warnings = "Warnings"
+    Errors = "Errors"
---
+++ packages/code-loader/code_loader-0.1.16-py3-none-any.whl/code_loader/dataset_binder/datasetbinder.py
@@ -0,0 +1,32 @@
+from typing import Callable, List, Optional, Dict, Any
+
+from code_loader.contract.datasetclasses import SubsetResponse, SectionCallableInterface, SubsetHandler, InputHandler, \
+    GroundTruthHandler, MetadataHandler, DatasetIntegrationSetup, MetadataSectionCallableInterface
+from code_loader.contract.enums import DatasetInputType, DatasetOutputType, DatasetMetadataType
+from code_loader.utils import to_numpy_return_wrapper
+
+
+class DatasetBinder:
+
+    def __init__(self) -> None:
+        self.setup_container = DatasetIntegrationSetup()
+        self.cache_container: Dict[str, Any] = {"word_to_index": {}}
+
+    def set_subset(self, function: Callable[[], List[SubsetResponse]], name: str) -> None:
+        self.setup_container.subsets.append(SubsetHandler(function, name))
+
+    def set_input(self, function: SectionCallableInterface, subset: str,
+                  input_type: DatasetInputType, name: str) -> None:
+        function = to_numpy_return_wrapper(function)
+        self.setup_container.inputs.append(InputHandler(name, function, subset, input_type, []))
+
+    def set_ground_truth(self, function: SectionCallableInterface, subset: str,
+                         ground_truth_type: DatasetOutputType, name: str, labels: Optional[List[str]],
+                         masked_input: Optional[str]) -> None:
+        function = to_numpy_return_wrapper(function)
+        self.setup_container.ground_truths.append(GroundTruthHandler(name, function, subset,
+                                                                     ground_truth_type, labels, masked_input, []))
+
+    def set_metadata(self, function: MetadataSectionCallableInterface, subset: str,
+                     metadata_type: DatasetMetadataType, name: str) -> None:
+        self.setup_container.metadata.append(MetadataHandler(name, function, subset, metadata_type))
