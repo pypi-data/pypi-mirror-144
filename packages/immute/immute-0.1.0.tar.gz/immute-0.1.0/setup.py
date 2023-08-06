28b87f1adf0de3cfeb948f2d4f603cc95b90ccfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:04 +0000

    Add package immute/0.1.0/immute-0.1.0.tar.gz

---
+++ packages/immute/immute-0.1.0.tar.gz/immute-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['immute']
+setup_kwargs = {
+    'name': 'immute',
+    'version': '0.1.0',
+    'description': 'Create immutable python classes',
+    'long_description': None,
+    'author': 'fitzypop',
+    'author_email': 'fitzypop@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
