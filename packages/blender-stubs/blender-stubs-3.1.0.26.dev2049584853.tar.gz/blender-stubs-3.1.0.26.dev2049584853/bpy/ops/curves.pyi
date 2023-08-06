3a53edd6b655ecdaf115363557707c9c059c3afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package blender-stubs/3.1.0.26.dev2049584853/blender-stubs-3.1.0.26.dev2049584853.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.1.0.26.dev2049584853.tar.gz/blender-stubs-3.1.0.26.dev2049584853/bpy/ops/curves.pyi
@@ -0,0 +1,21 @@
+"""
+
+
+Curves Operators
+****************
+
+:func:`sculptmode_toggle`
+
+"""
+
+import typing
+
+def sculptmode_toggle() -> None:
+
+  """
+
+  Enter/Exit sculpt mode for curves
+
+  """
+
+  ...
