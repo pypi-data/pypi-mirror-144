a72a1349d87701f878191be2aaea32cdc565c667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package mypy-boto3-finspace/1.21.27/mypy-boto3-finspace-1.21.27.tar.gz

---
+++ packages/mypy-boto3-finspace/mypy-boto3-finspace-1.21.27.tar.gz/mypy-boto3-finspace-1.21.27/mypy_boto3_finspace/type_defs.pyi
@@ -0,0 +1,207 @@
+"""
+Type annotations for finspace service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_finspace/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_finspace.type_defs import CreateEnvironmentRequestRequestTypeDef
+
+    data: CreateEnvironmentRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List, Mapping, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import EnvironmentStatusType, FederationModeType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "CreateEnvironmentRequestRequestTypeDef",
+    "CreateEnvironmentResponseTypeDef",
+    "DeleteEnvironmentRequestRequestTypeDef",
+    "EnvironmentTypeDef",
+    "FederationParametersTypeDef",
+    "GetEnvironmentRequestRequestTypeDef",
+    "GetEnvironmentResponseTypeDef",
+    "ListEnvironmentsRequestRequestTypeDef",
+    "ListEnvironmentsResponseTypeDef",
+    "ListTagsForResourceRequestRequestTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "ResponseMetadataTypeDef",
+    "SuperuserParametersTypeDef",
+    "TagResourceRequestRequestTypeDef",
+    "UntagResourceRequestRequestTypeDef",
+    "UpdateEnvironmentRequestRequestTypeDef",
+    "UpdateEnvironmentResponseTypeDef",
+)
+
+CreateEnvironmentRequestRequestTypeDef = TypedDict(
+    "CreateEnvironmentRequestRequestTypeDef",
+    {
+        "name": str,
+        "description": NotRequired[str],
+        "kmsKeyId": NotRequired[str],
+        "tags": NotRequired[Mapping[str, str]],
+        "federationMode": NotRequired[FederationModeType],
+        "federationParameters": NotRequired["FederationParametersTypeDef"],
+        "superuserParameters": NotRequired["SuperuserParametersTypeDef"],
+        "dataBundles": NotRequired[Sequence[str]],
+    },
+)
+
+CreateEnvironmentResponseTypeDef = TypedDict(
+    "CreateEnvironmentResponseTypeDef",
+    {
+        "environmentId": str,
+        "environmentArn": str,
+        "environmentUrl": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DeleteEnvironmentRequestRequestTypeDef = TypedDict(
+    "DeleteEnvironmentRequestRequestTypeDef",
+    {
+        "environmentId": str,
+    },
+)
+
+EnvironmentTypeDef = TypedDict(
+    "EnvironmentTypeDef",
+    {
+        "name": NotRequired[str],
+        "environmentId": NotRequired[str],
+        "awsAccountId": NotRequired[str],
+        "status": NotRequired[EnvironmentStatusType],
+        "environmentUrl": NotRequired[str],
+        "description": NotRequired[str],
+        "environmentArn": NotRequired[str],
+        "sageMakerStudioDomainUrl": NotRequired[str],
+        "kmsKeyId": NotRequired[str],
+        "dedicatedServiceAccountId": NotRequired[str],
+        "federationMode": NotRequired[FederationModeType],
+        "federationParameters": NotRequired["FederationParametersTypeDef"],
+    },
+)
+
+FederationParametersTypeDef = TypedDict(
+    "FederationParametersTypeDef",
+    {
+        "samlMetadataDocument": NotRequired[str],
+        "samlMetadataURL": NotRequired[str],
+        "applicationCallBackURL": NotRequired[str],
+        "federationURN": NotRequired[str],
+        "federationProviderName": NotRequired[str],
+        "attributeMap": NotRequired[Mapping[str, str]],
+    },
+)
+
+GetEnvironmentRequestRequestTypeDef = TypedDict(
+    "GetEnvironmentRequestRequestTypeDef",
+    {
+        "environmentId": str,
+    },
+)
+
+GetEnvironmentResponseTypeDef = TypedDict(
+    "GetEnvironmentResponseTypeDef",
+    {
+        "environment": "EnvironmentTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListEnvironmentsRequestRequestTypeDef = TypedDict(
+    "ListEnvironmentsRequestRequestTypeDef",
+    {
+        "nextToken": NotRequired[str],
+        "maxResults": NotRequired[int],
+    },
+)
+
+ListEnvironmentsResponseTypeDef = TypedDict(
+    "ListEnvironmentsResponseTypeDef",
+    {
+        "environments": List["EnvironmentTypeDef"],
+        "nextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListTagsForResourceRequestRequestTypeDef = TypedDict(
+    "ListTagsForResourceRequestRequestTypeDef",
+    {
+        "resourceArn": str,
+    },
+)
+
+ListTagsForResourceResponseTypeDef = TypedDict(
+    "ListTagsForResourceResponseTypeDef",
+    {
+        "tags": Dict[str, str],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+SuperuserParametersTypeDef = TypedDict(
+    "SuperuserParametersTypeDef",
+    {
+        "emailAddress": str,
+        "firstName": str,
+        "lastName": str,
+    },
+)
+
+TagResourceRequestRequestTypeDef = TypedDict(
+    "TagResourceRequestRequestTypeDef",
+    {
+        "resourceArn": str,
+        "tags": Mapping[str, str],
+    },
+)
+
+UntagResourceRequestRequestTypeDef = TypedDict(
+    "UntagResourceRequestRequestTypeDef",
+    {
+        "resourceArn": str,
+        "tagKeys": Sequence[str],
+    },
+)
+
+UpdateEnvironmentRequestRequestTypeDef = TypedDict(
+    "UpdateEnvironmentRequestRequestTypeDef",
+    {
+        "environmentId": str,
+        "name": NotRequired[str],
+        "description": NotRequired[str],
+        "federationMode": NotRequired[FederationModeType],
+        "federationParameters": NotRequired["FederationParametersTypeDef"],
+    },
+)
+
+UpdateEnvironmentResponseTypeDef = TypedDict(
+    "UpdateEnvironmentResponseTypeDef",
+    {
+        "environment": "EnvironmentTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
