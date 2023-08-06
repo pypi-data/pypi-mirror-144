3a67f24b828512dc8f1273823af94db97e72251f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:47 +0000

    Add package colorio/0.12.7/colorio-0.12.7-py3-none-any.whl

---
+++ packages/colorio/colorio-0.12.7-py3-none-any.whl/colorio/cs/_oklab.pyi
@@ -0,0 +1,17 @@
+import numpy as np
+from ._color_space import ColorSpace as ColorSpace
+from ._helpers import register as register
+from numpy.typing import ArrayLike as ArrayLike
+from typing import Any
+
+class OKLAB(ColorSpace):
+    name: str
+    labels: Any
+    k0: int
+    M1: Any
+    M1inv: Any
+    M2: Any
+    M2inv: Any
+    def __init__(self) -> None: ...
+    def from_xyz100(self, xyz100: ArrayLike) -> np.ndarray: ...
+    def to_xyz100(self, lab: ArrayLike) -> np.ndarray: ...
