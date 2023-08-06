1edbcdd17767c4e845e3914613dc333c3c01913c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package senkalib/0.2.0/senkalib-0.2.0.tar.gz

---
+++ packages/senkalib/senkalib-0.2.0.tar.gz/senkalib-0.2.0/src/senkalib/senka_setting.py
@@ -0,0 +1,13 @@
+from turtle import back
+from typing import List
+import os
+
+class SenkaSetting:
+  def __init__(self, settings:dict):
+    self.settings = settings
+
+  def set_settings(self, settings:dict):
+    self.settings = settings
+
+  def get_settings(self) -> dict:
+    return self.settings
