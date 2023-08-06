dafbb17d6453e1e00c0d11f3882b61926c1d7674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package pysolo/1.0.8/pysolo-1.0.8.tar.gz

---
+++ packages/pysolo/pysolo-1.0.8.tar.gz/pysolo-1.0.8/src/pysolo/c_wrapper/masked_op.py
@@ -0,0 +1,58 @@
+'''
+    This module is a wrapper for run_solo. Allows use of run_solo with 2D masked arrays rather than 1D arrays.
+'''
+
+import numpy as np
+
+
+def masked_func(func, masked_array, *args, boundary_masks=None, second_masked_array=None, usesBadFlags=False):
+    """ 
+        Performs a solo function on a 2D masked array.
+        
+        Args:
+            masked_array: A numpy masked array data structure with input data (usually this contains info on a single field),
+            args: A list of args, listed in order, as required by the solo function
+            (optional) boundary_masks: A boolean list designating which region to perform the operation. (Default = all True, so entire region is operated on.)
+            (optional) second_masked_array: Some solo functions do operations on two fields. If so, assign this parameter to the masked array of that other field.
+            (optional) usesBadFlags: Some solo functions do operations on masks rather than data. If so, set to true.
+
+        Returns:
+            Numpy masked array
+
+        Throws:
+            AttributeError: if masked_array arg is not a numpy masked array.
+    """
+
+    missing = masked_array.fill_value
+    mask = masked_array.mask.tolist()
+    data_list = masked_array.tolist(missing)
+    if not mask:
+        mask = [False] * len(data_list)
+    if not boundary_masks:
+        boundary_masks = [[True] * len(data_list[0])] * len(data_list)
+    
+    # initialize lists with data/masks. These will become lists of lists
+    output_data = []
+    output_mask = []
+
+    # iterate through each ray
+    for i in range(len(data_list)):
+        input_data = data_list[i] # gates
+        input_mask = mask[i] # mask for gates
+
+        if second_masked_array is not None: # if using second masked array, obtain gates for that
+            second_missing = second_masked_array.fill_value
+            second_input_data = second_masked_array.tolist(second_missing)[i]
+            # in pysolo, second parameter of functions always designates data list for secondary masked list, for functions that have secondary masked arrays.
+            func_ma = func(input_data, second_input_data, missing, *args, boundary_mask=boundary_masks[i])
+        elif usesBadFlags:
+            func_ma = func(input_data, missing, *args, input_mask, boundary_mask=boundary_masks[i])
+        else:
+            func_ma = func(input_data, missing, *args, boundary_mask=boundary_masks[i])
+
+        output_data.append(np.ma.getdata(func_ma)) # add data to output list of lists
+        output_mask.append(np.ma.getdata(func_ma.mask)) # add mask as well
+
+    # convert the list of lists, to a 2D masked array
+    output_masked_array = np.ma.masked_array(data=output_data, mask=output_mask, fill_value=missing)
+    return output_masked_array
