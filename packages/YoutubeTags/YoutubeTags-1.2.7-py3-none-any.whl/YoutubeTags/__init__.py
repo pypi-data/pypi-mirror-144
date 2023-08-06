1055ddc9afd07891bc064dde98d21e189a2560f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:54 +0000

    Add package YoutubeTags/1.2.7/YoutubeTags-1.2.7-py3-none-any.whl

---
+++ packages/YoutubeTags/YoutubeTags-1.2.7-py3-none-any.whl/YoutubeTags/__init__.py
@@ -0,0 +1,27 @@
+import html5lib
+import requests 
+import bs4
+from bs4 import BeautifulSoup
+
+def videotags(url):
+      
+         try:
+              request = requests.get(url)
+              soup = BeautifulSoup(request.content, 'html5lib') 
+              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+              return tags
+      	
+         except:
+              return None
+
+def channeltags(url):
+
+         try:
+              request = requests.get(url)
+              soup = BeautifulSoup(request.content, 'html5lib') 
+              tags = ', '.join([ meta.attrs.get("content") for meta in soup.find_all("meta",{"property": "og:video:tag"}) ])
+              return tags
+      	
+         except:
+              return None
+            
