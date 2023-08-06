43a503dab409fd90968f679580f80f73cd92bbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:05 +0000

    Add package VFunctions/0.0.3/VFunctions-0.0.3.tar.gz

---
+++ packages/VFunctions/VFunctions-0.0.3.tar.gz/VFunctions-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Some things that will make my work easier'
+
+# Setting up
+setup(
+    name="VFunctions",
+    version=VERSION,
+    author="sonkraft1",
+    author_email="<skraft2727@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[]
+)
