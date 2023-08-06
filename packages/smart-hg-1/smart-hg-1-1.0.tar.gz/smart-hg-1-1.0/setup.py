7f83418782e9be736f5f37ae970eddec3de6fd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package smart-hg-1/1.0/smart-hg-1-1.0.tar.gz

---
+++ packages/smart-hg-1/smart-hg-1-1.0.tar.gz/smart-hg-1-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-1",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.16.6', 'seaborn==0.8.1', 'scikit-learn==0.18.2', 'matplotlib==2.1.2'],
+)
