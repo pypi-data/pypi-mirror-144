833857b409033046b46f66defd5dd9786577f3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package mypy-boto3-sagemaker-runtime/1.21.27/mypy-boto3-sagemaker-runtime-1.21.27.tar.gz

---
+++ packages/mypy-boto3-sagemaker-runtime/mypy-boto3-sagemaker-runtime-1.21.27.tar.gz/mypy-boto3-sagemaker-runtime-1.21.27/mypy_boto3_sagemaker_runtime/type_defs.pyi
@@ -0,0 +1,90 @@
+"""
+Type annotations for sagemaker-runtime service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sagemaker_runtime/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_sagemaker_runtime.type_defs import InvokeEndpointAsyncInputRequestTypeDef
+
+    data: InvokeEndpointAsyncInputRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import IO, Dict, Union
+
+from botocore.response import StreamingBody
+from typing_extensions import NotRequired
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "InvokeEndpointAsyncInputRequestTypeDef",
+    "InvokeEndpointAsyncOutputTypeDef",
+    "InvokeEndpointInputRequestTypeDef",
+    "InvokeEndpointOutputTypeDef",
+    "ResponseMetadataTypeDef",
+)
+
+InvokeEndpointAsyncInputRequestTypeDef = TypedDict(
+    "InvokeEndpointAsyncInputRequestTypeDef",
+    {
+        "EndpointName": str,
+        "InputLocation": str,
+        "ContentType": NotRequired[str],
+        "Accept": NotRequired[str],
+        "CustomAttributes": NotRequired[str],
+        "InferenceId": NotRequired[str],
+        "RequestTTLSeconds": NotRequired[int],
+    },
+)
+
+InvokeEndpointAsyncOutputTypeDef = TypedDict(
+    "InvokeEndpointAsyncOutputTypeDef",
+    {
+        "InferenceId": str,
+        "OutputLocation": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+InvokeEndpointInputRequestTypeDef = TypedDict(
+    "InvokeEndpointInputRequestTypeDef",
+    {
+        "EndpointName": str,
+        "Body": Union[bytes, IO[bytes], StreamingBody],
+        "ContentType": NotRequired[str],
+        "Accept": NotRequired[str],
+        "CustomAttributes": NotRequired[str],
+        "TargetModel": NotRequired[str],
+        "TargetVariant": NotRequired[str],
+        "TargetContainerHostname": NotRequired[str],
+        "InferenceId": NotRequired[str],
+    },
+)
+
+InvokeEndpointOutputTypeDef = TypedDict(
+    "InvokeEndpointOutputTypeDef",
+    {
+        "Body": StreamingBody,
+        "ContentType": str,
+        "InvokedProductionVariant": str,
+        "CustomAttributes": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
