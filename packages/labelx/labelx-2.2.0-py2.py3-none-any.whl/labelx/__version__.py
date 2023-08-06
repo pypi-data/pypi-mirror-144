7424078aed3b845487319019df7634dd73dd2693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package labelx/2.2.0/labelx-2.2.0-py2.py3-none-any.whl

---
+++ packages/labelx/labelx-2.2.0-py2.py3-none-any.whl/labelx/__version__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Package Metadata
+__package_name__ = "labelx"
+__author__ = "Dalwar Hossain"
+__author_email = "dalwar23@pm.me"
+__version__ = "2.2.0"
+__copyright__ = "Copyright 2020, Dalwar Hossain"
+__license__ = "GNU General Public License v3"
