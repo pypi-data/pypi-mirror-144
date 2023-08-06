783b3649d0f79944ce696f76d78cf57faa561903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:52 +0000

    Add package docarray/0.9.19.dev11/docarray-0.9.19.dev11.tar.gz

---
+++ packages/docarray/docarray-0.9.19.dev11.tar.gz/docarray-0.9.19.dev11/docarray/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.9.19.dev11'
+
+import os
+
+from .document import Document
+from .array import DocumentArray
+
+
+if 'DA_NO_RICH_HANDLER' not in os.environ:
+    from rich.traceback import install
+
+    install()
