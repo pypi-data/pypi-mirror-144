fafe2c0c84a09a6ea76942c034c56b64a0baf647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:45 +0000

    Add package smart-prospective-api/1.3.0/smart_prospective_api-1.3.0-py3-none-any.whl

---
+++ packages/smart-prospective-api/smart_prospective_api-1.3.0-py3-none-any.whl/smart_prospective_api/exceptions.py
@@ -0,0 +1,20 @@
+class APIError(RuntimeError):
+    """
+    Exception for a API error, can be converted into an error
+
+    :param string msg: The msg
+    """
+    status = 400
+
+    def __init__(self, msg):
+        super(APIError, self).__init__(msg)
+        self.msg = msg
+
+    def __str__(self):
+        """
+        Format for str
+
+        :return: The exception message
+        :rtype: string
+        """
+        return f"APIError: {self.msg}"
