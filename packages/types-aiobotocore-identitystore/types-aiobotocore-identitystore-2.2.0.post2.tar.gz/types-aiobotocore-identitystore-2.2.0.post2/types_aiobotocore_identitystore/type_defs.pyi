3185ab9a14e7e489323f748b8610ed4608d026ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package types-aiobotocore-identitystore/2.2.0.post2/types-aiobotocore-identitystore-2.2.0.post2.tar.gz

---
+++ packages/types-aiobotocore-identitystore/types-aiobotocore-identitystore-2.2.0.post2.tar.gz/types-aiobotocore-identitystore-2.2.0.post2/types_aiobotocore_identitystore/type_defs.pyi
@@ -0,0 +1,144 @@
+"""
+Type annotations for identitystore service type definitions.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_identitystore/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_identitystore.type_defs import DescribeGroupRequestRequestTypeDef
+
+    data: DescribeGroupRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List, Sequence
+
+from typing_extensions import NotRequired
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "DescribeGroupRequestRequestTypeDef",
+    "DescribeGroupResponseTypeDef",
+    "DescribeUserRequestRequestTypeDef",
+    "DescribeUserResponseTypeDef",
+    "FilterTypeDef",
+    "GroupTypeDef",
+    "ListGroupsRequestRequestTypeDef",
+    "ListGroupsResponseTypeDef",
+    "ListUsersRequestRequestTypeDef",
+    "ListUsersResponseTypeDef",
+    "ResponseMetadataTypeDef",
+    "UserTypeDef",
+)
+
+DescribeGroupRequestRequestTypeDef = TypedDict(
+    "DescribeGroupRequestRequestTypeDef",
+    {
+        "IdentityStoreId": str,
+        "GroupId": str,
+    },
+)
+
+DescribeGroupResponseTypeDef = TypedDict(
+    "DescribeGroupResponseTypeDef",
+    {
+        "GroupId": str,
+        "DisplayName": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DescribeUserRequestRequestTypeDef = TypedDict(
+    "DescribeUserRequestRequestTypeDef",
+    {
+        "IdentityStoreId": str,
+        "UserId": str,
+    },
+)
+
+DescribeUserResponseTypeDef = TypedDict(
+    "DescribeUserResponseTypeDef",
+    {
+        "UserName": str,
+        "UserId": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+FilterTypeDef = TypedDict(
+    "FilterTypeDef",
+    {
+        "AttributePath": str,
+        "AttributeValue": str,
+    },
+)
+
+GroupTypeDef = TypedDict(
+    "GroupTypeDef",
+    {
+        "GroupId": str,
+        "DisplayName": str,
+    },
+)
+
+ListGroupsRequestRequestTypeDef = TypedDict(
+    "ListGroupsRequestRequestTypeDef",
+    {
+        "IdentityStoreId": str,
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+        "Filters": NotRequired[Sequence["FilterTypeDef"]],
+    },
+)
+
+ListGroupsResponseTypeDef = TypedDict(
+    "ListGroupsResponseTypeDef",
+    {
+        "Groups": List["GroupTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListUsersRequestRequestTypeDef = TypedDict(
+    "ListUsersRequestRequestTypeDef",
+    {
+        "IdentityStoreId": str,
+        "MaxResults": NotRequired[int],
+        "NextToken": NotRequired[str],
+        "Filters": NotRequired[Sequence["FilterTypeDef"]],
+    },
+)
+
+ListUsersResponseTypeDef = TypedDict(
+    "ListUsersResponseTypeDef",
+    {
+        "Users": List["UserTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+UserTypeDef = TypedDict(
+    "UserTypeDef",
+    {
+        "UserName": str,
+        "UserId": str,
+    },
+)
