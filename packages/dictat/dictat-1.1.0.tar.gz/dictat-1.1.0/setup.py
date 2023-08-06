e411934e1a17a9ed3576ea0b419ddbead0bccdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:29 +0000

    Add package dictat/1.1.0/dictat-1.1.0.tar.gz

---
+++ packages/dictat/dictat-1.1.0.tar.gz/dictat-1.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='dictat',
+    version='1.1.0',
+    description='Adict is an attribute-accessible dynamic dictionary wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Janos Kutscherauer',
+    author_email='janoskut@gmail.com',
+    url='https://gitlab.com/janoskut/dictat',
+    license_files = ['UNLICENSE'],
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+)
