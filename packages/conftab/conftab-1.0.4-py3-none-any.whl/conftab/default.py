6ee072302c4a9012bd85d6b3888bb0b8653af7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:05 +0000

    Add package conftab/1.0.4/conftab-1.0.4-py3-none-any.whl

---
+++ packages/conftab/conftab-1.0.4-py3-none-any.whl/conftab/default.py
@@ -0,0 +1,17 @@
+WEB_HOST = '127.0.0.1'
+WEB_PORT = 7788
+
+PROJECT_NAME = 'default'
+
+ENV = 'dev'
+
+VERSION = '1.0.0'
+
+# SQLALCHEMY_DATABASE_URL: str = 'sqlite:///:memory:'
+SQLALCHEMY_DATABASE_URL: str = 'sqlite:///conftab.db'
+SQLALCHEMY_DATABASE_URL_SECRET: str = 'sqlite:///conftab_secret.db'
+
+
+def set_url(url):
+    global SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL = url
