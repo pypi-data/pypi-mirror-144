69237ed481be0fbb62081815beb4c56ad947e346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:56 +0000

    Add package nonebot-plugin-params/0.1.1/nonebot-plugin-params-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-params/nonebot-plugin-params-0.1.1.tar.gz/nonebot-plugin-params-0.1.1/nonebot_plugin_params/__init__.py
@@ -0,0 +1,11 @@
+from .consts import FEISHU as FEISHU
+from .consts import ONEBOT as ONEBOT
+from .consts import QQGUILD as QQGUILD
+from .consts import TELEGRAM as TELEGRAM
+from .deps import AdapterName as AdapterName
+from .deps import EventName as EventName
+from .deps import ImageSegmentMethod as ImageSegmentMethod
+from .deps import MessageSegmentClass as MessageSegmentClass
+from .permission import PRIVATEMESSAGE as PRIVATEMESSAGE
+from .rule import allow_adapters as allow_adapters
+from .rule import is_private_message as is_private_message
