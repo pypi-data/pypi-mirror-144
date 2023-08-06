130b54f74cb1e3192cf32c3e2ccfa029d8aed8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:43 +0000

    Add package umaudemc/0.9.1/umaudemc-0.9.1-py3-none-any.whl

---
+++ packages/umaudemc/umaudemc-0.9.1-py3-none-any.whl/umaudemc/usermsgs.py
@@ -0,0 +1,23 @@
+#
+# Generic user message functions
+#
+
+from .terminal import terminal as tmn
+
+
+def print_warning(msg):
+	print(f'{tmn.yellow}{msg}{tmn.reset}')
+
+
+def print_error(msg):
+	print(f'{tmn.bright_red}{msg}{tmn.reset}')
+
+
+def print_info(msg):
+	print(msg)
+
+
+def print_error_loc(unit, line, column, msg):
+	"""Print an error with a location"""
+
+	print(f'{tmn.bold}{unit}:{line}:{column}: {tmn.red}error:{tmn.reset} {msg}')
