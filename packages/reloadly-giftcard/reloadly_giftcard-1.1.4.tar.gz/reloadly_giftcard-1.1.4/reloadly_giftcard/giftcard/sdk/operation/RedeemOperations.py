3396f5576d972e2003231f2c41c57904d9b895cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:45 +0000

    Add package reloadly-giftcard/1.1.4/reloadly_giftcard-1.1.4.tar.gz

---
+++ packages/reloadly-giftcard/reloadly_giftcard-1.1.4.tar.gz/reloadly_giftcard-1.1.4/reloadly_giftcard/giftcard/sdk/operation/RedeemOperations.py
@@ -0,0 +1,26 @@
+from reloadly_giftcard.giftcard.sdk.dto.Response.RedeemInstructions import RedeemInstructions
+from reloadly_core.core.internal.dto.request.interfaces.Request import Request
+from reloadly_core.core.internal.Filter.QueryFilter import QueryFilter
+from reloadly_core.core.internal.util.Asserter import Asserter
+from reloadly_giftcard.giftcard.sdk.operation.BaseGiftCardOperation import BaseGiftCardOperation
+
+class RedeemOperations(BaseGiftCardOperation):
+    END_POINT = "brands"
+    REDEEM_PATH = "redeem-instructions"
+    def __init__(self, client, baseUrl : str, apiToken : str):
+        self.baseUrl = baseUrl
+        self.client = client
+        self.apiToken = apiToken
+        super().__init__(self.client, self.baseUrl, self.apiToken)
+
+    def List(self):
+        return super().createGetRequest(super().getBuilder(self.REDEEM_PATH))
+
+    def getById(self,brandId : int):
+        self.validateBrandId(brandId)
+        builder = super().getBuilder(self.REDEEM_PATH + "/" + str(brandId))
+        return super().createGetRequest(str(builder))
+
+    def validateBrandId(self,brandId : int):
+        Asserter().assertNotNull(brandId, "Brand id")
+        Asserter().assertGreaterThanZero(brandId, "Brand id")
