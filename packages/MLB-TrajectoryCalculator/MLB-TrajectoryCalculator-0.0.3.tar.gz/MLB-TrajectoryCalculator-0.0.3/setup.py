768a44787d23815c1665a65b9fd3e51402b36fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:54 +0000

    Add package MLB-TrajectoryCalculator/0.0.3/MLB-TrajectoryCalculator-0.0.3.tar.gz

---
+++ packages/MLB-TrajectoryCalculator/MLB-TrajectoryCalculator-0.0.3.tar.gz/MLB-TrajectoryCalculator-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="MLB-TrajectoryCalculator",
+    version="0.0.3",
+    author="Jacob Lee",
+    author_email="JLpython@outlook.com",
+    description="A package containing utilities for calculating MLB ball trajectories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JLpython-py/MLB-TrajectoryCalculator",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
