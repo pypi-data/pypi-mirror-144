96f6e3171fd8760b23e01f3e1828f3a9eb260fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:42 +0000

    Add package edgedb-orm/0.1.1/edgedb-orm-0.1.1.tar.gz

---
+++ packages/edgedb-orm/edgedb-orm-0.1.1.tar.gz/edgedb-orm-0.1.1/edgedb_orm/unset.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class Unset(BaseModel):
+    pass
+
+
+def is_unset(val: Unset) -> bool:
+    return isinstance(val, Unset)
+
+
+class ComputedPropertyException(Exception):
+    pass
