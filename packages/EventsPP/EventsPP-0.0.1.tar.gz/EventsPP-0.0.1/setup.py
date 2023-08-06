2b059921b4b63ae2f6b32fd0a22fba6f6978de1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:04 +0000

    Add package EventsPP/0.0.1/EventsPP-0.0.1.tar.gz

---
+++ packages/EventsPP/EventsPP-0.0.1.tar.gz/EventsPP-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EventsPP',
+    version='0.0.1',
+    description='A package for event-driven paradigm',
+    packages=find_packages(),
+    url='https://github.com/Mateus-SF/EventsPP',
+    author='Mateus Ferreira',
+    author_email='mateus03ferreira04@gmail.com',
+    license='MIT',
+    zip_safe=False
+)
