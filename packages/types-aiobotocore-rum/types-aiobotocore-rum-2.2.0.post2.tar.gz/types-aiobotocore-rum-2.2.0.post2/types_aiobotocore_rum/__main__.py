40b2cea8be9f743c34abdc0fef58d07d01e509f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package types-aiobotocore-rum/2.2.0.post2/types-aiobotocore-rum-2.2.0.post2.tar.gz

---
+++ packages/types-aiobotocore-rum/types-aiobotocore-rum-2.2.0.post2.tar.gz/types-aiobotocore-rum-2.2.0.post2/types_aiobotocore_rum/__main__.py
@@ -0,0 +1,38 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for aiobotocore.CloudWatchRUM 2.2.0\nVersion:        "
+        " 2.2.0.post2\nBuilder version: 7.5.3\nDocs:           "
+        " https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_rum//\nBoto3 docs:     "
+        " https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rum.html#CloudWatchRUM\nOther"
+        " services:  https://pypi.org/project/boto3-stubs/\nChangelog:      "
+        " https://github.com/vemel/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("2.2.0.post2")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
