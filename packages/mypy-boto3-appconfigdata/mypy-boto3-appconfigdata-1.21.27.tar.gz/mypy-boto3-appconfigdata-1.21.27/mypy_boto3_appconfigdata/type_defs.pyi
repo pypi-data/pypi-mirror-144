7de1ad6aed6311ca719d036ad87b915ec56ff97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package mypy-boto3-appconfigdata/1.21.27/mypy-boto3-appconfigdata-1.21.27.tar.gz

---
+++ packages/mypy-boto3-appconfigdata/mypy-boto3-appconfigdata-1.21.27.tar.gz/mypy-boto3-appconfigdata-1.21.27/mypy_boto3_appconfigdata/type_defs.pyi
@@ -0,0 +1,78 @@
+"""
+Type annotations for appconfigdata service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_appconfigdata/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_appconfigdata.type_defs import GetLatestConfigurationRequestRequestTypeDef
+
+    data: GetLatestConfigurationRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict
+
+from botocore.response import StreamingBody
+from typing_extensions import NotRequired
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "GetLatestConfigurationRequestRequestTypeDef",
+    "GetLatestConfigurationResponseTypeDef",
+    "ResponseMetadataTypeDef",
+    "StartConfigurationSessionRequestRequestTypeDef",
+    "StartConfigurationSessionResponseTypeDef",
+)
+
+GetLatestConfigurationRequestRequestTypeDef = TypedDict(
+    "GetLatestConfigurationRequestRequestTypeDef",
+    {
+        "ConfigurationToken": str,
+    },
+)
+
+GetLatestConfigurationResponseTypeDef = TypedDict(
+    "GetLatestConfigurationResponseTypeDef",
+    {
+        "NextPollConfigurationToken": str,
+        "NextPollIntervalInSeconds": int,
+        "ContentType": str,
+        "Configuration": StreamingBody,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+StartConfigurationSessionRequestRequestTypeDef = TypedDict(
+    "StartConfigurationSessionRequestRequestTypeDef",
+    {
+        "ApplicationIdentifier": str,
+        "EnvironmentIdentifier": str,
+        "ConfigurationProfileIdentifier": str,
+        "RequiredMinimumPollIntervalInSeconds": NotRequired[int],
+    },
+)
+
+StartConfigurationSessionResponseTypeDef = TypedDict(
+    "StartConfigurationSessionResponseTypeDef",
+    {
+        "InitialConfigurationToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
