15e418421a71632a1c4de6beeb55b85756ab5cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:23 +0000

    Add package rupineHeroku/0.0.8/rupineHeroku-0.0.8.tar.gz

---
+++ packages/rupineHeroku/rupineHeroku-0.0.8.tar.gz/rupineHeroku-0.0.8/RupineHeroku/rupine_db/herokuToken.py
@@ -0,0 +1,13 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+import json
+from RupineHeroku.rupine_db import herokuDbAccess
+
+def getTokenWithouName(connection, schema, chain_id, gteCreatedAt):
+    
+    sql = "SELECT * FROM {}.token \
+           WHERE chain_id = {} AND name = 'n/a' AND created_at >= {}".format(schema, chain_id, gteCreatedAt)
+    
+    result = herokuDbAccess.fetchDataInDatabase(sql, connection)    
+    return result[0][0]
