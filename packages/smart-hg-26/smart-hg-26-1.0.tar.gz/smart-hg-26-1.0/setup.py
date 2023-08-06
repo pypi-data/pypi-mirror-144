29d080d50367108110577e6bcc54b2343f1ba521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:26 +0000

    Add package smart-hg-26/1.0/smart-hg-26-1.0.tar.gz

---
+++ packages/smart-hg-26/smart-hg-26-1.0.tar.gz/smart-hg-26-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="smart-hg-26",
+    version="1.0",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.19.4', 'scipy==1.5.4', 'opencv-python==4.4.0.46', 'matplotlib==3.3.3', 'scikit-image==0.18.0'],
+)
