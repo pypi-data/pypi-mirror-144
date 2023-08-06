f2ae3b429cb9f88013eb7c0ee3f469ce5a86667b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:14 +0000

    Add package cameo-fastapi/0.2.7/cameo_fastapi-0.2.7.tar.gz

---
+++ packages/cameo-fastapi/cameo_fastapi-0.2.7.tar.gz/cameo_fastapi-0.2.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cameo_fastapi',
+    version='0.2.7',
+    description='CAMEO FastAPI'
+                '2022-04-01 v0.2.7 remove call init()'
+                '2022-04-01 v0.2.6 init_fastapi() to init()',
+    url='https://github.com/bohachu/cameo_fastapi',
+    author='Bowen Chiu',
+    author_email='bohachu@gmail.com',
+    license='BSD 0-clause license',
+    packages=['cameo_fastapi'],
+    install_requires=['uvicorn', 'fastapi', ],
+    classifiers=['Programming Language :: Python :: 3']
+)
