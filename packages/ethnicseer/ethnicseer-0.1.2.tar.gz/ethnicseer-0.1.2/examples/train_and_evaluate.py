3b67c9185b7a228daf7641d668a2ab99c6b3f84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:48 +0000

    Add package ethnicseer/0.1.2/ethnicseer-0.1.2.tar.gz

---
+++ packages/ethnicseer/ethnicseer-0.1.2.tar.gz/ethnicseer-0.1.2/examples/train_and_evaluate.py
@@ -0,0 +1,7 @@
+from ethnicseer import EthnicClassifier
+from ethnicseer import load_from_directory, train_model, train_and_evaluate
+
+names, ethnics = load_from_directory('data/names')
+print(names[:10])
+print(ethnics[:10])
+train_and_evaluate(names, ethnics)
