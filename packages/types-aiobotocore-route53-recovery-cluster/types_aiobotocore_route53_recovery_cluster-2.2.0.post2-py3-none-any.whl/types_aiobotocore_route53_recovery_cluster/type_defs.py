7786552bfa47c296105fcaddab850ba2ad9951df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package types-aiobotocore-route53-recovery-cluster/2.2.0.post2/types_aiobotocore_route53_recovery_cluster-2.2.0.post2-py3-none-any.whl

---
+++ packages/types-aiobotocore-route53-recovery-cluster/types_aiobotocore_route53_recovery_cluster-2.2.0.post2-py3-none-any.whl/types_aiobotocore_route53_recovery_cluster/type_defs.py
@@ -0,0 +1,86 @@
+"""
+Type annotations for route53-recovery-cluster service type definitions.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_route53_recovery_cluster/type_defs/)
+
+Usage::
+
+    ```python
+    from types_aiobotocore_route53_recovery_cluster.type_defs import GetRoutingControlStateRequestRequestTypeDef
+
+    data: GetRoutingControlStateRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, Sequence
+
+from typing_extensions import NotRequired
+
+from .literals import RoutingControlStateType
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "GetRoutingControlStateRequestRequestTypeDef",
+    "GetRoutingControlStateResponseTypeDef",
+    "ResponseMetadataTypeDef",
+    "UpdateRoutingControlStateEntryTypeDef",
+    "UpdateRoutingControlStateRequestRequestTypeDef",
+    "UpdateRoutingControlStatesRequestRequestTypeDef",
+)
+
+GetRoutingControlStateRequestRequestTypeDef = TypedDict(
+    "GetRoutingControlStateRequestRequestTypeDef",
+    {
+        "RoutingControlArn": str,
+    },
+)
+
+GetRoutingControlStateResponseTypeDef = TypedDict(
+    "GetRoutingControlStateResponseTypeDef",
+    {
+        "RoutingControlArn": str,
+        "RoutingControlState": RoutingControlStateType,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+UpdateRoutingControlStateEntryTypeDef = TypedDict(
+    "UpdateRoutingControlStateEntryTypeDef",
+    {
+        "RoutingControlArn": str,
+        "RoutingControlState": RoutingControlStateType,
+    },
+)
+
+UpdateRoutingControlStateRequestRequestTypeDef = TypedDict(
+    "UpdateRoutingControlStateRequestRequestTypeDef",
+    {
+        "RoutingControlArn": str,
+        "RoutingControlState": RoutingControlStateType,
+        "SafetyRulesToOverride": NotRequired[Sequence[str]],
+    },
+)
+
+UpdateRoutingControlStatesRequestRequestTypeDef = TypedDict(
+    "UpdateRoutingControlStatesRequestRequestTypeDef",
+    {
+        "UpdateRoutingControlStateEntries": Sequence["UpdateRoutingControlStateEntryTypeDef"],
+        "SafetyRulesToOverride": NotRequired[Sequence[str]],
+    },
+)
