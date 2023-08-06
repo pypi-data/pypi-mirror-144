a8435d05ec61db81033eac8197cfecffa26adf0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:34 +0000

    Add package operacoes-matematicas/0.0.1/operacoes_matematicas-0.0.1-py3-none-any.whl

---
+++ packages/operacoes-matematicas/operacoes_matematicas-0.0.1-py3-none-any.whl/math-operations/operations.py
@@ -0,0 +1,18 @@
+from xml.etree.ElementTree import tostring
+
+def soma(a, b):
+    c = a + b
+    print(c)
+
+def subtracao(a,b):
+    c = a - b
+    print(c)
+
+def multiplicacao(a,b):
+    c = a*b
+    print(c)
+
+def divisao(a,b):
+    c = a/b
+    print(c)
+
