65ce230b40dcbbad2c7a7a455cb71e5f23dde241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:33 +0000

    Add package shuzhen/1.1.1/shuzhen-1.1.1.tar.gz

---
+++ packages/shuzhen/shuzhen-1.1.1.tar.gz/shuzhen-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="shuzhen", 
+    version="1.1.1", 
+    author="Li shuzhen", 
+    author_email="shuzhen_li97@163.com", 
+    description="Just a test", 
+    long_description=open("README.md").read(), 
+    license="MIT", 
+    url="https://github.com/lishuzhen97/lishuzhen",
+) 
---
+++ packages/shuzhen/shuzhen-1.1.1.tar.gz/shuzhen-1.1.1/shuzhen/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.1.1'
+VERSION = __version__
+from shuzhen import hello.py
+
+__all__=[
+'hello.py',
+]
