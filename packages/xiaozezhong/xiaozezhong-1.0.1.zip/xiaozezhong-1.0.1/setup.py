75fd18b2948b1256d70b8bfbccc837fe61ccc392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:27 +0000

    Add package xiaozezhong/1.0.1/xiaozezhong-1.0.1.zip

---
+++ packages/xiaozezhong/xiaozezhong-1.0.1.zip/xiaozezhong-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+def readme_file():
+      with open("README.rst", encoding="utf-8") as rf:
+            return rf.read()
+setup(name="xiaozezhong", version="1.0.1", description="this is a lib",
+      packages=["xzz"], py_modules=["Tool"],author="xzz",author_email="xiaozz98@yeah.net",
+      long_description=readme_file(), url="https://github.com/xiaozezhong/Python_code", license="MIT")
