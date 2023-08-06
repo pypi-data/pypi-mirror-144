9ec94ef9717e82710b257a521c9640e537c83bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:41 +0000

    Add package MeUtils/2022.3.31.13.56.6/MeUtils-2022.3.31.13.56.6-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.3.31.13.56.6-py3-none-any.whl/examples/betterme/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : demo
+# @Time         : 2021/9/10 上午10:24
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+import pysnooper
+
+
+pysnooper.snoop
