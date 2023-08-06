30c168fa8f5eb8a4a9e8a3b31e013a503a4f8be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:23 +0000

    Add package edgedb-orm/0.1.8/edgedb_orm-0.1.8-py3-none-any.whl

---
+++ packages/edgedb-orm/edgedb_orm-0.1.8-py3-none-any.whl/edgedb_orm/__init__.py
@@ -0,0 +1,4 @@
+from .node import Node, NodeException
+from .resolver import Resolver, ResolverException, UpdateOperation
+from .batch import Batch
+from .unset import Unset, ComputedPropertyException, is_unset
