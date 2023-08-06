98b299d21a3253395d50d37da35adb9067de65d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:00 +0000

    Add package copiedMultielo/0.3.1/copiedMultielo-0.3.1.tar.gz

---
+++ packages/copiedMultielo/copiedMultielo-0.3.1.tar.gz/copiedMultielo-0.3.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+install_requires = ["numpy", "pandas"]
+test_requires = ["pytest"]
+dev_requires = test_requires
+
+# read version from _version.py
+with open("copiedMultielo/_version.py") as version_file:
+    version = version_file.read().strip().split("=")[1].replace('"', "").replace("'", "").strip()
+
+setup(
+    name="copiedMultielo",
+    version=version,
+    description="Elo ratings for multiplayer matchups.",
+    long_description= "Elo ratings for multiplayer matchups",
+    keywords="elo ratings rankings multiplayer",
+    url="https://github.com/djcunningham0/multielo",
+    author="Danny Cunningham",
+    author_email="djcunningham0@gmail.com",
+    license="MIT",
+    packages=["copiedMultielo"],
+    python_requires=">=3",
+    install_requires=install_requires,
+    extras_require={"tests": test_requires, "dev": dev_requires},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+)
