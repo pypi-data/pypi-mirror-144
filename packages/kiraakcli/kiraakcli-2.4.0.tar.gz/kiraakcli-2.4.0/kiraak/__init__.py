245a4e92f6f4afbeb1a0ff595e9dc759a65f02b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:28 +0000

    Add package kiraakcli/2.4.0/kiraakcli-2.4.0.tar.gz

---
+++ packages/kiraakcli/kiraakcli-2.4.0.tar.gz/kiraakcli-2.4.0/kiraak/__init__.py
@@ -0,0 +1,11 @@
+"""Initializes the project"""
+from colorama import init
+from rich.console import Console
+
+import kiraak.config
+
+# This is the console that helps us print tables, etc.
+console = Console()
+
+# Initializes coloring in the terminal
+init()
