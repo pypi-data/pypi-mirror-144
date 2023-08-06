00c56de3a488b37991a8d95bd39c0ba91cdf077a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:24:46 +0000

    Add package LivePeerSDK/0.5/LivePeerSDK-0.5.tar.gz

---
+++ packages/LivePeerSDK/LivePeerSDK-0.5.tar.gz/LivePeerSDK-0.5/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup, find_packages
+import setuptools
+print("AWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"+os.getcwd())
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='LivePeerSDK',
+    version='0.5',
+    license='MIT',
+    author="Ragavendiran Balasubramanian",
+    author_email='bgragavendiran@gmail.com',
+    packages=find_packages('LivePeerSDKPython'),
+    package_dir={'': 'LivePeerSDKPython'},
+    url='https://pypi.org/project/LivePeerPython/0.5/',
+    project_urls={
+        "Git Link": "https://github.com/RAGANITHI/EthernalHacks-LivePeerSDK",
+    },
+    keywords='LivePeerSDK',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    python_requires='>=3',
+
+)
